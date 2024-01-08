f717ad05218bb10a520f0a314cc0374d6324efab
Harish V <harish@open.gov.sg>
2024-01-09 00:18:44 +0800

    fix: rollup errors

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-const packageJson = require("./package.json");
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
