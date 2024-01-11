e15ddd3feb1fd38b3bbf1a1573a90952283a90e2
Alexander Lee <alexander@open.gov.sg>
2024-01-11 17:14:43 +0800

    chore: add theme colours and text styles

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import type { Preview } from "@storybook/react"
+
+import { ThemeProvider } from "@opengovsg/design-system-react"
+
+import { theme } from "../src/theme/theme"
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+}
+
+export default preview
