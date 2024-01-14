047338f4d229733a30e764d24be647009f782f5d
Harish V <harish@open.gov.sg>
2024-01-15 00:05:34 +0800

    fix: build configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,9 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
 
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from "./package.json" assert { type: "json" }
 
 export default [
   {
@@ -25,10 +25,16 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
+    onwarn: function (warning, warn) {
+      if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+        return
+      }
+      warn(warning)
+    },
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
-];
+]
