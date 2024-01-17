1a4e1ae99ab8fda2e16cbfdbc257400b0c6d4d75
Alexander Lee <alexander@open.gov.sg>
2024-01-17 18:39:16 +0800

    chore: remove chakra and ogp-design system and related components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,2 @@
 export { default as Button } from "./Button"
-export { default as Infopic } from "./Infopic"
 export { default as Footer } from "./Footer"
-export { default as Infobar } from "./Infobar"
--- src/theme/foundations/breakpoints.ts
+++
@@ -1,8 +0,0 @@
-export const breakpoints = {
-  base: "0px",
-  sm: "431px",
-  md: "769px",
-  lg: "1024px",
-  xl: "1280px",
-  xxl: "1408px",
-}
