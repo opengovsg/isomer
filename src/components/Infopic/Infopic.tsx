4a0aaf66bce7f35bb8f996437d1572179d2d6579
Alexander Lee <alexander@open.gov.sg>
2024-01-18 17:35:36 +0800

    fix: alternate infopic layouts

--- src/components/Infopic/Infopic.tsx
+++ src/components/Infopic/Infopic.tsx
@@ -13,6 +13,79 @@ export interface InfopicProps {
   url?: string
 }
 
+const InfopicContentWrapper = ({
+  children,
+  shouldShowMobile,
+}: {
+  children: JSX.Element | JSX.Element[]
+  shouldShowMobile: boolean
+}) => (
+  <div
+    className={`mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 ${
+      shouldShowMobile ? "block lg:!hidden" : "hidden lg:!grid"
+    }`}
+  >
+    {children}
+  </div>
+)
+
+const TextComponent = ({
+  title,
+  subtitle,
+  description,
+  button,
+  url,
+}: Omit<InfopicProps, "sectionIndex" | "image" | "alt">) => {
+  return (
+    <div className="lg:max-w-lg">
+      <div className="mt-4">
+        <div>
+          <p className="text-base text-gray-500 uppercase">{subtitle}</p>
+        </div>
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {title}
+        </h1>
+      </div>
+
+      <section aria-labelledby="information-heading" className="mt-4">
+        <div className="mt-4 space-y-6">
+          <p className="text-base ">{description}</p>
+        </div>
+      </section>
+
+      <p>
+        <a
+          href={url}
+          className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          {button}
+          <BiRightArrowAlt />
+        </a>
+      </p>
+    </div>
+  )
+}
+
+const ImageComponent = ({
+  image,
+  alt,
+}: {
+  image: InfopicProps["image"]
+  alt: InfopicProps["alt"]
+}) => {
+  return (
+    <div className="mt-10  lg:row-span-2 lg:mt-0">
+      <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
+        <img
+          src={image}
+          alt={alt}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+  )
+}
+
 const Infopic = ({
   sectionIndex,
   title,
@@ -25,46 +98,39 @@ const Infopic = ({
 }: InfopicProps) => {
   return (
     <HomepageSectionWrapper sectionIndex={sectionIndex}>
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-        {/* Product details */}
-        <div className="lg:max-w-lg lg:self-end">
-          <div className="mt-4">
-            <div>
-              <p className="text-base text-gray-500 uppercase">{subtitle}</p>
-            </div>
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              {title}
-            </h1>
-          </div>
-
-          <section aria-labelledby="information-heading" className="mt-4">
-            <div className="mt-4 space-y-6">
-              <p className="text-base ">{description}</p>
-            </div>
-          </section>
-
-          <p>
-            <a
-              href={url}
-              className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              {button}
-              <BiRightArrowAlt />
-            </a>
-          </p>
-        </div>
-
-        {/* Product image */}
-        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
-          <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
-            <img
-              src={image}
-              alt={alt}
-              className="h-full w-full object-cover object-center"
-            />
-          </div>
-        </div>
-      </div>
+      {sectionIndex % 2 === 0 ? (
+        <InfopicContentWrapper shouldShowMobile={false}>
+          <TextComponent
+            title={title}
+            subtitle={subtitle}
+            description={description}
+            button={button}
+            url={url}
+          />
+          <ImageComponent image={image} alt={alt} />
+        </InfopicContentWrapper>
+      ) : (
+        <InfopicContentWrapper shouldShowMobile={false}>
+          <ImageComponent image={image} alt={alt} />
+          <TextComponent
+            title={title}
+            subtitle={subtitle}
+            description={description}
+            button={button}
+            url={url}
+          />
+        </InfopicContentWrapper>
+      )}
+      <InfopicContentWrapper shouldShowMobile>
+        <TextComponent
+          title={title}
+          subtitle={subtitle}
+          description={description}
+          button={button}
+          url={url}
+        />
+        <ImageComponent image={image} alt={alt} />
+      </InfopicContentWrapper>
     </HomepageSectionWrapper>
   )
 }
