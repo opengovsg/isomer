46b849d306fc350892f69fb7262f3f7694b25e7a
Harish V <harish@open.gov.sg>
2024-02-07 12:00:55 +0800

    add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#6031b6",
+      secondary: "#4372d6",
+      subtitle: "#344054",
+      paragraph: "#344054",
+    },
     extend: {},
   },
   plugins: [
