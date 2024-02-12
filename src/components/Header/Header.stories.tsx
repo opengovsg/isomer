1decc18c2bf8337a867486324a84ae2af9ff7597
Harish V <harish@open.gov.sg>
2024-02-12 22:41:20 +0800

    refactor code

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from "@storybook/react"
 import Header, { HeaderProps } from "./Header"
+import Sitemap from "../../sitemap.json"
 
 export default {
   title: "Isomer/Header",
@@ -14,62 +15,5 @@ const Template: Story<HeaderProps> = (args) => <Header {...args} />
 export const Default = Template.bind({})
 Default.args = {
   permalink: "/hello/world",
-  sitemap: {
-    title: "Home",
-    permalink: "/",
-    paths: [
-      {
-        permalink: "/about-isomer",
-        title: "about-isomer",
-        paths: [
-          {
-            permalink: "/about-isomer/our-background/",
-            title: "Our background",
-            paths: [],
-          },
-          {
-            permalink: "/about-isomer/what-is-isomer",
-            title: "What is Isomer",
-            paths: [
-              {
-                permalink: "/about-isomer/what-is-isomer/overview/",
-                title: "Overview",
-                paths: [],
-              },
-              {
-                permalink: "/about-isomer/what-is-isomer/isomer-template/",
-                title: "The Isomer template",
-                paths: [],
-              },
-              {
-                permalink: "/about-isomer/what-is-isomer/isomer-infra/",
-                title: "Isomer's infrastructure",
-                paths: [],
-              },
-              {
-                permalink: "/about-isomer/what-is-isomer/isomercms/",
-                title: "IsomerCMS",
-                paths: [],
-              },
-            ],
-          },
-          {
-            permalink: "/about-isomer/who-uses-isomer/",
-            title: "Who uses Isomer",
-            paths: [],
-          },
-          {
-            permalink: "/about-isomer/why-use-isomer/",
-            title: "Why use Isomer",
-            paths: [],
-          },
-        ],
-      },
-      {
-        permalink: "/contact-us/",
-        title: "Contact Us",
-        paths: [],
-      },
-    ],
-  },
+  sitemap: Sitemap,
 }
