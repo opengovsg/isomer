253aeec4483b7688e9a423d1979175afc9954126
Harish V <harish@open.gov.sg>
2024-02-12 15:13:41 +0800

    feat: add SidePane

---
+++ src/components/SidePane/SidePane.stories.tsx
@@ -0,0 +1,75 @@
+import { Story, Meta } from "@storybook/react"
+import SidePane, { SidePaneProps } from "./SidePane"
+
+export default {
+  title: "Isomer/SidePane",
+  component: SidePane,
+  argTypes: {},
+} as Meta
+
+// Template for stories
+const Template: Story<SidePaneProps> = (args) => <SidePane {...args} />
+
+// Default scenario
+export const Default = Template.bind({})
+Default.args = {
+  currentPermalink: "/about-isomer/what-is-isomer/overview/",
+  sitemap: {
+    title: "Home",
+    permalink: "/",
+    paths: [
+      {
+        permalink: "/about-isomer",
+        title: "about-isomer",
+        paths: [
+          {
+            permalink: "/about-isomer/our-background/",
+            title: "Our background",
+            paths: [],
+          },
+          {
+            permalink: "/about-isomer/what-is-isomer",
+            title: "What is Isomer",
+            paths: [
+              {
+                permalink: "/about-isomer/what-is-isomer/overview/",
+                title: "Overview",
+                paths: [],
+              },
+              {
+                permalink: "/about-isomer/what-is-isomer/isomer-template/",
+                title: "The Isomer template",
+                paths: [],
+              },
+              {
+                permalink: "/about-isomer/what-is-isomer/isomer-infra/",
+                title: "Isomer's infrastructure",
+                paths: [],
+              },
+              {
+                permalink: "/about-isomer/what-is-isomer/isomercms/",
+                title: "IsomerCMS",
+                paths: [],
+              },
+            ],
+          },
+          {
+            permalink: "/about-isomer/who-uses-isomer/",
+            title: "Who uses Isomer",
+            paths: [],
+          },
+          {
+            permalink: "/about-isomer/why-use-isomer/",
+            title: "Why use Isomer",
+            paths: [],
+          },
+        ],
+      },
+      {
+        permalink: "/contact-us/",
+        title: "Contact Us",
+        paths: [],
+
+    ],
+  },
+}
---
+++ src/components/SidePane/SidePane.tsx
@@ -0,0 +1,115 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline"
+import { Sitemap, SitemapEntry } from "~/engine/render"
+
+export interface SidePaneProps {
+  sitemap: Sitemap
+  currentPermalink: string
+}
+
+export type SectionWithSiblings = {
+  parentTitle?: string
+  parentPermalink?: string
+  siblings: SitemapEntry[]
+}
+
+const SidePane = ({ sitemap, currentPermalink }: SidePaneProps) => {
+  function findSectionAndSiblings(
+    paths: SitemapEntry[],
+    currentPermalink: string,
+  ): SectionWithSiblings | null {
+    for (const section of paths) {
+      // Check if this is the parent section
+      if (section.paths) {
+        console.log(`Checking `, section.paths)
+        const match = section.paths.find(
+          (child) => child.permalink === currentPermalink,
+        )
+        if (match) {
+          // Exact match found, return the current section's siblings and parent title
+          console.log(`Exact match`)
+          return {
+            parentTitle: section.title,
+            parentPermalink: section.permalink, // This is the title of the dropdown
+            siblings: section.paths, // These are the items in the dropdown
+          }
+        }
+
+        // Recursively check in child paths
+        const found = findSectionAndSiblings(section.paths, currentPermalink)
+        if (found) return found
+      }
+    }
+    return null // Not found
+  }
+
+  const sectionWithSiblings = findSectionAndSiblings(
+    sitemap.paths,
+    currentPermalink,
+  )
+  const parentNodeSiblings = findSectionAndSiblings(
+    sitemap.paths,
+    sectionWithSiblings?.parentPermalink || "",
+  )
+  console.log("Section with siblings", sectionWithSiblings)
+  console.log(`parent`, parentNodeSiblings)
+
+  return (
+    <aside className="w-64">
+      <ul className="divide-navItems divide-y divide-solid">
+        {parentNodeSiblings && (
+          <>
+            {parentNodeSiblings.siblings.map((sibling, index) => (
+              <li
+                key={index}
+                className={`list-none py-2  ${
+                  currentPermalink === sibling.permalink ? "font-bold" : ""
+                }`}
+              >
+                <>
+                  <div className="flex">
+                    <a
+                      href={sibling.permalink}
+                      className={`p-1 m-1 ${
+                        sibling.permalink ===
+                        sectionWithSiblings?.parentPermalink
+                          ? "font-semibold text-headings"
+                          : "text-navItems"
+                      }`}
+                    >
+                      {sibling.title}
+                    </a>
+                    {sibling.permalink ===
+                      sectionWithSiblings?.parentPermalink &&
+                      sectionWithSiblings.siblings.length > 0 && (
+                        <a className="cursor-pointer">
+                          <ChevronDownIcon className="mx-3 mt-2.5 h-5 w-5 text-headings" />
+                        </a>
+                      )}
+                  </div>
+                  <ul>
+                    {sibling.permalink ===
+                      sectionWithSiblings?.parentPermalink &&
+                      sectionWithSiblings?.siblings.map((sibling, index) => (
+                        <li
+                          key={index}
+                          className={`ml-4 py-2 mt-2 list-none ${
+                            currentPermalink === sibling.permalink
+                              ? "font-semibold text-headings"
+                              : "text-navItems"
+                          }`}
+                        >
+                          <a href={sibling.permalink}>{sibling.title}</a>
+                        </li>
+                      ))}
+                  </ul>
+                </>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+    </aside>
+  )
+}
+
+export default SidePane
