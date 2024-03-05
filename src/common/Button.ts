ced21610802e3b782a68508c24ba57a1333a0956
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-05 13:25:19 +0800

    feat: add Next-theme Button

--- src/common/Button.ts
+++ src/common/Button.ts
@@ -1,10 +1,27 @@
+import { IconType } from "react-icons"
+import { BiRightArrowAlt } from "react-icons/bi"
+import { NextButtonColorVariant } from "~/next/components/Button/Button"
+
+// TODO: refactor to another file when more components need to use this
+export const SUPPORTED_ICON_NAMES = ["right-arrow"] as const
+export type SupportedIconName = (typeof SUPPORTED_ICON_NAMES)[number]
+// TODO: use union types to support more icon libraries apart from react-icons
+export type SupportedIconType = IconType
+export const SUPPORTED_ICONS_MAP: Record<SupportedIconName, SupportedIconType> =
+  {
+    "right-arrow": BiRightArrowAlt,
+  }
+
+// extend this with union types when there are more themes in the future
+export type ButtonColorVariant = NextButtonColorVariant
+
 export interface ButtonProps {
   label: string
   href: string
-  buttonColour?: string
-  textColour?: string
+  colorVariant?: ButtonColorVariant
   rounded?: boolean
-  openInNewTab?: boolean
+  leftIcon?: SupportedIconName
+  rightIcon?: SupportedIconName
 }
 
 export default ButtonProps
---
+++ src/next/components/Button/Button.tsx
@@ -0,0 +1,48 @@
+import { ButtonProps } from "~/common"
+import { SUPPORTED_ICONS_MAP } from "~/common/Button"
+
+// Theme specific config
+const NEXT_BUTTON_COLOR_VARIANTS = ["black", "white"] as const
+export type NextButtonColorVariant = (typeof NEXT_BUTTON_COLOR_VARIANTS)[number]
+// https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+const colorVariantToClassMap: Record<NextButtonColorVariant, string> = {
+  black: "bg-[#333333] text-white",
+  white: "bg-white text-[#333333]",
+}
+
+const Button = ({ label, colorVariant, href, rightIcon }: ButtonProps) => {
+  const Label = () => (
+    <span className="text-center text-lg font-semibold leading-tight">
+      {label}
+    </span>
+  )
+
+  const RightIcon = () => {
+    if (!rightIcon) {
+      return null
+    }
+    const Icon = SUPPORTED_ICONS_MAP[rightIcon]
+    return (
+      <div>
+        <Icon className="size-6" />
+      </div>
+    )
+  }
+
+  const colorVariantClass = colorVariantToClassMap[colorVariant ?? "black"]
+
+  return (
+    <a
+      href={href}
+      target={href.startsWith("http") ? "_blank" : undefined}
+      rel={href.startsWith("http") ? "noopener noreferrer nofollow" : undefined}
+      type="button"
+      className={`${colorVariantClass} inline-flex items-center gap-1 px-5 py-4`}
+    >
+      <Label />
+      <RightIcon />
+    </a>
+  )
+}
+
+export default Button
