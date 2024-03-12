4ad0e2126739cb225e5736c78128ec22bdcc3260
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-12 18:21:04 +0800

    feat: add Next Infobar component

---
+++ src/next/components/Infobar/Infobar.tsx
@@ -0,0 +1,41 @@
+import { InfobarProps } from "~/common"
+import Button from "../Button"
+
+const Infobar = ({
+  sectionIdx,
+  title,
+  description,
+  buttonLabel,
+  buttonUrl,
+  secondaryButtonLabel,
+  secondaryButtonUrl,
+}: InfobarProps) => {
+  return (
+    <section className="py-16 px-5 sm:px-14 lg:py-24">
+      <div className="flex flex-col gap-12 items-center mx-auto text-center lg:max-w-3xl">
+        <div className="flex flex-col gap-7">
+          <h1 className="text-[#333333] text-4xl leading-tight font-semibold lg:text-5xl lg:leading-tight">
+            {title}
+          </h1>
+          {description && (
+            <p className="text-[#333333] text-xl">{description}</p>
+          )}
+        </div>
+        <div className="flex flex-row gap-6">
+          {buttonLabel && buttonUrl && (
+            <Button label={buttonLabel} href={buttonUrl} />
+          )}
+          {secondaryButtonLabel && secondaryButtonUrl && (
+            <Button
+              label={secondaryButtonLabel}
+              href={secondaryButtonUrl}
+              colorVariant="white"
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Infobar
