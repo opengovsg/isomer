37de41410f1f20464db1a9bda0ae0f43ad46dc4b
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-03-13 15:03:19 +0800

    chore: fix based on design feedback

--- src/templates/next/components/KeyStatistics/KeyStatistics.tsx
+++ src/templates/next/components/KeyStatistics/KeyStatistics.tsx
@@ -3,12 +3,12 @@ import { type KeyStatisticsProps } from "~/common"
 const KeyStatistics = ({ variant, title, statistics }: KeyStatisticsProps) => {
   return (
     <div
-      className={`flex flex-col gap-10 ${
+      className={`flex flex-col px-5 py-12 xs:px-10 xs:py-24 gap-10 ${
         variant === "side" ? "lg:flex-row lg:gap-16" : ""
-      } gap-10`}
+      }`}
     >
       <h2
-        className={`text-4xl leading-[3rem] w-full ${
+        className={`text-2xl xs:text-4xl xs:leading-[2.75rem] text-[#333333] font-semibold w-full ${
           variant === "side" ? "lg:w-1/3" : ""
         }`}
       >
@@ -19,7 +19,9 @@ const KeyStatistics = ({ variant, title, statistics }: KeyStatisticsProps) => {
           .slice(0, variant === "side" ? 3 : 4)
           .map(({ label, value }) => (
             <div className="flex flex-col gap-3">
-              <h3 className="text-5xl leading-[3.5rem]">{value}</h3>
+              <h3 className="text-5xl leading-[3.5rem] text-[#2c2e34] font-semibold">
+                {value}
+              </h3>
               <p className="text-sm text-[#5d5d5d]">{label}</p>
             </div>
           ))}
