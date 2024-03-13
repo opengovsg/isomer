19575bb13ce16555476d1829d3bc4b82f4b45e20
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2024-03-13 15:26:33 +0800

    feat: add infocols component (#10)
    * feat: add infocols component
    
    * chore: move infocols to /classic
    
    * feat: add Next-theme InfoCols and move Icons to own file
    
    * chore: remove unneeded divs
    
    * fix: decouple Next-InfoCols from homepage
    
    * chore: move infocols to /templates
    
    * fix: allow overriding of LinkComponent
    
    * chore: add content colors to tailwind config

---
+++ src/templates/classic/components/InfoCols/InfoCols.stories.tsx
@@ -0,0 +1,195 @@
+import { Meta, StoryFn } from "@storybook/react"
+import InfoCols from "./InfoCols"
+import { InfoColsProps } from "~/common"
+
+export default {
+  title: "Classic/Components/InfoCols",
+  component: InfoCols,
+  argTypes: {},
+} as Meta
+
+// Template for stories
+const Template: StoryFn<InfoColsProps> = (args) => <InfoCols {...args} />
+
+// Default in CMS is 3 infoboxes
+export const Default = Template.bind({})
+Default.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const GrayBackground = Template.bind({})
+GrayBackground.args = {
+  sectionIdx: 1,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const NoButton = Template.bind({})
+NoButton.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const OneInfoBox = Template.bind({})
+OneInfoBox.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const TwoInfoBoxes = Template.bind({})
+TwoInfoBoxes.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const ThreeInfoBoxesLongText = Template.bind({})
+ThreeInfoBoxesLongText.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Long title that should wrap to the next line",
+      description: "Long description that should wrap to the next line",
+    },
+    {
+      title: "Long title that should wrap to the next line",
+      description: "Long description that should wrap to the next line",
+    },
+    {
+      title: "Long title that should wrap to the next line",
+      description: "Long description that should wrap to the next line",
+    },
+  ],
+}
+
+export const FourInfoBoxes = Template.bind({})
+FourInfoBoxes.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+    {
+      title: "Infobox title",
+      description: "Infobox description",
+    },
+  ],
+}
+
+export const FourInfoBoxesLongText = Template.bind({})
+FourInfoBoxesLongText.args = {
+  sectionIdx: 0,
+  title: "Infobar title",
+  subtitle: "subtitle",
+  buttonLabel: "Button text",
+  buttonUrl: "https://google.com",
+  infoBoxes: [
+    {
+      title: "Long title that should wrap to the next line",
+      description:
+        "Long description that should wrap to the next line. Long description that should wrap to the next line",
+    },
+    {
+      title: "Long title that should wrap to the next line",
+      description:
+        "Long description that should wrap to the next line. Long description that should wrap to the next line",
+    },
+    {
+      title: "Long title that should wrap to the next line",
+      description:
+        "Long description that should wrap to the next line. Long description that should wrap to the next line",
+    },
+    {
+      title: "Long title that should wrap to the next line",
+      description:
+        "Long description that should wrap to the next line. Long description that should wrap to the next line",
+    },
+  ],
+}
