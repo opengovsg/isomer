df85d39418e5974f42eaa78ab21310fed202b285
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-13 15:35:27 +0800

    chore: use content-default colors

--- src/templates/next/components/Infobar/Infobar.tsx
+++ src/templates/next/components/Infobar/Infobar.tsx
@@ -14,11 +14,11 @@ const Infobar = ({
     <section className="py-16 px-5 sm:px-14 lg:py-24">
       <div className="flex flex-col gap-12 items-center mx-auto text-center lg:max-w-3xl">
         <div className="flex flex-col gap-7">
-          <h1 className="text-[#333333] text-4xl leading-tight font-semibold lg:text-5xl lg:leading-tight">
+          <h1 className="text-content-default text-4xl leading-tight font-semibold lg:text-5xl lg:leading-tight">
             {title}
           </h1>
           {description && (
-            <p className="text-[#333333] text-xl">{description}</p>
+            <p className="text-content-default text-xl">{description}</p>
           )}
         </div>
         <div className="flex flex-row gap-6">
