3b8e8761b1605501b07263e0173a3f0f44a5f4a2
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-15 01:41:35 +0800

    fix: add underline offset to paragraph

--- src/templates/next/components/Paragraph/Paragraph.tsx
+++ src/templates/next/components/Paragraph/Paragraph.tsx
@@ -5,7 +5,7 @@ const Paragraph = ({ content }: ParagraphProps) => {
   const sanitizedContent = getSanitizedInlineContent(content)
   return (
     <p
-      className="[&_a]:underline [&_a]:text-blue-500 hover:[&_a]:text-blue-700"
+      className="[&_a]:underline [&_a]:underline-offset-2 [&_a]:text-blue-500 hover:[&_a]:text-blue-700"
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
     />
   )
