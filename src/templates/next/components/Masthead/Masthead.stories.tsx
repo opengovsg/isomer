4f20bd56bf8c5e1a740bef883d8fc6b47a09af88
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2024-03-19 15:57:57 +0800

    feat: next masthead (#35)

---
+++ src/templates/next/components/Masthead/Masthead.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryFn, Meta } from "@storybook/react"
+import Masthead from "./Masthead"
+import type { MastheadProps } from "~/common"
+
+export default {
+  title: "Next/Components/Masthead",
+  component: Masthead,
+  argTypes: {},
+  parameters: {
+    themes: {
+      themeOverride: "Isomer Next",
+    },
+  },
+} as Meta
+
+// Template for stories
+const Template: StoryFn<MastheadProps> = (args) => <Masthead {...args} />
+
+// Default scenario
+export const Default = Template.bind({})
+Default.args = {
+  isStaging: false,
+}
+
+export const Staging = Template.bind({})
+Staging.args = {
+  isStaging: true,
+}
