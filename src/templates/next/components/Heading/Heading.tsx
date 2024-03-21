3ac9cb30b8980613686c2d2fb478f2a2f82d5344
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-22 03:36:59 +0800

    chore: cleanup Callout and Heading components

--- src/templates/next/components/Heading/Heading.tsx
+++ src/templates/next/components/Heading/Heading.tsx
@@ -1,23 +1,47 @@
 import { HeadingProps } from "~/common"
 import { Heading as HeadingStyles } from "~/templates/next/typography/Heading"
 
-const Heading = ({ id, content, level }: HeadingProps) => {
+const Heading = ({ id, content, level }: Omit<HeadingProps, "type">) => {
   if (level === 1) {
-    return <h1 id={id} className={`${HeadingStyles[1]} text-content`}>{content}</h1>
+    return (
+      <h1 id={id} className={`${HeadingStyles[1]} text-content`}>
+        {content}
+      </h1>
+    )
   }
   if (level === 2) {
-    return <h2 id={id} className={`${HeadingStyles[2]} text-content`}>{content}</h2>
+    return (
+      <h2 id={id} className={`${HeadingStyles[2]} text-content`}>
+        {content}
+      </h2>
+    )
   }
   if (level === 3) {
-    return <h3 id={id} className={`${HeadingStyles[3]} text-content`}>{content}</h3>
+    return (
+      <h3 id={id} className={`${HeadingStyles[3]} text-content`}>
+        {content}
+      </h3>
+    )
   }
   if (level === 4) {
-    return <h4 id={id} className={`${HeadingStyles[4]} text-content`}>{content}</h4>
+    return (
+      <h4 id={id} className={`${HeadingStyles[4]} text-content`}>
+        {content}
+      </h4>
+    )
   }
   if (level === 5) {
-    return <h5 id={id} className={`${HeadingStyles[5]} text-content`}>{content}</h5>
+    return (
+      <h5 id={id} className={`${HeadingStyles[5]} text-content`}>
+        {content}
+      </h5>
+    )
   }
-  return <h6 id={id} className={`${HeadingStyles[6]} text-content`}>{content}</h6>
+  return (
+    <h6 id={id} className={`${HeadingStyles[6]} text-content`}>
+      {content}
+    </h6>
+  )
 }
 
 export default Heading
