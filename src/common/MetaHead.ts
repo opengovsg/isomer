f7dc591254749236e4d8c8ce713560be5a68d263
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-03-25 10:17:13 +0800

    chore: remove unused layout prop

--- src/common/MetaHead.ts
+++ src/common/MetaHead.ts
@@ -1,12 +1,9 @@
-import { IsomerLayout } from "~/engine"
-
 export interface MetaHeadProps {
   type: "metahead"
   title: string
   description?: string
   noIndex?: boolean
   favicon?: string
-  layout?: IsomerLayout
   HeadComponent?: any // Next.js Head
 }
 
