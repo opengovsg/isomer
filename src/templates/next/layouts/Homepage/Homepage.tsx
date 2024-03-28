319bacd6a2cc9e1440fc098548305161343f8ddf
Jacob Kwan <jacobkwan1@gmail.com>
2024-03-28 13:45:45 +0800

    chore: document why Homepage cannot use ComponentContent directly

--- src/templates/next/layouts/Homepage/Homepage.tsx
+++ src/templates/next/layouts/Homepage/Homepage.tsx
@@ -11,6 +11,8 @@ const HomepageLayout = ({
   return (
     <Skeleton site={site} page={page}>
       <div
+        // ComponentContent = "component-content" (customCssClass.ts) is imported by all Homepage components,
+        // but cannot be used here as tailwind does not support dynamic class names
         className={`[&_.component-content]:max-w-[1240px] [&_.component-content]:px-6 [&_.component-content]:md:px-10 [&_.component-content]:mx-auto`}
       >
         {content.map((component) =>
