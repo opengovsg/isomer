a3785610e906ce9457d1d2fe0a2eace159f17f88
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-04-04 12:51:30 +0800

    chore: change width to min-width to force icon width

--- src/templates/next/components/Accordion/Accordion.tsx
+++ src/templates/next/components/Accordion/Accordion.tsx
@@ -11,7 +11,7 @@ const Accordion = ({ summary, details }: AccordionProps) => {
     <details className="[&_>_summary]:open:font-semibold [&_>_summary_>_svg]:open:rotate-180 border-y has-[+_details]:border-b-0 border-divider-medium">
       <summary className="my-2 px-4 py-3 flex flex-row gap-3 outline-none list-none focus:ring-2 focus:ring-offset-0 focus:ring-focus-outline">
         {summary}
-        <BiChevronDown className="ml-auto mb-auto w-8 text-2xl transition-all duration-200 ease-in-out" />
+        <BiChevronDown className="ml-auto mb-auto min-w-6 text-2xl transition-all duration-200 ease-in-out" />
       </summary>
 
       <div className="mt-2 mb-4 ml-4 mr-6">
