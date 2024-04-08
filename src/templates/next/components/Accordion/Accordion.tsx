beeae4b90aa4964cbd4a2d1dbe6ec92c19ac8e56
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-04-08 16:27:01 +0800

    chore: add text colour for summary

--- src/templates/next/components/Accordion/Accordion.tsx
+++ src/templates/next/components/Accordion/Accordion.tsx
@@ -9,7 +9,7 @@ import UnorderedList from "../UnorderedList"
 const Accordion = ({ summary, details }: AccordionProps) => {
   return (
     <details className="[&_>_summary]:open:font-semibold [&_>_summary_>_svg]:open:rotate-180 border-y has-[+_details]:border-b-0 border-divider-medium">
-      <summary className="my-2 px-4 py-3 flex flex-row gap-3 outline-none list-none focus:ring-2 focus:ring-offset-0 focus:ring-focus-outline">
+      <summary className="my-2 px-4 py-3 flex flex-row gap-3 outline-none list-none text-content focus:ring-2 focus:ring-offset-0 focus:ring-focus-outline">
         {summary}
         <BiChevronDown className="ml-auto mb-auto min-w-6 text-2xl transition-all duration-200 ease-in-out" />
       </summary>
