4971a29e36241e44be3f0b3a494b6d7bbff00456
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-04-09 15:29:26 +0800

    feat(token): use themeing

---
+++ src/templates/next/components/shared/Notification/Notification.tsx
@@ -0,0 +1,29 @@
+import { NotificationProps } from "~/common/Notification"
+import BaseParagraph from "../shared/Paragraph"
+import { BiX } from "react-icons/bi"
+import { useState } from "react"
+
+const Notification = ({ content: title }: NotificationProps) => {
+  const [isShown, setIsShown] = useState(true)
+  const onDismiss = () => {
+    setIsShown(false)
+  }
+
+  return (
+    isShown && (
+      <div className="flex flex-col sm:flex-row justify-between gap-6 bg-site-primary-100 py-4 px-5 sm:px-10">
+        <BaseParagraph className="text-base " content={title}></BaseParagraph>
+        <button
+          className="flex flex-row ml-auto items-center gap-1"
+          aria-label="Dismiss notification"
+          onClick={onDismiss}
+        >
+          <p className="text-lg text-content">Dismiss</p>
+          <BiX className="h-full" />
+        </button>
+      </div>
+    )
+  )
+}
+
+export default Notification
