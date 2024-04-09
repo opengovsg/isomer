712ae02ec51e18afb8df41eab31c85a1023c74b1
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-04-09 12:33:35 +0800

    chore: add single summary item example

--- src/templates/next/components/shared/ArticlePageHeader/ArticlePageHeader.stories.tsx
+++ src/templates/next/components/shared/ArticlePageHeader/ArticlePageHeader.stories.tsx
@@ -18,8 +18,36 @@ const Template: StoryFn<ArticlePageHeaderProps> = (args) => (
   <ArticlePageHeader {...args} />
 )
 
-export const Default = Template.bind({})
-Default.args = {
+export const SingleSummaryItem = Template.bind({})
+SingleSummaryItem.args = {
+  breadcrumb: {
+    links: [
+      {
+        title: "Newsroom",
+        url: "/newsroom",
+      },
+      {
+        title: "News",
+        url: "/newsroom/news",
+      },
+      {
+        title:
+          "Man sentenced to 24 months' imprisonment for smuggling 34.7 kg of rhinoceros horns",
+        url: "/newsroom/news/man-sentenced-to-24-months-imprisonment-for-smuggling-34-7-kg-of-rhinoceros-horns",
+      },
+    ],
+  },
+  category: "NParks Happenings",
+  title:
+    "Man sentenced to 24 months' imprisonment for smuggling 34.7 kg of rhinoceros horns",
+  date: "1 May 2024",
+  summary: [
+    "20 pieces of rhinoceros horns were found in two pieces of transit baggage bound for Laos. The 34.7 kg seizure is the largest seizure of rhinoceros horns in Singapore to date.",
+  ],
+}
+
+export const MultipleSummaryItems = Template.bind({})
+MultipleSummaryItems.args = {
   breadcrumb: {
     links: [
       {
