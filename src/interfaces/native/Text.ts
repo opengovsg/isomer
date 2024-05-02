9db37c3e830ecc735d8aaf6acc27baf04b74e5e9
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-05-02 22:06:25 +0800

    feat: parse marks and render as HTML

--- src/interfaces/native/Text.ts
+++ src/interfaces/native/Text.ts
@@ -1,4 +1,48 @@
+interface BoldMark {
+  type: "bold"
+}
+
+interface CodeMark {
+  type: "code"
+}
+
+interface ItalicMark {
+  type: "italic"
+}
+
+interface LinkMark {
+  type: "link"
+  href: string
+}
+
+interface StrikeMark {
+  type: "strike"
+}
+
+interface SubscriptMark {
+  type: "subscript"
+}
+
+interface SuperscriptMark {
+  type: "superscript"
+}
+
+interface UnderlineMark {
+  type: "underline"
+}
+
+export type Marks =
+  | BoldMark
+  | CodeMark
+  | ItalicMark
+  | LinkMark
+  | StrikeMark
+  | SubscriptMark
+  | SuperscriptMark
+  | UnderlineMark
+
 export interface TextProps {
   type: "text"
+  marks: Marks[]
   text: string
 }
--- src/utils/getTextAsHtml.ts
+++ src/utils/getTextAsHtml.ts
@@ -1,5 +1,42 @@
-import { TextProps } from "~/interfaces/native/Text"
+import type { Marks, TextProps } from "~/interfaces/native/Text"
 
+type MarkTypes = Marks["type"]
+
+const MARK_DOM_MAPPING: Record<MarkTypes, string> = {
+  bold: "b",
+  code: "code",
+  italic: "i",
+  link: "a",
+  strike: "s",
+  subscript: "sub",
+  superscript: "sup",
+  underline: "u",
+}
+
+// Converts the text node with marks into the appropriate HTML
 export const getTextAsHtml = (content: TextProps[]) => {
-  return content.map((node) => node.text).join("")
+  return content
+    .map((node) => {
+      if (!Object.keys(node).includes("marks")) {
+        return node.text
+      }
+
+      let output = node.text
+
+      node.marks.forEach((mark) => {
+        if (mark.type === "link") {
+          output = `<${MARK_DOM_MAPPING[mark.type]} href="${
+            mark.href
+          }">${output}</${MARK_DOM_MAPPING[mark.type]}>`
+          return
+        }
+
+        output = `<${MARK_DOM_MAPPING[mark.type]}>${output}</${
+          MARK_DOM_MAPPING[mark.type]
+        }>`
+      })
+
+      return output
+    })
+    .join("")
 }
