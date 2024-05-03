7dd8f28b1a83802ffc57163af2230b430a3f7e7a
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-05-03 15:53:24 +0800

    fix: design fixes for keystatistics component

--- src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
+++ src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
@@ -1,7 +1,26 @@
 import type { KeyStatisticsProps } from "~/interfaces"
 import { ComponentContent } from "../../internal/customCssClass"
 
+const ITEM_WIDTHS: Record<
+  KeyStatisticsProps["variant"],
+  Record<number, string>
+> = {
+  side: {
+    1: "basis-full",
+    2: "basis-[calc((100%-2.5rem)/2)]",
+    3: "basis-[calc((100%-5rem)/3)]",
+  },
+  top: {
+    1: "basis-full",
+    2: "basis-[calc((100%-2.5rem)/2)]",
+    3: "basis-[calc((100%-5rem)/3)]",
+    4: "basis-[calc((100%-7.5rem)/4)]",
+  },
+}
+
 const KeyStatistics = ({ variant, title, statistics }: KeyStatisticsProps) => {
+  const maxItems = variant === "side" ? 3 : 4
+
   return (
     <section>
       <div
@@ -16,17 +35,19 @@ const KeyStatistics = ({ variant, title, statistics }: KeyStatisticsProps) => {
         >
           {title}
         </h2>
-        <div className="flex flex-col md:flex-row gap-10">
-          {statistics
-            .slice(0, variant === "side" ? 3 : 4)
-            .map(({ label, value }) => (
-              <div className="flex flex-col gap-3">
-                <h3 className="text-4xl xs:text-5xl leading-[2.75rem] xs:leading-[3.5rem] text-content-strong font-semibold">
-                  {value}
-                </h3>
-                <p className="text-sm text-content-medium">{label}</p>
-              </div>
-            ))}
+        <div className="flex flex-wrap flex-col md:flex-row gap-10">
+          {statistics.slice(0, maxItems).map(({ label, value }) => (
+            <div
+              className={`flex flex-col gap-3 flex-shrink-0 ${
+                ITEM_WIDTHS[variant][Math.min(maxItems, statistics.length)]
+              }`}
+            >
+              <h3 className="text-4xl xs:text-5xl leading-[2.75rem] xs:leading-[3.5rem] text-content-strong font-semibold text-pretty">
+                {value}
+              </h3>
+              <p className="text-sm text-content-medium">{label}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
