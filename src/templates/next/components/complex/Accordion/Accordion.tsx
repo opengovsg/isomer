17d91f07f1798952c60d4f979701f3159487e65e
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-05-12 18:28:16 +0800

    chore(accordion): styling fixes

--- src/templates/next/components/complex/Accordion/Accordion.tsx
+++ src/templates/next/components/complex/Accordion/Accordion.tsx
@@ -1,11 +1,12 @@
 import { BiChevronDown } from "react-icons/bi"
 import type { AccordionProps } from "~/interfaces"
 import { Prose } from "../../native"
+import { Paragraph } from "~/templates/next/typography/Paragraph"
 
 const Accordion = ({ summary, details }: AccordionProps) => {
   return (
-    <details className="[&_>_summary]:open:font-semibold [&_>_summary_>_svg]:open:rotate-180 border-y has-[+_details]:border-b-0 border-divider-medium">
-      <summary className="my-2 px-4 py-3 flex flex-row gap-3 outline-none list-none text-content focus:ring-2 focus:ring-offset-0 focus:ring-focus-outline">
+    <details className="[&_>_summary]:open:font-semibold [&_>_summary_>_svg]:open:rotate-180 border-y has-[+_details]:border-b-0 border-divider-medium hover:cursor-pointer">
+      <summary className={`${Paragraph[1]} my-[0.125rem] px-4 py-5 flex flex-row gap-3 outline-none list-none text-content focus:ring-2 focus:ring-offset-0 focus:ring-focus-outline`}>
         {summary}
         <BiChevronDown className="ml-auto mb-auto min-w-6 text-2xl transition-all duration-200 ease-in-out" />
       </summary>
