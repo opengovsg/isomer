7677bd3f04ea6898d89fd694125fc55bba94db1f
Park Se Hyun <>
2024-05-21 10:31:02 +0800

    use letterspacing token

--- src/templates/next/typography/Heading.ts
+++ src/templates/next/typography/Heading.ts
@@ -1,8 +1,8 @@
 export const Heading = {
-  1: "text-[2.75rem] leading-[3.25rem] lg:text-[3.75rem] lg:leading-[4rem] font-semibold tracking-[-0.022em]",
-  2: "text-[2.375rem] leading-[2.75rem] lg:text-[3rem] lg:leading-[3.625rem] font-semibold tracking[-0.022em]",
-  3: "text-[1.625rem] leading-[2rem] lg:text-[2.25rem] lg:leading-[3rem] font-semibold tracking-[-0.022em]",
-  4: "text-[1.125rem] leading-[1.5rem] lg:text-[1.5rem] lg:leading-[2.25rem] font-semibold tracking[-0.022em]",
+  1: "text-[2.75rem] leading-[3.25rem] lg:text-[3.75rem] lg:leading-[4rem] font-semibold tracking-tight",
+  2: "text-[2.375rem] leading-[2.75rem] lg:text-[3rem] lg:leading-[3.625rem] font-semibold tracking-tight",
+  3: "text-[1.625rem] leading-[2rem] lg:text-[2.25rem] lg:leading-[3rem] font-semibold tracking-tight",
+  4: "text-[1.125rem] leading-[1.5rem] lg:text-[1.5rem] lg:leading-[2.25rem] font-semibold tracking-tight",
   "4-medium":
     "text-[1.125rem] leading-[1.5rem] lg:text-[1.5rem] lg:leading-[2.25rem] font-medium",
   5: "text-[1.125rem] leading-[1.5rem] lg:text-[1.25rem] lg:leading-[1.5rem] font-semibold",
