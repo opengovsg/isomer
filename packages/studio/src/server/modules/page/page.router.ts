50184e53a753894dccc8ddb0b44442770553bfb7
seaerchin <jiachin@open.gov.sg>
2024-05-28 18:35:18 +0800

    feat: dto
    create initial dtos for create + update

--- packages/studio/src/server/modules/page/page.router.ts
+++ packages/studio/src/server/modules/page/page.router.ts
@@ -1,16 +1,17 @@
-import { agnosticProcedure, router } from '~/server/trpc'
-import { getEditPageSchema } from "~/schemas/page"
+import { protectedProcedure, router } from '~/server/trpc'
+import { createPageSchema, getEditPageSchema, updatePageBlobSchema, updatePageSchema } from "~/schemas/page"
 
 export const pageRouter = router({
-  get: agnosticProcedure
+  readPageAndBlob: protectedProcedure
     .input(getEditPageSchema)
     // NOTE: Unique constraint on (site_id, page_id)
     // file: B 
     // tree: A -> (B -> D) + C 
+    // tree: Folders -> Folders -> Page (meta) => <page_blob> 
     .query(async ({ input, ctx }) => {
 
       // TODO: Fill these in later
-      const fileName: string = ""
+      const pageName: string = ""
       const theme = {}
       const isGovernment = false
       const navbar = {}
@@ -23,8 +24,7 @@ export const pageRouter = router({
       const content = ""
 
       return {
-        // File name
-        fileName,
+        pageName,
         // NOTE: might shift theme, isGovt, navbar, footer out into separate function?
         // because this is shared across the whole site (site level props)
         theme,
@@ -35,5 +35,28 @@ export const pageRouter = router({
         sitemap,
         content
       }
-    })
+    }),
+  updatePage: protectedProcedure.input(updatePageSchema).query(async ({ input, ctx }) => {
+    const parentId = ""
+    const pageName = ""
+
+    return {
+      // NOTE: This should adhere to our db schema
+      parentId,
+      pageName,
+      // NOTE: Don't need `variant` here 
+      // because the router itself discriminates for us
+    }
+  }),
+  updatePageBlob: protectedProcedure.input(updatePageBlobSchema).query(async ({ input, ctx }) => {
+    // NOTE: Not returning the `content` first because 
+    // 1. it might potentially be huge
+    // 2. frontend already knows 
+    // ahead of time what is the content (it sent the content over)
+    return {}
+  }),
+  createPage: protectedProcedure.input(createPageSchema).query(async ({ input, ctx }) => {
+    return { pageId: "" }
+  })
+  // TODO: Delete page stuff here 
 })
