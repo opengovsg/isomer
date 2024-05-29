4f3d01fa0b2b6a704ede03f16e7f869b6f2cbc92
Park Se Hyun <>
2024-05-29 22:31:27 +0800

    fix(styling): non-last child paragraphs should have a margin bottom

--- packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
+++ packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
@@ -10,7 +10,7 @@ export const BaseParagraph = ({
 
   return (
     <p
-      className={`[&_a]:text-hyperlink hover:[&_a]:text-hyperlink-hover [&:not(:first-child)]:mt-6 sm:[&:not(:first-child)]:mt-7 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:underline visited:[&_a]:text-purple-600 ${className}`}
+      className={`[&_a]:text-hyperlink hover:[&_a]:text-hyperlink-hover [&:not(:first-child)]:mt-6 sm:[&:not(:first-child)]:mt-7 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:underline visited:[&_a]:text-purple-600 ${className}`}
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       id={id}
     />
