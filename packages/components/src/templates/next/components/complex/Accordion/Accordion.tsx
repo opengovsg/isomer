0cebb2bbd197e3f890035cd82a4c4ecdf2db52d8
Park Se Hyun <>
2024-05-29 19:21:00 +0800

    fix(style): remove focus state on accordion

--- packages/components/src/templates/next/components/complex/Accordion/Accordion.tsx
+++ packages/components/src/templates/next/components/complex/Accordion/Accordion.tsx
@@ -4,10 +4,10 @@ import { Prose } from "../../native"
 
 const Accordion = ({ summary, details }: AccordionProps) => {
   return (
-    <details className="border-y border-divider-medium hover:cursor-pointer has-[+_details]:border-b-0 [&_>_summary]:open:font-semibold [&_>_summary_>_svg]:open:rotate-180">
-      <summary className="text-paragraph-01 my-[0.125rem] flex list-none flex-row gap-3 px-4 py-5 text-content outline-none focus:ring-2 focus:ring-focus-outline focus:ring-offset-0">
+    <details className="border-divider-medium border-y hover:cursor-pointer has-[+_details]:border-b-0 [&_>_summary_>_svg]:open:rotate-180">
+      <summary className="text-paragraph-01 text-content my-[0.125rem] flex list-none flex-row items-center gap-3 px-4 py-5 font-medium outline-none">
         {summary}
-        <BiChevronDown className="mb-auto ml-auto min-w-6 text-2xl transition-all duration-200 ease-in-out" />
+        <BiChevronDown className="ml-auto min-w-6 text-2xl transition-all duration-200 ease-in-out" />
       </summary>
 
       <div className="mb-4 ml-4 mr-6 mt-2">
