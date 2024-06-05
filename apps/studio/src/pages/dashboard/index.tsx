37b35b7910149b7b2215c5aa425963b8eeac9de0
Harish <harish@open.gov.sg>
2024-06-05 16:15:01 +0800

    ISOM-1142: feat(modal): page create modal with validations (#153)
    * feat(modal): page create modal with validations
    
    * refactor: location of modal component

---
+++ apps/studio/src/pages/dashboard/index.tsx
@@ -0,0 +1,17 @@
+import { useDisclosure } from '@chakra-ui/react'
+import { Button } from '@opengovsg/design-system-react'
+import PageCreateModal from '~/features/editing-experience/PageCreateModal'
+
+export default function Dashboard() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <div>
+      {/* TODO: Dashboard layout */}
+      <h1>DASHBOARD</h1>
+
+      <Button onClick={onOpen}>Create new page</Button>
+      <PageCreateModal isOpen={isOpen} onClose={onClose} />
+    </div>
+  )
+}
