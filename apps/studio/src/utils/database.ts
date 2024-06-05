93eb470c2f9f29c54d39813cba220f35de2a4947
Harish V <harish@open.gov.sg>
2024-06-05 16:52:59 +0800

    feat: add Kysely db object

---
+++ apps/studio/src/utils/database.ts
@@ -0,0 +1,15 @@
+import { Kysely, PostgresDialect } from 'kysely'
+import { DB } from '../../prisma/generated/types'
+import { Pool } from 'pg'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString,
+  }),
+})
+
+export const db = new Kysely<DB>({
+  dialect,
+})
