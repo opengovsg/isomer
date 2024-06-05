ebb6b6ee4868c5bbd723b90f418c255747d7eb3c
Harish V <harish@open.gov.sg>
2024-06-05 13:42:58 +0800

    feat(modal): add validation and react-hook-form

--- apps/studio/src/components/features/editing-experience/PageCreateModal.tsx
+++ apps/studio/src/components/features/editing-experience/PageCreateModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -16,10 +16,32 @@ import {
   Stack,
   Box,
 } from '@chakra-ui/react'
-import { Button, ModalCloseButton } from '@opengovsg/design-system-react'
+import {
+  Button,
+  FormErrorMessage,
+  ModalCloseButton,
+} from '@opengovsg/design-system-react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 interface PageCreateModalProps {
   isOpen: boolean
   onClose: () => void
+}
+
+type PageCreateFormFields = {
+  title: string
+  url: string
+}
+
+/* TODO: Can extract these out to a constants file if can be reused */
+const ERROR_MESSAGES = {
+  MIN_LENGTH: 'Minimum length should be 1',
+  MAX_LENGTH: (max: number) => `Maximum length should be ${max}`,
+  TITLE: {
+    REQUIRED: 'Enter a title for this page',
+  },
+  URL: {
+    REQUIRED: 'Enter a URL for this page.',
+  },
 }
 
 export const PageCreateModal = ({
@@ -27,9 +49,28 @@ export const PageCreateModal = ({
   onClose,
 }: PageCreateModalProps): JSX.Element => {
   const MAX_TITLE_LENGTH = 100
-  const MAX_PAGE_URL_LENGTH = 200
+  const MAX_PAGE_URL_LENGTH = 150
   const [titleLen, setTitleLen] = useState(0)
   const [pageUrlLen, setPageUrlLen] = useState(0)
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<PageCreateFormFields>()
+
+  const watchAllFields = watch()
+  console.log(watchAllFields)
+  console.log(`errors`, errors)
+  useEffect(() => {
+    setTitleLen(watchAllFields.title?.length || 0)
+    setPageUrlLen(watchAllFields.url?.length || 0)
+  }, [watchAllFields])
+
+  /* TODO: When integrating with BE */
+  const onSubmit: SubmitHandler<PageCreateFormFields> = (data) =>
+    console.log(data)
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -40,14 +81,14 @@ export const PageCreateModal = ({
           Tell us about your new page
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <FormControl>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ModalBody>
             <Stack gap={'1.5em'}>
               <Text fontSize="md" color="base.content.default">
                 You can change these later.
               </Text>
               {/* Section 1: Page Title */}
-              <Box>
+              <FormControl isInvalid={!!errors.title}>
                 <FormLabel color="base.content.strong">
                   Page title
                   <FormHelperText color="base.content.default">
@@ -58,16 +99,34 @@ export const PageCreateModal = ({
                 <Input
                   type="text"
                   placeholder="This is a title for your new page"
-                  onChange={(event) => setTitleLen(event.target.value.length)}
-                  maxLength={MAX_TITLE_LENGTH}
+                  id="title"
+                  {...register('title', {
+                    required: ERROR_MESSAGES.TITLE.REQUIRED,
+                    validate: (value) =>
+                      value.trim().length !== 0 ||
+                      ERROR_MESSAGES.TITLE.REQUIRED,
+                    minLength: {
+                      value: 1,
+                      message: ERROR_MESSAGES.MIN_LENGTH,
+                    },
+                    maxLength: {
+                      value: MAX_TITLE_LENGTH,
+                      message: ERROR_MESSAGES.MAX_LENGTH(MAX_TITLE_LENGTH),
+                    },
+                  })}
+                  isInvalid={!!errors.title}
                 />
-                <FormHelperText mt={'0.5em'} color="base.content.medium">
-                  {MAX_TITLE_LENGTH - titleLen} characters left
-                </FormHelperText>
-              </Box>
+                {errors.title && errors.title.message ? (
+                  <FormErrorMessage>{errors.title.message}</FormErrorMessage>
+                ) : (
+                  <FormHelperText mt={'0.5em'} color="base.content.medium">
+                    {MAX_TITLE_LENGTH - titleLen} characters left
+                  </FormHelperText>
+                )}
+              </FormControl>
 
               {/* Section 2: Page URL */}
-              <Box>
+              <FormControl isInvalid={!!errors.url}>
                 <FormLabel>
                   Page URL
                   <FormHelperText>
@@ -85,32 +144,47 @@ export const PageCreateModal = ({
                     type="tel"
                     defaultValue={'hello-world'}
                     color="base.content.default"
-                    onChange={(event) =>
-                      setPageUrlLen(event.target.value.length)
-                    }
-                    maxLength={MAX_PAGE_URL_LENGTH}
+                    {...register('url', {
+                      required: ERROR_MESSAGES.URL.REQUIRED,
+                      minLength: {
+                        value: 1,
+                        message: ERROR_MESSAGES.MIN_LENGTH,
+                      },
+                      maxLength: {
+                        value: MAX_PAGE_URL_LENGTH,
+                        message: ERROR_MESSAGES.MAX_LENGTH(MAX_PAGE_URL_LENGTH),
+                      },
+                    })}
+                    isInvalid={!!errors.url}
                   />
                 </InputGroup>
-                <FormHelperText mt={'0.5em'} color="base.content.medium">
-                  {MAX_PAGE_URL_LENGTH - pageUrlLen} characters left
-                </FormHelperText>
-              </Box>
-            </Stack>
-          </FormControl>
-        </ModalBody>
 
-        <ModalFooter>
-          <Button
-            variant="link"
-            mr={5}
-            onClick={onClose}
-            fontWeight={500}
-            color={'base.content.strong'}
-          >
-            Cancel
-          </Button>
-          <Button bgColor="interaction.main.default">Create page</Button>
-        </ModalFooter>
+                {errors.url && errors.url.message ? (
+                  <FormErrorMessage>{errors.url.message}</FormErrorMessage>
+                ) : (
+                  <FormHelperText mt={'0.5em'} color="base.content.medium">
+                    {MAX_PAGE_URL_LENGTH - pageUrlLen} characters left
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              variant="link"
+              mr={5}
+              onClick={onClose}
+              fontWeight={500}
+              color={'base.content.strong'}
+            >
+              Cancel
+            </Button>
+            <Button bgColor="interaction.main.default" type="submit">
+              Create page
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   )
