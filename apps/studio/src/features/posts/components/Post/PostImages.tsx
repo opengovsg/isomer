a66cff9c8a5bdc1cec88e12b8cfb885be9a6acac
Harish V <harish@open.gov.sg>
2024-06-06 14:02:02 +0800

    fix: types

--- apps/studio/src/features/posts/components/Post/PostImages.tsx
+++ apps/studio/src/features/posts/components/Post/PostImages.tsx
@@ -23,7 +23,7 @@ export const PostImages = ({ images }: PostImagesProps): JSX.Element | null => {
       columns={Math.min(Math.max(images.length, 1), 2)}
       spacing="2px"
     >
-      {images.map((url, index) => (
+      {images.map((url: string, index: number) => (
         <Box height={imageContainerHeight} data-value="post-action" key={index}>
           <Image
             alt="Uploaded image"
