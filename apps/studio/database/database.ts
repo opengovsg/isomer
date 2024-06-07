d461bd032923a3054341b7cd1d051bdec72dd0dd
Harish V <harish@open.gov.sg>
2024-06-07 23:06:22 +0800

    refactor: db folder structure

--- apps/studio/database/database.ts
+++ apps/studio/database/database.ts
@@ -1,5 +1,5 @@
 import { Kysely, PostgresDialect } from 'kysely'
-import { DB } from '../../prisma/generated/types'
+import { DB } from './prisma/generated/types'
 import { Pool } from 'pg'
 
 const connectionString = `${process.env.DATABASE_URL}`
@@ -10,6 +10,6 @@ const dialect = new PostgresDialect({
   }),
 })
 
-export const db = new Kysely<DB>({
+export const db: Kysely<DB> = new Kysely<DB>({
   dialect,
 })
