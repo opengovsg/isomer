a915651965c55f32283ea5199230f5057d142d7a
Alexander Lee <alexander@open.gov.sg>
2024-06-10 16:57:58 +0800

    chore: add max lengths to folder params

--- apps/studio/src/schemas/folder.ts
+++ apps/studio/src/schemas/folder.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod'
 
+const MAX_FOLDER_TITLE_LENGTH = 100
+const MAX_FOLDER_PERMALINK_LENGTH = 200
+const MAX_FOLDER_DESCRIPTION_LENGTH = 300
+
 export const createFolderSchema = z.object({
-  folderTitle: z.string(),
-  folderDescription: z.string(),
-  permalink: z.string(),
+  folderTitle: z.string().max(MAX_FOLDER_TITLE_LENGTH),
+  folderDescription: z.string().max(MAX_FOLDER_DESCRIPTION_LENGTH),
+  permalink: z.string().max(MAX_FOLDER_PERMALINK_LENGTH),
   siteId: z.string(),
   // Nullable for top level folder
   parentFolderId: z.string().nullable(),
