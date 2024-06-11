ec43d12d74afccc3883eb9544dceca70ff472a10
Harish <harish@open.gov.sg>
2024-06-11 14:19:24 +0800

    ISOM-1104: Create live preview for editing pages (#158)
    * feat: add tailwindcss
    
    * feat: add tailwind imports
    
    * fix: allow images csp
    
    * temp: add placeholder data
    
    * feat: add preview
    
    * fix: remove unrequired imports
    
    * fix: remove comments

---
+++ apps/studio/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+import { NextPreset } from '@opengovsg/isomer-components'
+
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@opengovsg/isomer-components/**/*.{js,ts,jsx,tsx}',
+    '../../node_modules/@opengovsg/isomer-components/**/*.{js,ts,jsx,tsx}',
+  ],
+  presets: [
+    //   // Note: This is here temporarily until we can figure out how to load the
+    //   // presets dynamically depending on the template being used.
+    NextPreset,
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Site-specific colors, will be overwritten by individual sites
+        site: {
+          primary: {
+            DEFAULT: '#f78f1e',
+            100: '#fef4e8',
+            200: '#ffeec2',
+          },
+          secondary: {
+            DEFAULT: '#4E4541',
+            100: '#f4f2F1',
+          },
+        },
+      },
+    },
+  },
+}
