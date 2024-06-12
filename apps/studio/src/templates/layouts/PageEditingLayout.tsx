dd4cb4f9fdef47c1440826da66652263362babbf
Harish V <harish@open.gov.sg>
2024-06-13 01:02:33 +0800

    feat: implement simple layout for edit page

---
+++ apps/studio/src/templates/layouts/PageEditingLayout.tsx
@@ -0,0 +1,26 @@
+import { Flex, Grid } from '@chakra-ui/react'
+import { AppNavbar } from '~/components/AppNavbar'
+import { EnforceLoginStatePageWrapper } from '~/components/AuthWrappers'
+import { DashSidebar } from '~/components/DashSidebar'
+import { APP_GRID_TEMPLATE_AREA } from '~/constants/layouts'
+import { type GetLayout } from '~/lib/types'
+
+export const PageEditingLayout: GetLayout = (page) => {
+  return (
+    <EnforceLoginStatePageWrapper>
+      <Flex minH="$100vh" flexDir="column" bg="base.canvas.alt" pos="relative">
+        <Grid
+          flex={1}
+          width="100vw"
+          gridColumnGap={{ base: 0, md: '1rem' }}
+          gridTemplate={APP_GRID_TEMPLATE_AREA}
+        >
+          <AppNavbar />
+          <Flex flex={1} bg="base.canvas.alt" w="100vw">
+            {page}
+          </Flex>
+        </Grid>
+      </Flex>
+    </EnforceLoginStatePageWrapper>
+  )
+}
