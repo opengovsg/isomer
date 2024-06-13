d16bec9b6f2f2c261b3f3c455b244b3a62fee840
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-06-13 14:33:29 +0800

    chore: update URL path to download package

--- tooling/build/scripts/preBuild.sh
+++ tooling/build/scripts/preBuild.sh
@@ -15,8 +15,8 @@ curl https://raw.githubusercontent.com/opengovsg/isomer-next/main/tooling/templa
 #######################
 
 # Temporary until we start doing proper releases of the Isomer components
-curl -L https://raw.githubusercontent.com/opengovsg/isomer-next/main/docs/isomerpages-isomer-components-0.0.13.tgz -o isomerpages-isomer-components-0.0.13.tgz
-npm install isomerpages-isomer-components-0.0.13.tgz
+curl -L https://github.com/isomerpages/isomer-components-package/raw/main/opengovsg-isomer-components-0.0.13.tgz -o opengovsg-isomer-components-0.0.13.tgz
+npm install opengovsg-isomer-components-0.0.13.tgz
 
 #######################################################################
 # Generate sitemap.json and search index                              #
