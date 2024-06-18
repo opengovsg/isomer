aaf01b01a29586cba810e9f97732c37c2b6467ad
Hanpu Liu <26217378+hanpuliu-charles@users.noreply.github.com>
2024-06-18 14:59:25 +0800

    fix(schema.prism): changes to schema, remove variant from resource table

--- apps/studio/prisma/generated/generatedTypes.ts
+++ apps/studio/prisma/generated/generatedTypes.ts
@@ -1,52 +1,53 @@
-import type { ColumnType } from "kysely"
-export type Generated<T> =
-  T extends ColumnType<infer S, infer I, infer U>
-    ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>
-export type Timestamp = ColumnType<Date, Date | string, Date | string>
+import type { ColumnType } from "kysely";
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
-export type Accounts = {
-  id: string
-  provider: string
-  providerAccountId: string
-  userId: string
-}
-export type LikedPosts = {
-  postId: string
-  userId: string
-  createdAt: Generated<Timestamp>
-}
-export type Post = {
-  id: string
-  title: string | null
-  content: string
-  contentHtml: string
-  authorId: string
-  images: string[]
-  parentPostId: string | null
-  createdAt: Generated<Timestamp>
-  updatedAt: Generated<Timestamp>
-  deletedAt: Timestamp | null
-}
+import type { ResourceType, RoleType, PageStatus } from "./generatedEnums";
+
+export type Blob = {
+    id: string;
+    content: unknown;
+};
+export type Permission = {
+    id: string;
+    resourceId: string;
+    userId: string;
+    role: RoleType;
+};
+export type Resource = {
+    id: string;
+    name: string;
+    siteId: string;
+    parentId: string | null;
+    lastEditTime: Timestamp | null;
+    lastEditUserId: string;
+    blobId: string | null;
+    variant: ResourceType;
+    status: PageStatus;
+};
+export type Site = {
+    id: string;
+    name: string;
+};
 export type User = {
-  id: string
-  name: string | null
-  username: string | null
-  email: string | null
-  emailVerified: Timestamp | null
-  image: string | null
-  bio: string | null
-}
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+};
 export type VerificationToken = {
-  identifier: string
-  token: string
-  attempts: Generated<number>
-  expires: Timestamp
-}
+    identifier: string;
+    token: string;
+    attempts: Generated<number>;
+    expires: Timestamp;
+};
 export type DB = {
-  Accounts: Accounts
-  LikedPosts: LikedPosts
-  Post: Post
-  User: User
-  VerificationToken: VerificationToken
-}
+    Blob: Blob;
+    Permission: Permission;
+    Resource: Resource;
+    Site: Site;
+    User: User;
+    VerificationToken: VerificationToken;
+};
