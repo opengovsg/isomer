ff522f46c3c136fd55a8b323cfcbdc64ff2dd240
Alexander Lee <alexander@open.gov.sg>
2024-06-18 15:05:09 +0800

    fix: merge errors

--- apps/studio/src/server/modules/folder/folder.router.ts
+++ apps/studio/src/server/modules/folder/folder.router.ts
@@ -1,5 +1,8 @@
 import { protectedProcedure, router } from '~/server/trpc'
-import { createFolderSchema } from '~/schemas/folder'
+import {
+  createFolderSchema,
+  readFolderOrTopLevelFolderSchema,
+} from '~/schemas/folder'
 
 export const folderRouter = router({
   create: protectedProcedure
@@ -7,26 +10,19 @@ export const folderRouter = router({
     .mutation(async ({ ctx, input }) => {
       return { folderId: '' }
     }),
-})
-import { readFolderOrTopLevelFolderSchema } from "~/schemas/folder";
-import { protectedProcedure, router } from '~/server/trpc'
-
-
-export const folderRouter = router({
-    create: protectedProcedure
-        .input(createFolderSchema)
-        .mutation(async ({ ctx, input }) => {
-          return { folderId: '' }
-        }),
-    readFolderOrTopLevelFolder: protectedProcedure.input(readFolderOrTopLevelFolderSchema).query(async ({ input, ctx }) => {     
-        // TODO: Fill these in later
-        const folderName: string = ""
-        const children: {id: string, name: string, type: 'page' | 'folder'}[] = []
-        // Not sure if a backpointer is needed here
-        const parentId: string = ""
-        return {
-            folderName, children, parentId
-        }
-    })
-
+  readFolderOrTopLevelFolder: protectedProcedure
+    .input(readFolderOrTopLevelFolderSchema)
+    .query(async ({ input, ctx }) => {
+      // TODO: Fill these in later
+      const folderName: string = ''
+      const children: { id: string; name: string; type: 'page' | 'folder' }[] =
+        []
+      // Not sure if a backpointer is needed here
+      const parentId: string = ''
+      return {
+        folderName,
+        children,
+        parentId,
+      }
+    }),
 })
