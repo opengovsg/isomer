8f0c555285255aee1be8176596739fcbd5a775a5
seaerchin <jiachin@open.gov.sg>
2024-06-18 15:34:27 +0800

    refactor: use extends

--- apps/studio/src/schemas/page.ts
+++ apps/studio/src/schemas/page.ts
@@ -1,24 +1,22 @@
 import { z } from 'zod'
 
+const PAGE_LAYOUTS = ['content'] as const
+
 export const getEditPageSchema = z.object({
   pageId: z.string(),
   siteId: z.string(),
 })
 
-export const updatePageSchema = z.object({
+export const updatePageSchema = getEditPageSchema.extend({
   // NOTE: We allow both to be empty now,
   // in which case this is a no-op.
   // We are ok w/ this because it doesn't
   // incur any db writes
   parentId: z.string().nullable(),
   pageName: z.string().nullable(),
-  siteId: z.string(),
-  pageId: z.string(),
 })
 
-export const updatePageBlobSchema = z.object({
-  pageId: z.string(),
-  siteId: z.string(),
+export const updatePageBlobSchema = getEditPageSchema.extend({
   content: z.string(),
 })
 
@@ -26,7 +24,7 @@ export const createPageSchema = z.object({
   pageName: z.string(),
   pageTitle: z.string(),
   // TODO: add the actual layouts in here
-  layout: z.enum(['content']).default('content'),
+  layout: z.enum(PAGE_LAYOUTS).default('content'),
   siteId: z.string(),
   // NOTE: implies that top level pages are allowed
   folderId: z.string().nullable(),
