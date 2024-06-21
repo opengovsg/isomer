76ed243cea27a39bac8fa0fea334dfd308a1cdd4
Hanpu Liu <26217378+hanpuliu-charles@users.noreply.github.com>
2024-06-21 17:54:04 +0800

    fix(folder-router): prevent passing nullable resource id to read folder

--- apps/studio/src/server/modules/folder/folder.router.ts
+++ apps/studio/src/server/modules/folder/folder.router.ts
@@ -1,9 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { protectedProcedure, router } from '~/server/trpc'
-import {
-  createFolderSchema,
-  readFolderOrTopLevelFolderSchema,
-} from '~/schemas/folder'
+import { createFolderSchema, readFolderSchema } from '~/schemas/folder'
 
 export const folderRouter = router({
   create: protectedProcedure
@@ -11,61 +8,23 @@ export const folderRouter = router({
     .mutation(async ({ ctx, input }) => {
       return { folderId: '' }
     }),
-  readFolderOrTopLevelFolder: protectedProcedure
-    .input(readFolderOrTopLevelFolderSchema)
+  readFolder: protectedProcedure
+    .input(readFolderSchema)
     .query(async ({ ctx, input }) => {
       // Things that aren't working yet:
       // 0. Perm checking
       // 1. Last Edited user and time
       // 2. Page status(draft, published)
-      if (input.resourceId) {
-        // Non-root level lookup
-        const folderResult = await ctx.db
-          .selectFrom('Resource')
-          .select(['name', 'parentId'])
-          .where('id', '=', input.resourceId)
-          .executeTakeFirstOrThrow()
-        const childrenResult = await ctx.db
-          .selectFrom('Resource')
-          .select(['id', 'name', 'blobId'])
-          .where('parentId', '=', input.resourceId)
-          .execute()
-        const children = childrenResult.map((c) => {
-          if (c.blobId) {
-            return {
-              id: c.id,
-              name: c.name,
-              type: 'page',
-              lastEditDate: new Date(0),
-              lastEditUser: 'Coming Soon',
-              permalink: '/placeholder',
-              status: 'published',
-            }
-          }
-          return {
-            id: c.id,
-            name: c.name,
-            type: 'folder',
-            lastEditDate: 'folder',
-            lastEditUser: 'Coming Soon',
-            permalink: '/placeholder',
-            status: 'folder',
-          }
-        })
 
-        const { parentId } = folderResult
-        const folderName = folderResult.name
-
-        return {
-          folderName,
-          children,
-          parentId,
-        }
-      }
+      const folderResult = await ctx.db
+        .selectFrom('Resource')
+        .select(['name', 'parentId'])
+        .where('id', '=', input.resourceId)
+        .executeTakeFirstOrThrow()
       const childrenResult = await ctx.db
         .selectFrom('Resource')
         .select(['id', 'name', 'blobId'])
-        .where('parentId', 'is', null)
+        .where('parentId', '=', input.resourceId)
         .execute()
       const children = childrenResult.map((c) => {
         if (c.blobId) {
@@ -89,8 +48,9 @@ export const folderRouter = router({
           status: 'folder',
         }
       })
-      const parentId = null
-      const folderName = 'Root-level Site Folder'
+
+      const { parentId } = folderResult
+      const folderName = folderResult.name
 
       return {
         folderName,
