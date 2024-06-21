1558b6923554568166b733cd1f6dffa9697d7c4e
hanpuliu-charles <26217378+hanpuliu-charles@users.noreply.github.com>
2024-06-21 14:32:20 +0800

    ISOM-1086 DB schema crude draft for comment (#178)
    * feat(Prisma-Schema): added a initial draft of the db schema for comment
    
    * fix(Prisma-Schema): remove starterkit migration
    
    * fix(package-lock.json): remove redundant dependency zod
    
    * fix(schema): allow lastedittime and lastedituser to be null in Resource table, since folder doesn't have this concept
    
    * fix(schema.prism): addresses some issues from the comments
    
    * fix(schema.prism): changes to schema, remove variant from resource table
    
    * fix(schema.prisma): add many to many relationship between users and sites
    
    * fix(schema.prisma): update schema according to Chin's comments, generate initial migration
    
    * fix(database.ts): modify kysely type file name in import
    
    * Merge changes from main
    
    * style(schema): reordered relation fields beside scaler fields
    
    * fix(schema.prisma): add preferredName field to User
    
    * fix(schema.prisma): mark preferredName optional
    
    * fix(migration): update migration
    
    * fix(generatedTyoes): update generated types
    
    * chore: fix imports
    
    * fix: post.router
    
    update types
    
    * fix: post
    
    ---------
    
    Co-authored-by: seaerchin <jiachin@open.gov.sg>
    Co-authored-by: seaerchin <44049504+seaerchin@users.noreply.github.com>

--- apps/studio/database/database.ts
+++ apps/studio/database/database.ts
@@ -1,5 +1,5 @@
 import { Kysely, PostgresDialect } from 'kysely'
-import { DB } from '../prisma/generated/types'
+import { DB } from '../prisma/generated/generatedTypes'
 import { Pool } from 'pg'
 
 const connectionString = `${process.env.DATABASE_URL}`
