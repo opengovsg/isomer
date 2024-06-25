72330379878b9d87db315458bfde2e47300c2146
seaerchin <jiachin@open.gov.sg>
2024-06-25 12:32:48 +0800

    feat: page router
    update docs, shift to pageProcedure

--- apps/studio/src/server/modules/page/page.router.ts
+++ apps/studio/src/server/modules/page/page.router.ts
@@ -14,14 +14,18 @@ import {
 } from '../resource/resource.service'
 import { getSiteConfig } from '../site/site.service'
 
+// NOTE: Need to do validation like checking for existence of the page
+// and whether the user has write-access to said page
+const pageProcedure = protectedProcedure
+
 export const pageRouter = router({
-  readPageAndBlob: protectedProcedure
+  readPageAndBlob: pageProcedure
     .input(getEditPageSchema)
     .query(async ({ input, ctx }) => {
       const { pageId, siteId } = input
       const page = await getFullPageById(pageId)
-      // TODO: Fill these in later
       const pageName: string = page.name
+      // NOTE: Sitemap in siteMeta is immediate parent, immediate children and siblings
       const siteMeta = getSiteConfig(siteId)
       const navbar = getNavBar(siteId)
       const footer = getFooter(siteId)
@@ -29,46 +33,30 @@ export const pageRouter = router({
 
       return {
         pageName,
-        // NOTE: might shift theme, isGovt, navbar, footer out into separate function?
-        // because this is shared across the whole site (site level props)
-        ...siteMeta,
         navbar,
         footer,
-        // NOTE: This is immediate parent, immediate children and siblings
         content,
+        ...siteMeta,
       }
     }),
 
-  updatePage: protectedProcedure
+  updatePage: pageProcedure
     .input(updatePageSchema)
     .query(async ({ input, ctx }) => {
-      // NOTE: Need to do validation like checking for existence of the page
-      // and whether the user has write-access to said page
       await updatePageById({ ...input, id: input.pageId })
 
-      return {
-        // NOTE: This should adhere to our db schema
-        ...input,
-        // NOTE: Don't need `variant` here
-        // because the router itself discriminates for us
-      }
-    }),
-
-  updatePageBlob: protectedProcedure
-    .input(updatePageBlobSchema)
-    .query(async ({ input, ctx }) => {
-      // NOTE: Need to do validation like checking for existence of the page
-      // and whether the user has write-access to said page
-      await updateBlobById({ ...input, id: input.pageId })
-
-      // NOTE: Not returning the `content` first because
-      // 1. it might potentially be huge
-      // 2. frontend already knows
-      // ahead of time what is the content (it sent the content over)
       return input
     }),
 
-  createPage: protectedProcedure
+  updatePageBlob: pageProcedure
+    .input(updatePageBlobSchema)
+    .query(async ({ input, ctx }) => {
+      await updateBlobById({ ...input, id: input.pageId })
+
+      return input
+    }),
+
+  createPage: pageProcedure
     .input(createPageSchema)
     .query(async ({ input, ctx }) => {
       return { pageId: '' }
