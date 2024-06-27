5c5a2d037eed6a9f99750bceb78c60cb49e7f8e6
Hanpu Liu <26217378+hanpuliu-charles@users.noreply.github.com>
2024-06-27 17:31:26 +0800

    fix(layout-selection): fix according to Kar Rui's feedback

--- apps/studio/src/features/editing-experience/components/LayoutSelection.tsx
+++ apps/studio/src/features/editing-experience/components/LayoutSelection.tsx
@@ -12,11 +12,12 @@ import {
   useToken,
   Icon,
 } from '@chakra-ui/react'
-import { Button } from '@opengovsg/design-system-react'
+import { Button, Link } from '@opengovsg/design-system-react'
 import { BiLeftArrowAlt, BiRightArrowAlt, BiShow } from 'react-icons/bi'
 import Preview from '~/features/editing-experience/components/Preview'
 import articleLayoutPreview from '~/features/editing-experience/data/articleLayoutPreview.json'
 import contentLayoutPreview from '~/features/editing-experience/data/contentLayoutPreview.json'
+import { trpc } from '~/utils/trpc'
 
 type LayoutType = 'article' | 'content'
 
@@ -27,16 +28,6 @@ export interface LayoutSelectionProps {
   siteId: string
   folderId: string
 }
-
-// export const createPageSchema = z.object({
-//   pageName: z.string(),
-//   pageTitle: z.string(),
-//   // TODO: add the actual layouts in here
-//   layout: z.enum(PAGE_LAYOUTS).default('content'),
-//   siteId: z.string(),
-//   // NOTE: implies that top level pages are allowed
-//   folderId: z.string().nullable(),
-// })
 
 const LAYOUT_DATA: {
   layoutDisplayName: string
@@ -49,7 +40,7 @@ const LAYOUT_DATA: {
     layoutDisplayName: 'Default',
     layoutTypename: 'content',
     layoutDescription: 'This is the most basic layout for your content.',
-    imageSource: '/assets/layout-card/default_layout_card.png',
+    imageSource: '/assets/layout-card/default_layout_card.webp',
     previewJson: contentLayoutPreview,
   },
   {
@@ -57,7 +48,7 @@ const LAYOUT_DATA: {
     layoutTypename: 'article',
     layoutDescription:
       'Designed for the perfect reading experience. Use this layout for text-heavy content, such as news, press releases, and speeches',
-    imageSource: '/assets/layout-card/article_layout_card.png',
+    imageSource: '/assets/layout-card/article_layout_card.webp',
     previewJson: articleLayoutPreview,
   },
 ]
@@ -66,20 +57,19 @@ const LAYOUT_DATA: {
 function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
   // need state here to keep track of selected layout
   const [selectedLayout, setSelectedLayout] = useState(LAYOUT_DATA[0]!)
+
   return (
     <>
       {/* This is the floating header */}
       <VStack position="fixed" h="100%">
         <VStack p="1.5rem 2rem" w="full" gap="0.5rem" alignItems="flex-start">
-          <Button variant="link">
-            <Icon as={BiLeftArrowAlt} />
+          <Link variant="standalone" href="/" gap="0.25rem" size="sm" p="0">
+            <Icon as={BiLeftArrowAlt} boxSize="1.25rem" />
             Back to all pages
-          </Button>
+          </Link>
           <HStack p="0" justifyContent="space-between" w="100%">
             <Text textStyle="h4" color="base.content.default">
-              {`Choose a layout for "`}
-              {props.pageName}
-              {`"`}
+              {`Choose a layout for "${props.pageName}"`}
             </Text>
             <Button size="xs">
               Start with this layout
@@ -102,59 +92,45 @@ function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
                     key={layoutEntry.layoutTypename}
                     p="0"
                     gap="0"
-                    cursor="pointer"
                     onClick={() => {
                       setSelectedLayout(layoutEntry)
                     }}
                     role="group"
+                    as="button"
                   >
                     <Box
-                      className="hover-border"
+                      // className="hover-border"
                       borderRadius="8px"
                       border="2px"
                       p="1.5rem 1.5rem 0 1.5rem"
                       mb="1.25rem"
                       position="relative"
-                      borderColor={
+                      borderColor="base.divider.medium"
+                      bgColor="interaction.muted.main.hover"
+                      data-active={
                         selectedLayout.layoutTypename ===
                         layoutEntry.layoutTypename
-                          ? 'base.divider.brand'
-                          : 'base.divider.medium'
-                      }
-                      bgColor={
-                        selectedLayout.layoutTypename ===
-                        layoutEntry.layoutTypename
-                          ? 'interaction.muted.main.active'
-                          : 'interaction.muted.main.hover'
-                      }
-                      sx={
-                        selectedLayout.layoutTypename !==
-                        layoutEntry.layoutTypename
-                          ? {
-                              transition:
-                                'border-color 300ms ease-out, opacity 300ms ease-out, background-color 300ms ease-out',
-                            }
-                          : {}
-                      }
-                      _groupHover={
-                        selectedLayout.layoutTypename !==
-                        layoutEntry.layoutTypename
-                          ? {
-                              borderColor: useToken(
-                                'colors',
-                                'interaction.main-subtle.hover',
-                              ),
-                              bgColor: useToken(
-                                'colors',
-                                'interaction.main-subtle.default',
-                              ),
-                            }
-                          : {}
-                      }
+                          ? true
+                          : null
+                      } // have to use null so that React will not parse this jsx attribute. having the [data-active] selection attribute will trigger _active.
+                      _active={{
+                        borderColor: 'base.divider.brand',
+                        bgColor: 'interaction.muted.main.active',
+                        _groupHover: {
+                          // Override the group hover behavior
+                          bgColor: 'interaction.main-subtle.default',
+                          borderColor: 'base.divider.brand',
+                        },
+                      }}
+                      _groupHover={{
+                        borderColor: 'interaction.main-subtle.hover',
+                        bgColor: 'interaction.main-subtle.default',
+                      }}
+                      transition="border-color 300ms ease-out, opacity 300ms ease-out, background-color 300ms ease-out"
                     >
                       <Image
                         src={layoutEntry.imageSource}
-                        borderRadius="4px 4px 0px 0px"
+                        borderTopRadius="4px"
                         boxShadow="md"
                         alt={`Image of ${layoutEntry.layoutDisplayName} layout`}
                       />
@@ -164,30 +140,23 @@ function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
                         <>
                           {/* for opacity, since we need the text to be clear */}
                           <Box
-                            className="hover-blur"
                             position="absolute"
                             top="0"
                             left="0"
-                            opacity="0.6"
+                            opacity="0"
                             alignItems="center"
                             w="100%"
                             h="100%"
                             borderRadius="6px" // due to this being smaller than the outer Box
                             bgColor="interaction.main-subtle.default"
-                            sx={{
-                              transition: 'opacity 300ms ease-out',
-                              opacity: 0,
-                            }}
+                            transition="opacity 300ms ease-out"
                             _groupHover={{
                               opacity: 0.6,
                             }}
                           />
                           <VStack
-                            sx={{
-                              transition: 'opacity 300ms ease-out',
-                              opacity: 0,
-                            }}
-                            className="hover-text"
+                            transition="opacity 300ms ease-out"
+                            opacity="0"
                             w="100%"
                             h="100%"
                             position="absolute"
@@ -195,11 +164,11 @@ function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
                             left="0"
                             justify="center"
                             gap="0.25rem"
-                            opacity="0"
                             color="base.content.brand"
                             _groupHover={{
                               opacity: 1,
                             }}
+                            as="button"
                           >
                             <Icon as={BiShow} />
                             <Text textStyle="caption-1">Click to preview</Text>
@@ -219,7 +188,7 @@ function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
                             : 'base.content.strong'
                         }
                         mb="0.5rem"
-                        sx={{ transition: 'color 300ms ease-out' }}
+                        transition="color 300ms ease-out"
                         _groupHover={{ textColor: 'base.content.brand' }}
                       >
                         {layoutEntry.layoutDisplayName} layout
@@ -246,14 +215,14 @@ function LayoutSelection(props: LayoutSelectionProps): JSX.Element {
             overflow="auto"
           >
             <Box
-              borderRadius="8px 8px 0px 0px"
+              borderTopRadius="8px"
               w="100%"
               minH="100%"
               boxShadow="md"
               position="relative"
             >
               <HStack
-                borderRadius="8px 8px 0px 0px"
+                borderTopRadius="8px"
                 w="100%"
                 bgColor="slate.200"
                 textColor="white"
