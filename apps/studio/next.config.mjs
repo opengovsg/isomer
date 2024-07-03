ecd64369fe836fd3b7f00fd8717ab8fc9aba5404
Kar Rui Lau <karrui@open.gov.sg>
2024-07-03 13:16:47 +0800

    fix: pull env from package

--- apps/studio/next.config.mjs
+++ apps/studio/next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-const { env } = await import("./src/env.mjs");
+import { env } from "@isomer/env";
 
 /*
 TODO: Removing this CSP first
