942c66e7a7b7ac6653ef87f078fee94e814fa7e3
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-07-03 13:32:06 +0800

    feat: introduce JSONForms group layout renderer (#247)
    * feat: introduce JSONForms group layout renderer
    
    * fix: switch to transformation during variable init
    
    * chore: increase type safety and add notes
    
    * chore: remove unused imports

--- apps/studio/src/constants/formBuilder.ts
+++ apps/studio/src/constants/formBuilder.ts
@@ -1,3 +1,4 @@
+// Rankings are adapted from JSONForms' Material Renderers package
 export const JSON_FORMS_RANKING = {
   ArrayControl: 3,
   BooleanControl: 2,
@@ -8,5 +9,6 @@ export const JSON_FORMS_RANKING = {
   OneOfControl: 3,
   ProseControl: 2,
   RadioControl: 3,
+  GroupLayoutRenderer: 1,
   VerticalLayoutRenderer: 1,
 }
--- apps/studio/src/features/editing-experience/components/form-builder/renderers/layouts/JsonFormsVerticalLayout.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/layouts/JsonFormsVerticalLayout.tsx
@@ -1,18 +1,84 @@
 import { Box, VStack } from '@chakra-ui/react'
 import {
-  type VerticalLayout,
   rankWith,
   uiTypeIs,
   type LayoutProps,
   type RankedTester,
+  type UISchemaElement,
 } from '@jsonforms/core'
 import { JsonFormsDispatch, withJsonFormsLayoutProps } from '@jsonforms/react'
 import { JSON_FORMS_RANKING } from '~/constants/formBuilder'
+import { isVerticalLayout, type IsomerJsonSchema } from '~/types/schema'
+
+type UISchemaElementWithScope = UISchemaElement & {
+  scope?: string
+  label?: string
+  elements?: UISchemaElementWithScope[]
+}
 
 export const jsonFormsVerticalLayoutTester: RankedTester = rankWith(
   JSON_FORMS_RANKING.VerticalLayoutRenderer,
   uiTypeIs('VerticalLayout'),
 )
+
+function getUiSchemaWithGroup(
+  jsonSchema: IsomerJsonSchema,
+  uiSchema: UISchemaElementWithScope[],
+) {
+  const { groups } = jsonSchema
+
+  if (!groups) {
+    return uiSchema
+  }
+
+  const groupMap = new Map<string, string[]>(
+    new Map(groups.map(({ label, fields }) => [label, fields])),
+  )
+
+  const propertiesNotInGroup = Object.keys(jsonSchema.properties || {}).filter(
+    (property) => !groups.some(({ fields }) => fields.includes(property)),
+  )
+
+  const tempUiSchema = [...uiSchema]
+  const newUiSchema: UISchemaElementWithScope[] = []
+
+  // Iterate through the UI schema and combine properties that belong to the
+  // same group, while preserving the original order of the schema
+  tempUiSchema.forEach((element) => {
+    if (
+      element.scope === undefined ||
+      propertiesNotInGroup.includes(element.scope.split('/').pop() || '')
+    ) {
+      newUiSchema.push(element)
+      return
+    }
+
+    const group = groups.find(({ fields }) =>
+      fields.includes(element.scope?.split('/').pop() || ''),
+    )
+
+    if (group) {
+      const { label } = group
+      const groupFields = groupMap.get(label) || []
+      const groupElements = uiSchema.filter((el) =>
+        groupFields.includes(el.scope?.split('/').pop() || ''),
+      )
+
+      newUiSchema.push({
+        type: 'Group',
+        label,
+        elements: groupElements,
+      })
+
+      groupElements.forEach((el) => {
+        const index = tempUiSchema.indexOf(el)
+        tempUiSchema.splice(index, 1)
+      })
+    }
+  })
+
+  return newUiSchema
+}
 
 export function JsonFormsVerticalLayoutRenderer({
   uischema,
@@ -22,11 +88,15 @@ export function JsonFormsVerticalLayoutRenderer({
   renderers,
   cells,
 }: LayoutProps) {
-  const { elements } = uischema as VerticalLayout
+  // Note: We have to perform this check here due to inaccuracies in JSONForms'
+  // type definitions.
+  // Ref: https://github.com/eclipsesource/jsonforms/blob/c3cead71d08ff11837bdeb5fbea66e5313137218/packages/material-renderers/src/layouts/MaterialVerticalLayout.tsx#L57
+  const elements = isVerticalLayout(uischema) ? uischema.elements : []
+  const newElements = getUiSchemaWithGroup(schema, elements)
 
   return (
     <VStack spacing={2}>
-      {elements.map((element, index) => (
+      {newElements.map((element, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <Box key={`${path}-${index}`} w="100%">
           <JsonFormsDispatch
