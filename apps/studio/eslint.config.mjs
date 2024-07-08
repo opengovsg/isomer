23d776aaef7c7a32a9385391ff984dede42dafca
Kar Rui Lau <karrui@open.gov.sg>
2024-07-08 14:14:28 +0800

    feat: add expect error since ts cannot find eslint types zzz

--- apps/studio/eslint.config.mjs
+++ apps/studio/eslint.config.mjs
@@ -1,5 +1,8 @@
+// @ts-expect-error typescript somehow cannot find types
 import baseConfig, { restrictEnvAccess } from "@isomer/eslint-config/base"
+// @ts-expect-error typescript somehow cannot find types
 import nextjsConfig from "@isomer/eslint-config/nextjs"
+// @ts-expect-error typescript somehow cannot find types
 import reactConfig from "@isomer/eslint-config/react"
 
 /** @type {import('typescript-eslint').Config} */
