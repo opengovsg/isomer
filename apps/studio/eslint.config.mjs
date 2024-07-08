e24a15147334f66c881d09a72c4785ebc5f76d8d
Kar Rui Lau <karrui@open.gov.sg>
2024-07-08 12:19:55 +0800

    feat(studio): add temp rules for eslint

--- apps/studio/eslint.config.mjs
+++ apps/studio/eslint.config.mjs
@@ -11,4 +11,23 @@ export default [
   ...reactConfig,
   ...nextjsConfig,
   ...restrictEnvAccess,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    // TODO: Remove all the warn rules once errors are fixed
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": [
+        "error",
+        { ignorePrimitives: true },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/unbound-method": "warn",
+    },
+  },
 ]
