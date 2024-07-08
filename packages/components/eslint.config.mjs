51c5ce6d5753cafae7b46bf656386fc66f45f4dd
Kar Rui Lau <karrui@open.gov.sg>
2024-07-08 11:59:29 +0800

    feat(components): change offending rules to warn as temp measure
    if not too many things to change in the PR

--- packages/components/eslint.config.mjs
+++ packages/components/eslint.config.mjs
@@ -1,5 +1,5 @@
-import baseConfig from "@isomer/eslint-config/base"
-import reactConfig from "@isomer/eslint-config/react"
+import baseConfig from "@isomer/eslint-config/base";
+import reactConfig from "@isomer/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
@@ -15,6 +15,15 @@ export default [
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": [
+        "error",
+        { ignorePrimitives: true },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": "warn",
     },
   },
-]
+];
