466c8a1ef803d8b0d45baba3b138077a23a3d317
Kar Rui Lau <karrui.lau@gmail.com>
2024-07-09 18:25:07 +0800

    feat: add eslint and prettier tooling packages to monorepo (#279)
    ### TL;DR
    
    This PR introduces a new `tooling` workspace along with various plugins and configurations for ESLint and Prettier.
    
    Not used by other packages for now, that will come in later PRs
    
    ### What changed?
    
    1. **Workspaces Update**: Added `tooling/*` to the list of workspaces in `package.json`.
    2. **New tooling packages**:
       - **@isomer/eslint-config**: Added configurations and plugins for ESLint, including rules for TypeScript, React, and Next.js.
       - **@isomer/prettier-config**:  includes plugins for sorting imports and Tailwind CSS (if needed).
       - **@isomer/tsconfig**:  shared typescript configs for consistency.
    
    ### How to test?
    
    1. Clone the repository.
    2. Run `npm install` to install the new dependencies and set up the workspaces.
    
    ### Why make this change?
    
    To streamline the development process by providing a consistent tsconfig, linting and formatting experience across the codebase. This will help in maintaining code quality and reducing the overhead of manually configuring these tools for each package.

---
+++ tooling/prettier/index.js
@@ -0,0 +1,33 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+module.exports = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: false,
+  useTabs: false,
+  tailwindFunctions: ["cn", "cva", "tv"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@isomer",
+    "^@isomer/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.5.3",
+}
