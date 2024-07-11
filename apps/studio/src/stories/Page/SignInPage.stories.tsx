20874f94b8ee0cd439c3252e84a2c39031749f69
Kar Rui Lau <karrui@open.gov.sg>
2024-07-11 18:22:22 +0800

    feat: set loginState to true by default easier to mock stories

--- apps/studio/src/stories/Page/SignInPage.stories.tsx
+++ apps/studio/src/stories/Page/SignInPage.stories.tsx
@@ -14,6 +14,7 @@ const meta: Meta<typeof SignInPage> = {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
+    loginState: false,
     msw: {
       handlers: [
         meHandlers.unauthorized(),
