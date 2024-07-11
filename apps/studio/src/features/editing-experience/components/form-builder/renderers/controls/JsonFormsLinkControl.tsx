6294fb5cd406cef740d17d06c1eb25ed585b4398
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-07-11 10:08:11 +0800

    chore: fix prettier formatting

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsLinkControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsLinkControl.tsx
@@ -2,20 +2,20 @@ import type { ControlProps, RankedTester } from "@jsonforms/core"
 import {
   Box,
   FormControl,
+  HStack,
+  Icon,
   TabList,
-  Tabs,
   TabPanel,
   TabPanels,
-  Icon,
-  HStack,
+  Tabs,
   Text,
 } from "@chakra-ui/react"
 import { and, isStringControl, rankWith, schemaMatches } from "@jsonforms/core"
 import { withJsonFormsControlProps } from "@jsonforms/react"
 import { FormLabel, Input, Tab } from "@opengovsg/design-system-react"
+import { BiEnvelopeOpen, BiFile, BiFileBlank, BiLink } from "react-icons/bi"
 
 import { JSON_FORMS_RANKING } from "~/constants/formBuilder"
-import { BiEnvelopeOpen, BiFile, BiFileBlank, BiLink } from "react-icons/bi"
 
 export const jsonFormsLinkControlTester: RankedTester = rankWith(
   JSON_FORMS_RANKING.LinkControl,
