35d7af2ae240dc4d18aaf995e61cf1335ebd7145
Kar Rui Lau <karrui@open.gov.sg>
2024-07-11 23:37:42 +0800

    feat: update padding and layout of dashboard page

--- apps/studio/src/features/dashboard/SiteList.tsx
+++ apps/studio/src/features/dashboard/SiteList.tsx
@@ -1,16 +1,17 @@
 import NextLink from "next/link"
-import { Card, CardHeader, SimpleGrid } from "@chakra-ui/react"
+import { Card, CardHeader, SimpleGrid, Skeleton } from "@chakra-ui/react"
 import { Link } from "@opengovsg/design-system-react"
 
+import { withSuspense } from "~/hocs/withSuspense"
 import { trpc } from "~/utils/trpc"
 
-export const SiteList = (): JSX.Element => {
+const SuspendableSiteList = (): JSX.Element => {
   // TODO: Only return sites that the user has access to
   const [sites] = trpc.site.list.useSuspenseQuery()
   return (
-    <SimpleGrid columns={3}>
+    <SimpleGrid columns={3} gap="2.5rem" width="100%">
       {sites.map((site) => (
-        <Card key={site.id}>
+        <Card key={site.id} width="100%">
           <CardHeader>
             <Link href={`/sites/${site.id}`} as={NextLink}>
               {site.name}
@@ -21,3 +22,19 @@ export const SiteList = (): JSX.Element => {
     </SimpleGrid>
   )
 }
+
+const SiteListSkeleton = (): JSX.Element => {
+  return (
+    <SimpleGrid columns={3} gap="2.5rem">
+      {[1, 2, 3].map((index) => (
+        <Card key={index} width="100%">
+          <Skeleton>
+            <CardHeader>Loading...</CardHeader>
+          </Skeleton>
+        </Card>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+export const SiteList = withSuspense(SuspendableSiteList, <SiteListSkeleton />)
--- apps/studio/src/pages/dashboard.tsx
+++ apps/studio/src/pages/dashboard.tsx
@@ -5,9 +5,9 @@ import { SiteList } from "~/features/dashboard/SiteList"
 import { type NextPageWithLayout } from "~/lib/types"
 import { AdminLayout } from "~/templates/layouts/AdminLayout"
 
-const Home: NextPageWithLayout = () => {
+const DashboardPage: NextPageWithLayout = () => {
   return (
-    <Flex flexDir="column" py="2rem">
+    <Flex flexDir="column" py="2rem" maxW="57rem" mx="auto" width="100%">
       <Flex gap="0.75rem" flexDirection="column">
         <Text as="h3" size="lg" textStyle="h3">
           My sites
@@ -25,6 +25,6 @@ const Home: NextPageWithLayout = () => {
   )
 }
 
-Home.getLayout = AdminLayout
+DashboardPage.getLayout = AdminLayout
 
-export default Home
+export default DashboardPage
