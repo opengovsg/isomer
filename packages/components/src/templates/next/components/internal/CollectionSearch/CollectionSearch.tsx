ef725e9514dc8997e5840c172110cbc74df043c6
Park Se Hyun <>
2024-07-12 10:12:14 +0800

    fix: restyle search bar

--- packages/components/src/templates/next/components/internal/CollectionSearch/CollectionSearch.tsx
+++ packages/components/src/templates/next/components/internal/CollectionSearch/CollectionSearch.tsx
@@ -14,7 +14,7 @@ const CollectionSearch = ({
         <BiSearch className="h-5 w-5 fill-interaction-support-placeholder" />
       </span>
       <input
-        className="w-full rounded-lg py-4 pl-[3.25rem] ring-divider-medium placeholder:text-interaction-support-placeholder focus:ring-2 focus:ring-focus-outline active:bg-interaction-main-subtle-hover active:ring-2 active:ring-focus-outline"
+        className="w-full rounded py-4 pl-[3.25rem] placeholder:gray-600 placeholder:text-base border-[1.5px] border-gray-900 ring-divider-medium placeholder:text-interaction-support-placeholder focus:ring-2 focus:ring-focus-outline active:bg-interaction-main-subtle-hover active:ring-2 active:ring-focus-outline"
         placeholder={placeholder}
         type="text"
         name="search"
