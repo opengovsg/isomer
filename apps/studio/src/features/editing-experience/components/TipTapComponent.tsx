07d9d0203c02a84b8eb438f06913e452b2cc42ff
seaerchin <jiachin@open.gov.sg>
2024-07-16 18:15:30 +0800

    chore: delete old imports

--- apps/studio/src/features/editing-experience/components/TipTapComponent.tsx
+++ apps/studio/src/features/editing-experience/components/TipTapComponent.tsx
@@ -1,38 +1,12 @@
 import { Box, Flex, Icon, VStack, Text as ChakraText } from '@chakra-ui/react'
 import { Button, IconButton } from '@opengovsg/design-system-react'
-import { HardBreak } from '@tiptap/extension-hard-break'
-import { Blockquote } from '@tiptap/extension-blockquote'
-import { Bold } from '@tiptap/extension-bold'
-import { BulletList } from '@tiptap/extension-bullet-list'
-import { Document } from '@tiptap/extension-document'
-import { Dropcursor } from '@tiptap/extension-dropcursor'
-import { Gapcursor } from '@tiptap/extension-gapcursor'
-import { Heading } from '@tiptap/extension-heading'
-import { History } from '@tiptap/extension-history'
-import { HorizontalRule } from '@tiptap/extension-horizontal-rule'
-import { Italic } from '@tiptap/extension-italic'
-import { ListItem } from '@tiptap/extension-list-item'
-import { OrderedList } from '@tiptap/extension-ordered-list'
-import { Paragraph } from '@tiptap/extension-paragraph'
-import { Strike } from '@tiptap/extension-strike'
-import { Subscript } from '@tiptap/extension-subscript'
-import { Superscript } from '@tiptap/extension-superscript'
-import { Text } from '@tiptap/extension-text'
-import TableCell from '@tiptap/extension-table-cell'
-import TableHeader from '@tiptap/extension-table-header'
-import TableRow from '@tiptap/extension-table-row'
 import {
-  EditorContent,
-  useEditor,
   type JSONContent,
 } from '@tiptap/react'
 import { BiText, BiX } from 'react-icons/bi'
-import Underline from '@tiptap/extension-underline'
-import { MenuBar } from '~/components/PageEditor/MenuBar'
 
 import { useEditorDrawerContext } from '~/contexts/EditorDrawerContext'
 import { cloneDeep } from 'lodash'
-import { Table } from './extensions/Table'
 import type { ProseProps } from '@opengovsg/isomer-components'
 import { TiptapEditor } from './form-builder/renderers/TipTapEditor'
 
@@ -83,7 +57,7 @@ function TipTapComponent({ content }: TipTapComponentProps) {
           }}
         />
       </Flex>
-      <Box p="2rem" h="100%">
+      <Box w="100%" p="2rem" h="100%">
         <TiptapEditor data={content} handleChange={updatePageState} />
       </Box>
       <Flex
