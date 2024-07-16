8ca766931b9b8cdad91e14b8c566eefd2d934c64
Harish V <harish@open.gov.sg>
2024-07-16 13:56:07 +0800

    feat: update migration

--- apps/studio/prisma/migrations/20240716055232_add_draft_blob/migration.sql
+++ apps/studio/prisma/migrations/20240716055232_add_draft_blob/migration.sql
@@ -2,8 +2,9 @@
   Warnings:
 
   - You are about to drop the column `blobId` on the `Resource` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[publishedBlobId]` on the table `Resource` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[mainBlobId]` on the table `Resource` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[draftBlobId]` on the table `Resource` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `isFolder` to the `Resource` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -15,16 +16,18 @@ DROP INDEX "Resource_blobId_key";
 -- AlterTable
 ALTER TABLE "Resource" DROP COLUMN "blobId",
 ADD COLUMN     "draftBlobId" INTEGER,
-ADD COLUMN     "publishedBlobId" INTEGER;
+ADD COLUMN     "isFolder" BOOLEAN NOT NULL,
+ADD COLUMN     "isPublished" BOOLEAN DEFAULT false,
+ADD COLUMN     "mainBlobId" INTEGER;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Resource_publishedBlobId_key" ON "Resource"("publishedBlobId");
+CREATE UNIQUE INDEX "Resource_mainBlobId_key" ON "Resource"("mainBlobId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Resource_draftBlobId_key" ON "Resource"("draftBlobId");
 
 -- AddForeignKey
-ALTER TABLE "Resource" ADD CONSTRAINT "Resource_publishedBlobId_fkey" FOREIGN KEY ("publishedBlobId") REFERENCES "Blob"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Resource" ADD CONSTRAINT "Resource_mainBlobId_fkey" FOREIGN KEY ("mainBlobId") REFERENCES "Blob"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Resource" ADD CONSTRAINT "Resource_draftBlobId_fkey" FOREIGN KEY ("draftBlobId") REFERENCES "Blob"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- apps/studio/prisma/seed.ts
+++ apps/studio/prisma/seed.ts
@@ -212,7 +212,7 @@ async function main() {
 
   await db
     .insertInto("Resource")
-    .values({ blobId, name: "Home", siteId })
+    .values({ mainBlobId: blobId, name: "Home", siteId, isFolder: false })
     .executeTakeFirstOrThrow()
 
   await Promise.all(
