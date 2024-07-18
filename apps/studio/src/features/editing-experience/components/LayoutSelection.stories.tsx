49e5b9b91b7e12faaebd37458ba521b82b177746
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-07-18 11:58:47 +0800

    fix: add mock handlers for config, navbar and footer

--- apps/studio/src/features/editing-experience/components/LayoutSelection.stories.tsx
+++ apps/studio/src/features/editing-experience/components/LayoutSelection.stories.tsx
@@ -1,10 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react"
 
 import LayoutSelection from "./LayoutSelection"
+import { siteHandlers } from "tests/msw/handlers/site"
 
 const meta: Meta<typeof LayoutSelection> = {
   title: "Components/LayoutSelection",
   component: LayoutSelection,
+  parameters: {
+    msw: {
+      handlers: [
+        siteHandlers.getConfig(),
+        siteHandlers.getFooter(),
+        siteHandlers.getNavbar(),
+      ],
+    },
+  },
 }
 export default meta
 type Story = StoryObj<typeof LayoutSelection>
