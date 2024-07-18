9094bce424a3ee230d2933ce5e4f67cadb65be7b
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-07-18 10:07:01 +0800

    feat: introduce sub-component dnd custom renderer (#311)

--- apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
@@ -1,12 +1,48 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading, HStack, Icon } from "@chakra-ui/react"
+import { IconButton } from "@opengovsg/design-system-react"
+import { BiDollar, BiX } from "react-icons/bi"
 
 import FormBuilder from "./form-builder/FormBuilder"
 
 export default function ComplexEditorStateDrawer(): JSX.Element {
   return (
-    <Box p={4}>
-      <h1>Complex Editor State Drawer</h1>
-      <FormBuilder component="infocards" />
+    <Box position="relative" h="100%" w="100%">
+      <Box
+        bgColor="base.canvas.default"
+        borderBottomColor="base.divider.medium"
+        borderBottomWidth="1px"
+        px="2rem"
+        py="1.25rem"
+      >
+        <HStack justifyContent="space-between" w="100%">
+          <HStack spacing={3}>
+            <Icon
+              as={BiDollar}
+              fontSize="1.5rem"
+              p="0.25rem"
+              bgColor="slate.100"
+              textColor="blue.600"
+              borderRadius="base"
+            />
+            <Heading as="h3" size="sm" textStyle="h5" fontWeight="semibold">
+              {/* TODO: Replace this with the actual component name */}
+              Edit Infocols
+            </Heading>
+          </HStack>
+          <IconButton
+            icon={<Icon as={BiX} />}
+            variant="clear"
+            colorScheme="sub"
+            size="sm"
+            p="0.625rem"
+            onClick={() => console.log("Close drawer")}
+            aria-label="Close drawer"
+          />
+        </HStack>
+      </Box>
+      <Box px="2rem" py="1.5rem">
+        <FormBuilder component="infocols" />
+      </Box>
     </Box>
   )
 }
