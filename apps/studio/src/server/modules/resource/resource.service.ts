5c03e775a717363dec60fb5c6cb5ff32a94c401a
seaerchin <jiachin@open.gov.sg>
2024-07-19 15:03:12 +0800

    chore: fix resource typing

--- apps/studio/src/server/modules/resource/resource.service.ts
+++ apps/studio/src/server/modules/resource/resource.service.ts
@@ -1,5 +1,5 @@
 import { type DB } from "~prisma/generated/generatedTypes"
-import { SelectExpression } from "kysely"
+import type { SelectExpression } from "kysely"
 
 import { db } from "../database"
 import { type Footer, type Navbar, type Page } from "./resource.types"
@@ -100,7 +100,7 @@ export const updateBlobById = async (props: {
     const page = await tx
       .selectFrom("Resource")
       .where("Resource.id", "=", id)
-      .select("blobId")
+      .select("draftBlobId")
       .executeTakeFirstOrThrow()
 
     return (
