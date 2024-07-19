d95a9bfbd5b836ab05da822b73b1514cf3952d92
Kar Rui Lau <karrui@open.gov.sg>
2024-07-19 18:33:06 +0800

    feat: add rudimentary link to resource in dashboard

--- apps/studio/src/features/dashboard/components/ResourceTable/TitleCell.tsx
+++ apps/studio/src/features/dashboard/components/ResourceTable/TitleCell.tsx
@@ -1,18 +1,34 @@
+import { useMemo } from "react"
+import NextLink from "next/link"
 import { HStack, Icon, Text, VStack } from "@chakra-ui/react"
 import { BiFileBlank, BiFolder } from "react-icons/bi"
 
 import type { ResourceTableData } from "./types"
 
-export type TitleCellProps = Pick<
-  ResourceTableData,
-  "title" | "permalink" | "type"
->
+export interface TitleCellProps
+  extends Pick<ResourceTableData, "title" | "permalink" | "type" | "id"> {
+  siteId: number
+}
 
 export const TitleCell = ({
   title,
   permalink,
   type,
+  siteId,
+  id,
 }: TitleCellProps): JSX.Element => {
+  const linkToResource = useMemo(() => {
+    const resourceType = `${type.toLowerCase()}s`
+    return {
+      pathname: "/sites/[siteId]/[resourceType]/[id]",
+      query: {
+        siteId,
+        resourceType,
+        id,
+      },
+    }
+  }, [id, siteId, type])
+
   return (
     <HStack align="center" spacing="0.625rem">
       <Icon
@@ -21,7 +37,13 @@ export const TitleCell = ({
         color="base.content.strong"
       />
       <VStack spacing="0.25rem" align="start">
-        <Text title={title ?? ""} textStyle="subhead-2" noOfLines={1}>
+        <Text
+          as={NextLink}
+          href={linkToResource}
+          title={title}
+          textStyle="subhead-2"
+          noOfLines={1}
+        >
           {title}
         </Text>
         <Text
