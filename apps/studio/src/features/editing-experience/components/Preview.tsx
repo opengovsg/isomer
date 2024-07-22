6ae85368bc97bc3f32cb2cc12eee95bad34b70a3
Kar Rui Lau <karrui.lau@gmail.com>
2024-07-22 16:49:01 +0800

    feat: properly update page and site config in preview renderer (#335)
    ### TL;DR
    
    Enhance the Preview component to include `siteId` and dynamic overrides for date and permalink based on the selected layout. Various files have been updated to support these changes, including new properties, hooks, and configurations.
    
    ### What changed?
    
    - Added `siteId` to `useCreatePageWizardContext`. Adjusted all related components to pass `siteId` down to the Preview.
    - Introduced `useMemo` in `PreviewLayout` to create dynamic overrides for date and last modified fields based on the current layout.
    - Updated `Preview` component to accept new props: `overrides`, `siteId`, and `permalink`. Used `merge` for override handling.
    - Updated page fetching logic and trpc router to consistently use `permalink` instead of `pageName`.
    - Revised mock handlers and site configuration service to include additional site metadata such as `logoUrl`.
    
    ### How to test?
    
    1. Open the Create Page modal.
    2. Create a page with different layouts.
    3. Verify that the Preview component correctly displays the site-specific information and the dynamic fields based on the selected layout.
    
    ### Why make this change?
    
    To enable the accurate rendering of page previews with context-specific data and dynamic field updates based on user actions.
    
    ---

--- apps/studio/src/features/editing-experience/components/Preview.tsx
+++ apps/studio/src/features/editing-experience/components/Preview.tsx
@@ -1,34 +1,53 @@
-import type { IsomerSchema } from "@opengovsg/isomer-components"
+import type {
+  IsomerPageSchemaType,
+  IsomerSchema,
+} from "@opengovsg/isomer-components"
+import type { PartialDeep } from "type-fest"
 import { Skeleton } from "@chakra-ui/react"
 import { RenderEngine } from "@opengovsg/isomer-components"
+import { merge } from "lodash"
 
 import { withSuspense } from "~/hocs/withSuspense"
 import { trpc } from "~/utils/trpc"
 
-function SuspendablePreview(props: IsomerSchema) {
-  const [{ theme, isGovernment, sitemap, name }] =
-    trpc.site.getConfig.useSuspenseQuery({ id: 1 })
+type PreviewProps = IsomerSchema & {
+  permalink: string
+  siteId: number
+  overrides?: PartialDeep<IsomerPageSchemaType>
+}
+
+function SuspendablePreview({
+  permalink,
+  siteId,
+  overrides = {},
+  ...props
+}: PreviewProps) {
+  const [siteConfig] = trpc.site.getConfig.useSuspenseQuery({ id: siteId })
   const [{ content: footer }] = trpc.site.getFooter.useSuspenseQuery({
-    id: 1,
+    id: siteId,
   })
   const [{ content: navbar }] = trpc.site.getNavbar.useSuspenseQuery({
-    id: 1,
+    id: siteId,
+  })
+
+  const renderProps = merge(props, overrides, {
+    page: {
+      permalink,
+    },
   })
 
   return (
     <RenderEngine
-      {...props}
+      {...renderProps}
       site={{
-        siteName: name,
         // TODO: fixup all the typing errors
         // @ts-expect-error to fix when types are proper
         // TODO: dynamically generate sitemap
         siteMap: { title: "Home", permalink: "/", children: [] },
-        theme,
-        logoUrl: "https://www.isomer.gov.sg/images/isomer-logo.svg",
-        isGovernment,
         environment: "production",
+        // TODO: Fetch from DB in the future
         lastUpdated: "3 Apr 2024",
+        ...siteConfig,
         navBarItems: navbar,
         footerItems: footer,
       }}
