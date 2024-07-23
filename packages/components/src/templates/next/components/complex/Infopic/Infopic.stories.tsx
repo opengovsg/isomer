880a59f4902ccbd753cc68195b54fba6d45534c2
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-07-23 16:23:09 +0800

    ref: remove component variants and update titles

--- packages/components/src/templates/next/components/complex/Infopic/Infopic.stories.tsx
+++ packages/components/src/templates/next/components/complex/Infopic/Infopic.stories.tsx
@@ -19,8 +19,6 @@ type Story = StoryObj<typeof InfoPic>
 // Default scenario
 export const SideBySide: Story = {
   args: {
-    sectionIndex: 0,
-    variant: "side-by-side",
     title: "Lemon. Think small.",
     description:
       "Our little car isn't so much of a novelty anymore. An ode to Ogilvy.",
@@ -34,8 +32,6 @@ export const SideBySide: Story = {
 
 export const SideBySideRightVariant: Story = {
   args: {
-    sectionIndex: 0,
-    variant: "side-by-side",
     isTextOnRight: true,
     title: "Meet Simone Yi Ting Tan",
     description:
@@ -43,37 +39,6 @@ export const SideBySideRightVariant: Story = {
     imageAlt: "alt",
     imageSrc: "https://placehold.co/400x300",
     buttonLabel: "Simone's Journey",
-    buttonUrl: "https://www.google.com",
-  },
-}
-
-export const SidePart: Story = {
-  args: {
-    sectionIndex: 0,
-    variant: "side-part",
-    title:
-      "Explore your great neighbourhood with us can’t stretch all the way so this needs a max width",
-    description:
-      "They will try to close the door on you, just open it. Lion! The other day the grass was brown, now it’s green because I ain’t give up. Never surrender.",
-    imageAlt: "alt",
-    imageSrc: "https://placehold.co/1000x200",
-    buttonLabel: "Primary CTA",
-    buttonUrl: "https://www.google.com",
-  },
-}
-
-export const SidePartRightVariant: Story = {
-  args: {
-    sectionIndex: 0,
-    variant: "side-part",
-    isTextOnRight: true,
-    title:
-      "Explore your great neighbourhood with us can’t stretch all the way so this needs a max width",
-    description:
-      "They will try to close the door on you, just open it. Lion! The other day the grass was brown, now it’s green because I ain’t give up. Never surrender.",
-    imageAlt: "alt",
-    imageSrc: "https://placehold.co/200x200",
-    buttonLabel: "Primary CTA",
     buttonUrl: "https://www.google.com",
   },
 }
--- packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
+++ packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
@@ -1,21 +1,12 @@
 import type { KeyStatisticsProps } from "~/interfaces"
 import { ComponentContent } from "../../internal/customCssClass"
 
-const ITEM_WIDTHS: Record<
-  KeyStatisticsProps["variant"],
-  Record<number, string>
-> = {
-  side: {
-    1: "basis-full",
-    2: "basis-[calc((100%-2.5rem)/2)]",
-    3: "basis-[calc((100%-5rem)/3)] md:max-w-[calc((100%-2.5rem)/2)]",
-  },
-  top: {
-    1: "basis-full",
-    2: "basis-[calc((100%-2.5rem)/2)]",
-    3: "basis-[calc((100%-5rem)/3)] md:max-w-[calc((100%-2.5rem)/2)]",
-    4: "basis-[calc((100%-7.5rem)/4)] md:max-w-[calc((100%-5rem)/3)]",
-  },
+const MAX_ITEMS = 4
+const ITEM_WIDTHS: Record<number, string> = {
+  1: "basis-full",
+  2: "basis-[calc((100%-2.5rem)/2)]",
+  3: "basis-[calc((100%-5rem)/3)] md:max-w-[calc((100%-2.5rem)/2)]",
+  4: "basis-[calc((100%-7.5rem)/4)] md:max-w-[calc((100%-5rem)/3)]",
 }
 
 // This is the maximum number of characters in a key statistic value
@@ -25,27 +16,19 @@ const ITEM_WIDTHS: Record<
 // Example: +$1.23M, +235.2%, $123.4B
 const MAX_CHAR_LIMIT = 7
 
-const KeyStatistics = ({ variant, title, statistics }: KeyStatisticsProps) => {
-  const maxItems = variant === "side" ? 3 : 4
-
+const KeyStatistics = ({ title, statistics }: KeyStatisticsProps) => {
   return (
     <div
-      className={`${ComponentContent} flex flex-col gap-16 py-12 xs:py-24 lg:gap-24 ${
-        variant === "side" ? "lg:flex-row lg:gap-16" : ""
-      }`}
+      className={`${ComponentContent} flex flex-col gap-16 py-12 xs:py-24 lg:flex-row lg:gap-16 lg:gap-24`}
     >
-      <h2
-        className={`w-full text-2xl font-semibold text-content sm:text-4xl ${
-          variant === "side" ? "lg:w-1/3" : "md:max-w-[47.5rem]"
-        }`}
-      >
+      <h2 className="w-full text-2xl font-semibold text-content sm:text-4xl md:max-w-[47.5rem]">
         {title}
       </h2>
       <div className="flex flex-col flex-wrap justify-center gap-x-8 gap-y-10 md:flex-row">
-        {statistics.slice(0, maxItems).map(({ label, value }) => (
+        {statistics.slice(0, MAX_ITEMS).map(({ label, value }) => (
           <div
             className={`flex grow flex-col gap-3 text-center ${
-              ITEM_WIDTHS[variant][Math.min(maxItems, statistics.length)]
+              ITEM_WIDTHS[Math.min(MAX_ITEMS, statistics.length)]
             }`}
           >
             <h3 className="text-pretty text-4xl font-semibold leading-[2.75rem] text-content-strong xs:text-5xl xs:leading-[3.5rem]">
