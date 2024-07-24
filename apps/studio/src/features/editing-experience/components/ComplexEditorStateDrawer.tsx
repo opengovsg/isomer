e77cc11eb122cfa6902e6f95696af03ef622fb2b
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-07-24 13:17:00 +0800

    fix: subcomponent overflowing in absolute position (#345)
    * fix: subcomponent overflowing in absolute position
    
    * chore: fix label for KeyStatistics component
    
    * fix: move remove item button as part of box
    
    * chore: changes based on feedback from comments

--- apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
@@ -29,7 +29,7 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
   const { title } = getComponentSchema(component.type)
 
   return (
-    <Box position="relative" h="100%" w="100%">
+    <Box position="relative" h="100%" w="100%" overflow="auto">
       <Box
         bgColor="base.canvas.default"
         borderBottomColor="base.divider.medium"
@@ -65,7 +65,7 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
           />
         </HStack>
       </Box>
-      <Box px="2rem" py="1.5rem">
+      <Box px="2rem" py="1rem">
         <FormBuilder />
       </Box>
       <Box px="2rem" pb="1.5rem">
