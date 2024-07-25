ac4b979b1fa698d6d841676a993630f8c618f54a
Kar Rui Lau <karrui@open.gov.sg>
2024-07-26 01:51:48 +0800

    feat: update mobile navbar according to design

---
+++ packages/components/src/templates/next/components/internal/Navbar/MobileNavItemAccordion.tsx
@@ -0,0 +1,93 @@
+import { BiChevronDown } from "react-icons/bi"
+import { tv } from "tailwind-variants"
+
+import type { NavbarItem, NavbarProps } from "~/interfaces/internal/Navbar"
+
+interface NavItemAccordionProps
+  extends NavbarItem,
+    Pick<NavbarProps, "LinkComponent"> {
+  isOpen: boolean
+  onClick: () => void
+  index: number
+}
+
+const mobileItemStyles = tv({
+  slots: {
+    container: "border-b-base-divider-subtle flex flex-col gap-6 border-b",
+    item: "text-base-content prose-headline-base-medium flex w-full justify-between gap-6 px-6 py-3 text-left",
+    sublist: "flex flex-col gap-3.5",
+    nestedItem: "text-base-content-medium prose-body-base py-1",
+    chevron:
+      "text-base transition-transform duration-300 ease-in-out motion-reduce:transition-none",
+  },
+  variants: {
+    isOpen: {
+      true: {
+        container: "pb-6",
+        chevron: "-rotate-180",
+      },
+    },
+  },
+})
+
+const { item, chevron, container, nestedItem, sublist } = mobileItemStyles()
+
+export const MobileNavItemAccordion = ({
+  name,
+  url,
+  items,
+  isOpen,
+  onClick,
+  index,
+  LinkComponent,
+}: NavItemAccordionProps) => {
+  if (!items || items.length === 0) {
+    return (
+      <div className={container()}>
+        <LinkComponent className={item()} href={url}>
+          {name}
+        </LinkComponent>
+      </div>
+    )
+  }
+  return (
+    <section className={container({ isOpen })}>
+      <div>
+        <h2>
+          <button
+            id={`accordion-button-${index}`}
+            aria-expanded={isOpen}
+            aria-controls={`menu-content-${index}`}
+            onClick={onClick}
+            className={item({ isOpen })}
+          >
+            {name}
+            <BiChevronDown className={chevron({ isOpen })} />
+          </button>
+        </h2>
+      </div>
+      <div
+        id={`menu-content-${index}`}
+        aria-labelledby={`accordion-button-${index}`}
+        hidden={!isOpen}
+        role="region"
+      >
+        <ul className={sublist()}>
+          {items.map((subItem) => (
+            <li key={subItem.name}>
+              {/* 44px */}
+              <LinkComponent
+                href={subItem.url}
+                className={item({
+                  className: nestedItem(),
+                })}
+              >
+                {subItem.name}
+              </LinkComponent>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
--- packages/components/src/templates/next/components/internal/Navbar/Navbar.tsx
+++ packages/components/src/templates/next/components/internal/Navbar/Navbar.tsx
@@ -1,39 +1,35 @@
 "use client"
 
 import { startTransition, useCallback, useRef, useState } from "react"
-import {
-  BiChevronRight,
-  BiLeftArrowAlt,
-  BiRightArrowAlt,
-  BiSearch,
-  BiX,
-} from "react-icons/bi"
+import { BiSearch, BiX } from "react-icons/bi"
 import { tv } from "tailwind-variants"
 import { useOnClickOutside } from "usehooks-ts"
 
 import type { NavbarProps } from "~/interfaces"
 import { LocalSearchInputBox, SearchSGInputBox } from "../../internal"
 import { HamburgerIcon } from "./HamburgerIcon"
+import { MobileNavItemAccordion } from "./MobileNavItemAccordion"
 import { NavItem } from "./NavItem"
 
 const navbarStyles = tv({
   slots: {
     icon: "my-3 flex h-[2.125rem] w-[2.125rem] items-center justify-center text-[1.25rem] lg:my-[1.1875rem]",
     logo: "my-3 h-10 w-32 max-w-[6.625rem] object-contain object-center lg:h-12 lg:max-w-32",
+    navbarContainer: "flex min-h-16 w-full lg:min-h-[4.25rem]",
     navbar:
-      "mx-auto flex min-h-16 w-full max-w-screen-xl gap-x-4 px-6 lg:min-h-[4.25rem] lg:gap-x-6 lg:px-10",
+      "mx-auto flex w-full max-w-screen-xl gap-x-4 px-6 lg:gap-x-6 lg:px-10",
     navItemContainer: "hidden flex-wrap items-center gap-x-6 lg:flex",
   },
   variants: {
     isMenuOpen: {
       true: {
-        navbar: "sticky top-0 z-10 bg-white",
+        navbarContainer: "sticky top-0 z-10 bg-white",
       },
     },
   },
 })
 
-const { navItemContainer, navbar, logo, icon } = navbarStyles()
+const { navItemContainer, navbarContainer, navbar, logo, icon } = navbarStyles()
 
 export const Navbar = ({
   logoUrl,
@@ -63,89 +59,98 @@ export const Navbar = ({
 
   const megaMenuRef = useRef(null)
   const activeNavRef = useRef(null)
+  const mobileMenuRef = useRef(null)
 
-  useOnClickOutside([activeNavRef, megaMenuRef], handleClickOutside, "mouseup")
+  useOnClickOutside(
+    [activeNavRef, megaMenuRef, mobileMenuRef],
+    handleClickOutside,
+    "mouseup",
+  )
 
   return (
     <div className="relative flex flex-col">
       {/* Site header */}
-      <div className={navbar({ isMenuOpen })} ref={siteHeaderRef}>
-        {/* Logo */}
-        <LinkComponent href="/">
-          <img src={logoUrl} alt={logoAlt} className={logo()} />
-        </LinkComponent>
+      <div className={navbarContainer({ isMenuOpen })} ref={siteHeaderRef}>
+        <div className={navbar()}>
+          {/* Logo */}
+          <LinkComponent href="/">
+            <img src={logoUrl} alt={logoAlt} className={logo()} />
+          </LinkComponent>
 
-        {/* Navigation items (for desktop) */}
-        <ul className={navItemContainer()} ref={navDesktopRef}>
-          {items.map((item, idx) => (
-            <NavItem
-              key={`${item.name}-{idx}`}
-              megaMenuRef={megaMenuRef}
-              ref={openNavItemIdx === idx ? activeNavRef : null}
-              {...item}
-              onCloseMegamenu={handleClickOutside}
-              onClick={() => {
-                setIsSearchOpen(false)
-                setOpenNavItemIdx((currIdx) => (currIdx === idx ? -1 : idx))
-              }}
-              isOpen={openNavItemIdx === idx}
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              LinkComponent={LinkComponent}
-            />
-          ))}
-        </ul>
+          {/* Navigation items (for desktop) */}
+          <ul className={navItemContainer()} ref={navDesktopRef}>
+            {items.map((item, index) => (
+              <NavItem
+                key={`${item.name}-${index}`}
+                megaMenuRef={megaMenuRef}
+                ref={openNavItemIdx === index ? activeNavRef : null}
+                {...item}
+                onCloseMegamenu={handleClickOutside}
+                onClick={() => {
+                  setIsSearchOpen(false)
+                  setOpenNavItemIdx((currIdx) =>
+                    currIdx === index ? -1 : index,
+                  )
+                }}
+                isOpen={openNavItemIdx === index}
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                LinkComponent={LinkComponent}
+              />
+            ))}
+          </ul>
 
-        {/* Spacer */}
-        <div className="flex-1" />
+          {/* Spacer */}
+          <div className="flex-1" />
 
-        {/* Search icon */}
-        {search && !isHamburgerOpen && (
-          <>
-            {isSearchOpen ? (
+          {/* Search icon */}
+          {search && !isHamburgerOpen && (
+            <>
+              {isSearchOpen ? (
+                <button
+                  onClick={() => setIsSearchOpen(!isSearchOpen)}
+                  aria-label="Close search bar"
+                  className={icon({ className: "text-[1.5rem]" })}
+                >
+                  <BiX />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setIsSearchOpen(!isSearchOpen)}
+                  aria-label="Open search bar"
+                  className={icon()}
+                >
+                  <BiSearch />
+                </button>
+              )}
+            </>
+          )}
+
+          {/* Hamburger menu for small screens */}
+          <div className="flex lg:hidden">
+            {isHamburgerOpen ? (
               <button
-                onClick={() => setIsSearchOpen(!isSearchOpen)}
-                aria-label="Close search bar"
-                className="mt-[5px]"
+                onClick={() => {
+                  setIsHamburgerOpen(false)
+                  setOpenNavItemIdx(-1)
+                }}
+                className={icon({ className: "text-[1.5rem]" })}
+                aria-label="Close navigation menu"
               >
-                <BiX className="text-2xl" />
+                <BiX />
               </button>
             ) : (
               <button
-                onClick={() => setIsSearchOpen(!isSearchOpen)}
-                aria-label="Open search bar"
+                onClick={() => {
+                  setIsHamburgerOpen(true)
+                  setIsSearchOpen(false)
+                }}
+                aria-label="Open navigation menu"
                 className={icon()}
               >
-                <BiSearch />
+                <HamburgerIcon />
               </button>
             )}
-          </>
-        )}
-
-        {/* Hamburger menu for small screens */}
-        <div className="flex lg:hidden">
-          {isHamburgerOpen ? (
-            <button
-              onClick={() => {
-                setIsHamburgerOpen(false)
-                setOpenNavItemIdx(-1)
-              }}
-              className={icon({ className: "text-[1.5rem]" })}
-              aria-label="Close navigation menu"
-            >
-              <BiX />
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                setIsHamburgerOpen(true)
-                setIsSearchOpen(false)
-              }}
-              aria-label="Open navigation menu"
-              className={icon()}
-            >
-              <HamburgerIcon />
-            </button>
-          )}
+          </div>
         </div>
       </div>
 
@@ -169,77 +174,24 @@ export const Navbar = ({
           )}
         </div>
       )}
-
-      {/* Navigation items, first level (for mobile/tablet) */}
-      {isHamburgerOpen && openNavItemIdx === -1 && (
-        <div className="block lg:hidden">
-          <ul className="px-6 pt-4">
-            {items.map(({ name, url, items }, idx) => {
-              if (!items || items.length === 0) {
-                return (
-                  <li key={Math.random()} className="w-full py-3">
-                    <LinkComponent
-                      className="text-md block w-full text-content hover:text-content-medium"
-                      href={url}
-                    >
-                      {name}
-                    </LinkComponent>
-                  </li>
-                )
+      {isHamburgerOpen && (
+        <div
+          ref={mobileMenuRef}
+          className="border-t-base-divider-subtle border-t"
+        >
+          {items.map((item, index) => (
+            <MobileNavItemAccordion
+              key={`${item.name}-${index}`}
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              LinkComponent={LinkComponent}
+              index={index}
+              isOpen={index === openNavItemIdx}
+              onClick={() =>
+                setOpenNavItemIdx((currIdx) => (currIdx === index ? -1 : index))
               }
-
-              return (
-                <li key={Math.random()} className="w-full py-3">
-                  <button
-                    onClick={() => setOpenNavItemIdx(idx)}
-                    className="w-full"
-                  >
-                    <div className="flex w-full flex-row justify-between">
-                      <p className="text-md text-content">{name}</p>
-                      <BiChevronRight className="text-2xl" />
-                    </div>
-                  </button>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      )}
-
-      {/* Navigation items, second level (for mobile/tablet) */}
-      {isHamburgerOpen && openNavItemIdx !== -1 && (
-        <div className="block lg:hidden">
-          <div className="px-6 pt-4">
-            <button
-              className="flex flex-row gap-3 pb-4 pt-2.5"
-              onClick={() => setOpenNavItemIdx(-1)}
-              aria-label="Return to main navigation menu"
-            >
-              <BiLeftArrowAlt className="text-2xl" />
-              <h5 className="text-md">{items[openNavItemIdx]?.name}</h5>
-            </button>
-
-            <ul className="flex flex-row flex-wrap gap-x-36 gap-y-5 px-9 py-4 md:gap-y-8">
-              {items[openNavItemIdx]?.items?.map(
-                ({ name, url, description }) => (
-                  <li key={name} className="w-full md:w-1/3">
-                    <div className="flex flex-col gap-1">
-                      <LinkComponent
-                        href={url}
-                        className="text-content-medium text-paragraph-01 md:text-content"
-                      >
-                        {name}
-                        <BiRightArrowAlt className="-mt-0.5 ml-1 hidden text-lg md:inline" />
-                      </LinkComponent>
-                      <p className="hidden text-content-medium text-paragraph-02 md:block">
-                        {description}
-                      </p>
-                    </div>
-                  </li>
-                ),
-              )}
-            </ul>
-          </div>
+              {...item}
+            />
+          ))}
         </div>
       )}
     </div>
