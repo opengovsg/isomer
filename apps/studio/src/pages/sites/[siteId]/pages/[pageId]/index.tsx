e9c5b47404f11bef94141ae64d728dae7617ff41
Kar Rui Lau <karrui@open.gov.sg>
2024-07-29 17:17:05 +0800

    feat: handle scroll lock triggering on parent when to lock in iframe

--- apps/studio/src/pages/sites/[siteId]/pages/[pageId]/index.tsx
+++ apps/studio/src/pages/sites/[siteId]/pages/[pageId]/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/react"
-import Frame from "react-frame-component"
+import { Flex, Grid, GridItem } from "@chakra-ui/react"
 import { z } from "zod"
 
 import { useEditorDrawerContext } from "~/contexts/EditorDrawerContext"
 import EditPageDrawer from "~/features/editing-experience/components/EditPageDrawer"
 import Preview from "~/features/editing-experience/components/Preview"
+import { PreviewIframe } from "~/features/editing-experience/components/PreviewIframe"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { PageEditingLayout } from "~/templates/layouts/PageEditingLayout"
 import { trpc } from "~/utils/trpc"
@@ -51,34 +51,17 @@ function EditPage(): JSX.Element {
       </GridItem>
       {/* TODO: Implement preview */}
       <GridItem colSpan={2} overflow="auto">
-        <Box p="2rem" bg="gray.100" h="100%">
-          <Flex
-            borderRadius="8px"
-            bg="white"
-            shadow="md"
-            overflow="auto"
-            h="100%"
-          >
-            <Frame
-              style={{
-                flexGrow: 1,
-                border: "none",
-                margin: 0,
-                padding: 0,
-              }}
-              initialContent='<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="/assets/css/preview-tw.css" /></head><body><div id="preview-mount"></div></body></html>'
-              mountTarget="#preview-mount"
-            >
-              <Preview
-                siteId={siteId}
-                {...page}
-                permalink={permalink}
-                version="0.1.0"
-                content={previewPageState}
-              />
-            </Frame>
-          </Flex>
-        </Box>
+        <Flex justify="center" p="2rem" bg="gray.100" h="100%">
+          <PreviewIframe>
+            <Preview
+              siteId={siteId}
+              {...page}
+              permalink={permalink}
+              version="0.1.0"
+              content={previewPageState}
+            />
+          </PreviewIframe>
+        </Flex>
       </GridItem>
     </Grid>
   )
