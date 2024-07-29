527242a47c0266260065babaaa2b3b32d777e3ef
seaerchin <jiachin@open.gov.sg>
2024-07-30 04:20:39 +0800

    fix: errorneous string cast

--- apps/studio/src/server/modules/page/page.router.ts
+++ apps/studio/src/server/modules/page/page.router.ts
@@ -126,7 +126,7 @@ export const pageRouter = router({
               title,
               permalink,
               siteId,
-              parentId: String(folderId),
+              ...(folderId && { parentId: String(folderId) }),
               draftBlobId: blob.id,
               type: ResourceType.Page,
             })
