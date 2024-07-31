2c66664fbe9302f71fe9c45a48fb9e0bea5bf7b1
Harish V <harish@open.gov.sg>
2024-07-31 21:59:04 +0800

    add serializable isolation

--- apps/studio/src/server/modules/version/version.service.ts
+++ apps/studio/src/server/modules/version/version.service.ts
@@ -54,43 +54,45 @@ export const addNewVersion = async ({
   pageId: number
   userId: string
 }) => {
-  return await db.transaction().execute(async (tx) => {
-    const page = await getPageById(tx, { siteId, resourceId: pageId })
+  return await db
+    .transaction()
+    .setIsolationLevel("serializable")
+    .execute(async (tx) => {
+      const page = await getPageById(tx, { siteId, resourceId: pageId })
 
-    if (!page.draftBlobId) {
-      return { error: "No drafts to publish for this page" }
-    }
+      if (!page.draftBlobId) {
+        return { error: "No drafts to publish for this page" }
+      }
 
-    let newVersionNum = 1
-    if (page.publishedVersionId) {
-      const currentVersion = await getVersionById({
-        versionId: page.publishedVersionId,
+      let newVersionNum = 1
+      if (page.publishedVersionId) {
+        const currentVersion = await getVersionById({
+          versionId: page.publishedVersionId,
+        })
+        newVersionNum = Number(currentVersion.versionNum) + 1
+      }
+
+      // Create the new version
+      const newVersion = await createVersion(tx, {
+        versionNum: newVersionNum,
+        resourceId: pageId,
+        blobId: Number(page.draftBlobId),
+        publisherId: userId,
       })
-      newVersionNum = Number(currentVersion.versionNum) + 1
-    }
 
-    // Create the new version
-    // TODO: To pass in the tx object
-    const newVersion = await createVersion(tx, {
-      versionNum: newVersionNum,
-      resourceId: pageId,
-      blobId: Number(page.draftBlobId),
-      publisherId: userId,
+      // Update resource with new versionId and draft to be null
+      await updatePageById(
+        {
+          ...page,
+          id: parseInt(page.id),
+          publishedVersionId: newVersion.versionId,
+          draftBlobId: null,
+          state: "Published",
+          siteId,
+          parentId: page.parentId ? parseInt(page.parentId) : undefined,
+        },
+        tx,
+      )
+      return { versionId: newVersion }
     })
-
-    // Update resource with new versionId and draft to be null
-    await updatePageById(
-      {
-        ...page,
-        id: parseInt(page.id),
-        publishedVersionId: newVersion.versionId,
-        draftBlobId: null,
-        state: "Published",
-        siteId,
-        parentId: page.parentId ? parseInt(page.parentId) : undefined,
-      },
-      tx,
-    )
-    return { versionId: newVersion }
-  })
 }
