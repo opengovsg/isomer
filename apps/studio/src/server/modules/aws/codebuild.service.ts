e7159d214f59041e56b4686adb9a65a5f2109b66
Harish V <harish@open.gov.sg>
2024-08-01 00:33:18 +0800

    refactor code

--- apps/studio/src/server/modules/aws/codebuild.service.ts
+++ apps/studio/src/server/modules/aws/codebuild.service.ts
@@ -1,47 +1,14 @@
-import {
-  BatchGetProjectsCommand,
-  CodeBuildClient,
-  CreateProjectCommand,
-  StartBuildCommand,
-} from "@aws-sdk/client-codebuild"
+import type { StartBuildCommandOutput } from "@aws-sdk/client-codebuild"
+import type pino from "pino"
+import { CodeBuildClient, StartBuildCommand } from "@aws-sdk/client-codebuild"
 import { ServiceException } from "@aws-sdk/smithy-client"
-import * as yaml from "yaml"
-
-import buildspec from "./buildspec.json"
 
 const client = new CodeBuildClient({ region: "ap-southeast-1" })
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getProjectById = async (projectId: string): Promise<any> => {
-  const command = new BatchGetProjectsCommand({ names: [projectId] })
-
-  try {
-    const response = await client.send(command)
-    console.log("AWS response", JSON.stringify(response))
-    if (response.projects && response.projects.length > 0) {
-      return response.projects[0]
-    } else {
-      throw new Error(`Project with ID ${projectId} not found`)
-    }
-  } catch (error) {
-    if (error instanceof ServiceException) {
-      console.error("Service error:", error)
-    } else {
-      console.error("Unexpected error:", error)
-    }
-    throw error
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const startProjectById = async (projectId: string): Promise<any> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const project = await getProjectById(projectId)
-
-  if (!project) {
-    throw new Error(`Project with ID ${projectId} not found`)
-  }
-
+export const startProjectById = async (
+  logger: pino.Logger<string>,
+  projectId: string,
+): Promise<StartBuildCommandOutput> => {
   const command = new StartBuildCommand({ projectName: projectId })
 
   try {
@@ -49,65 +16,12 @@ export const startProjectById = async (projectId: string): Promise<any> => {
     return response
   } catch (error) {
     if (error instanceof ServiceException) {
-      console.error("Service error:", error)
+      logger.error({ projectId, error }, "AWS CodeBuild service exception")
     } else {
-      console.error("Unexpected error:", error)
-    }
-    throw error
-  }
-}
-
-export const createCodeBuildProject = async ({
-  projectId,
-  siteId,
-}: {
-  projectId: string
-  siteId: number
-}): Promise<any> => {
-  const stringifiedBuildspec = yaml.stringify(buildspec)
-
-  /* TODO: Things to add
-1. VPC config
-2. Default service role
-3. Move buildspec out
-4. Setup logs
-*/
-  const command = new CreateProjectCommand({
-    name: projectId,
-    source: {
-      type: "NO_SOURCE",
-      buildspec: stringifiedBuildspec,
-    },
-    artifacts: {
-      type: "NO_ARTIFACTS",
-    },
-    environment: {
-      type: "LINUX_CONTAINER",
-      image: "aws/codebuild/amazonlinux2-x86_64-standard:5.0",
-      imagePullCredentialsType: "CODEBUILD",
-      computeType: "BUILD_GENERAL1_SMALL",
-      environmentVariables: [{ name: "SITE_ID", value: siteId.toString() }],
-    },
-    serviceRole: "",
-    logsConfig: {
-      cloudWatchLogs: {
-        status: "DISABLED",
-      },
-      s3Logs: {
-        status: "DISABLED",
-      },
-    },
-  })
-
-  try {
-    const response = await client.send(command)
-    console.log("AWS Create project res: ", JSON.stringify(response))
-    return response
-  } catch (error) {
-    if (error instanceof ServiceException) {
-      console.error("Service error:", error)
-    } else {
-      console.error("Unexpected error:", error)
+      logger.error(
+        { projectId, error },
+        "Unexpected error when starting CodeBuild project run",
+      )
     }
     throw error
   }
