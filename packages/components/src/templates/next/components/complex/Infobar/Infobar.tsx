d1072281b65ac9ba8569d01aa8b49e6d235c3f0b
Park Se Hyun <>
2024-07-31 22:54:42 +0800

    style: apply type/colour tokens, fix spacing

--- packages/components/src/templates/next/components/complex/Infobar/Infobar.tsx
+++ packages/components/src/templates/next/components/complex/Infobar/Infobar.tsx
@@ -13,32 +13,36 @@ const Infobar = ({
 }: InfobarProps) => {
   return (
     <section>
-      <div
-        className={`${ComponentContent} mx-auto flex flex-col items-center gap-12 py-16 text-center lg:max-w-3xl lg:py-24`}
-      >
-        <div className="flex flex-col gap-7">
-          <h1 className="text-4xl font-semibold leading-tight text-content lg:text-5xl lg:leading-tight">
-            {title}
-          </h1>
-          {description && <p className="text-xl text-content">{description}</p>}
-        </div>
-        <div className="flex flex-col items-center gap-6 sm:flex-row">
-          {buttonLabel && buttonUrl && (
-            <Button
-              label={buttonLabel}
-              href={buttonUrl}
-              LinkComponent={LinkComponent}
-            />
-          )}
-          {secondaryButtonLabel && secondaryButtonUrl && (
-            <Button
-              label={secondaryButtonLabel}
-              href={secondaryButtonUrl}
-              colorScheme="black"
-              variant="outline"
-              LinkComponent={LinkComponent}
-            />
-          )}
+      <div className={`${ComponentContent} mx-6 py-16 sm:mx-10 lg:py-24`}>
+        <div className="mx-auto flex flex-col items-center gap-9 text-center lg:max-w-3xl">
+          <div className="flex flex-col gap-6">
+            <h1 className="prose-display-lg text-base-content-strong">
+              {title}
+            </h1>
+            {description && (
+              <p className="prose-headline-lg-regular text-base-content">
+                {description}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col items-center gap-x-5 gap-y-4 sm:flex-row">
+            {buttonLabel && buttonUrl && (
+              <Button
+                label={buttonLabel}
+                href={buttonUrl}
+                LinkComponent={LinkComponent}
+              />
+            )}
+            {secondaryButtonLabel && secondaryButtonUrl && (
+              <Button
+                label={secondaryButtonLabel}
+                href={secondaryButtonUrl}
+                colorScheme="black"
+                variant="outline"
+                LinkComponent={LinkComponent}
+              />
+            )}
+          </div>
         </div>
       </div>
     </section>
