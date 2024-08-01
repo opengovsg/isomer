4a6cb64e59c0192a6b17f879a67f66dea9f3e74e
Kar Rui Lau <karrui@open.gov.sg>
2024-08-01 17:46:00 +0800

    feat: add IconButton component (only for navbar use tbh)

---
+++ packages/components/src/templates/next/components/internal/IconButton/IconButton.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { BiSearch } from "react-icons/bi"
+
+import { IconButton } from "./IconButton"
+
+const meta: Meta<typeof IconButton> = {
+  title: "Next/Internal Components/IconButton",
+  component: IconButton,
+  parameters: {
+    themes: {
+      themeOverride: "Isomer Next",
+    },
+  },
+}
+export default meta
+type Story = StoryObj<typeof IconButton>
+
+export const Default: Story = {
+  args: {
+    icon: BiSearch,
+  },
+}
