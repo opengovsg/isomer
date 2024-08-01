e7fcc8506010d27c1d550adf21a4424c480589d7
Kar Rui Lau <karrui@open.gov.sg>
2024-08-01 18:10:46 +0800

    feat: add attribution to files modified from react-aria-components

--- packages/components/src/templates/next/components/internal/Link/Link.tsx
+++ packages/components/src/templates/next/components/internal/Link/Link.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * Original file: https://github.com/adobe/react-spectrum/blob/d6d5bc03cf6cb39d37b2a9e2de201f950b0d8994/packages/react-aria-components/src/Link.tsx
+ * Original file license: Apache-2.0
+ *
+ * This is a modified version of the original file.
+ */
+
 import type { ElementType } from "react"
 import type {
   LinkProps as BaseLinkProps,
