4835abf319f6c87cd2070f85454d718c777267bd
Harish V <harish@open.gov.sg>
2024-08-03 23:18:23 +0800

    update script

--- tooling/build/scripts/publisher.sh
+++ tooling/build/scripts/publisher.sh
@@ -1,60 +1,93 @@
 #!/bin/bash
-# run recursive.ts
+
+# Helper function to calculate duration
+calculate_duration() {
+  start_time=$1
+  end_time=$(date +%s)
+  duration=$((end_time - start_time))
+  echo "Time taken: $duration seconds"
+}
+
+# Cloning the repository
+echo "Cloning repository..."
+start_time=$(date +%s)
 git clone https://github.com/opengovsg/isomer.git
 cd isomer/
+calculate_duration $start_time
 
 echo $(pwd)
 
-# TODO: remove later
+# Checkout specific branch
+echo "Checking out branch..."
+start_time=$(date +%s)
 git checkout 08-03-add_publishing_scripts
+calculate_duration $start_time
 
 echo $(git branch)
 
+# Install dependencies
+echo "Installing dependencies..."
+start_time=$(date +%s)
 npm install
+calculate_duration $start_time
 
+# Build components
+echo "Building components..."
+start_time=$(date +%s)
 cd packages/components
 npm run build
 cd ../.. # back to root
 echo $(pwd)
+calculate_duration $start_time
 
 # Fetch from database
+echo "Fetching from database..."
+start_time=$(date +%s)
 cd tooling/build/scripts/publishing
 echo $(pwd)
 npm install
 npm install ts-node -g
 npm run start
+calculate_duration $start_time
 
 # Build site
-# TODO: Fix the schemas for data
-# mv data/ isomer/tooling/template/data/
-rm -rf ../../../template/schema/
+echo "Building site..."
+start_time=$(date +%s)
+rm -rf ../../../template/
 mv schema/ ../../../template/
 cd ../../../template
 echo $(pwd)
 npm install
 
-# prebuild
+# Prebuild
+echo "Prebuilding..."
 rm -rf node_modules && rm -rf .next
 curl https://raw.githubusercontent.com/opengovsg/isomer/main/tooling/build/scripts/preBuild.sh | bash
 
-# build
+# Build
+echo "Building..."
 rm -rf scripts/
 curl https://raw.githubusercontent.com/opengovsg/isomer/main/tooling/build/scripts/build.sh | bash
 
 ls -al
 find ./out -type f | wc -l
+calculate_duration $start_time
 
 cd out/
 echo $(pwd)
 ls -al
 
-# zip
+# Zip the build
+echo "Zipping build..."
+start_time=$(date +%s)
 zip -r ../build.zip .
 cd ../
 echo $(pwd)
+calculate_duration $start_time
 
-# upload to amplify
-echo "Creating deployment in AWS Amplify..."
+# Upload to AWS Amplify
+echo "Uploading to AWS Amplify..."
+start_time=$(date +%s)
 DEPLOY_DATA=$(aws amplify create-deployment --app-id "$AMPLIFY_APP_ID" --branch-name "staging")
 JOB_ID=$(echo $DEPLOY_DATA | jq -r '.jobId')
 echo "JOB_ID: $JOB_ID"
@@ -62,8 +95,11 @@ UPLOAD_URL=$(echo $DEPLOY_DATA | jq -r '.zipUploadUrl')
 echo "UPLOAD_URL: $UPLOAD_URL"
 echo "Uploading build artifacts..."
 curl -T build.zip "$UPLOAD_URL"
+calculate_duration $start_time
 
-# start amplify deployment
+# Start AWS Amplify deployment
 echo "Starting deployment..."
+start_time=$(date +%s)
 aws amplify start-deployment --app-id "$AMPLIFY_APP_ID" --branch-name "staging" --job-id $JOB_ID
 echo "Deployment created in Amplify successfully"
+calculate_duration $start_time
