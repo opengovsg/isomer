3e44e50243aed7f273678145bb2c54a10091c1fd
Park Se Hyun <>
2024-08-05 16:24:19 +0800

    apply tokens to hyperlinks

--- packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
+++ packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
@@ -10,7 +10,7 @@ export const BaseParagraph = ({
 
   return (
     <p
-      className={`[&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-hyperlink [&_a]:underline visited:[&_a]:text-purple-600 hover:[&_a]:text-hyperlink-hover ${className}`}
+      className={`hover:[&_a]text-link-hover [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-link [&_a]:underline ${className}`}
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       id={id}
     />
