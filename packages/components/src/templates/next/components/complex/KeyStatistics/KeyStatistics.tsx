8e8a8f8c9ebbebcef377d6b0ac64c45309bfeb1a
Park Se Hyun <>
2024-08-05 09:42:35 +0800

    apply tokens and fix layout

--- packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
+++ packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
@@ -2,12 +2,6 @@ import type { KeyStatisticsProps } from "~/interfaces"
 import { ComponentContent } from "../../internal/customCssClass"
 
 const MAX_ITEMS = 4
-const ITEM_WIDTHS: Record<number, string> = {
-  1: "basis-full",
-  2: "basis-[calc((100%-2.5rem)/2)]",
-  3: "basis-[calc((100%-5rem)/3)] md:max-w-[calc((100%-2.5rem)/2)]",
-  4: "basis-[calc((100%-7.5rem)/4)] md:max-w-[calc((100%-5rem)/3)]",
-}
 
 // This is the maximum number of characters in a key statistic value
 // This is required because we make all columns have the same width. If there is
@@ -21,20 +15,20 @@ const KeyStatistics = ({ title, statistics }: KeyStatisticsProps) => {
     <div
       className={`${ComponentContent} flex flex-col gap-16 py-12 xs:py-24 lg:gap-24`}
     >
-      <h2 className="w-full text-2xl font-semibold text-content sm:text-4xl md:max-w-[47.5rem]">
+      <h2 className="prose-display-md w-full max-w-[47.5rem] text-base-content-strong">
         {title}
       </h2>
-      <div className="flex flex-col flex-wrap justify-center gap-x-8 gap-y-10 md:flex-row">
+      <div className="flex flex-col flex-wrap gap-x-8 gap-y-10 md:flex-row">
         {statistics.slice(0, MAX_ITEMS).map(({ label, value }) => (
           <div
-            className={`flex grow flex-col gap-3 text-center ${
-              ITEM_WIDTHS[Math.min(MAX_ITEMS, statistics.length)]
-            }`}
+            className={`flex max-w-[50%] grow flex-col gap-3 md:basis-1/3 lg:basis-0`}
           >
-            <h3 className="text-pretty text-4xl font-semibold leading-[2.75rem] text-content-strong xs:text-5xl xs:leading-[3.5rem]">
+            <h3 className="prose-display-lg text-pretty text-brand-canvas-inverse">
               {value.slice(0, MAX_CHAR_LIMIT)}
             </h3>
-            <p className="text-sm font-medium text-neutral-500">{label}</p>
+            <p className="prose-label-md-medium text-base-content-subtle">
+              {label}
+            </p>
           </div>
         ))}
       </div>
