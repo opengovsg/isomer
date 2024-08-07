82f6ee623bfccb7ee614352f66ea552632f5a09b
Kar Rui Lau <karrui@open.gov.sg>
2024-08-07 11:11:13 +0800

    fix: padding and layout of infopic content in smaller screen widths

--- packages/components/src/templates/next/components/complex/Infopic/Infopic.tsx
+++ packages/components/src/templates/next/components/complex/Infopic/Infopic.tsx
@@ -11,8 +11,10 @@ const infopicStyles = tv({
     image: "inset-0 h-full w-full object-cover lg:absolute",
     imageContainer:
       "relative max-h-[400px] w-full [grid-area:img] md:max-h-[560px] lg:max-h-full",
+    // max-width of content in desktop is HALF of max-w-screen-xl for correct alignment of content
+    // since content is half of screen width in desktop.
     content:
-      "px-6 pb-16 pt-10 text-base-content [grid-area:content] md:max-w-[760px] md:px-10 md:pb-20 md:pt-16 lg:max-w-[676px] lg:p-24",
+      "px-6 pb-16 pt-10 text-base-content [grid-area:content] md:max-w-[760px] md:px-10 md:pb-20 md:pt-16 lg:max-w-[620px] lg:py-24 lg:pl-10",
     title: "prose-display-md text-base-content-strong",
     description: "prose-body-base mt-6",
     button: "mt-9",
@@ -21,11 +23,11 @@ const infopicStyles = tv({
     isTextOnRight: {
       true: {
         container: "lg:[grid-template-areas:'img_content']",
-        content: "lg:justify-self-start",
+        content: "lg:justify-self-start lg:pl-24",
       },
       false: {
         container: "lg:[grid-template-areas:'content_img']",
-        content: "lg:justify-self-end",
+        content: "lg:justify-self-end lg:pr-24",
       },
     },
   },
