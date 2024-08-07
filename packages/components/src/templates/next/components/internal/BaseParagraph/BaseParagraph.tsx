98f409ecfc54a9141fc3f583c14bd530f74d4234
Kar Rui Lau <karrui@open.gov.sg>
2024-08-07 17:32:15 +0800

    refactor: use twMerge when appending classNames for dedupe

--- packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
+++ packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
@@ -1,16 +1,20 @@
 import type { BaseParagraphProps } from "~/interfaces/native/Paragraph"
+import { twMerge } from "~/lib/twMerge"
 import { getSanitizedInlineContent } from "~/utils"
 
 export const BaseParagraph = ({
   content,
-  className = "",
+  className,
   id,
 }: BaseParagraphProps) => {
   const sanitizedContent = getSanitizedInlineContent(content)
 
   return (
     <p
-      className={`hover:[&_a]text-link-hover [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-link [&_a]:underline ${className}`}
+      className={twMerge(
+        `hover:[&_a]text-link-hover [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-link [&_a]:underline`,
+        className,
+      )}
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       id={id}
     />
