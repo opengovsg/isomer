cca9620a9e2daf639a180607ef9eb22b8537e5fa
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-08 14:04:56 +0800

    feat: handle malware scan result and add soft delete

--- apps/studio/src/lib/s3.ts
+++ apps/studio/src/lib/s3.ts
@@ -1,5 +1,14 @@
-import type { PutObjectCommandInput } from "@aws-sdk/client-s3"
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import type {
+  CopyObjectCommandInput,
+  GetObjectCommandInput,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3"
+import {
+  CopyObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 import { env } from "~/env.mjs"
@@ -11,8 +20,9 @@ export const storage = new S3Client({
 })
 
 export const generateSignedPutUrl = async ({
+  Bucket,
   Key,
-}: Pick<PutObjectCommandInput, "Key">): Promise<string> => {
+}: Pick<PutObjectCommandInput, "Bucket" | "Key">): Promise<string> => {
   return getSignedUrl(
     storage,
     new PutObjectCommand({
@@ -22,5 +32,43 @@ export const generateSignedPutUrl = async ({
     {
       expiresIn: 60 * 5, // 5 minutes
     },
+  )
+}
+
+type CopyFileParams = Pick<CopyObjectCommandInput, "Key"> & {
+  SourceBucket: CopyObjectCommandInput["Bucket"]
+  DestinationBucket: CopyObjectCommandInput["Bucket"]
+}
+
+export const copyFile = async ({
+  Key,
+  SourceBucket,
+  DestinationBucket,
+}: CopyFileParams) => {
+  return storage.send(
+    new CopyObjectCommand({
+      Bucket: DestinationBucket,
+      CopySource: `${SourceBucket}/${Key}`,
+      Key,
+    }),
+  )
+}
+
+export const deleteFile = async ({
+  Key,
+  Bucket,
+}: Pick<CopyObjectCommandInput, "Key" | "Bucket">) => {
+  return storage.send(
+    new CopyObjectCommand({
+      Bucket,
+      CopySource: `${Bucket}/${Key}`,
+      Key,
+      MetadataDirective: "REPLACE",
+      // NOTE: We perform a soft delete here so the file can be kept available
+      // until the page is published
+      Metadata: {
+        deleted: "true",
+      },
+    }),
   )
 }
--- apps/studio/src/schemas/asset.ts
+++ apps/studio/src/schemas/asset.ts
@@ -6,3 +6,30 @@ export const getPresignedPutUrlSchema = z.object({
     required_error: "Missing file name",
   }),
 })
+
+// Reference: https://docs.aws.amazon.com/guardduty/latest/ug/monitor-with-eventbridge-s3-malware-protection.html
+export const postFileScanResultSchema = z.object({
+  detail: z.object({
+    scanStatus: z.string(),
+    s3ObjectDetails: z.object({
+      objectKey: z.string(),
+      bucketName: z.string(),
+    }),
+    scanResultDetails: z.object({
+      scanResultStatus: z.string(),
+      threats: z
+        .array(
+          z.object({
+            name: z.string(),
+          }),
+        )
+        .nullable(),
+    }),
+  }),
+})
+
+export const deleteAssetSchema = z.object({
+  fileKey: z.string({
+    required_error: "Missing file key",
+  }),
+})
