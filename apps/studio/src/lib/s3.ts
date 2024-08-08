fea7b0f236cacf735bc55e05b84700de7c6c5aeb
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-08 14:04:56 +0800

    feat: handle malware scan result and add soft delete

--- apps/studio/src/lib/s3.ts
+++ apps/studio/src/lib/s3.ts
@@ -1,26 +1,78 @@
-import type { PutObjectCommandInput } from "@aws-sdk/client-s3"
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import type {
+  CopyObjectCommandInput,
+  GetObjectCommandInput,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3"
+import {
+  CopyObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 import { env } from "~/env.mjs"
 
-const { S3_REGION, S3_UNSAFE_ASSETS_BUCKET_NAME } = env
+const {
+  S3_REGION,
+  S3_UNSAFE_ASSETS_BUCKET_NAME,
+  S3_PUBLIC_ASSETS_BUCKET_NAME,
+} = env
 
 export const storage = new S3Client({
   region: S3_REGION,
 })
 
 export const generateSignedPutUrl = async ({
+  Bucket,
   Key,
-}: Pick<PutObjectCommandInput, "Key">): Promise<string> => {
+}: Pick<PutObjectCommandInput, "Bucket" | "Key">): Promise<string> => {
   return getSignedUrl(
     storage,
     new PutObjectCommand({
-      Bucket: S3_UNSAFE_ASSETS_BUCKET_NAME,
+      Bucket,
       Key,
     }),
     {
       expiresIn: 60 * 5, // 5 minutes
     },
+  )
+}
+
+type CopyFileParams = Pick<CopyObjectCommandInput, "Key"> & {
+  SourceBucket: CopyObjectCommandInput["Bucket"]
+  DestinationBucket: CopyObjectCommandInput["Bucket"]
+}
+
+export const copyFile = async ({
+  Key,
+  SourceBucket,
+  DestinationBucket,
+}: CopyFileParams) => {
+  return storage.send(
+    new CopyObjectCommand({
+      Bucket: DestinationBucket,
+      CopySource: `${SourceBucket}/${Key}`,
+      Key,
+    }),
+  )
+}
+
+export const deleteFile = async ({
+  Key,
+  Bucket,
+}: Pick<CopyObjectCommandInput, "Key" | "Bucket">) => {
+  return storage.send(
+    new CopyObjectCommand({
+      Bucket,
+      CopySource: `${Bucket}/${Key}`,
+      Key,
+      MetadataDirective: "REPLACE",
+      // NOTE: We perform a soft delete here so the file can be kept available
+      // until the page is published
+      Metadata: {
+        deleted: "true",
+      },
+    }),
   )
 }
