5005337b85a3c00ae30a5a785f809182b46f126a
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-08 17:05:19 +0800

    feat: hook up deleting of assets to save function

--- apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/ComplexEditorStateDrawer.tsx
@@ -61,6 +61,8 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
 
   const { mutateAsync: uploadAsset, isLoading: isUploadingAsset } =
     useUploadAssetMutation({ siteId })
+  const { mutateAsync: deleteAsset, isLoading: isDeletingAsset } =
+    trpc.asset.deleteAsset.useMutation()
 
   if (
     currActiveIdx === -1 ||
@@ -137,7 +139,18 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
       )
 
       // TODO: Mark removed images/files as deleted
-      // const assetsToDelete = modifiedAssets.filter((asset) => !asset.file)
+      const assetsToDelete = modifiedAssets.filter(
+        (asset) => asset.src !== undefined,
+      )
+      await Promise.all(
+        assetsToDelete.map(({ src }) => {
+          if (!src) {
+            return
+          }
+
+          return deleteAsset({ fileKey: src })
+        }),
+      )
 
       updatedBlocks[currActiveIdx] = newBlock
       newPageState = {
@@ -163,6 +176,8 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
       },
     )
   }
+
+  const isLoading = isSavingPage || isUploadingAsset || isDeletingAsset
 
   return (
     <>
@@ -213,7 +228,7 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
               colorScheme="sub"
               size="sm"
               p="0.625rem"
-              isDisabled={isSavingPage || isUploadingAsset}
+              isDisabled={isLoading}
               onClick={() => {
                 if (!_.isEqual(previewPageState, savedPageState)) {
                   onDiscardChangesModalOpen()
@@ -253,11 +268,7 @@ export default function ComplexEditorStateDrawer(): JSX.Element {
             onClick={onDeleteBlockModalOpen}
           />
           <Box w="100%">
-            <Button
-              w="100%"
-              onClick={handleSave}
-              isLoading={isSavingPage || isUploadingAsset}
-            >
+            <Button w="100%" onClick={handleSave} isLoading={isLoading}>
               Save changes
             </Button>
           </Box>
