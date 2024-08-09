386f1e2615673ca7950ab1e6a5d76bc02e68c7aa
Harish V <harish@open.gov.sg>
2024-08-10 01:33:46 +0800

    minor fix

--- apps/studio/src/features/editing-experience/components/PublishButton.tsx
+++ apps/studio/src/features/editing-experience/components/PublishButton.tsx
@@ -30,7 +30,6 @@ const SuspendablePublishButton = ({
       })
       await utils.page.readPage.invalidate({ pageId, siteId })
     },
-    // TODO: This is not triggered
     onError: async (error) => {
       console.error(`Error occurred when publishing page: ${error.message}`)
       toast({
