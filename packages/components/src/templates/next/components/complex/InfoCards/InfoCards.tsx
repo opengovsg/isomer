69e458f64bb8b3988b74724ee675df0db7a7bfa1
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-08-12 16:44:09 +0800

    feat: redesign InfoCards component and update schema (#414)
    * feat: redesign InfoCards component and update schema
    
    * feat: update auto-generated index page and sort items
    
    * fix: change isCardWithImages to variant
    
    * chore: use switch case for exhaustive type check
    
    * fix: add lint and format fix commands to build scripts
    
    * chore: add eslint and prettier config as dev deps
    
    * add story with white bg image
    
    * add more details and sensible sample text to infocard stories
    
    * update to match design (hover states, type tokens, icon size, remove line clamp, spacing tweaks)
    
    * add transition to image hover
    
    * optical adjustment to arrow
    
    * add better homepage stories to illustrate various card types
    
    * chore: fix issues raised in comments
    
    * chore: run format fix on tooling
    
    ---------
    
    Co-authored-by: Park Se Hyun <>

--- packages/components/src/templates/next/components/complex/InfoCards/InfoCards.tsx
+++ packages/components/src/templates/next/components/complex/InfoCards/InfoCards.tsx
@@ -1,48 +1,189 @@
-import type { InfoCardsProps } from "~/interfaces"
-import Card from "../../internal/Card"
-import { ComponentContent } from "../../internal/customCssClass"
+import type { PropsWithChildren } from "react"
+import { BiRightArrowAlt } from "react-icons/bi"
 
-const TitleSection = ({
+import type { InfoCardsProps } from "~/interfaces"
+import type {
+  SingleCardNoImageProps,
+  SingleCardWithImageProps,
+} from "~/interfaces/complex/InfoCards"
+import { tv } from "~/lib/tv"
+import { ComponentContent } from "../../internal/customCssClass"
+import { Link } from "../../internal/Link"
+
+const createInfoCardsStyles = tv({
+  slots: {
+    container: `${ComponentContent} mx-auto flex flex-col py-12 first:pt-3.5 lg:py-24`,
+    headingContainer: "flex flex-col gap-2.5 pb-8 sm:pb-12 lg:max-w-3xl",
+    headingTitle: "prose-display-md text-base-content-strong",
+    headingSubtitle: "prose-headline-lg-regular text-base-content",
+    grid: "grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-7 lg:grid-cols-3 lg:gap-x-16 lg:gap-y-12",
+    cardContainer: "group flex flex-col gap-5 outline-offset-4",
+    cardImageContainer:
+      "h-[11.875rem] w-full overflow-hidden rounded-lg border border-base-divider-subtle drop-shadow-none transition ease-in md:h-52",
+    cardImage: "h-full w-full object-cover object-center",
+    cardTextContainer: "flex flex-col gap-2.5 sm:gap-3",
+    cardTitle: "prose-headline-lg-semibold text-base-content-strong",
+    cardTitleArrow:
+      "mb-0.5 ml-1 inline h-auto w-6 transition ease-in group-hover:translate-x-1",
+    cardDescription: "prose-body-base text-base-content",
+  },
+  variants: {
+    isClickableCard: {
+      true: {
+        cardTitle: "group-hover:text-brand-canvas-inverse",
+        cardImageContainer: "group-hover:drop-shadow-md",
+      },
+    },
+  },
+})
+
+const compoundStyles = createInfoCardsStyles()
+
+const InfoCardsHeadingSection = ({
   title,
   subtitle,
-  className = "",
-}: {
-  title: InfoCardsProps["title"]
-  subtitle: InfoCardsProps["subtitle"]
-  className?: string
-}) => {
-  return (
-    <div
-      className={`flex max-w-3xl flex-col gap-8 self-start pb-8 sm:pb-12 ${className}`}
+}: Pick<InfoCardsProps, "title" | "subtitle">): JSX.Element => (
+  <div className={compoundStyles.headingContainer()}>
+    <h2 className={compoundStyles.headingTitle()}>{title}</h2>
+
+    {subtitle && <p className={compoundStyles.headingSubtitle()}>{subtitle}</p>}
+  </div>
+)
+
+const InfoCardContainer = ({
+  url,
+  LinkComponent,
+  children,
+}: PropsWithChildren<
+  Pick<SingleCardNoImageProps, "url" | "LinkComponent">
+>): JSX.Element => {
+  return url ? (
+    <Link
+      href={url}
+      className={compoundStyles.cardContainer()}
+      LinkComponent={LinkComponent}
     >
-      <h2 className="text-content-strong text-heading-03">{title}</h2>
-      {subtitle && (
-        <p className="text-sm text-content sm:text-lg">{subtitle}</p>
-      )}
-    </div>
+      {children}
+    </Link>
+  ) : (
+    <div className={compoundStyles.cardContainer()}>{children}</div>
   )
 }
 
-const InfoCards = ({ cards, title, subtitle }: InfoCardsProps) => {
+const InfoCardImage = ({
+  imageUrl,
+  imageAlt,
+  url,
+}: Pick<
+  SingleCardWithImageProps,
+  "imageUrl" | "imageAlt" | "url"
+>): JSX.Element => (
+  <div
+    className={compoundStyles.cardImageContainer({ isClickableCard: !!url })}
+  >
+    <img src={imageUrl} alt={imageAlt} className={compoundStyles.cardImage()} />
+  </div>
+)
+
+const InfoCardText = ({
+  title,
+  description,
+  url,
+}: Pick<
+  SingleCardWithImageProps,
+  "title" | "description" | "url"
+>): JSX.Element => (
+  <div className={compoundStyles.cardTextContainer()}>
+    <h4 className={compoundStyles.cardTitle({ isClickableCard: !!url })}>
+      {title}
+
+      {url && (
+        <BiRightArrowAlt
+          aria-hidden
+          className={compoundStyles.cardTitleArrow()}
+        />
+      )}
+    </h4>
+    <p className={compoundStyles.cardDescription()}>{description}</p>
+  </div>
+)
+
+const InfoCardNoImage = ({
+  title,
+  description,
+  url,
+  LinkComponent,
+}: SingleCardNoImageProps): JSX.Element => {
   return (
-    <section>
-      <div
-        className={`${ComponentContent} mx-auto flex flex-col items-center py-12 lg:py-24`}
-      >
-        <TitleSection title={title} subtitle={subtitle} />
-        <div className={`grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3`}>
-          {cards.map((card) => (
-            <Card
-              title={card.title}
-              url={card.url}
-              imageUrl={card.imageUrl}
-              description={card.description}
-              imageAlt={card.imageAlt}
-              buttonLabel={card.buttonLabel}
-              variant="vertical"
-            ></Card>
-          ))}
-        </div>
+    <InfoCardContainer url={url} LinkComponent={LinkComponent}>
+      <InfoCardText title={title} description={description} url={url} />
+    </InfoCardContainer>
+  )
+}
+
+const InfoCardWithImage = ({
+  title,
+  description,
+  imageUrl,
+  imageAlt,
+  url,
+  LinkComponent,
+}: SingleCardWithImageProps): JSX.Element => {
+  return (
+    <InfoCardContainer url={url} LinkComponent={LinkComponent}>
+      <InfoCardImage imageUrl={imageUrl} imageAlt={imageAlt} url={url} />
+      <InfoCardText title={title} description={description} url={url} />
+    </InfoCardContainer>
+  )
+}
+
+const InfoCards = ({
+  title,
+  subtitle,
+  variant,
+  cards,
+  LinkComponent,
+}: InfoCardsProps): JSX.Element => {
+  const InfoCardtoRender = () => {
+    switch (variant) {
+      case "cardsWithImages":
+        return (
+          <>
+            {cards.map((card, idx) => (
+              <InfoCardWithImage
+                key={idx}
+                {...card}
+                LinkComponent={LinkComponent}
+              />
+            ))}
+          </>
+        )
+      case "cardsWithoutImages":
+        return (
+          <>
+            {cards.map((card, idx) => (
+              <InfoCardNoImage
+                key={idx}
+                {...card}
+                LinkComponent={LinkComponent}
+              />
+            ))}
+          </>
+        )
+      default:
+        const _: never = variant
+        return <></>
+    }
+  }
+
+  return (
+    <section className={compoundStyles.container()}>
+      {(title || subtitle) && (
+        <InfoCardsHeadingSection title={title} subtitle={subtitle} />
+      )}
+
+      <div className={compoundStyles.grid()}>
+        <InfoCardtoRender />
       </div>
     </section>
   )
