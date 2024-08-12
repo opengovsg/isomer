8ea5c58cb44d472db70ce86cc1b6ccfe66f19c0c
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-12 11:22:37 +0800

    feat: load existing asset if present

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsImageControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsImageControl.tsx
@@ -13,10 +13,13 @@ import {
 import type { ModifiedAsset } from "~/types/assets"
 import { JSON_FORMS_RANKING } from "~/constants/formBuilder"
 import { useEditorDrawerContext } from "~/contexts/EditorDrawerContext"
+import { env } from "~/env.mjs"
 import {
   IMAGE_UPLOAD_ACCEPTED_MIME_TYPES,
   MAX_IMG_FILE_SIZE_BYTES,
 } from "./constants"
+
+const { NEXT_PUBLIC_S3_PUBLIC_ASSETS_DOMAIN_NAME } = env
 
 export const jsonFormsImageControlTester: RankedTester = rankWith(
   JSON_FORMS_RANKING.ImageControl,
@@ -33,6 +36,7 @@ export function JsonFormsImageControl({
   description,
   required,
   errors,
+  data,
 }: ControlProps) {
   const toast = useToast()
   const { modifiedAssets, setModifiedAssets } = useEditorDrawerContext()
@@ -50,6 +54,36 @@ export function JsonFormsImageControl({
       setModifiedAssets([...modifiedAssets, { ...pendingAsset }])
     }
   }, [modifiedAssets, path, pendingAsset, setModifiedAssets])
+
+  useEffect(() => {
+    const urlToFile = async (
+      url: string,
+      filename: string,
+      mimeType: string,
+    ) => {
+      const response = await fetch(url)
+      const blob = await response.blob()
+      return new File([blob], filename, { type: mimeType })
+    }
+
+    async function convertImage(url: string) {
+      const fileName = url.split("/").pop()
+      const fileType = `image/${url.split(".").pop()}`
+      const imageUrl = url.startsWith("/")
+        ? `${NEXT_PUBLIC_S3_PUBLIC_ASSETS_DOMAIN_NAME}${url}`
+        : url
+      const file = await urlToFile(imageUrl, fileName || "", fileType)
+      setPendingAsset({ path, src: imageUrl, file })
+    }
+
+    if (!data) {
+      return
+    }
+
+    convertImage(data)
+    // NOTE: We only want to run this once if there is initial data provided
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <Box py={2}>
