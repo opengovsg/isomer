dcd790529013d82d6425db7ab5bb0de8ec7c9982
seaerchin <jiachin@open.gov.sg>
2024-08-13 04:18:40 +0800

    fix: incompatible as

--- apps/studio/src/components/CmsSidebar/CmsSideNav.tsx
+++ apps/studio/src/components/CmsSidebar/CmsSideNav.tsx
@@ -145,7 +145,15 @@ const SideNavItem = ({
                   }}
                   onDoubleClick={async () => {
                     const urlType = getResourceType(resourceType)
-                    await router.push({
+                    if (resourceType === "RootPage") {
+                      return router.push({
+                        pathname: "/sites/[siteId]",
+                        query: {
+                          siteId,
+                        },
+                      })
+                    }
+                    return router.push({
                       pathname: "/sites/[siteId]/[resourceType]/[id]",
                       query: {
                         siteId,
