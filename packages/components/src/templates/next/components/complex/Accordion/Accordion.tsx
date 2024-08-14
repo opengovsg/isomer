2505bad3ab6c83e6e50f2f5bf642863d2532514d
Kar Rui Lau <karrui@open.gov.sg>
2024-08-14 11:08:32 +0800

    feat: use pure css to handle accordion icon states

--- packages/components/src/templates/next/components/complex/Accordion/Accordion.tsx
+++ packages/components/src/templates/next/components/complex/Accordion/Accordion.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type { VariantProps } from "tailwind-variants"
-import { useState } from "react"
 import { BiMinus, BiPlus } from "react-icons/bi"
 
 import type { AccordionProps as BaseAccordionProps } from "~/interfaces"
@@ -14,7 +13,7 @@ const createAccordionStyles = tv({
       "group border-y border-divider-medium px-4 py-5 has-[+_details]:border-b-0",
     summary:
       "prose-headline-lg-medium flex list-none flex-row items-center justify-between gap-3 text-base-content-strong outline-none outline outline-0 outline-offset-2 outline-brand-interaction hover:cursor-pointer focus-visible:outline-2",
-    icon: "h-6 w-6 flex-shrink-0",
+    icon: "h-6 w-6 flex-shrink-0 [&.minus]:hidden [&.minus]:group-open:block [&.plus]:block [&.plus]:group-open:hidden",
     content: "pt-5 text-base-content-strong",
   },
 })
@@ -23,24 +22,21 @@ const accordionStyles = createAccordionStyles()
 
 interface AccordionProps
   extends BaseAccordionProps,
-    VariantProps<typeof createAccordionStyles> {
-  name?: string
-}
+    VariantProps<typeof createAccordionStyles> {}
 
-const Accordion = ({ summary, details, name }: AccordionProps) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const Icon = isOpen ? BiMinus : BiPlus
-
+const Accordion = ({ summary, details }: AccordionProps) => {
   return (
-    <details
-      name={name}
-      onToggle={(e) => setIsOpen(e.currentTarget.open)}
-      open={isOpen}
-      className={accordionStyles.details()}
-    >
+    <details className={accordionStyles.details()}>
       <summary className={accordionStyles.summary()}>
         {summary}
-        <Icon aria-hidden className={accordionStyles.icon()} />
+        <BiMinus
+          aria-hidden
+          className={accordionStyles.icon({ className: "minus" })}
+        />
+        <BiPlus
+          aria-hidden
+          className={accordionStyles.icon({ className: "plus" })}
+        />
       </summary>
 
       <div className={accordionStyles.content()}>
