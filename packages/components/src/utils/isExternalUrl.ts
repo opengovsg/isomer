20c2282436a284bfeb29fc8a3faa650432525cd0
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-14 10:49:24 +0800

    fix: use definition of internal link instead

--- packages/components/src/utils/isExternalUrl.ts
+++ packages/components/src/utils/isExternalUrl.ts
@@ -1,9 +1,4 @@
 // Determine if the provided URL is an external link
 export const isExternalUrl = (url: string) => {
-  return (
-    url.startsWith("http://") ||
-    url.startsWith("https://") ||
-    url.startsWith("tel:") ||
-    url.startsWith("mailto:")
-  )
+  return !url.startsWith("/")
 }
