07025ba98b9ae9ea3477990ff79145f909972bc7
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-16 09:48:01 +0800

    feat: remove distinction between unsafe and public

--- apps/studio/next.config.mjs
+++ apps/studio/next.config.mjs
@@ -3,7 +3,7 @@
  * for Docker builds.
  */
 const { env } = await import("./src/env.mjs")
-const { S3_REGION, S3_UNSAFE_ASSETS_BUCKET_NAME } = env
+const { S3_REGION, S3_ASSETS_BUCKET_NAME } = env
 
 /*
 TODO: Removing this CSP first
@@ -32,7 +32,7 @@ const ContentSecurityPolicy = `
     https://browser-intake-datadoghq.com
     https://*.browser-intake-datadoghq.com
     https://vitals.vercel-insights.com/v1/vitals
-    https://${S3_UNSAFE_ASSETS_BUCKET_NAME}.s3.${S3_REGION}.amazonaws.com
+    https://${S3_ASSETS_BUCKET_NAME}.s3.${S3_REGION}.amazonaws.com
     ;
   worker-src 'self' blob:;
   ${env.NODE_ENV === "production" ? "upgrade-insecure-requests" : ""}
@@ -55,10 +55,7 @@ const config = {
   /** We run eslint as a separate task in CI */
   eslint: { ignoreDuringBuilds: true },
   images: {
-    domains: [
-      env.S3_UNSAFE_ASSETS_DOMAIN_NAME ?? "",
-      env.S3_PUBLIC_ASSETS_DOMAIN_NAME ?? "",
-    ].filter((d) => d),
+    domains: [env.S3_ASSETS_DOMAIN_NAME ?? ""].filter((d) => d),
   },
   async headers() {
     return [
