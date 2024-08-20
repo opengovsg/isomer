9896a71ee0fc0a1afc9658c3e4e6f4ba2e4c9506
Kar Rui Lau <karrui@open.gov.sg>
2024-08-20 18:11:45 +0800

    fix(Contentpic): remove unnecessary image absolute css layout

--- packages/components/src/templates/next/components/complex/Contentpic/Contentpic.tsx
+++ packages/components/src/templates/next/components/complex/Contentpic/Contentpic.tsx
@@ -7,9 +7,8 @@ import { Prose } from "../../native"
 const contentpicStyles = tv({
   slots: {
     container: "flex flex-col gap-7 py-7 md:flex-row",
-    image: "inset-0 h-full w-full object-cover lg:absolute",
-    imageContainer:
-      "relative max-h-[400px] w-full md:h-[240px] md:max-h-full md:w-[200px]",
+    image:
+      "max-h-[400px] w-full object-cover md:h-[240px] md:max-h-full md:w-[200px]",
     content: "flex-1 text-base-content lg:justify-self-start",
   },
 })
@@ -28,9 +27,7 @@ export const Contentpic = ({
 }: ContentpicProps): JSX.Element => {
   return (
     <div className={compoundStyles.container()}>
-      <div className={compoundStyles.imageContainer()}>
-        <img className={compoundStyles.image()} alt={imageAlt} src={imageSrc} />
-      </div>
+      <img className={compoundStyles.image()} alt={imageAlt} src={imageSrc} />
       <div className={compoundStyles.content()}>
         <Prose {...content} />
       </div>
