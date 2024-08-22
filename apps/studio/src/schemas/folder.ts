5a7e960c46787da14dae06a5009595f78cc6c303
seaerchin <jiachin@open.gov.sg>
2024-08-22 16:20:01 +0800

    fix: permalink
    use stricter regex to validate

--- apps/studio/src/schemas/folder.ts
+++ apps/studio/src/schemas/folder.ts
@@ -1,8 +1,16 @@
 import { z } from "zod"
 
+import { generateBasePermalinkSchema } from "./common"
+
 export const MAX_FOLDER_TITLE_LENGTH = 100
 export const MAX_FOLDER_PERMALINK_LENGTH = 200
 export const MAX_FOLDER_DESCRIPTION_LENGTH = 300
+
+const permalinkSchema = generateBasePermalinkSchema("folder")
+  .min(1, { message: "Enter a URL for this folder" })
+  .max(MAX_FOLDER_PERMALINK_LENGTH, {
+    message: `Folder URL should be shorter than ${MAX_FOLDER_PERMALINK_LENGTH} characters.`,
+  })
 
 export const createFolderSchema = z.object({
   folderTitle: z
@@ -11,12 +19,7 @@ export const createFolderSchema = z.object({
     .max(MAX_FOLDER_TITLE_LENGTH, {
       message: `Folder title should be shorter than ${MAX_FOLDER_TITLE_LENGTH} characters.`,
     }),
-  permalink: z
-    .string()
-    .min(1, { message: "Enter a URL for this folder" })
-    .max(MAX_FOLDER_PERMALINK_LENGTH, {
-      message: `Folder URL should be shorter than ${MAX_FOLDER_PERMALINK_LENGTH} characters.`,
-    }),
+  permalink: permalinkSchema,
   siteId: z.number().min(1),
   // Nullable for top level folder
   parentFolderId: z.number().optional(),
@@ -29,7 +32,7 @@ export const readFolderSchema = z.object({
 
 export const baseEditFolderSchema = z.object({
   resourceId: z.string(),
-  permalink: z.optional(z.string()),
+  permalink: z.optional(permalinkSchema),
   title: z.optional(z.string()),
   siteId: z.string(),
 })
