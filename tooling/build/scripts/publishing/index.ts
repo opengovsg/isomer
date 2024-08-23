ec4ccd674681ed3c6f3e3fa30b439f4068c6dfed
seaerchin <jiachin@open.gov.sg>
2024-08-23 12:34:35 +0800

    fix: update directory paths

--- tooling/build/scripts/publishing/index.ts
+++ tooling/build/scripts/publishing/index.ts
@@ -124,12 +124,21 @@ async function writeContentToFile(
       return
     }
 
+    const sanitizedPermalink = path
+      // NOTE: normalization here will remove dual backslashes
+      // and also strip .. filepaths except as a prefix
+      .normalize(fullPermalink)
+      // NOTE: this matches on a leading ../
+      // or a leading ..\
+      // or a plain .. without any paths
+      .replace(/^(\.\.(\/|\\|$))+/, "")
+
     const directoryPath =
       parentId === null
         ? path.join(__dirname, "schema")
-        : path.join(__dirname, "schema", path.dirname(fullPermalink))
+        : path.join(__dirname, "schema", path.dirname(sanitizedPermalink))
 
-    const fileName = `${path.basename(fullPermalink)}.json`
+    const fileName = `${path.basename(sanitizedPermalink)}.json`
     const filePath = path.join(directoryPath, fileName)
 
     // Create directories if they don't exist
