62a5477cceddc9d9a984bcc64d250bb680166ee8
Kar Rui Lau <karrui@open.gov.sg>
2024-08-26 20:39:27 +0800

    feat: add header row and column toggling to tiptap menubar

--- apps/studio/src/components/PageEditor/MenuBar.tsx
+++ apps/studio/src/components/PageEditor/MenuBar.tsx
@@ -240,6 +240,23 @@ export const MenuBar = ({ editor }: { editor: Editor }) => {
         type: "divider",
         isHidden: !editor.isActive("table"),
       },
+
+      {
+        type: "item",
+        icon: () => (
+          <Icon color="base.content.medium" as={RiLayoutColumnFill} />
+        ),
+        title: "Toggle header column",
+        action: () => editor.chain().focus().toggleHeaderColumn().run(),
+        isHidden: !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon color="base.content.medium" as={RiLayoutRowFill} />,
+        title: "Toggle header row",
+        action: () => editor.chain().focus().toggleHeaderRow().run(),
+        isHidden: !editor.isActive("table"),
+      },
       {
         type: "item",
         icon: () => (
