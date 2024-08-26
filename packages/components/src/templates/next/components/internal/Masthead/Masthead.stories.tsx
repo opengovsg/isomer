19f1a218ec3925d5b2907311f617f1a52b98f718
Kar Rui Lau <karrui@open.gov.sg>
2024-08-26 17:27:22 +0800

    feat: update masthead styling

--- packages/components/src/templates/next/components/internal/Masthead/Masthead.stories.tsx
+++ packages/components/src/templates/next/components/internal/Masthead/Masthead.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react"
+import { userEvent, within } from "@storybook/test"
 
 import type { MastheadProps } from "~/interfaces"
 import Masthead from "./Masthead"
@@ -26,5 +27,14 @@ export const Default: Story = {
 export const Staging: Story = {
   args: {
     isStaging: true,
+  },
+}
+
+export const expanded: Story = {
+  play: async ({ canvasElement }) => {
+    const screen = within(canvasElement)
+    await userEvent.click(
+      screen.getByRole("button", { name: /how to identify/i }),
+    )
   },
 }
