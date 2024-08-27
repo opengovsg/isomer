981cea224f6413f66c24eb93c20ab61a98f976eb
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-08-27 10:19:31 +0800

    fix: attempt to insert credentials directly to S3Client

--- apps/studio/src/lib/s3.ts
+++ apps/studio/src/lib/s3.ts
@@ -12,10 +12,14 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 import { env } from "~/env.mjs"
 
-const { NEXT_PUBLIC_S3_REGION } = env
+const { NEXT_PUBLIC_S3_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = env
 
 export const storage = new S3Client({
   region: NEXT_PUBLIC_S3_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: AWS_SECRET_ACCESS_KEY ?? "",
+  },
 })
 
 export const generateSignedPutUrl = async ({
