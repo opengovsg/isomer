4abcd225eaee49115330235af3ec040801e3c869
Kar Rui Lau <karrui@open.gov.sg>
2024-08-28 12:48:01 +0800

    feat(BackToTopLink): add fv highlight

--- packages/components/src/templates/next/components/internal/BackToTopLink.tsx
+++ packages/components/src/templates/next/components/internal/BackToTopLink.tsx
@@ -1,14 +1,22 @@
 import type { VariantProps } from "tailwind-variants"
+import { composeRenderProps } from "react-aria-components"
 import { BiUpArrowAlt } from "react-icons/bi"
 
 import { tv } from "~/lib/tv"
+import { focusVisibleHighlight } from "~/utils/rac"
 import { Link } from "./Link"
 
-const styles = tv({
-  base: "prose-body-base sticky top-8 my-8 flex items-center text-link underline-offset-4 hover:underline",
+const linkStyle = tv({
+  extend: focusVisibleHighlight,
+  base: "prose-body-base sticky top-8 my-8 inline-flex items-center text-link underline-offset-4 hover:underline",
+  variants: {
+    isFocusVisible: {
+      true: "text-base-content-strong",
+    },
+  },
 })
 
-interface BackToTopLinkProps extends VariantProps<typeof styles> {
+interface BackToTopLinkProps extends VariantProps<typeof linkStyle> {
   className?: string
 }
 
@@ -16,7 +24,12 @@ export const BackToTopLink = ({
   className,
 }: BackToTopLinkProps): JSX.Element => {
   return (
-    <Link href="#" className={styles({ className })}>
+    <Link
+      href="#"
+      className={composeRenderProps(className, (className, renderProps) =>
+        linkStyle({ className, ...renderProps }),
+      )}
+    >
       <BiUpArrowAlt aria-hidden className="h-6 w-6" />
       Back to top
     </Link>
