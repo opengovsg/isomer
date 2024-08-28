15a065e3c1bca6ba0bbfc442860038984ba13f22
Kar Rui Lau <karrui@open.gov.sg>
2024-08-28 13:07:41 +0800

    feat(BaseParagraph): update fv highlight

--- packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
+++ packages/components/src/templates/next/components/internal/BaseParagraph/BaseParagraph.tsx
@@ -12,7 +12,7 @@ export const BaseParagraph = ({
   return (
     <p
       className={twMerge(
-        `hover:[&_a]text-link-hover [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-link [&_a]:underline`,
+        `hover:[&_a]text-link-hover focus-visible:[&_a]:shadow-focus-visible focus-visible:[&_a]:bg-utility-highlight [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 after:[&_a[target*="blank"]]:content-['_↗'] [&_a]:text-link [&_a]:underline [&_a]:outline-none focus-visible:[&_a]:text-base-content-strong focus-visible:[&_a]:decoration-transparent focus-visible:[&_a]:transition-none`,
         className,
       )}
       dangerouslySetInnerHTML={{ __html: sanitizedContent }}
