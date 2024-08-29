e30eb0ff8295966232f20bb91d8aa0816dfb386b
seaerchin <jiachin@open.gov.sg>
2024-08-29 14:28:55 +0800

    fix: tiptap
    reset to default when clicking x

--- apps/studio/src/features/editing-experience/components/TipTapComponent.tsx
+++ apps/studio/src/features/editing-experience/components/TipTapComponent.tsx
@@ -44,6 +44,8 @@ function TipTapComponent({ content }: TipTapComponentProps) {
     previewPageState,
     setPreviewPageState,
     currActiveIdx,
+    addedBlockIndex,
+    setAddedBlockIndex,
   } = useEditorDrawerContext()
 
   const { pageId, siteId } = useQueryParse(editPageSchema)
@@ -64,6 +66,7 @@ function TipTapComponent({ content }: TipTapComponentProps) {
       content: updatedBlocks,
     }
     setPreviewPageState(newPageState)
+    setAddedBlockIndex(null)
   }
 
   const handleDeleteBlock = () => {
@@ -77,10 +80,23 @@ function TipTapComponent({ content }: TipTapComponentProps) {
     setPreviewPageState(newPageState)
     onDeleteBlockModalClose()
     setDrawerState({ state: "root" })
+    setAddedBlockIndex(null)
   }
 
   const handleDiscardChanges = () => {
-    setPreviewPageState(savedPageState)
+    if (addedBlockIndex !== null) {
+      const updatedBlocks = Array.from(savedPageState.content)
+      updatedBlocks.splice(addedBlockIndex, 1)
+      const newPageState = {
+        ...previewPageState,
+        content: updatedBlocks,
+      }
+      setSavedPageState(newPageState)
+      setPreviewPageState(newPageState)
+    } else {
+      setPreviewPageState(savedPageState)
+    }
+    setAddedBlockIndex(null)
     onDiscardChangesModalClose()
     setDrawerState({ state: "root" })
   }
