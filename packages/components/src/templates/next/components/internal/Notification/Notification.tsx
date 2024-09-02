8e7719cb7165c9cd28e04cf5618b200736dadfae
Kar Rui Lau <karrui@open.gov.sg>
2024-09-02 16:57:53 +0800

    fix(Notification): add 2px mt to icons

--- packages/components/src/templates/next/components/internal/Notification/Notification.tsx
+++ packages/components/src/templates/next/components/internal/Notification/Notification.tsx
@@ -18,7 +18,7 @@ const NotificationBanner = ({ content, title }: NotificationProps) => {
     isShown && (
       <div className="bg-base-canvas-backdrop">
         <div className="relative mx-auto flex max-w-screen-xl flex-row gap-4 px-6 py-8 text-base-content md:px-10 md:py-6">
-          <BiInfoCircle className="h-6 w-6 shrink-0" />
+          <BiInfoCircle className="mt-0.5 h-6 w-6 shrink-0" />
           <div className="flex flex-1 flex-col gap-1">
             {!!title && <h2 className="prose-headline-lg-medium">{title}</h2>}
             <BaseParagraph
@@ -30,7 +30,7 @@ const NotificationBanner = ({ content, title }: NotificationProps) => {
           <IconButton
             onPress={onDismiss}
             icon={BiX}
-            className="absolute right-3 top-5 md:right-7 md:top-3"
+            className="absolute right-3 top-[22px] md:right-7 md:top-3.5"
             aria-label="Dismiss notification temporarily"
           />
         </div>
