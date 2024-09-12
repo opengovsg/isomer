b33e9932d1472367233236d8023f84b256a758bc
seaerchin <jiachin@open.gov.sg>
2024-09-12 20:39:53 +0800

    chore: revert change

--- apps/studio/src/components/PageEditor/constants.ts
+++ apps/studio/src/components/PageEditor/constants.ts
@@ -78,7 +78,7 @@ export const DEFAULT_BLOCKS: Record<
     title: "This is an optional title of the Infocards component",
     subtitle: "This is an optional subtitle for the Infocards component",
     variant: "cardsWithImages",
-    maxColumns: 3,
+    maxColumns: "3",
     cards: [
       {
         title: "This is the first card",
