eddf941fa43a60533cc8c68ff83aa73bfbaa88b6
seaerchin <44049504+seaerchin@users.noreply.github.com>
2024-09-13 16:20:13 +0800

    chore(table): allow tables for accordion and prose (#605)
    * chore: refactor editor hooks
    
    abstract out shared code
    also added table back into accordion and prose
    
    * refactor: add table back to menu bars for text and accordion
    
    * fix: allow lists in table cells
    
    * fix: remove horizontal rule from base editor
    
    * fix: add table
    
    * chore: remove toggle headers capability

--- apps/studio/src/components/PageEditor/MenuBar/AccordionMenuBar.tsx
+++ apps/studio/src/components/PageEditor/MenuBar/AccordionMenuBar.tsx
@@ -1,19 +1,38 @@
 import type { Editor } from "@tiptap/react"
 import { useMemo } from "react"
+import { Icon, useDisclosure } from "@chakra-ui/react"
 import {
   BiBold,
+  BiCog,
   BiItalic,
   BiListOl,
   BiListUl,
   BiStrikethrough,
+  BiTable,
   BiUnderline,
 } from "react-icons/bi"
 import { MdSubscript, MdSuperscript } from "react-icons/md"
+import { RiLayoutColumnFill, RiLayoutRowFill } from "react-icons/ri"
 
-import type { MenuBarEntry } from "./MenuBar"
-import { MenuBar } from "./MenuBar"
+import {
+  IconAddColLeft,
+  IconAddColRight,
+  IconAddRowAbove,
+  IconAddRowBelow,
+  IconDelCol,
+  IconDelRow,
+  IconMergeCells,
+  IconSplitCell,
+} from "~/components/icons"
+import { TableSettingsModal } from "../TableSettingsModal"
+import { MenuBar, MenuBarEntry } from "./MenuBar"
 
 export const AccordionMenuBar = ({ editor }: { editor: Editor }) => {
+  const {
+    isOpen: isTableSettingsModalOpen,
+    onOpen: onTableSettingsModalOpen,
+    onClose: onTableSettingsModalClose,
+  } = useDisclosure()
   const items: MenuBarEntry[] = useMemo(
     () => [
       {
@@ -83,9 +102,103 @@ export const AccordionMenuBar = ({ editor }: { editor: Editor }) => {
           },
         ],
       },
-    ],
-    [editor],
-  )
+      {
+        type: "item",
+        icon: BiTable,
+        title: "Table",
+        action: () => {
+          if (editor.isActive("table")) {
+            return editor.chain().focus().deleteTable().run()
+          }
+          return editor.chain().focus().insertTable().run()
+        },
+        isActive: () => editor.isActive("table"),
+      },
 
-  return <MenuBar items={items} />
+      // Table-specific commands
+      {
+        type: "divider",
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconAddColRight} />,
+        title: "Add column after",
+        action: () => editor.chain().focus().addColumnAfter().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconAddColLeft} />,
+        title: "Add column before",
+        action: () => editor.chain().focus().addColumnBefore().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconDelCol} />,
+        title: "Delete column",
+        action: () => editor.chain().focus().deleteColumn().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconAddRowAbove} />,
+        title: "Add row before",
+        action: () => editor.chain().focus().addRowBefore().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconAddRowBelow} />,
+        title: "Add row after",
+        action: () => editor.chain().focus().addRowAfter().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconDelRow} />,
+        title: "Delete row",
+        action: () => editor.chain().focus().deleteRow().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "divider",
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconMergeCells} />,
+        title: "Merge cells",
+        action: () => editor.chain().focus().mergeCells().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: () => <Icon as={IconSplitCell} />,
+        title: "Split cell",
+        action: () => editor.chain().focus().splitCell().run(),
+        isHidden: () => !editor.isActive("table"),
+      },
+      {
+        type: "item",
+        icon: BiCog,
+        title: "Table settings",
+        action: onTableSettingsModalOpen,
+        isHidden: () => !editor.isActive("table"),
+      },
+    ],
+    [editor, onTableSettingsModalOpen],
+  )
+  return (
+    <>
+      <TableSettingsModal
+        editor={editor}
+        isOpen={isTableSettingsModalOpen}
+        onClose={onTableSettingsModalClose}
+      />
+
+      <MenuBar items={items} />
+    </>
+  )
 }
