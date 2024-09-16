159cdd48669fd26f5995f699fa0f445c2d2f8dd5
Kar Rui Lau <karrui@open.gov.sg>
2024-09-16 17:44:27 +0800

    feat: add link to page settings also on collections

--- apps/studio/src/features/dashboard/components/CollectionTable/CollectionTableMenu.tsx
+++ apps/studio/src/features/dashboard/components/CollectionTable/CollectionTableMenu.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react"
+import NextLink from "next/link"
 import { MenuButton, MenuList, Portal } from "@chakra-ui/react"
 import { IconButton, Menu } from "@opengovsg/design-system-react"
 import { useSetAtom } from "jotai"
@@ -5,20 +7,29 @@ import { BiCog, BiDotsHorizontalRounded, BiTrash } from "react-icons/bi"
 
 import type { CollectionTableData } from "./types"
 import { MenuItem } from "~/components/Menu"
+import { getLinkToResource } from "~/utils/resource"
 import { deleteResourceModalAtom } from "../../atoms"
 
 interface CollectionTableMenuProps {
   title: CollectionTableData["title"]
   resourceId: CollectionTableData["id"]
-  resourceType: CollectionTableData["resourceType"]
+  resourceType: CollectionTableData["type"]
+  siteId: number
 }
 
 export const CollectionTableMenu = ({
+  siteId,
   title,
   resourceId,
   resourceType,
 }: CollectionTableMenuProps) => {
   const setValue = useSetAtom(deleteResourceModalAtom)
+
+  const linkToResourceSettings = useMemo(
+    () =>
+      `${getLinkToResource({ siteId, resourceId, type: resourceType })}/settings`,
+    [siteId, resourceId, resourceType],
+  )
 
   return (
     <Menu isLazy size="sm">
@@ -31,7 +42,12 @@ export const CollectionTableMenu = ({
       />
       <Portal>
         <MenuList>
-          <MenuItem icon={<BiCog fontSize="1rem" />}>
+          <MenuItem
+            icon={<BiCog fontSize="1rem" />}
+            as={NextLink}
+            // @ts-expect-error type inconsistency with `as`
+            href={linkToResourceSettings}
+          >
             Edit page settings
           </MenuItem>
           <MenuItem
