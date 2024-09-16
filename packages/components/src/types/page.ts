48f4c3b505c059b26b785904aa12b447d14866a5
seaerchin <jiachin@open.gov.sg>
2024-09-16 21:07:59 +0800

    chore: change noindex back to optional
    oddly enough, this broke our typing in a few places so i chnged it back
    for now

--- packages/components/src/types/page.ts
+++ packages/components/src/types/page.ts
@@ -16,11 +16,13 @@ const BaseItemSchema = Type.Object({
 const BasePageSchema = Type.Composite([
   BaseItemSchema,
   Type.Object({
-    noIndex: Type.Boolean({
-      description:
-        "If this is turned on, the page won't appear on Google search results.",
-      title: "Prevent search engines from indexing this page?",
-    }),
+    noIndex: Type.Optional(
+      Type.Boolean({
+        description:
+          "If this is turned on, the page won't appear on Google search results.",
+        title: "Prevent search engines from indexing this page?",
+      }),
+    ),
   }),
 ])
 
