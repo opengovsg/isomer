739caef7d38df391e5c4140a702c7e8dadc7118b
seaerchin <jiachin@open.gov.sg>
2024-09-17 14:30:14 +0800

    chore: fix lint by using pick

--- apps/studio/src/hooks/useBanner.ts
+++ apps/studio/src/hooks/useBanner.ts
@@ -1,9 +1,8 @@
 import { useFeatureValue } from "@growthbook/growthbook-react"
 import { BannerProps } from "@opengovsg/design-system-react"
 
-type BannerFeature = {
-  variant: BannerProps["variant"]
-  message: BannerProps["children"] | string
+type BannerFeature = Pick<BannerProps, "variant"> & {
+  message: BannerProps["children"]
 }
 const BANNER_FEATURE_KEY = "isomer-next-banner"
 export const useBanner = () => {
