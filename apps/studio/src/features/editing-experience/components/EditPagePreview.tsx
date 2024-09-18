b1ab6bbbb69061a1d8113089c9d5285af97bd553
Kar Rui Lau <karrui@open.gov.sg>
2024-09-18 12:38:53 +0800

    refactor: extract EditPagePreview component

--- apps/studio/src/features/editing-experience/components/EditPagePreview.tsx
+++ apps/studio/src/features/editing-experience/components/EditPagePreview.tsx
@@ -1,20 +1,37 @@
+import { Flex } from "@chakra-ui/react"
+import merge from "lodash/merge"
+
 import { useEditorDrawerContext } from "~/contexts/EditorDrawerContext"
 import { useSiteThemeCssVars } from "~/features/preview/hooks/useSiteThemeCssVars"
 import Preview from "./Preview"
 import { PreviewIframe } from "./PreviewIframe"
 
 export const EditPagePreview = (): JSX.Element => {
-  const { previewPageState, siteId, permalink } = useEditorDrawerContext()
+  const { previewPageState, pageId, updatedAt, siteId, permalink, title } =
+    useEditorDrawerContext()
   const themeCssVars = useSiteThemeCssVars({ siteId })
 
   return (
-    <PreviewIframe style={themeCssVars}>
-      <Preview
-        {...previewPageState}
-        siteId={siteId}
-        permalink={permalink}
-        version="0.1.0"
-      />
-    </PreviewIframe>
+    <Flex
+      shrink={0}
+      justify="flex-start"
+      px="2rem"
+      pb="2rem"
+      pt="1rem"
+      bg="base.canvas.backdrop"
+      h="100%"
+      overflowX="auto"
+    >
+      <PreviewIframe style={themeCssVars}>
+        <Preview
+          {...merge(previewPageState, { page: { title } })}
+          siteId={siteId}
+          resourceId={pageId}
+          permalink={permalink}
+          lastModified={updatedAt}
+          version="0.1.0"
+        />
+      </PreviewIframe>
+    </Flex>
   )
 }
