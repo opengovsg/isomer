c0f1d5cff5c4e27c721bc97a6da6943160163755
Kar Rui Lau <karrui@open.gov.sg>
2024-09-20 12:32:04 +0800

    fix: remove (now) unnecessary ts-expect-error directives

--- tooling/template/eslint.config.mjs
+++ tooling/template/eslint.config.mjs
@@ -1,8 +1,5 @@
-// @ts-expect-error typescript somehow cannot find types
 import baseConfig from "@isomer/eslint-config/base"
-// @ts-expect-error typescript somehow cannot find types
 import nextjsConfig from "@isomer/eslint-config/nextjs"
-// @ts-expect-error typescript somehow cannot find types
 import reactConfig from "@isomer/eslint-config/react"
 
 /** @type {import('typescript-eslint').Config} */
