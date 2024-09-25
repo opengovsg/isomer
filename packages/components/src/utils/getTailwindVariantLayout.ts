3aca94cc0985d87b03efa6f86d5b1ffeb0b3b1e7
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-09-25 18:26:20 +0800

    fix: classify not found layout as homepage (#687)
    * fix: classify not found layout as homepage
    
    * chore: synchronise design of index with content page

--- packages/components/src/utils/getTailwindVariantLayout.ts
+++ packages/components/src/utils/getTailwindVariantLayout.ts
@@ -3,7 +3,7 @@ import type { IsomerPageLayoutType } from "~/types"
 // This is a simplified layout used for determining the variant to use for
 // components that vary the design depending on the layout of the page
 export const getTailwindVariantLayout = (layout: IsomerPageLayoutType) => {
-  if (layout === "homepage") {
+  if (layout === "homepage" || layout === "notfound") {
     return "homepage"
   }
 
