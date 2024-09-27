8f6bf38fd426e02d2c08fd3d8a13c0a148ba5514
seaerchin <jiachin@open.gov.sg>
2024-09-27 15:31:32 +0800

    chore: update validator
    1. add regex for alphanumeric, hyphen and underscore
    2. this validator is used for the put url, so we will disallow creation
    of teh put url if the filename is invalid

--- apps/studio/src/schemas/asset.ts
+++ apps/studio/src/schemas/asset.ts
@@ -2,9 +2,20 @@ import { z } from "zod"
 
 export const getPresignedPutUrlSchema = z.object({
   siteId: z.number().min(1),
-  fileName: z.string({
-    required_error: "Missing file name",
-  }),
+  fileName: z
+    .string({
+      required_error: "Missing file name",
+    })
+    .refine(
+      (s) => {
+        const allowedStartingChars = new RegExp(/^[a-zA-Z0-9-_]/)
+        return allowedStartingChars.test(s)
+      },
+      {
+        message:
+          "File name must start with a letter, number, hyphen, or underscore",
+      },
+    ),
 })
 
 export const deleteAssetsSchema = z.object({
