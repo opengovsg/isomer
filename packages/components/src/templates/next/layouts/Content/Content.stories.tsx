3cd0cdb1a1f7c58d916b46bcce57ac60217ff73d
Park Se Hyun <>
2024-09-29 14:27:15 +0800

    add embed to a content page story to test out margin

--- packages/components/src/templates/next/layouts/Content/Content.stories.tsx
+++ packages/components/src/templates/next/layouts/Content/Content.stories.tsx
@@ -388,6 +388,12 @@ export const Default: Story = {
         },
       },
       {
+        type: "iframe",
+        content:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ggGGn4uvFWAIelWD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        title: "Rick Astley - Never Gonna Give You Up",
+      },
+      {
         type: "prose",
         content: [
           {
