85f0a2540bb02250c9ec5fa0896a4ed209ed279c
Kar Rui Lau <karrui@open.gov.sg>
2024-09-30 17:20:19 +0800

    refactor: use vite-tsconfig-paths for vitest config

--- apps/studio/vitest.config.ts
+++ apps/studio/vitest.config.ts
@@ -1,14 +1,11 @@
-import { fileURLToPath } from "url"
+import tsconfigPaths from "vite-tsconfig-paths"
 import { configDefaults, defineConfig } from "vitest/config"
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     exclude: [...configDefaults.exclude, "**/playwright/**", "tests/load/**"],
-    alias: {
-      "~/": fileURLToPath(new URL("./src/", import.meta.url)),
-      "~prisma": fileURLToPath(new URL("./prisma/", import.meta.url)),
-    },
     setupFiles: ["vitest.setup.ts"],
   },
 })
