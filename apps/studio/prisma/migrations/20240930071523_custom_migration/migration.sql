d33dd13957f9ea3edab51ff7f04ed8fb936f08bf
Kar Rui Lau <karrui@open.gov.sg>
2024-09-30 15:17:31 +0800

    feat: add nanoid custom function to postgres

--- apps/studio/prisma/migrations/20240930071523_custom_migration/migration.sql
+++ apps/studio/prisma/migrations/20240930071523_custom_migration/migration.sql
@@ -1,8 +1,3 @@
--- Override Prisma's unique index with unique NOT DISTINCT index
-DROP INDEX IF EXISTS "Resource_siteId_parentId_permalink_key";
-
-CREATE UNIQUE INDEX IF NOT EXISTS "Resource_siteId_parentId_permalink_key" ON "Resource"("siteId", "parentId", "permalink") NULLS NOT DISTINCT;
-
 /*
  * Copyright 2024 Viascom Ltd liab. Co
  *
@@ -11,7 +6,9 @@ CREATE UNIQUE INDEX IF NOT EXISTS "Resource_siteId_parentId_permalink_key" ON "R
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * "License");
+
+you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
@@ -32,6 +29,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- While it comes with a default configuration, the function is designed to be flexible,
 -- allowing for customization to meet specific needs.
 DROP FUNCTION IF EXISTS nanoid(int, text, float);
+
 CREATE OR REPLACE FUNCTION nanoid(
     size int DEFAULT 21, -- The number of symbols in the NanoId String. Must be greater than 0.
     alphabet text DEFAULT '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', -- The symbols used in the NanoId String. Must contain between 1 and 255 symbols.
@@ -47,32 +45,45 @@ AS
 $$
 DECLARE
     alphabetArray  text[];
-    alphabetLength int := 64;
-    mask           int := 63;
-    step           int := 34;
+
+alphabetLength int := 64;
+
+mask           int := 63;
+
+step           int := 34;
+
 BEGIN
     IF size IS NULL OR size < 1 THEN
         RAISE EXCEPTION 'The size must be defined and greater than 0!';
-    END IF;
 
-    IF alphabet IS NULL OR length(alphabet) = 0 OR length(alphabet) > 255 THEN
+END IF;
+
+IF alphabet IS NULL OR length(alphabet) = 0 OR length(alphabet) > 255 THEN
         RAISE EXCEPTION 'The alphabet can''t be undefined, zero or bigger than 255 symbols!';
-    END IF;
 
-    IF additionalBytesFactor IS NULL OR additionalBytesFactor < 1 THEN
+END IF;
+
+IF additionalBytesFactor IS NULL OR additionalBytesFactor < 1 THEN
         RAISE EXCEPTION 'The additional bytes factor can''t be less than 1!';
+
+END IF;
+
+alphabetArray := regexp_split_to_array(alphabet, '');
+
+alphabetLength := array_length(alphabetArray, 1);
+
+mask := (2 << cast(floor(log(alphabetLength - 1) / log(2)) as int)) - 1;
+
+step := cast(ceil(additionalBytesFactor * mask * size / alphabetLength) AS int);
+
+IF step > 1024 THEN
+        step := 1024;
+
+-- The step size % can''t be bigger then 1024!
     END IF;
 
-    alphabetArray := regexp_split_to_array(alphabet, '');
-    alphabetLength := array_length(alphabetArray, 1);
-    mask := (2 << cast(floor(log(alphabetLength - 1) / log(2)) as int)) - 1;
-    step := cast(ceil(additionalBytesFactor * mask * size / alphabetLength) AS int);
+RETURN nanoid_optimized(size, alphabet, mask, step);
 
-    IF step > 1024 THEN
-        step := 1024; -- The step size % can''t be bigger then 1024!
-    END IF;
-
-    RETURN nanoid_optimized(size, alphabet, mask, step);
 END
 $$;
 
@@ -80,6 +91,7 @@ $$;
 -- This optimized version is designed for higher performance and lower memory overhead.
 -- No checks are performed! Use it only if you really know what you are doing.
 DROP FUNCTION IF EXISTS nanoid_optimized(int, text, int, int);
+
 CREATE OR REPLACE FUNCTION nanoid_optimized(
     size int, -- The desired length of the generated string.
     alphabet text, -- The set of characters to choose from for generating the string.
@@ -96,27 +108,43 @@ AS
 $$
 DECLARE
     idBuilder      text := '';
-    counter        int  := 0;
-    bytes          bytea;
-    alphabetIndex  int;
-    alphabetArray  text[];
-    alphabetLength int  := 64;
+
+counter        int  := 0;
+
+bytes          bytea;
+
+alphabetIndex  int;
+
+alphabetArray  text[];
+
+alphabetLength int  := 64;
+
 BEGIN
     alphabetArray := regexp_split_to_array(alphabet, '');
-    alphabetLength := array_length(alphabetArray, 1);
 
-    LOOP
+alphabetLength := array_length(alphabetArray, 1);
+
+LOOP
         bytes := gen_random_bytes(step);
-        FOR counter IN 0..step - 1
+
+FOR counter IN 0..step - 1
             LOOP
                 alphabetIndex := (get_byte(bytes, counter) & mask) + 1;
-                IF alphabetIndex <= alphabetLength THEN
+
+IF alphabetIndex <= alphabetLength THEN
                     idBuilder := idBuilder || alphabetArray[alphabetIndex];
-                    IF length(idBuilder) = size THEN
+
+IF length(idBuilder) = size THEN
                         RETURN idBuilder;
-                    END IF;
-                END IF;
-            END LOOP;
-    END LOOP;
+
+END IF;
+
+END IF;
+
+END LOOP;
+
+END LOOP;
+
 END
 $$;
+
