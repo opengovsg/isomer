62b8a4c5606af5b34bc66412cb54a845406130e3
Kar Rui Lau <karrui@open.gov.sg>
2024-10-02 11:32:07 +0800

    test: simplify page.router.test setup

--- apps/studio/src/server/modules/page/__tests__/page.router.test.ts
+++ apps/studio/src/server/modules/page/__tests__/page.router.test.ts
@@ -14,13 +14,22 @@ import { pageRouter } from "../page.router"
 const createCaller = createCallerFactory(pageRouter)
 
 describe("page.router", () => {
+  let caller: ReturnType<typeof createCaller>
+  beforeAll(async () => {
+    const session = await applyAuthedSession()
+    caller = createCaller(createMockRequest(session))
+  })
+
   describe("readPage", () => {
+    beforeEach(async () => {
+      await resetTables("Site")
+    })
+
     it("should throw 401 if not logged in", async () => {
       const unauthedSession = applySession()
-      const ctx = createMockRequest(unauthedSession)
-      const caller = createCaller(ctx)
+      const unauthedCaller = createCaller(createMockRequest(unauthedSession))
 
-      const result = caller.readPage({
+      const result = unauthedCaller.readPage({
         siteId: 1,
         pageId: 1,
       })
@@ -30,112 +39,96 @@ describe("page.router", () => {
       )
     })
 
-    describe("authenticated", () => {
-      let caller: ReturnType<typeof createCaller>
-      let session: ReturnType<typeof applySession>
-
-      beforeEach(async () => {
-        await resetTables("Site")
+    it("should return 404 if page does not exist", async () => {
+      // Act
+      const result = caller.readPage({
+        siteId: 1,
+        pageId: 1,
       })
 
-      beforeAll(async () => {
-        session = await applyAuthedSession()
-        const ctx = createMockRequest(session)
-        caller = createCaller(ctx)
+      // Assert
+      await expect(result).rejects.toThrow(
+        new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
+      )
+    })
+
+    it("should return the resource if resource type is Page and exists", async () => {
+      // Arrange
+      const { site, page: expectedPage } = await setupPageResource({
+        resourceType: "Page",
       })
 
-      it("should return 404 if page does not exist", async () => {
-        // Act
-        const result = caller.readPage({
-          siteId: 1,
-          pageId: 1,
-        })
-
-        // Assert
-        await expect(result).rejects.toThrow(
-          new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
-        )
+      // Act
+      const result = await caller.readPage({
+        siteId: site.id,
+        pageId: Number(expectedPage.id),
       })
 
-      it("should return the resource if resource type is Page and exists", async () => {
-        // Arrange
-        const { site, page: expectedPage } = await setupPageResource({
-          resourceType: "Page",
-        })
+      // Assert
+      expect(result.siteId).toEqual(site.id)
+      expect(result.type).toEqual("Page")
+      expect(result).toMatchObject(expectedPage)
+    })
 
-        // Act
-        const result = await caller.readPage({
-          siteId: site.id,
-          pageId: Number(expectedPage.id),
-        })
-
-        // Assert
-        expect(result.siteId).toEqual(site.id)
-        expect(result.type).toEqual("Page")
-        expect(result).toMatchObject(expectedPage)
+    it("should return the resource if resource type is CollectionPage and exists", async () => {
+      // Arrange
+      const { site, page: expectedPage } = await setupPageResource({
+        resourceType: "CollectionPage",
       })
 
-      it("should return the resource if resource type is CollectionPage and exists", async () => {
-        // Arrange
-        const { site, page: expectedPage } = await setupPageResource({
-          resourceType: "CollectionPage",
-        })
-
-        // Act
-        const result = await caller.readPage({
-          siteId: site.id,
-          pageId: Number(expectedPage.id),
-        })
-
-        // Assert
-        expect(result.siteId).toEqual(site.id)
-        expect(result.type).toEqual("CollectionPage")
-        expect(result).toMatchObject(expectedPage)
+      // Act
+      const result = await caller.readPage({
+        siteId: site.id,
+        pageId: Number(expectedPage.id),
       })
 
-      it("should return the resource if resource type is RootPage and exists", async () => {
-        // Arrange
-        const { site, page: expectedPage } = await setupPageResource({
-          resourceType: "RootPage",
-        })
+      // Assert
+      expect(result.siteId).toEqual(site.id)
+      expect(result.type).toEqual("CollectionPage")
+      expect(result).toMatchObject(expectedPage)
+    })
 
-        // Act
-        const result = await caller.readPage({
-          siteId: site.id,
-          pageId: Number(expectedPage.id),
-        })
-
-        // Assert
-        expect(result.siteId).toEqual(site.id)
-        expect(result.type).toEqual("RootPage")
-        expect(result).toMatchObject(expectedPage)
+    it("should return the resource if resource type is RootPage and exists", async () => {
+      // Arrange
+      const { site, page: expectedPage } = await setupPageResource({
+        resourceType: "RootPage",
       })
 
-      it("should return 404 if resource type is not a page", async () => {
-        // Arrange
-        const { site, folder } = await setupFolder()
-
-        // Act
-        const result = caller.readPage({
-          siteId: site.id,
-          pageId: Number(folder.id),
-        })
-
-        // Assert
-        await expect(result).rejects.toThrow(
-          new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
-        )
+      // Act
+      const result = await caller.readPage({
+        siteId: site.id,
+        pageId: Number(expectedPage.id),
       })
+
+      // Assert
+      expect(result.siteId).toEqual(site.id)
+      expect(result.type).toEqual("RootPage")
+      expect(result).toMatchObject(expectedPage)
+    })
+
+    it("should return 404 if resource type is not a page", async () => {
+      // Arrange
+      const { site, folder } = await setupFolder()
+
+      // Act
+      const result = caller.readPage({
+        siteId: site.id,
+        pageId: Number(folder.id),
+      })
+
+      // Assert
+      await expect(result).rejects.toThrow(
+        new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
+      )
     })
   })
 
   describe("readPageAndBlob", () => {
     it("should throw 401 if not logged in", async () => {
       const unauthedSession = applySession()
-      const ctx = createMockRequest(unauthedSession)
-      const caller = createCaller(ctx)
+      const unauthedCaller = createCaller(createMockRequest(unauthedSession))
 
-      const result = caller.readPageAndBlob({
+      const result = unauthedCaller.readPageAndBlob({
         siteId: 1,
         pageId: 1,
       })
@@ -145,117 +138,102 @@ describe("page.router", () => {
       )
     })
 
-    describe("authenticated", () => {
-      let caller: ReturnType<typeof createCaller>
-      let session: ReturnType<typeof applySession>
-
-      beforeEach(async () => {
-        await resetTables("Site")
+    it("should return 404 if page does not exist", async () => {
+      // Act
+      const result = caller.readPageAndBlob({
+        siteId: 1,
+        pageId: 1,
       })
 
-      beforeAll(async () => {
-        session = await applyAuthedSession()
-        const ctx = createMockRequest(session)
-        caller = createCaller(ctx)
+      // Assert
+      await expect(result).rejects.toThrow(
+        new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
+      )
+    })
+
+    it("should return the resource if resource type is Page and exists", async () => {
+      // Arrange
+      const { site, page, blob, navbar, footer } = await setupPageResource({
+        resourceType: "Page",
+      })
+      const expected = {
+        ...pick(page, ["permalink", "title", "type"]),
+        navbar: omit(navbar, ["createdAt", "updatedAt"]),
+        footer: omit(footer, ["createdAt", "updatedAt"]),
+        content: blob.content,
+      }
+
+      // Act
+      const result = await caller.readPageAndBlob({
+        siteId: site.id,
+        pageId: Number(page.id),
       })
 
-      it("should return 404 if page does not exist", async () => {
-        // Act
-        const result = caller.readPageAndBlob({
-          siteId: 1,
-          pageId: 1,
-        })
+      // Assert
+      expect(result.type).toEqual("Page")
+      expect(result).toMatchObject(expected)
+    })
 
-        // Assert
-        await expect(result).rejects.toThrow(
-          new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
-        )
+    it("should return the resource if resource type is RootPage and exists", async () => {
+      // Arrange
+      const { site, page, blob, navbar, footer } = await setupPageResource({
+        resourceType: "RootPage",
+      })
+      const expected = {
+        ...pick(page, ["permalink", "title", "type"]),
+        navbar: omit(navbar, ["createdAt", "updatedAt"]),
+        footer: omit(footer, ["createdAt", "updatedAt"]),
+        content: blob.content,
+      }
+
+      // Act
+      const result = await caller.readPageAndBlob({
+        siteId: site.id,
+        pageId: Number(page.id),
       })
 
-      it("should return the resource if resource type is Page and exists", async () => {
-        // Arrange
-        const { site, page, blob, navbar, footer } = await setupPageResource({
-          resourceType: "Page",
-        })
-        const expected = {
-          ...pick(page, ["permalink", "title", "type"]),
-          navbar: omit(navbar, ["createdAt", "updatedAt"]),
-          footer: omit(footer, ["createdAt", "updatedAt"]),
-          content: blob.content,
-        }
+      // Assert
+      expect(result.type).toEqual("RootPage")
+      expect(result).toMatchObject(expected)
+    })
 
-        // Act
-        const result = await caller.readPageAndBlob({
-          siteId: site.id,
-          pageId: Number(page.id),
-        })
+    it("should return the resource if resource type is CollectionPage and exists", async () => {
+      // Arrange
+      const { site, page, blob, navbar, footer } = await setupPageResource({
+        resourceType: "CollectionPage",
+      })
+      const expected = {
+        ...pick(page, ["permalink", "title", "type"]),
+        navbar: omit(navbar, ["createdAt", "updatedAt"]),
+        footer: omit(footer, ["createdAt", "updatedAt"]),
+        content: blob.content,
+      }
 
-        // Assert
-        expect(result.type).toEqual("Page")
-        expect(result).toMatchObject(expected)
+      // Act
+      const result = await caller.readPageAndBlob({
+        siteId: site.id,
+        pageId: Number(page.id),
       })
 
-      it("should return the resource if resource type is RootPage and exists", async () => {
-        // Arrange
-        const { site, page, blob, navbar, footer } = await setupPageResource({
-          resourceType: "RootPage",
-        })
-        const expected = {
-          ...pick(page, ["permalink", "title", "type"]),
-          navbar: omit(navbar, ["createdAt", "updatedAt"]),
-          footer: omit(footer, ["createdAt", "updatedAt"]),
-          content: blob.content,
-        }
+      // Assert
+      expect(result.type).toEqual("CollectionPage")
+      expect(result).toMatchObject(expected)
+    })
 
-        // Act
-        const result = await caller.readPageAndBlob({
-          siteId: site.id,
-          pageId: Number(page.id),
-        })
+    it("should return 404 if resource type is not a page", async () => {
+      // Arrange
+      const { site, folder } = await setupFolder()
 
-        // Assert
-        expect(result.type).toEqual("RootPage")
-        expect(result).toMatchObject(expected)
+      // Act
+      const result = caller.readPageAndBlob({
+        siteId: site.id,
+        pageId: Number(folder.id),
       })
 
-      it("should return the resource if resource type is CollectionPage and exists", async () => {
-        // Arrange
-        const { site, page, blob, navbar, footer } = await setupPageResource({
-          resourceType: "CollectionPage",
-        })
-        const expected = {
-          ...pick(page, ["permalink", "title", "type"]),
-          navbar: omit(navbar, ["createdAt", "updatedAt"]),
-          footer: omit(footer, ["createdAt", "updatedAt"]),
-          content: blob.content,
-        }
-
-        // Act
-        const result = await caller.readPageAndBlob({
-          siteId: site.id,
-          pageId: Number(page.id),
-        })
-
-        // Assert
-        expect(result.type).toEqual("CollectionPage")
-        expect(result).toMatchObject(expected)
-      })
-
-      it("should return 404 if resource type is not a page", async () => {
-        // Arrange
-        const { site, folder } = await setupFolder()
-
-        // Act
-        const result = caller.readPageAndBlob({
-          siteId: site.id,
-          pageId: Number(folder.id),
-        })
-
-        // Assert
-        await expect(result).rejects.toThrow(
-          new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
-        )
-      })
+      // Assert
+      await expect(result).rejects.toThrow(
+        new TRPCError({ code: "NOT_FOUND", message: "Resource not found" }),
+      )
     })
   })
 })
