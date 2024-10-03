e49c36209675b56de17c8948f07a06b928709c1a
Kar Rui Lau <karrui@open.gov.sg>
2024-10-03 11:59:09 +0800

    fix: apply migrations on db mock setup

--- apps/studio/tests/mocks/db.ts
+++ apps/studio/tests/mocks/db.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto"
 import { readdirSync, readFileSync, statSync } from "fs"
 import { dirname, join } from "path"
 import { fileURLToPath } from "url"
@@ -37,32 +38,39 @@ const username = configuration.environment?.POSTGRES_USER ?? "postgres"
 const password = configuration.environment?.POSTGRES_PASSWORD ?? "postgres"
 const databaseId = configuration.environment?.POSTGRES_DB ?? "test"
 
-const connectionString = `postgres://${username}:${password}@${host}:${
+const testSpecificDb = randomUUID()
+
+const originalConnectionString = `postgres://${username}:${password}@${host}:${
   ports.get(5432)?.toString() ?? "5432"
 }/${databaseId}`
 
-const pgClient = new Client({
-  connectionString,
-})
+const connectionString = `postgres://${username}:${password}@${host}:${
+  ports.get(5432)?.toString() ?? "5432"
+}/${testSpecificDb}`
+
+const setupPgClient = async () => {
+  const _pgClient = new Client({
+    connectionString: originalConnectionString,
+  })
+  await _pgClient.connect()
+  await _pgClient.query(`CREATE DATABASE "${testSpecificDb}";`)
+  await _pgClient.end()
+
+  const client = new Client({
+    connectionString,
+  })
+  return client
+}
 
 // Running migrations manually; dd-trace intercepts `exec` usage and prevents runs
-const applyMigrations = async () => {
+const applyMigrations = async (client: Client) => {
   const directory = readdirSync(prismaMigrationDir).sort()
   for (const file of directory) {
     const name = `${prismaMigrationDir}/${file}`
     if (statSync(name).isDirectory()) {
       const migration = readFileSync(`${name}/migration.sql`, "utf8")
-      await pgClient.query(migration)
+      await client.query(migration)
     }
-  }
-}
-
-const resetDb = async () => {
-  try {
-    await pgClient.query(`DROP SCHEMA public CASCADE`)
-    await pgClient.query(`CREATE SCHEMA public`)
-  } catch (error) {
-    console.log({ error })
   }
 }
 
@@ -86,12 +94,7 @@ vi.mock("../../src/server/prisma", () => ({
   prisma,
 }))
 
-beforeAll(async () => {
-  await pgClient.connect()
-  await applyMigrations()
-})
-
-afterAll(async () => {
-  await resetDb()
-  await pgClient.end()
-})
+const pgClient = await setupPgClient()
+await pgClient.connect()
+await applyMigrations(pgClient)
+await pgClient.end()
