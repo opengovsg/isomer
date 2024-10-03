e90f9d36fbc6be163bbcd59f2acaa2535dbe7d42
seaerchin <jiachin@open.gov.sg>
2024-10-04 04:47:57 +0800

    chore: format + lint

--- apps/studio/prisma/generated/generatedEnums.ts
+++ apps/studio/prisma/generated/generatedEnums.ts
@@ -1,20 +1,20 @@
 export const ResourceState = {
-    Draft: "Draft",
-    Published: "Published"
-} as const;
-export type ResourceState = (typeof ResourceState)[keyof typeof ResourceState];
+  Draft: "Draft",
+  Published: "Published",
+} as const
+export type ResourceState = (typeof ResourceState)[keyof typeof ResourceState]
 export const ResourceType = {
-    RootPage: "RootPage",
-    Page: "Page",
-    Folder: "Folder",
-    Collection: "Collection",
-    CollectionPage: "CollectionPage",
-    IndexPage: "IndexPage"
-} as const;
-export type ResourceType = (typeof ResourceType)[keyof typeof ResourceType];
+  RootPage: "RootPage",
+  Page: "Page",
+  Folder: "Folder",
+  Collection: "Collection",
+  CollectionPage: "CollectionPage",
+  IndexPage: "IndexPage",
+} as const
+export type ResourceType = (typeof ResourceType)[keyof typeof ResourceType]
 export const RoleType = {
-    Admin: "Admin",
-    Editor: "Editor",
-    Publisher: "Publisher"
-} as const;
-export type RoleType = (typeof RoleType)[keyof typeof RoleType];
+  Admin: "Admin",
+  Editor: "Editor",
+  Publisher: "Publisher",
+} as const
+export type RoleType = (typeof RoleType)[keyof typeof RoleType]
