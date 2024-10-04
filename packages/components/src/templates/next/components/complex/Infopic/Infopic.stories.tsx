2329224b5c9341ec8d2771a5b221aa53d6e85dc0
sehyunidaaa <139780851+sehyunidaaa@users.noreply.github.com>
2024-10-04 13:24:40 +0800

    feat(component): enhance Infopic component to look nice in edge cases (#703)
    * add image min height, content centering and default grid template rows to resolve for edge cases
    
    * add new stories with extreme images
    
    * use rounded number for min height instead of tailwind value
    
    * use copyright free image from unsplash
    
    ---------
    
    Co-authored-by: Park Se Hyun <>

--- packages/components/src/templates/next/components/complex/Infopic/Infopic.stories.tsx
+++ packages/components/src/templates/next/components/complex/Infopic/Infopic.stories.tsx
@@ -75,3 +75,30 @@ export const NoButton: Story = {
     buttonUrl: "",
   },
 }
+
+export const TallImage: Story = {
+  args: {
+    buttonUrl: "",
+    description: "",
+    imageSrc:
+      "https://images.unsplash.com/photo-1724390495674-5f28d72c686f?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+}
+
+export const LongImage: Story = {
+  args: {
+    title: "Don't put all your baskets in one egg",
+    description: "",
+    buttonUrl: "",
+    imageSrc:
+      "https://images.unsplash.com/photo-1444858440655-e7cf0269024e?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+}
+
+export const LongImageWithDesc: Story = {
+  args: {
+    title: "Don't put all your baskets in one egg",
+    imageSrc:
+      "https://images.unsplash.com/photo-1713098372674-cbf10e8c2bba?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+}
