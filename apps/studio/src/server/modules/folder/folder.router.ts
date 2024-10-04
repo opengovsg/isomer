46997d6a5484b22802b4d56cf19e07923b44cf83
seaerchin <jiachin@open.gov.sg>
2024-10-04 14:28:19 +0800

    refactor: shift shared function out
    1. not doing this for `move` because the checking there is subtly
    different. will see if i can do it in a future pr

--- apps/studio/src/server/modules/folder/folder.router.ts
+++ apps/studio/src/server/modules/folder/folder.router.ts
@@ -9,37 +9,55 @@ import {
 import { protectedProcedure, router } from "~/server/trpc"
 import { db } from "../database"
 import { PG_ERROR_CODES } from "../database/constants"
+import { validateUserPermissions } from "../permissions/permissions.service"
 import { defaultFolderSelect } from "./folder.select"
 
 export const folderRouter = router({
   create: protectedProcedure
     .input(createFolderSchema)
-    .mutation(async ({ input: { folderTitle, parentFolderId, ...rest } }) => {
-      const folder = await db
-        .insertInto("Resource")
-        .values({
-          ...rest,
-          type: "Folder",
-          title: folderTitle,
-          parentId: parentFolderId ? String(parentFolderId) : null,
+    .mutation(
+      async ({
+        ctx,
+        input: { siteId, folderTitle, parentFolderId, ...rest },
+      }) => {
+        await validateUserPermissions({
+          siteId,
+          action: "create",
+          userId: ctx.user.id,
+          resourceId: !!parentFolderId ? String(parentFolderId) : null,
         })
-        .executeTakeFirstOrThrow()
-        .catch((err) => {
-          if (get(err, "code") === PG_ERROR_CODES.uniqueViolation) {
-            throw new TRPCError({
-              code: "CONFLICT",
-              message: "A resource with the same permalink already exists",
-            })
-          }
-          throw err
-        })
-      return { folderId: folder.insertId }
-    }),
+
+        const folder = await db
+          .insertInto("Resource")
+          .values({
+            ...rest,
+            siteId,
+            type: "Folder",
+            title: folderTitle,
+            parentId: parentFolderId ? String(parentFolderId) : null,
+          })
+          .executeTakeFirstOrThrow()
+          .catch((err) => {
+            if (get(err, "code") === PG_ERROR_CODES.uniqueViolation) {
+              throw new TRPCError({
+                code: "CONFLICT",
+                message: "A resource with the same permalink already exists",
+              })
+            }
+            throw err
+          })
+        return { folderId: folder.insertId }
+      },
+    ),
   getMetadata: protectedProcedure
     .input(readFolderSchema)
     .query(async ({ ctx, input }) => {
+      await validateUserPermissions({
+        siteId: input.siteId,
+        action: "read",
+        userId: ctx.user.id,
+      })
       // Things that aren't working yet:
-      // 0. Perm checking
       // 1. Last Edited user and time
       // 2. Page status(draft, published)
 
@@ -51,26 +69,34 @@ export const folderRouter = router({
     }),
   editFolder: protectedProcedure
     .input(editFolderSchema)
-    .mutation(async ({ input: { resourceId, permalink, title, siteId } }) => {
-      return db
-        .updateTable("Resource")
-        .where("Resource.id", "=", resourceId)
-        .where("Resource.siteId", "=", Number(siteId))
-        .where("Resource.type", "in", ["Folder", "Collection"])
-        .set({
-          permalink,
-          title,
+    .mutation(
+      async ({ ctx, input: { resourceId, permalink, title, siteId } }) => {
+        await validateUserPermissions({
+          siteId: Number(siteId),
+          action: "create",
+          userId: ctx.user.id,
         })
-        .returning(defaultFolderSelect)
-        .execute()
-        .catch((err) => {
-          if (get(err, "code") === PG_ERROR_CODES.uniqueViolation) {
-            throw new TRPCError({
-              code: "CONFLICT",
-              message: "A resource with the same permalink already exists",
-            })
-          }
-          throw err
-        })
-    }),
+
+        return db
+          .updateTable("Resource")
+          .where("Resource.id", "=", resourceId)
+          .where("Resource.siteId", "=", Number(siteId))
+          .where("Resource.type", "in", ["Folder", "Collection"])
+          .set({
+            permalink,
+            title,
+          })
+          .returning(defaultFolderSelect)
+          .execute()
+          .catch((err) => {
+            if (get(err, "code") === PG_ERROR_CODES.uniqueViolation) {
+              throw new TRPCError({
+                code: "CONFLICT",
+                message: "A resource with the same permalink already exists",
+              })
+            }
+            throw err
+          })
+      },
+    ),
 })
