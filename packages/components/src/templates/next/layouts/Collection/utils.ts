6fe440e97c211834775373de7cae4f3aa925828c
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-10-08 10:05:43 +0800

    fix: improve date parsing for collection items

--- packages/components/src/templates/next/layouts/Collection/utils.ts
+++ packages/components/src/templates/next/layouts/Collection/utils.ts
@@ -1,5 +1,10 @@
+import dayjs from "dayjs"
+import customParseFormat from "dayjs/plugin/customParseFormat"
+
 import type { AppliedFilter, Filter as FilterType } from "../../types/Filter"
 import type { CollectionCardProps } from "~/interfaces"
+
+dayjs.extend(customParseFormat)
 
 export const getAvailableFilters = (
   items: CollectionCardProps[],
@@ -25,7 +30,7 @@ export const getAvailableFilters = (
 
     // Step 3: Get all available years
     if (lastUpdated) {
-      const year = new Date(lastUpdated).getFullYear().toString()
+      const year = dayjs(lastUpdated).year().toString()
       if (year in years && years[year]) {
         years[year] += 1
       } else {
