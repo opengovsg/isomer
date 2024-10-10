2daaaf2a62d74b125ad0aa22c79cf93e98e11c26
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-10-10 10:02:04 +0800

    fix: use reducer pattern and add branded type

--- packages/components/src/utils/getFormattedDate.ts
+++ packages/components/src/utils/getFormattedDate.ts
@@ -1,14 +1,15 @@
 import { format } from "date-fns"
 
+import type { FormattedDate } from "~/types"
 import { getParsedDate } from "./getParsedDate"
 
 // Standardise the format of dates displayed on the site
-export const getFormattedDate = (dateString?: string) => {
+export const getFormattedDate = (dateString?: string): FormattedDate => {
   if (dateString === undefined) {
-    return format(new Date(), "d MMMM yyyy")
+    return format(new Date(), "d MMMM yyyy") as FormattedDate
   }
 
   const date = getParsedDate(dateString)
 
-  return format(date, "d MMMM yyyy")
+  return format(date, "d MMMM yyyy") as FormattedDate
 }
--- packages/components/src/utils/getParsedDate.ts
+++ packages/components/src/utils/getParsedDate.ts
@@ -1,37 +1,39 @@
 import { isMatch, parse } from "date-fns"
 
+const SUPPORTED_DATE_FORMATS = [
+  "dd/MM/yyyy",
+  "d MMM yyyy",
+  "d MMMM yyyy",
+  "dd MMM yyyy",
+  "dd MMMM yyyy",
+  "yyyy-MM-dd",
+  "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+]
+
 export const getParsedDate = (dateString: string) => {
   try {
-    if (isMatch(dateString, "dd/MM/yyyy")) {
-      return parse(dateString, "dd/MM/yyyy", new Date())
+    const parsedDate = SUPPORTED_DATE_FORMATS.reduce<Date | undefined>(
+      (acc, format) => {
+        if (acc) {
+          // Date has already been parsed by an earlier format
+          return acc
+        }
+
+        if (isMatch(dateString, format)) {
+          return parse(dateString, format, new Date())
+        }
+
+        return acc
+      },
+      undefined,
+    )
+
+    if (parsedDate) {
+      return parsedDate
     }
 
-    if (isMatch(dateString, "d MMM yyyy")) {
-      return parse(dateString, "d MMM yyyy", new Date())
-    }
-
-    if (isMatch(dateString, "d MMMM yyyy")) {
-      return parse(dateString, "d MMMM yyyy", new Date())
-    }
-
-    if (isMatch(dateString, "dd MMM yyyy")) {
-      return parse(dateString, "dd MMM yyyy", new Date())
-    }
-
-    if (isMatch(dateString, "dd MMMM yyyy")) {
-      return parse(dateString, "dd MMMM yyyy", new Date())
-    }
-
-    if (isMatch(dateString, "yyyy-MM-dd")) {
-      return parse(dateString, "yyyy-MM-dd", new Date())
-    }
-
-    if (isMatch(dateString, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")) {
-      return parse(dateString, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", new Date())
-    }
+    return new Date()
   } catch (e) {
     return new Date()
   }
-
-  return new Date()
 }
