4172bc096d5c2681ed1b6d64f7fb26580cac38f7
Adrian Goh Jun Wei <adriangohjw@gmail.com>
2024-10-10 17:51:08 +0800

    fix: missing empty state for "My Sites" (#734)
    * fix: missing empty state for "My Sites"
    
    * refactor: replace px with rem + remove additional flex container
    
    * fix(SiteList): handle empty site list with early return
    
    * fix: eslint + fix gap
    
    * enhancement: optimize SVG + move conditional rendering into SiteList
    
    * add storybook for LandingPage empty state
    
    * refactor: move icon into standalone component
    
    * enhancment: same height for loader state

--- apps/studio/src/features/dashboard/SiteList.tsx
+++ apps/studio/src/features/dashboard/SiteList.tsx
@@ -1,15 +1,71 @@
 import NextLink from "next/link"
-import { Card, CardHeader, SimpleGrid, Skeleton } from "@chakra-ui/react"
+import {
+  Card,
+  CardHeader,
+  Flex,
+  SimpleGrid,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react"
 import { Link } from "@opengovsg/design-system-react"
 
+import { NoResultIcon } from "~/components/Svg/NoResultIcon"
 import { withSuspense } from "~/hocs/withSuspense"
 import { trpc } from "~/utils/trpc"
+
+const SiteListSection = ({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element => {
+  return (
+    <Flex flexDirection="column" gap="1.5rem" marginTop="0.75rem">
+      <Text textStyle="body-2">
+        Don't see a site that you're supposed to have access to?{" "}
+        <Link variant="inline" href="mailto:support@isomer.gov.sg">
+          Let us know
+        </Link>
+        .
+      </Text>
+      <SimpleGrid columns={3} gap="2.5rem" width="100%">
+        {children}
+      </SimpleGrid>
+    </Flex>
+  )
+}
 
 const SuspendableSiteList = (): JSX.Element => {
   // TODO: Only return sites that the user has access to
   const [sites] = trpc.site.list.useSuspenseQuery()
+
+  if (sites.length === 0) {
+    return (
+      <Flex
+        flexDirection="column"
+        gap="1.5rem"
+        alignItems="center"
+        marginTop="6rem"
+      >
+        <NoResultIcon />
+        <Flex flexDirection="column" gap="0.5rem" alignItems="center">
+          <Text textStyle="h5" textAlign="center">
+            You don't have access to any sites yet.
+          </Text>
+          <Text textStyle="body-2" textAlign="center">
+            Speak to your System Owner to get access.<br></br>
+            If you think there is an error,{" "}
+            <Link variant="inline" href="mailto:support@isomer.gov.sg">
+              let us know
+            </Link>
+            .
+          </Text>
+        </Flex>
+      </Flex>
+    )
+  }
+
   return (
-    <SimpleGrid columns={3} gap="2.5rem" width="100%">
+    <SiteListSection>
       {sites.map((site) => (
         <Card key={site.id} width="100%">
           <CardHeader>
@@ -19,13 +75,13 @@ const SuspendableSiteList = (): JSX.Element => {
           </CardHeader>
         </Card>
       ))}
-    </SimpleGrid>
+    </SiteListSection>
   )
 }
 
 const SiteListSkeleton = (): JSX.Element => {
   return (
-    <SimpleGrid columns={3} gap="2.5rem">
+    <SiteListSection>
       {[1, 2, 3].map((index) => (
         <Card key={index} width="100%">
           <Skeleton>
@@ -33,7 +89,7 @@ const SiteListSkeleton = (): JSX.Element => {
           </Skeleton>
         </Card>
       ))}
-    </SimpleGrid>
+    </SiteListSection>
   )
 }
 
