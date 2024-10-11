79b919327bb233aecdb50516cbf9b580902f666c
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-10-11 16:16:57 +0800

    fix: improve date parsing for collection items (#732)
    * fix: improve date parsing for collection items
    
    * ref: switch to use date-fns instead of dayjs
    
    * chore: revert to use ISO date format instead
    
    * chore: fix different versions of date-fns
    
    * fix: use reducer pattern and add branded type
    
    * chore: move try/catch into reducer function

--- packages/components/src/templates/next/layouts/Collection/utils.ts
+++ packages/components/src/templates/next/layouts/Collection/utils.ts
@@ -1,5 +1,6 @@
 import type { AppliedFilter, Filter as FilterType } from "../../types/Filter"
 import type { CollectionCardProps } from "~/interfaces"
+import { getParsedDate } from "~/utils"
 
 export const getAvailableFilters = (
   items: CollectionCardProps[],
@@ -25,7 +26,7 @@ export const getAvailableFilters = (
 
     // Step 3: Get all available years
     if (lastUpdated) {
-      const year = new Date(lastUpdated).getFullYear().toString()
+      const year = getParsedDate(lastUpdated).getFullYear().toString()
       if (year in years && years[year]) {
         years[year] += 1
       } else {
