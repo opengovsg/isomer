20389d8db21aed64c95b0458434effb3af4c4d3d
seaerchin <jiachin@open.gov.sg>
2024-10-15 04:24:19 +0800

    refactor: use new preview for creation modals

--- apps/studio/src/features/editing-experience/components/CreateCollectionPageModal/PreviewLayout.tsx
+++ apps/studio/src/features/editing-experience/components/CreateCollectionPageModal/PreviewLayout.tsx
@@ -1,11 +1,26 @@
+import type { IsomerGeneratedSiteProps } from "@opengovsg/isomer-components"
 import { useMemo } from "react"
 import { Box, Flex, Stack } from "@chakra-ui/react"
 import { useIsMobile } from "@opengovsg/design-system-react"
 import { format } from "date-fns"
 
-import Preview from "../Preview"
+import collectionSitemap from "~/features/editing-experience/data/collectionSitemap.json"
 import { PreviewIframe } from "../PreviewIframe"
+import PreviewWithoutSitemap from "../PreviewWithoutSitemap"
 import { useCreateCollectionPageWizard } from "./CreateCollectionPageWizardContext"
+
+const generatePreviewSitemap = (
+  sitemap: typeof collectionSitemap,
+  title = "Your filename",
+) => {
+  return {
+    ...sitemap,
+    children: sitemap.children.map(({ children, ...rest }) => ({
+      ...rest,
+      children: children.map((props) => ({ ...props, title })),
+    })),
+  } as IsomerGeneratedSiteProps["siteMap"]
+}
 
 export const PreviewLayout = (): JSX.Element => {
   const isMobile = useIsMobile()
@@ -17,6 +32,7 @@ export const PreviewLayout = (): JSX.Element => {
   } = useCreateCollectionPageWizard()
 
   const currentPermalink = watch("permalink", "/")
+  const title = watch("title")
 
   const previewOverrides = useMemo(() => {
     switch (currentType) {
@@ -47,8 +63,8 @@ export const PreviewLayout = (): JSX.Element => {
             borderTopRadius="8px"
             width="100%"
             bg="slate.200"
-            color="white"
             textStyle="caption-2"
+            color="white"
             py="0.5rem"
             px="1rem"
             justify="center"
@@ -58,9 +74,10 @@ export const PreviewLayout = (): JSX.Element => {
           </Flex>
           <Box bg="white" overflow="auto" height="100%">
             <PreviewIframe preventPointerEvents keyForRerender={currentType}>
-              <Preview
+              <PreviewWithoutSitemap
                 overrides={previewOverrides}
                 siteId={siteId}
+                siteMap={generatePreviewSitemap(collectionSitemap, title)}
                 permalink={currentPermalink}
                 {...pagePreviewJson}
               />
