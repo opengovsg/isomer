abb4cd6c75a9bdf3b30426c45553863c2751d418
seaerchin <jiachin@open.gov.sg>
2024-10-15 03:51:15 +0800

    refactor: make resource id required

--- apps/studio/src/server/modules/resource/resource.service.ts
+++ apps/studio/src/server/modules/resource/resource.service.ts
@@ -258,20 +258,8 @@ export const moveResource = async (
 // - The immediate siblings of the resource (if any)
 export const getLocalisedSitemap = async (
   siteId: number,
-  resourceId?: number,
+  resourceId: number,
 ) => {
-  if (resourceId === undefined) {
-    // Provide a default sitemap with a single resource
-    return {
-      id: "0",
-      layout: "content",
-      title: "Root",
-      summary: "",
-      lastModified: new Date().toISOString(),
-      permalink: "/",
-    }
-  }
-
   // Get the actual resource first
   const resource = await getById(db, {
     resourceId,
