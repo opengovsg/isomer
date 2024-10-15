190f9bda2b0157c9a222aca3a3b178ff14b4f35b
adriangohjw <adriangohjw@gmail.com>
2024-10-15 15:22:32 +0800

    feat: add OutdatedBrowserWarning to DashboardPage

--- apps/studio/src/pages/index.tsx
+++ apps/studio/src/pages/index.tsx
@@ -1,19 +1,23 @@
 import { Flex, Text } from "@chakra-ui/react"
 
+import { OutdatedBrowserWarning } from "~/components/OutdatedBrowserWarning"
 import { SiteList } from "~/features/dashboard/SiteList"
 import { type NextPageWithLayout } from "~/lib/types"
 import { AdminLayout } from "~/templates/layouts/AdminLayout"
 
 const DashboardPage: NextPageWithLayout = () => {
   return (
-    <Flex flexDir="column" py="2rem" maxW="57rem" mx="auto" width="100%">
-      <Flex flexDirection="column">
-        <Text as="h3" size="lg" textStyle="h3">
-          My sites
-        </Text>
-        <SiteList />
+    <>
+      <Flex flexDir="column" py="2rem" maxW="57rem" mx="auto" width="100%">
+        <OutdatedBrowserWarning mb="2rem" />
+        <Flex flexDirection="column">
+          <Text as="h3" size="lg" textStyle="h3">
+            My sites
+          </Text>
+          <SiteList />
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
 
