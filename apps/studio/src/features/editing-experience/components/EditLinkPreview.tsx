5283337b9c3692e5de5390fc5ff932beaf0ae3dd
seaerchin <jiachin@open.gov.sg>
2024-10-16 06:38:29 +0800

    feat: edit link page

--- apps/studio/src/features/editing-experience/components/EditLinkPreview.tsx
+++ apps/studio/src/features/editing-experience/components/EditLinkPreview.tsx
@@ -1,12 +1,15 @@
 import type { IsomerSitemap } from "@opengovsg/isomer-components"
+import { useAtomValue } from "jotai"
 
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { editLinkSchema } from "~/pages/sites/[siteId]/links/[linkId]"
 import { trpc } from "~/utils/trpc"
+import { linkAtom } from "../atoms"
 import PreviewWithoutSitemap from "./PreviewWithoutSitemap"
 import { ViewportContainer } from "./ViewportContainer"
 
 export const EditLinkPreview = (): JSX.Element => {
+  const { summary } = useAtomValue(linkAtom)
   const { linkId, siteId } = useQueryParse(editLinkSchema)
   const [permalink] = trpc.page.getFullPermalink.useSuspenseQuery(
     {
@@ -16,12 +19,18 @@ export const EditLinkPreview = (): JSX.Element => {
     { refetchOnWindowFocus: false },
   )
 
+  const [parentResource] = trpc.resource.getParentOf.useSuspenseQuery({
+    resourceId: String(linkId),
+    siteId,
+  })
+
   const [children] = trpc.collection.getSiblingsOf.useSuspenseQuery({
     siteId,
     resourceId: linkId,
   })
 
   const parentPermalink = permalink.split("/").slice(0, -1).join("/")
+  const linkSuffix = permalink.split("/").slice(-1).join("/")
 
   const siteMap = {
     id: "root",
@@ -36,13 +45,16 @@ export const EditLinkPreview = (): JSX.Element => {
         permalink: parentPermalink,
         lastModified: "02 May 2023",
         layout: "collection",
-        title: "asdf",
+        title: parentResource.title,
         summary: "Learn more about PREPARE here.",
         children: children.map((props) => ({
           ...props,
           date: "12 May 2023",
-          ref: "asdf",
-          summary: "This is the summary of your page",
+          ref: "Test",
+          summary:
+            linkSuffix === props.permalink
+              ? summary || ""
+              : "This is the summary of your page",
           layout: "link",
           permalink: `${parentPermalink}/${props.permalink}`,
           lastModified: new Date().toString(),
@@ -55,8 +67,8 @@ export const EditLinkPreview = (): JSX.Element => {
     <ViewportContainer siteId={siteId}>
       <PreviewWithoutSitemap
         content={[]}
+        page={{ title: parentResource.title }}
         layout={"collection"}
-        {...{ page: { title: "Collection title here" } }}
         siteId={siteId}
         siteMap={siteMap}
         permalink={parentPermalink}
--- apps/studio/src/pages/sites/[siteId]/links/[linkId]/index.tsx
+++ apps/studio/src/pages/sites/[siteId]/links/[linkId]/index.tsx
@@ -1,12 +1,9 @@
 import { Grid, GridItem } from "@chakra-ui/react"
 import { z } from "zod"
 
-import { EditorDrawerProvider } from "~/contexts/EditorDrawerContext"
 import { EditLinkPreview } from "~/features/editing-experience/components/EditLinkPreview"
 import { LinkEditorDrawer } from "~/features/editing-experience/components/LinkEditorDrawer"
-import { useQueryParse } from "~/hooks/useQueryParse"
 import { LinkEditingLayout } from "~/templates/layouts/LinkEditingLayout"
-import { trpc } from "~/utils/trpc"
 
 export const editLinkSchema = z.object({
   linkId: z.coerce.number().min(1),
@@ -14,25 +11,6 @@ export const editLinkSchema = z.object({
 })
 
 function EditLink(): JSX.Element {
-  const { linkId, siteId } = useQueryParse(editLinkSchema)
-
-  const [{ content: page, type, updatedAt, title }] =
-    trpc.page.readPageAndBlob.useSuspenseQuery(
-      {
-        pageId: linkId,
-        siteId,
-      },
-      { refetchOnWindowFocus: false },
-    )
-
-  const [permalink] = trpc.page.getFullPermalink.useSuspenseQuery(
-    {
-      pageId: linkId,
-      siteId,
-    },
-    { refetchOnWindowFocus: false },
-  )
-
   // put the forms and state here?
   // then inside i just call
   // i can set the default state to be correct here actually
