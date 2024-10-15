b765641a782a08f8441873b05ecafefb9c8ceb43
seaerchin <jiachin@open.gov.sg>
2024-10-15 12:25:09 +0800

    feat: add new icon

--- apps/studio/src/features/dashboard/components/DirectorySidebar/constants.ts
+++ apps/studio/src/features/dashboard/components/DirectorySidebar/constants.ts
@@ -1,12 +1,19 @@
 import type { IconType } from "react-icons"
 import { ResourceType } from "~prisma/generated/generatedEnums"
-import { BiData, BiFile, BiFolder, BiHomeAlt } from "react-icons/bi"
+import {
+  BiData,
+  BiFile,
+  BiFolder,
+  BiHomeAlt,
+  BiSolidFilePdf,
+} from "react-icons/bi"
 
 export const ICON_MAPPINGS: Record<ResourceType, IconType> = {
   [ResourceType.Page]: BiFile,
   [ResourceType.Folder]: BiFolder,
   [ResourceType.Collection]: BiData,
   [ResourceType.CollectionPage]: BiFile,
+  [ResourceType.CollectionFile]: BiSolidFilePdf,
   [ResourceType.RootPage]: BiHomeAlt,
   [ResourceType.IndexPage]: BiFile,
 }
