ef54bc111ed6b58a662126dd92b6687ab4229aef
seaerchin <jiachin@open.gov.sg>
2024-10-16 13:57:09 +0800

    feat: seed with backend ata

--- apps/studio/src/features/editing-experience/components/LinkEditorDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/LinkEditorDrawer.tsx
@@ -1,23 +1,58 @@
 import type { Static } from "@sinclair/typebox"
 import { Flex, Text, VStack } from "@chakra-ui/react"
-import { Button } from "@opengovsg/design-system-react"
+import { Button, useToast } from "@opengovsg/design-system-react"
 import { LAYOUT_PAGE_MAP } from "@opengovsg/isomer-components"
 import Ajv from "ajv"
-import { useAtomValue, useSetAtom } from "jotai"
+import { useAtom } from "jotai"
 import { isEmpty } from "lodash"
+import { z } from "zod"
 
+import type { CollectionLinkProps } from "../atoms"
+import { useQueryParse } from "~/hooks/useQueryParse"
+import { trpc } from "~/utils/trpc"
 import { linkAtom } from "../atoms"
+import { BRIEF_TOAST_SETTINGS } from "./constants"
 import { ErrorProvider, useBuilderErrors } from "./form-builder/ErrorProvider"
 import FormBuilder from "./form-builder/FormBuilder"
 
 const ajv = new Ajv({ strict: false, logger: false })
 
+const editLinkSchema = z.object({
+  linkId: z.coerce.number(),
+  siteId: z.coerce.number(),
+})
+
 const InnerDrawer = () => {
   const schema = LAYOUT_PAGE_MAP.link
   const validateFn = ajv.compile<Static<typeof schema>>(schema)
-  const setLinkAtom = useSetAtom(linkAtom)
-  const data = useAtomValue(linkAtom)
+  const { linkId, siteId } = useQueryParse(editLinkSchema)
+  const [data, setLinkAtom] = useAtom(linkAtom)
   const { errors } = useBuilderErrors()
+  const utils = trpc.useUtils()
+  const toast = useToast()
+  const [] = trpc.collection.readCollectionLink.useSuspenseQuery(
+    {
+      linkId,
+      siteId,
+    },
+    {
+      onSuccess: (data) => {
+        setLinkAtom(data.content.page as CollectionLinkProps)
+      },
+      refetchOnWindowFocus: false,
+    },
+  )
+  const { mutate, isLoading } =
+    trpc.collection.updateCollectionLink.useMutation({
+      onSuccess: () => {
+        void utils.collection.readCollectionLink.invalidate()
+        toast({
+          title: "Link updated!",
+          status: "success",
+          ...BRIEF_TOAST_SETTINGS,
+        })
+      },
+    })
 
   return (
     <VStack gap="1.5rem" p="1.5rem">
@@ -38,13 +73,16 @@ const InnerDrawer = () => {
           schema={schema}
           validateFn={validateFn}
           data={data}
-          handleChange={setLinkAtom}
+          handleChange={(data) => setLinkAtom(data)}
         />
       </Flex>
       <Button
         alignSelf="flex-start"
-        onClick={() => console.log(data)}
+        onClick={() =>
+          mutate({ ref: data.ref ?? "", siteId, linkId, summary: data.summary })
+        }
         isDisabled={!isEmpty(errors) || !data.ref}
+        isLoading={isLoading}
       >
         Save
       </Button>
