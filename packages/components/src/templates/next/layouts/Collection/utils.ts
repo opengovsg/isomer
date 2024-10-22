65d24bc28df7216bfeca01c054620994c521f7d9
adriangohjw <adriangohjw@gmail.com>
2024-10-22 15:23:43 +0800

    update getFilteredItems for unspecified date + refactor (use const instead of hardcode string)

--- packages/components/src/templates/next/layouts/Collection/utils.ts
+++ packages/components/src/templates/next/layouts/Collection/utils.ts
@@ -2,6 +2,10 @@ import type { AppliedFilter, Filter as FilterType } from "../../types/Filter"
 import type { CollectionCardProps } from "~/interfaces"
 import { getParsedDate } from "~/utils"
 
+const FILTER_ID_CATEGORY = "category"
+const FILTER_ID_YEAR = "year"
+const NO_SPECIFIED_YEAR_FILTER_ID = "not_specified"
+
 export const getAvailableFilters = (
   items: CollectionCardProps[],
 ): FilterType[] => {
@@ -39,7 +43,7 @@ export const getAvailableFilters = (
 
   const availableFilters: FilterType[] = [
     {
-      id: "category",
+      id: FILTER_ID_CATEGORY,
       label: "Category",
       items: Object.entries(categories).map(([label, count]) => ({
         id: label.toLowerCase(),
@@ -48,7 +52,7 @@ export const getAvailableFilters = (
       })),
     },
     {
-      id: "year",
+      id: FILTER_ID_YEAR,
       label: "Year",
       items:
         // do not show "not specified" option if all items have undefined dates
@@ -59,7 +63,7 @@ export const getAvailableFilters = (
             : [
                 ...yearFilterItems,
                 {
-                  id: "",
+                  id: NO_SPECIFIED_YEAR_FILTER_ID,
                   label: "Not specified",
                   count: numberOfUndefinedDates,
                 },
@@ -88,7 +92,7 @@ export const getFilteredItems = (
 
     // Step 2: Remove items that do not match the applied category filters
     const categoryFilter = appliedFilters.find(
-      (filter) => filter.id === "category",
+      (filter) => filter.id === FILTER_ID_CATEGORY,
     )
     if (
       categoryFilter &&
@@ -100,13 +104,18 @@ export const getFilteredItems = (
     }
 
     // Step 3: Remove items that do not match the applied year filters
-    const yearFilter = appliedFilters.find((filter) => filter.id === "year")
+    const yearFilter = appliedFilters.find(
+      (filter) => filter.id === FILTER_ID_YEAR,
+    )
     if (
       yearFilter &&
-      !yearFilter.items.some(
-        (filterItem) =>
-          item.lastUpdated &&
-          new Date(item.lastUpdated).getFullYear().toString() === filterItem.id,
+      !yearFilter.items.some((filterItem) =>
+        item.lastUpdated
+          ? // if date is defined, check if year matches
+            new Date(item.lastUpdated).getFullYear().toString() ===
+            filterItem.id
+          : // if undefined date, check if "not specified" filter is applied
+            filterItem.id === NO_SPECIFIED_YEAR_FILTER_ID,
       )
     ) {
       return false
