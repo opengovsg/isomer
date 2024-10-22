bc9b580b729c3447fa5c39355444becab0491a13
adriangohjw <adriangohjw@gmail.com>
2024-10-23 00:59:34 +0800

    refactor and fix CollectionCard stories

--- packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
+++ packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
@@ -49,77 +49,73 @@ const meta: Meta<CollectionCardProps> = {
 export default meta
 type Story = StoryObj<typeof CollectionCard>
 
-export const Default: Story = {
-  args: {
-    lastUpdated: "December 2, 2023",
+const generateArgs = ({
+  variant,
+  shouldShowDate = true,
+  isLastUpdatedUndefined = false,
+  withoutImage = false,
+  fileDetails,
+}: {
+  variant: string
+  shouldShowDate?: boolean
+  isLastUpdatedUndefined?: boolean
+  withoutImage?: boolean
+  fileDetails?: {
+    type: string
+    size: string
+  }
+}): Partial<CollectionCardProps> & { shouldShowDate?: boolean } => {
+  return {
+    lastUpdated: isLastUpdatedUndefined ? undefined : "December 2, 2023",
     category: "Research",
     title:
       "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
     url: "/",
     description:
       "We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year. We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year.",
-    image: {
-      src: "https://placehold.co/500x500",
-      alt: "placeholder",
-    },
-    variant: "article",
-  },
+    image: withoutImage
+      ? undefined
+      : {
+          src: "https://plxacehold.co/500x500",
+          alt: "placeholder",
+        },
+    variant: variant as "link" | "article" | "file" | undefined,
+    fileDetails,
+    shouldShowDate,
+  }
+}
+
+export const Default: Story = {
+  args: generateArgs({ variant: "article" }),
+}
+
+export const UndefinedDate: Story = {
+  args: generateArgs({ variant: "article", isLastUpdatedUndefined: true }),
 }
 
 export const HideDate: Story = {
-  args: {
-    ...Default.args,
-    lastUpdated: undefined,
-  },
+  args: generateArgs({
+    variant: "article",
+    shouldShowDate: false,
+    isLastUpdatedUndefined: true,
+  }),
 }
 
 export const ArticleWithoutImage: Story = {
-  args: {
-    lastUpdated: "December 2, 2023",
-    category: "Research",
-    title:
-      "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
-    url: "/",
-    description:
-      "We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year. We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year.",
-    variant: "article",
-  },
+  args: generateArgs({ variant: "article", withoutImage: true }),
 }
 
 export const File: Story = {
-  args: {
-    lastUpdated: "December 2, 2023",
-    category: "Research",
-    title:
-      "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
-    url: "/",
-    description:
-      "We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year. We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year.",
-    image: {
-      src: "https://placehold.co/500x500",
-      alt: "placeholder",
-    },
+  args: generateArgs({
     variant: "file",
-    fileDetails: {
-      type: "pdf",
-      size: "2.3 MB",
-    },
-  },
+    fileDetails: { type: "pdf", size: "2.3 MB" },
+  }),
 }
 
 export const FileWithoutImage: Story = {
-  args: {
-    lastUpdated: "December 2, 2023",
-    category: "Research",
-    title:
-      "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
-    url: "/",
-    description:
-      "We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year. We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year.",
+  args: generateArgs({
     variant: "file",
-    fileDetails: {
-      type: "pdf",
-      size: "2.3 MB",
-    },
-  },
+    withoutImage: true,
+    fileDetails: { type: "pdf", size: "2.3 MB" },
+  }),
 }
