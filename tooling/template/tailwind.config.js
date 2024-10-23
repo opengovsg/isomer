af81ad049fc8b2249e4f05a37c02528a6203cb53
adriangohjw <adriangohjw@gmail.com>
2024-10-23 18:52:24 +0800

    add custom plugin to handle old browser version not supporting flex

--- tooling/template/tailwind.config.js
+++ tooling/template/tailwind.config.js
@@ -1,7 +1,10 @@
+// @ts-nocheck
 /** @type {import('tailwindcss').Config} */
 import { isomerSiteTheme, NextPreset } from "@opengovsg/isomer-components"
 
 import siteConfig from "./data/config.json"
+
+const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -13,6 +16,58 @@ const config = {
   plugins: [
     isomerSiteTheme({
       colors: siteConfig.colors.brand,
+    }),
+    plugin(function ({ addUtilities, theme, e }) {
+      // Gets all the gap values (like gap-1, gap-2, etc.) defined in the Tailwind theme
+      // It's based on the existing gap utility in Tailwind.
+      const gapValues = theme("gap")
+
+      // Generate margin-based fallback for gap in flexbox
+      const gapFallbackUtilities = Object.keys(gapValues).reduce((acc, key) => {
+        const value = gapValues[key]
+
+        // Only apply the margin fallback if gap is not supported
+        // using arbitrary @supports not (gap: 1rem)
+
+        // The current code doesn't account for reverse flex directions (flex-row-reverse and flex-column-reverse).
+        // To cover these cases, additional rules would be needed to apply margins to the opposite sides
+        // (left for row-reverse, bottom for column-reverse) and target different children.
+
+        // Horizontal gaps (gap-x)
+        acc[`.${e(`gap-x-${key}`)} > *:not(:last-child)`] = {
+          "@supports not (gap: 1rem)": {
+            marginRight: value,
+          },
+        }
+
+        // Vertical gaps (gap-y)
+        acc[`.${e(`gap-y-${key}`)} > *:not(:last-child)`] = {
+          "@supports not (gap: 1rem)": {
+            marginBottom: value,
+          },
+        }
+
+        // Horizontal gaps for flex-row layout
+        acc[`.${e(`gap-${key}`)} > *:not(:last-child)`] = {
+          "@supports not (gap: 1rem)": {
+            marginRight: value,
+          },
+        }
+
+        // Vertical gaps for flex-column layout
+        acc[`.${e(`gap-${key}`)} > *:not(:first-child)`] = {
+          "@supports not (gap: 1rem)": {
+            marginTop: value,
+          },
+        }
+
+        return acc
+      }, {})
+
+      addUtilities(gapFallbackUtilities, {
+        // Enable responsive support e.g., sm:gap-1, md:gap-2
+        variants: ["responsive"],
+      })
     }),
   ],
 }
