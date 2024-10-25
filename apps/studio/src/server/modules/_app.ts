a66fda281b23c712bbb0e2c91809338c7865721e
seaerchin <44049504+seaerchin@users.noreply.github.com>
2024-10-25 15:17:34 +0800

    feat(dd): add RUM + trace on studio (#818)
    * feat: add dd rum
    
    * chore: add tracer
    
    * feat: add env to front
    
    * fix: env and version
    
    * feat: update tag
    
    * chore: add vapt
    
    * fix: deps
    
    * chore: add env

--- apps/studio/src/server/modules/_app.ts
+++ apps/studio/src/server/modules/_app.ts
@@ -1,6 +1,8 @@
 /**
  * This file contains the root router of your tRPC-backend
  */
+import "./tracer"
+
 import { publicProcedure, router } from "../trpc"
 import { assetRouter } from "./asset/asset.router"
 import { authRouter } from "./auth/auth.router"
