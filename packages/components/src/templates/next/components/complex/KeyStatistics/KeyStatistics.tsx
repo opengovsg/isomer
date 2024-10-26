7c0d374cdc81a830459776194f312628ecad1ef5
adriangohjw <adriangohjw@gmail.com>
2024-10-27 02:19:43 +0800

    fix - incorrect width calculation (broken on old browsers)

--- packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
+++ packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.tsx
@@ -29,18 +29,16 @@ const createKeyStatisticsStyles = tv({
   variants: {
     noOfItems: {
       1: {
-        itemContainer: "basis-full",
+        itemContainer: "md:basis-full",
       },
       2: {
-        itemContainer: "basis-[calc((100%-2.5rem)/2)]",
+        itemContainer: "md:basis-[calc((100%-2.5rem)/2)]",
       },
       3: {
-        itemContainer:
-          "basis-[calc((100%-5rem)/3)] md:max-w-[calc((100%-2.5rem)/2)]",
+        itemContainer: "md:basis-[calc((100%-5rem)/3)]",
       },
       4: {
-        itemContainer:
-          "basis-[calc((100%-7.5rem)/4)] md:max-w-[calc((100%-5rem)/3)]",
+        itemContainer: "md:basis-[calc((100%-7.5rem)/4)]",
       },
     },
     layout: {
