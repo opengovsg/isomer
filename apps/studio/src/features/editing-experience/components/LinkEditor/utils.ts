9db0088bbcc1173a7a327bb8c641fdb7f1f0a695
adriangohjw <adriangohjw@gmail.com>
2024-10-27 03:06:06 +0800

    return const instead of hardcoded string + check for output type

--- apps/studio/src/features/editing-experience/components/LinkEditor/utils.ts
+++ apps/studio/src/features/editing-experience/components/LinkEditor/utils.ts
@@ -1,23 +1,32 @@
-export const getLinkHrefType = (href: string | undefined) => {
+import {
+  LINK_TYPE_EMAIL,
+  LINK_TYPE_EXTERNAL,
+  LINK_TYPE_FILE,
+  LINK_TYPE_PAGE,
+} from "@opengovsg/isomer-components"
+
+import type { LinkTypes } from "./constants"
+
+export const getLinkHrefType = (href: string | undefined): LinkTypes => {
   if (!href) {
     // We default to page if no href is provided, as that is the first option
-    return "page"
+    return LINK_TYPE_PAGE
   }
 
   if (href.startsWith("mailto:")) {
-    return "email"
+    return LINK_TYPE_EMAIL
   }
 
   // Internal links are in the format [resource:$siteId:$pageId]
   const referenceLinkMatch = /\[resource:(\d+):(\d+)\]/.exec(href)
   if (referenceLinkMatch && referenceLinkMatch.length === 3) {
-    return "page"
+    return LINK_TYPE_PAGE
   }
 
   // File links would be pointing to the assets bucket, so they start with a /
   if (href.startsWith("/")) {
-    return "file"
+    return LINK_TYPE_FILE
   }
 
-  return "external"
+  return LINK_TYPE_EXTERNAL
 }
