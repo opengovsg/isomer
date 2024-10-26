6c241d808c9c0f18e0f22fe1cb0bc23eba8265cd
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-10-26 09:39:37 +0800

    fix: hide skip to content button on mobile

--- packages/components/src/templates/next/components/internal/SkipToContent/SkipToContent.tsx
+++ packages/components/src/templates/next/components/internal/SkipToContent/SkipToContent.tsx
@@ -1,16 +1,13 @@
-import { tv } from "~/lib/tv"
+import type { SkipToContentProps } from "~/interfaces"
 import { SKIP_TO_CONTENT_ANCHOR_ID } from "~/templates/next/constants"
-import { focusVisibleHighlight } from "~/utils"
 
-const skipToContentStyle = tv({
-  extend: focusVisibleHighlight,
-  base: "absolute -left-[150%] -top-[150%] -z-50 bg-base-canvas p-2 focus-visible:left-0 focus-visible:top-0 focus-visible:z-50",
-})
-
-export const SkipToContent = () => {
+export const SkipToContent = ({ LinkComponent }: SkipToContentProps) => {
   return (
-    <a href={`#${SKIP_TO_CONTENT_ANCHOR_ID}`} className={skipToContentStyle()}>
+    <LinkComponent
+      href={`#${SKIP_TO_CONTENT_ANCHOR_ID}`}
+      className="focus:shadow-focus absolute -left-[150%] -top-[150%] -z-50 bg-base-canvas p-2 focus:left-0 focus:top-0 focus:z-50 focus:bg-utility-highlight focus:text-base-content-strong focus:decoration-transparent focus:outline-0 focus:transition-none focus:hover:decoration-transparent"
+    >
       Skip to main content
-    </a>
+    </LinkComponent>
   )
 }
