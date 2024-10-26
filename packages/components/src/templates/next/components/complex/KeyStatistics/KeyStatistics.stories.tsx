a7c6c3ab8147097394d3eefa9c7f568469473f86
adriangohjw <adriangohjw@gmail.com>
2024-10-26 10:50:13 +0800

    add stories

--- packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.stories.tsx
+++ packages/components/src/templates/next/components/complex/KeyStatistics/KeyStatistics.stories.tsx
@@ -116,3 +116,24 @@ export const WithLink: Story = {
     url: "https://www.google.com",
   },
 }
+
+export const WithLinkAndLabel: Story = {
+  args: {
+    title: "Short title",
+    statistics: [
+      {
+        label: "Average all nighters pulled in a typical calendar month",
+        value: "300,000",
+      },
+      { label: "Growth in tasks assigned Q4 2024 (YoY)", value: "+123.2%" },
+      {
+        label:
+          "Number of lies in this stat block along with a lot of content and text that may not be read by users",
+        value: "4.0",
+      },
+    ],
+    url: "https://www.google.com",
+    label: "We have no achievements",
+  },
+}
+
