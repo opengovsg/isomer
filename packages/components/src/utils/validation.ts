bfcb19eb07f0a4d4084549165ab6332b610dd220
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-10-30 15:52:04 +0800

    fix: allow legacy internal permalinks

--- packages/components/src/utils/validation.ts
+++ packages/components/src/utils/validation.ts
@@ -8,9 +8,12 @@ export const ALLOWED_URL_REGEXES = {
   // but `re.source` only gives us the actual string
   // regex for asset links: /^\/(\d+)\//
   files: "^\\/(\\d+)\\/",
+  // These are the standard internal links that are used by sites on GitHub.
+  // We can drop them once all sites have fully migrated to Studio.
+  legacy: "^\\/",
 } as const
 
 export const LINK_HREF_PATTERN =
-  `(${ALLOWED_URL_REGEXES.external})|(${ALLOWED_URL_REGEXES.mail})|(${ALLOWED_URL_REGEXES.internal})|(${ALLOWED_URL_REGEXES.files})` as const
+  `(${ALLOWED_URL_REGEXES.external})|(${ALLOWED_URL_REGEXES.mail})|(${ALLOWED_URL_REGEXES.internal})|(${ALLOWED_URL_REGEXES.files})|(${ALLOWED_URL_REGEXES.legacy})` as const
 export const REF_HREF_PATTERN =
-  `(${ALLOWED_URL_REGEXES.external})|(${ALLOWED_URL_REGEXES.internal})|(${ALLOWED_URL_REGEXES.files})` as const
+  `(${ALLOWED_URL_REGEXES.external})|(${ALLOWED_URL_REGEXES.internal})|(${ALLOWED_URL_REGEXES.files})|(${ALLOWED_URL_REGEXES.legacy})` as const
