34344b677180efed196a93c49ed2c8410503e744
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-11-06 10:00:12 +0800

    feat: alternate list marker type for nested lists (#857)

--- packages/components/src/templates/next/components/native/UnorderedList/UnorderedList.tsx
+++ packages/components/src/templates/next/components/native/UnorderedList/UnorderedList.tsx
@@ -1,17 +1,30 @@
 import type { UnorderedListProps } from "~/interfaces"
 import ListItem from "../ListItem"
 
+const getUnorderedListType = (level?: number) => {
+  // We rotate between disc, circle and square
+  if (!level || level % 3 === 0) {
+    return "list-disc"
+  } else if (level % 3 === 1) {
+    return "list-[circle]"
+  } else {
+    return "list-[square]"
+  }
+}
+
 const UnorderedList = ({
   content,
+  level,
   LinkComponent,
   site,
 }: UnorderedListProps) => {
   return (
-    <ul className="mt-6 list-disc ps-8">
+    <ul className={`mt-6 ps-8 ${getUnorderedListType(level)}`}>
       {content.map((item, index) => (
         <ListItem
           key={index}
           {...item}
+          level={level}
           LinkComponent={LinkComponent}
           site={site}
         />
