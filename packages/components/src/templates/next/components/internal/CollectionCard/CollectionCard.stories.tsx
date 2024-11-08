8b851af9ea82e62e44618dfe5def20ec8e2ad72d
adriangohjw <adriangohjw@gmail.com>
2024-11-08 17:25:07 +0800

    add stories

--- packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
+++ packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
@@ -55,6 +55,8 @@ const generateArgs = ({
   isLastUpdatedUndefined = false,
   withoutImage = false,
   fileDetails,
+  title = "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
+  description = "We've looked at how people's spending correlates with how much microscopic plastic they consumed over the year. We've looked at how people's spending correlates with how much microscopic plastic they consumed over the year.",
 }: {
   variant: string
   shouldShowDate?: boolean
@@ -64,15 +66,15 @@ const generateArgs = ({
     type: string
     size: string
   }
+  title?: string
+  description?: string
 }): Partial<CollectionCardProps> & { shouldShowDate?: boolean } => {
   return {
     lastUpdated: isLastUpdatedUndefined ? undefined : "December 2, 2023",
     category: "Research",
-    title:
-      "A journal on microscopic plastic and their correlation to the number of staycations enjoyed per millennials between the ages of 30-42, substantiated by research from IDK university",
+    title,
     url: "/",
-    description:
-      "We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year. We’ve looked at how people’s spending correlates with how much microscopic plastic they consumed over the year.",
+    description,
     image: withoutImage
       ? undefined
       : {
@@ -117,5 +119,13 @@ export const FileWithoutImage: Story = {
     variant: "file",
     withoutImage: true,
     fileDetails: { type: "pdf", size: "2.3 MB" },
+  }),
+}
+
+export const ShortDescription: Story = {
+  args: generateArgs({
+    variant: "article",
+    title: "Short title",
+    description: "Short description",
   }),
 }
