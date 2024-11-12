fefb91f03f934dfc7d940815d730167c087b06d0
seaerchin <jiachin@open.gov.sg>
2024-11-12 18:42:35 +0800

    chore: add tests

--- packages/components/src/utils/getWordsFromPermalink.ts
+++ packages/components/src/utils/getWordsFromPermalink.ts
@@ -1,10 +1,13 @@
 export const getWordsFromPermalink = (permalink: string): string => {
-  const lastUrlSegment = permalink.split("/").at(-1) ?? ""
+  const trimmedPermalink = permalink.endsWith("/")
+    ? permalink.slice(0, -1)
+    : permalink
+  const lastUrlSegment = trimmedPermalink.split("/").at(-1) ?? ""
   // NOTE: Replace all non-alphanumeric characters with spaces
   // then remove all spaces and join by `+`.
   // This is because we might have run-on spaces from sequences of symbols
   // like: `+=`, which would lead to 2 spaces
-  return lastUrlSegment
+  return decodeURIComponent(lastUrlSegment)
     .replaceAll(/[\W_]/gi, " ")
     .split(" ")
     .filter((v) => !!v)
