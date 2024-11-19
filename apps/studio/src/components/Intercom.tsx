718f38c734f56e764e7ecd99a4765a76caeaf99e
adriangohjw <adriangohjw@gmail.com>
2024-11-20 01:49:51 +0800

    use preferred name first

--- apps/studio/src/components/Intercom.tsx
+++ apps/studio/src/components/Intercom.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react"
 import { Intercom as IntercomSDK } from "@intercom/messenger-js-sdk"
 
 import { env } from "~/env.mjs"
@@ -10,11 +11,15 @@ const convertDateToUnixTimestamp = (date: Date): number => {
 export const Intercom = () => {
   const { me } = useMe()
 
+  const getIntercomName = useCallback(() => {
+    return me.preferredName || me.name || me.email.split("@")[0]
+  }, [me])
+
   if (env.NEXT_PUBLIC_INTERCOM_APP_ID) {
     IntercomSDK({
       app_id: env.NEXT_PUBLIC_INTERCOM_APP_ID,
       user_id: me.id,
-      name: me.name,
+      name: getIntercomName(),
       email: me.email,
       created_at: convertDateToUnixTimestamp(me.createdAt),
     })
