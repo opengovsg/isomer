55323edad3589b8b40ea759a91e9510b943f088d
adriangohjw <adriangohjw@gmail.com>
2024-11-27 20:12:17 +0800

    fix storybook

--- packages/components/src/templates/next/components/complex/DynamicStatistics/DynamicStatistics.stories.tsx
+++ packages/components/src/templates/next/components/complex/DynamicStatistics/DynamicStatistics.stories.tsx
@@ -5,35 +5,6 @@ import { DynamicStatisticsUI } from "./DynamicStatistics"
 const meta: Meta<typeof DynamicStatisticsUI> = {
   title: "Next/Components/DynamicStatistics",
   component: DynamicStatisticsUI,
-  args: {
-    site: {
-      siteName: "Isomer Next",
-      siteMap: {
-        id: "1",
-        title: "Home",
-        permalink: "/",
-        lastModified: "",
-        layout: "homepage",
-        summary: "",
-        children: [],
-      },
-      theme: "isomer-next",
-      isGovernment: true,
-      logoUrl: "https://www.isomer.gov.sg/images/isomer-logo.svg",
-      lastUpdated: "2021-10-01",
-      assetsBaseUrl: "https://cms.isomer.gov.sg",
-      navBarItems: [],
-      footerItems: {
-        privacyStatementLink: "https://www.isomer.gov.sg/privacy",
-        termsOfUseLink: "https://www.isomer.gov.sg/terms",
-        siteNavItems: [],
-      },
-      search: {
-        type: "localSearch",
-        searchUrl: "/search",
-      },
-    },
-  },
 }
 
 export default meta
