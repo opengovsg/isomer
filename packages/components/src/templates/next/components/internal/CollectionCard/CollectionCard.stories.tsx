bfa6fe40471ce92104e37d3a09c884f2da1188b8
seaerchin <jiachin@open.gov.sg>
2024-12-05 15:46:29 +0800

    chore: remove stories

--- packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
+++ packages/components/src/templates/next/components/internal/CollectionCard/CollectionCard.stories.tsx
@@ -86,8 +86,6 @@ export const TagsWithImage: Story = {
     title: "Collection card with tags",
     description: "This is a random description that will be on the card",
     tags: [
-      { category: "tagged", values: ["tagged"], selected: ["tagged"] },
-      { category: "tag", values: ["A tag"], selected: ["A tag"] },
       {
         category: "long",
         values: [
@@ -107,17 +105,6 @@ export const TagsWithoutImage: Story = {
     withoutImage: true,
     description: "This is a random description that will be on the card",
     tags: [
-      { category: "tag", values: ["A tag"], selected: ["A tag"] },
-      { category: "tagged", values: ["tagged"], selected: ["tagged"] },
-      {
-        category: "long",
-        values: [
-          "This is a very long tag that should be reflowed on smaller screens maybe",
-        ],
-        selected: [
-          "This is a very long tag that shuold be reflowed on smaller screens maybe",
-        ],
-      },
       {
         category: "very long",
         values: [
