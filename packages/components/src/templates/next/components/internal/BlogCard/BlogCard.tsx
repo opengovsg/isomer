db0ea7dacb76d15fc6275f3cdd5d5292e7ef480f
seaerchin <jiachin@open.gov.sg>
2024-12-10 11:56:19 +0800

    feat: add blog card

---
+++ packages/components/src/templates/next/components/internal/BlogCard/BlogCard.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { Text } from "react-aria-components"
+
+import type { CollectionCardProps } from "~/interfaces"
+import type { CollectionPageSchemaType } from "~/types"
+import { tv } from "~/lib/tv"
+import { focusVisibleHighlight, getFormattedDate } from "~/utils"
+import { ImageClient } from "../../complex/Image"
+import { Link } from "../Link"
+import { Tag } from "../Tag"
+
+const collectionCardLinkStyle = tv({
+  extend: focusVisibleHighlight,
+  base: "prose-title-md-semibold line-clamp-3 w-fit underline-offset-4 hover:underline",
+})
+
+export const BlogCard = ({
+  LinkComponent,
+  lastUpdated,
+  description,
+  category,
+  image,
+  referenceLinkHref,
+  imageSrc,
+  itemTitle,
+  siteAssetsBaseUrl,
+  shouldShowDate = true,
+  tags = [],
+}: CollectionCardProps & {
+  shouldShowDate?: boolean
+  siteAssetsBaseUrl: string | undefined
+  LinkComponent: CollectionPageSchemaType["LinkComponent"]
+}): JSX.Element => {
+  return (
+    <div className="flex flex-1 flex-col gap-3 py-5">
+      {
+        <div className="relative mb-3 h-40 min-h-40 w-full shrink-0">
+          {image ? (
+            <ImageClient
+              src={imageSrc || ""}
+              alt={image?.alt ?? ""}
+              width="100%"
+              className="absolute left-0 h-full w-full rounded object-cover"
+              assetsBaseUrl={siteAssetsBaseUrl}
+            />
+          ) : null}
+        </div>
+      }
+      {shouldShowDate && (
+        <Text className="prose-label-md-regular shrink-0 text-base-content-subtle">
+          {lastUpdated ? getFormattedDate(lastUpdated) : "-"}
+        </Text>
+      )}
+      <div className="flex flex-grow flex-col gap-3 text-base-content">
+        <h3 className="inline-block">
+          <Link
+            LinkComponent={LinkComponent}
+            href={referenceLinkHref}
+            className={collectionCardLinkStyle()}
+          >
+            <span title={itemTitle}>{itemTitle}</span>
+          </Link>
+        </h3>
+        {tags && tags.length > 0 && (
+          <div className="-mt-1">
+            {tags.flatMap(({ category, selected: labels }) => {
+              return (
+                <div className="flex w-full flex-wrap items-center gap-2">
+                  <p className="prose-label-sm">{category}</p>
+                  {labels.map((label) => {
+                    return <Tag>{label}</Tag>
+                  })}
+                </div>
+              )
+            })}
+          </div>
+        )}
+        {description && (
+          <Text className="prose-body-base line-clamp-3" title={description}>
+            {description}
+          </Text>
+        )}
+        {/* TODO: Feature enhancement? Filter by category when clicked */}
+        <Text className="prose-label-sm-medium text-base-content-subtle">
+          {category}
+        </Text>
+      </div>
+    </div>
+  )
+}
