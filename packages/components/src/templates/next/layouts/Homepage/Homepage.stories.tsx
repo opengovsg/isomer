d8e5097c1e493f9555ebac6072d6e38dedb6a5d4
Adrian Goh Jun Wei <adriangohjw@gmail.com>
2024-12-11 10:36:15 +0800

    isom-1678 dynamic blocks for muis fix (#922)
    * update CSP to use env.NEXT_PUBLIC_S3_ASSETS_DOMAIN_NAME
    
    * fix: replace useQuery with useState and useEffect
    
    * npm run format:fix
    
    * add error state
    
    * add errorMessage to schema
    
    * add error state UI + use twVariant
    
    * update stories
    
    * fix - separate error and loading state
    
    * add errorMessage to studio default block
    
    * format
    
    * update preview-tw
    
    * fix - screen wide component

--- packages/components/src/templates/next/layouts/Homepage/Homepage.stories.tsx
+++ packages/components/src/templates/next/layouts/Homepage/Homepage.stories.tsx
@@ -259,6 +259,25 @@ export const Default: Story = {
         ],
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         label: "View all dates",
+        errorMessage: [
+          {
+            text: "Not seeing the prayer times? ",
+            type: "text",
+          },
+          {
+            text: "Report an issue",
+            type: "text",
+            marks: [
+              { type: "bold" },
+              {
+                type: "link",
+                attrs: {
+                  href: "https://www.form.gov.sg/some-link",
+                },
+              },
+            ],
+          },
+        ],
       },
       {
         type: "infobar",
