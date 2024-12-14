d43e1e4cc795b6912c97ac804ea7934dd8b6c846
adriangohjw <adriangohjw@gmail.com>
2024-12-14 08:30:48 +0800

    allow generating of required prose schema

--- packages/components/src/interfaces/native/Prose.ts
+++ packages/components/src/interfaces/native/Prose.ts
@@ -9,38 +9,42 @@ import { ParagraphSchema } from "./Paragraph"
 import { TableSchema } from "./Table"
 import { UnorderedListSchema } from "./UnorderedList"
 
+const PROSE_CONTENT_SCHEMA = Type.Array(
+  Type.Union([
+    Type.Ref(DividerSchema),
+    Type.Ref(HeadingSchema),
+    Type.Ref(OrderedListSchema),
+    Type.Ref(ParagraphSchema),
+    Type.Ref(TableSchema),
+    Type.Ref(UnorderedListSchema),
+  ]),
+  {
+    title: "Content",
+    minItems: 1,
+  },
+)
+
 const BASE_PROSE_META = {
   title: "Content",
 }
 
-type ComponentsWithProse = "prose" | "accordion" | "callout"
+type ComponentsWithProse = "prose" | "accordion" | "callout" | "contentpic"
 
 const generateProseSchema = ({
   id,
   format = "prose",
+  isRequired = false,
 }: {
   id?: string
   format?: ComponentsWithProse
+  isRequired?: boolean
 }) => {
   return Type.Object(
     {
       type: Type.Literal("prose"),
-      content: Type.Optional(
-        Type.Array(
-          Type.Union([
-            Type.Ref(DividerSchema),
-            Type.Ref(HeadingSchema),
-            Type.Ref(OrderedListSchema),
-            Type.Ref(ParagraphSchema),
-            Type.Ref(TableSchema),
-            Type.Ref(UnorderedListSchema),
-          ]),
-          {
-            title: "Content",
-            minItems: 1,
-          },
-        ),
-      ),
+      content: isRequired
+        ? PROSE_CONTENT_SCHEMA
+        : Type.Optional(PROSE_CONTENT_SCHEMA),
     },
     {
       ...(id && { $id: id }),
@@ -56,9 +60,15 @@ export const ProseSchema = generateProseSchema({
 export const BaseProseSchema = generateProseSchema({})
 export const AccordionProseSchema = generateProseSchema({
   format: "accordion",
+  isRequired: true,
 })
 export const CalloutProseSchema = generateProseSchema({
   format: "callout",
+  isRequired: true,
+})
+export const ContentpicProseSchema = generateProseSchema({
+  format: "contentpic",
+  isRequired: true,
 })
 
 export type ProseProps = Static<typeof ProseSchema> & {
