cd999aa0def888cd4fb096781bd8a577469ca64b
seaerchin <jiachin@open.gov.sg>
2024-12-16 14:04:12 +0800

    chore: add migration for collection meta

--- apps/studio/prisma/generated/generatedEnums.ts
+++ apps/studio/prisma/generated/generatedEnums.ts
@@ -1,22 +1,23 @@
 export const ResourceState = {
-  Draft: "Draft",
-  Published: "Published",
-} as const
-export type ResourceState = (typeof ResourceState)[keyof typeof ResourceState]
+    Draft: "Draft",
+    Published: "Published"
+} as const;
+export type ResourceState = (typeof ResourceState)[keyof typeof ResourceState];
 export const ResourceType = {
-  RootPage: "RootPage",
-  Page: "Page",
-  Folder: "Folder",
-  Collection: "Collection",
-  CollectionLink: "CollectionLink",
-  CollectionPage: "CollectionPage",
-  IndexPage: "IndexPage",
-  FolderMeta: "FolderMeta",
-} as const
-export type ResourceType = (typeof ResourceType)[keyof typeof ResourceType]
+    RootPage: "RootPage",
+    Page: "Page",
+    Folder: "Folder",
+    Collection: "Collection",
+    CollectionMeta: "CollectionMeta",
+    CollectionLink: "CollectionLink",
+    CollectionPage: "CollectionPage",
+    IndexPage: "IndexPage",
+    FolderMeta: "FolderMeta"
+} as const;
+export type ResourceType = (typeof ResourceType)[keyof typeof ResourceType];
 export const RoleType = {
-  Admin: "Admin",
-  Editor: "Editor",
-  Publisher: "Publisher",
-} as const
-export type RoleType = (typeof RoleType)[keyof typeof RoleType]
+    Admin: "Admin",
+    Editor: "Editor",
+    Publisher: "Publisher"
+} as const;
+export type RoleType = (typeof RoleType)[keyof typeof RoleType];
