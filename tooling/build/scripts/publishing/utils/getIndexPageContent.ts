5a0713b397d28815b77b27934d2bf0f0a546e827
seaerchin <44049504+seaerchin@users.noreply.github.com>
2024-12-20 11:29:34 +0800

    feat(multi-tags): add filtering to collections (#916)
    ## Problem
    This PR adds filtering to collections through the use of tags. 
    
    ## Solution
    1. add `Tags` to the schema on `CollectionPagePageSchema`
        - note that in our publishing script, we explictly check for whether it's a `PAGE_RESOURCE_TYPE`, which means that even though the schema can only expose it on page items. we might want to expose it on files, which would necessitate a slight change inhow we generate hte `SitemapEntries`. not too sure if this is what we want, so i omitted it for now. need to think abit more here
    2. add tags to `publishing/index.ts` so that it gets written to the sitemap entry (and hence, to disk)
    3. in `Collection`, extract the `tags` from the `Sitemap`. add filtering logic onto our collection so that it will display the correct cards on filter click. take note that the logic here can be (and should be) reused for collection but not for years due to how the comparison is made
        - i didn't do it for collections because the id and the check is a lower-cased check via a hard-coded id + the property is different (takes from `item.category` and not `item.tags`  
    
    ## Videos
    
    https://github.com/user-attachments/assets/db0c6179-e18d-497a-ac2f-38f683a4286c

--- tooling/build/scripts/publishing/utils/getIndexPageContent.ts
+++ tooling/build/scripts/publishing/utils/getIndexPageContent.ts
@@ -1,9 +1,21 @@
 const ISOMER_SCHEMA_VERSION = "0.1.0"
 
-// Generate the index page content for a given directory
-export const getIndexPageContents = (title: string) => ({
+// Generate the index page content for a given folder
+export const getFolderIndexPageContents = (title: string) => ({
   version: ISOMER_SCHEMA_VERSION,
   layout: "index",
+  page: {
+    title,
+    contentPageHeader: {
+      summary: `Pages in ${title}`,
+    },
+  },
+  content: [],
+})
+
+export const getCollectionIndexPageContents = (title: string) => ({
+  version: ISOMER_SCHEMA_VERSION,
+  layout: "collection",
   page: {
     title,
     contentPageHeader: {
