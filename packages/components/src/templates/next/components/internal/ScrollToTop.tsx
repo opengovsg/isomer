88d74c09db2e96fa6c7490c42b2a9c2f21bc52c6
adriangohjw <adriangohjw@gmail.com>
2025-01-13 17:22:56 +0800

    fix

--- packages/components/src/templates/next/components/internal/ScrollToTop.tsx
+++ packages/components/src/templates/next/components/internal/ScrollToTop.tsx
@@ -1,22 +1,19 @@
 "use client"
 
 import { useEffect } from "react"
-import { usePathname } from "next/navigation"
 
 /*
+  NOTE: This is a temporary fix until we have a better solution.
+
   Fix with issue w/ "next/Link" not scrolling to the top when clicking on <Link />
   if we have a header with "sticky" that's not in its sticky position.
   Ref: https://github.com/vercel/next.js/issues/45187#issuecomment-1639518030
 */
 export const ScrollToTop = () => {
-  const pathname = usePathname()
-
   useEffect(() => {
-    // Required because "window" is a browser-only API that's not available
-    // during static site generation (SSG)
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (window) window.scroll(0, 0)
-  }, [pathname])
+    if (window) window.scrollTo(0, 0)
+  }, [])
 
   return <></>
 }
