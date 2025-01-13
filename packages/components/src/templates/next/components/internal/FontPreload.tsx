64776fb765d8be41c2a1a96be30c8e39dd5c72fb
adriangohjw <adriangohjw@gmail.com>
2025-01-13 17:46:35 +0800

    Refactor FontPreload and GoogleTagManagerPreload components to use PreloadHelper for improved reusability and cleaner code.

--- packages/components/src/templates/next/components/internal/FontPreload.tsx
+++ packages/components/src/templates/next/components/internal/FontPreload.tsx
@@ -1,22 +1,5 @@
-// The following links are included to improve SEO and Lighthouse performance by optimizing font loading.
-// Preconnecting to fonts.gstatic.com allows the browser to establish a connection early, reducing latency
-// when fetching fonts, which can lead to faster rendering and improved user experience.
+import { PreloadHelper } from "./utils"
 
 export const FontPreload = () => {
-  return (
-    <>
-      <link
-        // Establish an early connection to fonts.gstatic.com
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous" // required when making requests to a different origin
-      />
-      <link
-        // Serve as a fallback for browsers that don't support preconnect
-        // at least allowing early DNS resolution
-        rel="dns-prefetch"
-        href="https://fonts.gstatic.com"
-      />
-    </>
-  )
+  return <PreloadHelper href="https://fonts.gstatic.com" />
 }
--- packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerPreload.tsx
+++ packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerPreload.tsx
@@ -1,18 +1,5 @@
+import { PreloadHelper } from "../utils"
+
 export const GoogleTagManagerPreload = () => {
-  return (
-    <>
-      <link
-        // Establish an early connection
-        rel="preconnect"
-        href="https://www.googletagmanager.com"
-        crossOrigin="anonymous" // required when making requests to a different origin
-      />
-      <link
-        // Serve as a fallback for browsers that don't support preconnect
-        // at least allowing early DNS resolution
-        rel="dns-prefetch"
-        href="https://www.googletagmanager.com"
-      />
-    </>
-  )
+  return <PreloadHelper href="https://www.googletagmanager.com" />
 }
