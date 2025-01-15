6d549ecbe209ffe179b45f4a8dd546ba791391df
Harish V <harish@open.gov.sg>
2025-01-15 15:29:46 +0800

    fix pr comments

--- apps/studio/src/server/modules/user/__tests__/user.service.test.ts
+++ apps/studio/src/server/modules/user/__tests__/user.service.test.ts
@@ -1,35 +1,25 @@
 import { resetTables } from "tests/integration/helpers/db"
-import { setupUser, setUpWhitelist } from "tests/integration/helpers/seed"
+import { setupUser } from "tests/integration/helpers/seed"
 
 import { isUserDeleted } from "../user.service"
 
 describe("user.service", () => {
   beforeAll(async () => {
     await resetTables("User")
-    await setUpWhitelist({ email: "@example.com" })
-
-    // Setup active user
-    await setupUser({
-      name: "Active User",
-      userId: "active123",
-      email: "active@example.com",
-      phone: "12345678",
-      isDeleted: false,
-    })
-
-    // Setup deleted user
-    await setupUser({
-      name: "Deleted User",
-      userId: "deleted123",
-      email: "deleted@example.com",
-      phone: "12345678",
-      isDeleted: true,
-    })
   })
 
   it("should return false if user is not deleted", async () => {
     // Arrange
     const email = "active@example.com"
+    // Setup active user
+    await setupUser({
+      name: "Active User",
+      userId: "active123",
+      email: email,
+      phone: "12345678",
+      isDeleted: false,
+    })
+
     // Act
     const result = await isUserDeleted(email)
     // Assert
@@ -39,6 +29,15 @@ describe("user.service", () => {
   it("should return true if user is deleted", async () => {
     // Arrange
     const email = "deleted@example.com"
+    // Setup deleted user
+    await setupUser({
+      name: "Deleted User",
+      userId: "deleted123",
+      email: email,
+      phone: "12345678",
+      isDeleted: true,
+    })
+
     // Act
     const result = await isUserDeleted(email)
     // Assert
