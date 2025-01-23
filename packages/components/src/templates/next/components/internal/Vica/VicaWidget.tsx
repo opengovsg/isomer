96921d1d63a6323ad0cc7dc39463a25d26606196
adriangohjw <adriangohjw@gmail.com>
2025-01-23 15:13:49 +0800

    split into 2 files

--- packages/components/src/templates/next/components/internal/Vica/VicaWidget.tsx
+++ packages/components/src/templates/next/components/internal/Vica/VicaWidget.tsx
@@ -1,17 +1,5 @@
 import type { VicaProps } from "~/interfaces"
 
-export const VicaStylesheet = () => {
-  return (
-    <>
-      <link
-        href="https://webchat.vica.gov.sg/static/css/chat.css"
-        referrerPolicy="origin"
-        rel="stylesheet"
-      />
-    </>
-  )
-}
-
 export const VicaWidget = ({
   ScriptComponent = "script",
   ...props
