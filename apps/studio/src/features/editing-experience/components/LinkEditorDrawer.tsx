adf500dfa304064d4ad7b08903ff0dcb5961d046
dcshzj <27919917+dcshzj@users.noreply.github.com>
2025-01-23 16:43:36 +0800

    fix: invalidate publish button on link save

--- apps/studio/src/features/editing-experience/components/LinkEditorDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/LinkEditorDrawer.tsx
@@ -186,6 +186,7 @@ export const LinkEditorDrawer = () => {
     trpc.collection.updateCollectionLink.useMutation({
       onSuccess: () => {
         void utils.collection.readCollectionLink.invalidate()
+        void utils.page.readPage.invalidate()
         toast({
           title: "Link updated!",
           status: "success",
