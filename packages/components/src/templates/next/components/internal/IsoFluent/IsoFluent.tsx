147a52d755542b3fafa3882e5af78673e2bffad2
Harish V <harish@open.gov.sg>
2025-01-29 23:14:38 +0800

    enhance script load

--- packages/components/src/templates/next/components/internal/IsoFluent/IsoFluent.tsx
+++ packages/components/src/templates/next/components/internal/IsoFluent/IsoFluent.tsx
@@ -1,7 +1,16 @@
+"use client"
+
 import type { IsoFluentProps } from "~/interfaces"
 
 export const IsoFluent = ({ ScriptComponent = "script" }: IsoFluentProps) => {
+  // to not render during static site generation on the server
+  if (typeof window === "undefined") return null
   return (
-    <ScriptComponent src="https://isofluent-translate.hack2025.gov.sg/static/widget.iife.js" />
+    <ScriptComponent
+      async
+      type="text/javascript"
+      src="https://isofluent-translate.hack2025.gov.sg/static/widget.iife.js"
+      strategy="lazyOnload"
+    />
   )
 }
