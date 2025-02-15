320d4201582e2281732bb870130bc5dc923eac12
seaerchin <jiachin@open.gov.sg>
2025-02-15 21:19:41 +0800

    chore: add more tests

--- apps/studio/src/server/modules/folder/__tests__/folder.router.test.ts
+++ apps/studio/src/server/modules/folder/__tests__/folder.router.test.ts
@@ -346,7 +346,7 @@ describe("folder.router", async () => {
       )
     })
 
-    it("should throw 403 if user does not have write access to the site", async () => {
+    it("should throw 403 if user does not write access to the site", async () => {
       // Arrange
       const { folder, site } = await setupFolder()
 
@@ -365,6 +365,279 @@ describe("folder.router", async () => {
         }),
       )
     })
+
+    it("should return 200 ", async () => {
+      // Arrange
+      const { folder, site } = await setupFolder()
+      await setupAdminPermissions({ userId: session.userId, siteId: site.id })
+
+      // Act
+      const result = await caller.getMetadata({
+        siteId: site.id,
+        resourceId: Number(folder.id),
+      })
+
+      // Assert
+      const expected = await db
+        .selectFrom("Resource")
+        .select(["Resource.title", "Resource.permalink", "Resource.parentId"])
+        .where("id", "=", folder.id)
+        .executeTakeFirst()
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe("editFolder", () => {
+    afterEach(async () => {
+      clearPermissions()
+    })
+
+    it("should throw 401 if not logged in", async () => {
+      // Act
+      const result = unauthedCaller.editFolder({
+        folderTitle: "test folder",
+        siteId: "1",
+        permalink: "test-folder",
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({ code: "UNAUTHORIZED" }),
+      )
+    })
+
+    it("should throw 409 if permalink already exists", async () => {
+      // Arrange
+      const duplicatePermalink = "duplicate-permalink"
+      const { site } = await setupFolder({ permalink: duplicatePermalink })
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: duplicatePermalink,
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "CONFLICT",
+          message: "A resource with the same permalink already exists",
+        }),
+      )
+    })
+
+    it("should throw 404 if `siteId` does not exist", async () => {
+      // Arrange
+      const invalidSiteId = 999
+      const { site } = await setupSite()
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+      expect(site.id).not.toEqual(invalidSiteId)
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: invalidSiteId,
+        permalink: "test-folder",
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "NOT_FOUND",
+          message:
+            "You do not have sufficient permissions to perform this action",
+        }),
+      )
+    })
+
+    it("should throw 404 if `parentFolderId` does not exist", async () => {
+      // Arrange
+      const { site } = await setupSite()
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: "test-folder",
+        parentFolderId: 999,
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "NOT_FOUND",
+          message: "Parent folder does not exist",
+        }),
+      )
+    })
+
+    it("should throw 400 if `parentFolderId` is not a folder", async () => {
+      // Arrange
+      const { site, page } = await setupPageResource({
+        resourceType: "Page",
+      })
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: "test-folder",
+        parentFolderId: Number(page.id),
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Resource ID does not point to a folder",
+        }),
+      )
+    })
+
+    it("should editFolder a folder even with duplicate permalink if `siteId` is different", async () => {
+      // Arrange
+      const duplicatePermalink = "duplicate-permalink"
+      const { site: _firstSite } = await setupFolder({
+        permalink: duplicatePermalink,
+      })
+      const { site: secondSite } = await setupSite()
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: secondSite.id,
+      })
+
+      // Act
+      const result = await caller.editFolder({
+        folderTitle: "test folder",
+        siteId: secondSite.id,
+        permalink: duplicatePermalink,
+      })
+
+      // Assert
+      const actualFolder = await getFolderWithPermalink({
+        siteId: secondSite.id,
+        permalink: duplicatePermalink,
+      })
+      expect(result).toEqual({ folderId: actualFolder.id })
+    })
+
+    it("should editFolder a folder", async () => {
+      // Arrange
+      const permalinkToUse = "test-folder-999"
+      const { site } = await setupSite()
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+
+      // Act
+      const result = await caller.editFolder({
+        folderTitle: "test folder 999",
+        siteId: site.id,
+        permalink: permalinkToUse,
+      })
+
+      // Assert
+      const actualFolder = await getFolderWithPermalink({
+        permalink: permalinkToUse,
+        siteId: site.id,
+      })
+      expect(result).toEqual({ folderId: actualFolder.id })
+    })
+
+    it("should editFolder a nested folder if `parentFolderId` is provided", async () => {
+      // Arrange
+      const permalinkToUse = "test-folder-777"
+      const { folder: parentFolder, site } = await setupFolder()
+      await setupAdminPermissions({
+        userId: session.userId,
+        siteId: site.id,
+      })
+
+      // Act
+      const result = await caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: permalinkToUse,
+        parentFolderId: Number(parentFolder.id),
+      })
+
+      // Assert
+      const actualFolder = await getFolderWithPermalink({
+        permalink: permalinkToUse,
+        siteId: site.id,
+      })
+      expect(actualFolder.parentId).toEqual(parentFolder.id)
+      expect(result).toEqual({ folderId: actualFolder.id })
+    })
+
+    it("should throw 403 if user does not have access to the site", async () => {
+      // Arrange
+      const permalinkToUse = "test-folder-777"
+      const { site } = await setupSite()
+      await setupPermissions({
+        userId: session.userId,
+        siteId: site.id,
+        role: "Editor",
+      })
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: permalinkToUse,
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "NOT_FOUND",
+          message:
+            "You do not have sufficient permissions to perform this action",
+        }),
+      )
+    })
+
+    it("should throw 403 if user does not have admin access to the site and tries to editFolder a root level folder", async () => {
+      // Arrange
+      const permalinkToUse = "test-folder-777"
+      const { folder: parentFolder, site } = await setupFolder()
+
+      // Act
+      const result = caller.editFolder({
+        folderTitle: "test folder",
+        siteId: site.id,
+        permalink: permalinkToUse,
+        parentFolderId: Number(parentFolder.id),
+      })
+
+      // Assert
+      await expect(result).rejects.toThrowError(
+        new TRPCError({
+          code: "NOT_FOUND",
+          message:
+            "You do not have sufficient permissions to perform this action",
+        }),
+      )
+    })
+
+    it.skip("should throw 403 if user does not have write access to the parent folder", async () => {})
   })
 })
 
