6ab05c5cdd3d4ffa64cf7156ac170d0621801b09
seaerchin <jiachin@open.gov.sg>
2025-02-23 12:40:28 +0800

    chore: add resetting of error boundary

--- apps/studio/src/components/ErrorBoundary/DefaultTrpcError.tsx
+++ apps/studio/src/components/ErrorBoundary/DefaultTrpcError.tsx
@@ -1,32 +1,37 @@
-import { useEffect } from "react"
 import { useRouter } from "next/router"
 import { type TRPC_ERROR_CODE_KEY } from "@trpc/server/rpc"
+import { FallbackProps } from "react-error-boundary"
 
 import { trpc } from "~/utils/trpc"
+import { FullscreenSpinner } from "../FullscreenSpinner"
 import { DefaultNotFound } from "./DefaultNotFound"
 import { DefaultServerError } from "./DefaultServerError"
 import { UnexpectedErrorCard } from "./UnexpectedErrorCard"
 
-export const UnauthorizedError = () => {
+export const UnauthorizedError = ({
+  resetErrorBoundary,
+}: Pick<FallbackProps, "resetErrorBoundary">) => {
   const utils = trpc.useUtils()
   const router = useRouter()
-  useEffect(() => {
-    void utils.invalidate()
-    void router.push("/")
-  }, [utils, router])
+  void utils.invalidate()
+  void router.push("/")
+  void resetErrorBoundary()
 
-  return null
+  return <FullscreenSpinner />
 }
 
 // TODO: Make custom components for these
-export function DefaultTrpcError({ code }: { code: TRPC_ERROR_CODE_KEY }) {
+export function DefaultTrpcError({
+  code,
+  resetErrorBoundary,
+}: { code: TRPC_ERROR_CODE_KEY } & Pick<FallbackProps, "resetErrorBoundary">) {
   switch (code) {
     case "NOT_FOUND":
       return <DefaultNotFound />
 
     case "UNAUTHORIZED":
       // TODO: add the default error boundary for perms here
-      return <UnauthorizedError />
+      return <UnauthorizedError resetErrorBoundary={resetErrorBoundary} />
 
     case "TIMEOUT":
     case "INTERNAL_SERVER_ERROR":
