74dbae48cb5c9cdaa90f21b67ca3ec4a9e28ee3c
adriangohjw <adriangohjw@gmail.com>
2025-02-28 06:41:20 +0800

    fix: Refine DynamicDataBanner text and layout styling

--- packages/components/src/templates/next/components/complex/DynamicDataBanner/DynamicDataBannerClient.tsx
+++ packages/components/src/templates/next/components/complex/DynamicDataBanner/DynamicDataBannerClient.tsx
@@ -19,17 +19,17 @@ const createDynamicDataBannerStyles = tv({
     outerContainer: `${ComponentContent} md:gap-auto flex flex-col gap-6 p-6 md:flex-row md:items-center md:justify-between md:px-10 md:py-5`,
     basicInfoContainer:
       "flex flex-col items-center gap-0.5 md:items-start md:gap-1",
-    title: "text-base-content-default prose-headline-lg-semibold",
+    title: "prose-headline-lg-semibold whitespace-nowrap text-base-content",
     dateAndUrlContainer: "align-center flex justify-center gap-2",
-    date: "prose-body-sm text-base-content-medium",
-    url: "hover:text-link-hove prose-label-md-regular text-link underline-offset-4 visited:text-link-visited hover:underline",
+    date: "prose-body-sm whitespace-nowrap text-base-content-medium",
+    url: "prose-label-md-regular text-link underline-offset-4 visited:text-link-visited hover:text-link-hover hover:underline",
     dataInfoContainer:
-      "md:col-gap-10 md:gap-6lg:flex grid grid-cols-3 justify-items-center gap-y-4 md:grid-cols-[auto,1fr,auto] md:justify-items-end md:gap-x-6 md:gap-y-2 lg:flex lg:gap-11",
+      "md:col-gap-10 grid grid-cols-3 justify-items-center gap-y-4 md:grid-cols-[auto,1fr,auto] md:justify-items-end md:gap-6 md:gap-x-6 md:gap-y-2 lg:flex lg:gap-11",
     errorMessageContainer: `${ComponentContent} flex flex-row gap-2 px-6 py-3 md:items-center md:gap-1`,
     errorIcon: "h-full min-h-4 min-w-4",
     individualDataContainer:
       "flex w-fit flex-col items-center justify-center gap-0.5 md:flex-row md:gap-1.5 lg:flex-col lg:items-end",
-    individualDataLabel: "text-base-content-default prose-headline-base-medium",
+    individualDataLabel: "prose-headline-base-medium text-base-content",
     individualDataValue:
       "prose-headline-lg-semibold text-brand-interaction-hover",
     individualDataValueLoading:
