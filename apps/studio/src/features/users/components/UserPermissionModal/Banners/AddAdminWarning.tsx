6dcc6d8c2a1ecb3d15998631f316e32ccc8a709e
adriangohjw <adriangohjw@gmail.com>
2025-03-04 15:44:47 +0800

    refactor: Simplify warning banners with arrow function syntax

--- apps/studio/src/features/users/components/UserPermissionModal/Banners/AddAdminWarning.tsx
+++ apps/studio/src/features/users/components/UserPermissionModal/Banners/AddAdminWarning.tsx
@@ -1,18 +1,16 @@
 import { Infobox } from "@opengovsg/design-system-react"
 
-export const AddAdminWarning = () => {
-  return (
-    <Infobox
-      textStyle="body-2"
-      size="sm"
-      borderWidth="1px"
-      borderRadius="md"
-      variant="warning"
-      w="100%"
-      border="none"
-    >
-      You are adding a new admin to the website. An admin can make any change to
-      the site content, settings, and users.s admin. Select another role.
-    </Infobox>
-  )
-}
+export const AddAdminWarning = () => (
+  <Infobox
+    textStyle="body-2"
+    size="sm"
+    borderWidth="1px"
+    borderRadius="md"
+    variant="warning"
+    w="100%"
+    border="none"
+  >
+    You are adding a new admin to the website. An admin can make any change to
+    the site content, settings, and users.s admin. Select another role.
+  </Infobox>
+)
