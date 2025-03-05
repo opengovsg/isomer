e216230ffeec6d3557f23a6cc6eddd29c7064bf9
adriangohjw <adriangohjw@gmail.com>
2025-03-05 10:17:15 +0800

    Enhance AddNewUserButton with Chakra UI ButtonProps

--- apps/studio/src/features/users/components/AddNewUserButton.tsx
+++ apps/studio/src/features/users/components/AddNewUserButton.tsx
@@ -1,14 +1,18 @@
+import type { ButtonProps } from "@chakra-ui/react"
 import { useContext } from "react"
 import { Button, Tooltip } from "@chakra-ui/react"
 import { BiPlus } from "react-icons/bi"
 
 import { UserManagementContext } from "~/features/users"
 
-interface AddNewUserButtonProps {
+interface AddNewUserButtonProps extends Omit<ButtonProps, "onClick"> {
   siteId: number
 }
 
-export const AddNewUserButton = ({ siteId }: AddNewUserButtonProps) => {
+export const AddNewUserButton = ({
+  siteId,
+  ...buttonProps
+}: AddNewUserButtonProps) => {
   const ability = useContext(UserManagementContext)
   const canManageUsers = ability.can("manage", "UserManagement")
 
@@ -18,6 +22,7 @@ export const AddNewUserButton = ({ siteId }: AddNewUserButtonProps) => {
       leftIcon={<BiPlus />}
       onClick={() => console.log(`TODO: add new user for site ${siteId}`)}
       isDisabled={!canManageUsers}
+      {...buttonProps}
     >
       Add new user
     </Button>
