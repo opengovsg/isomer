3707ddc14dc600a9686c7bd4d2ee99613cf5530a
seaerchin <jiachin@open.gov.sg>
2025-03-12 12:58:50 +0800

    chore: fix typings

--- apps/studio/prisma/types.ts
+++ apps/studio/prisma/types.ts
@@ -34,9 +34,9 @@ declare global {
       _IsomerSiteWideComponentsProps["footerItems"],
       "JSONB"
     >
-    type AuditLogDeltaJsonContent = Tagged<
-      { before: Record<string, unknown>; after: Record<string, unknown> },
-      "JSONB"
-    >
+    interface AuditLogDeltaJsonContent {
+      before: Record<string, unknown> | null
+      after: Record<string, unknown> | null
+    }
   }
 }
