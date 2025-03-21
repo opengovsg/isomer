2a81cabb9a88eaaac5524cf8c0b7a8239fd740c7
seaerchin <jiachin@open.gov.sg>
2025-03-21 15:24:09 +0800

    chore: ask consumers to pass in tx
    we want to do this version incrementing as part of a larger tx as we
    want to also preserve audit logs

--- apps/studio/src/server/modules/version/version.service.ts
+++ apps/studio/src/server/modules/version/version.service.ts
@@ -3,7 +3,7 @@ import { TRPCError } from "@trpc/server"
 import { ResourceState } from "~prisma/generated/generatedEnums"
 import { type DB } from "~prisma/generated/generatedTypes"
 
-import type { SafeKysely } from "../database"
+import type { SafeKysely, Transaction } from "../database"
 import { db } from "../database"
 import { getPageById, updatePageById } from "../resource/resource.service"
 
@@ -51,64 +51,59 @@ export const incrementVersion = async ({
   siteId,
   resourceId,
   userId,
+  tx,
 }: {
   siteId: number
+  tx: Transaction<DB>
   resourceId: string
   userId: string
 }) => {
-  return await db
-    .transaction()
-    // serialize to prevent discrepancies from
-    // concurrent publishes from other users
-    .setIsolationLevel("serializable")
-    .execute(async (tx) => {
-      const page = await getPageById(tx, {
-        siteId,
-        resourceId: Number(resourceId),
-      })
-      if (!page) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Page not found",
-        })
-      }
-
-      if (!page.draftBlobId) {
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "No drafts to publish for this page",
-        })
-      }
-
-      let newVersionNum = 1
-      if (page.publishedVersionId) {
-        const currentVersion = await getVersionById({
-          versionId: page.publishedVersionId,
-        })
-        newVersionNum = Number(currentVersion.versionNum) + 1
-      }
-
-      // Create the new version
-      const newVersion = await createVersion(tx, {
-        versionNum: newVersionNum,
-        resourceId,
-        blobId: page.draftBlobId,
-        publisherId: userId,
-      })
-
-      // Update resource with new versionId and draft to be null
-      await updatePageById(
-        {
-          ...page,
-          id: parseInt(page.id),
-          publishedVersionId: newVersion.versionId,
-          draftBlobId: null,
-          state: ResourceState.Published,
-          siteId,
-          parentId: page.parentId ? parseInt(page.parentId) : undefined,
-        },
-        tx,
-      )
-      return { versionId: newVersion }
+  const page = await getPageById(tx, {
+    siteId,
+    resourceId: Number(resourceId),
+  })
+  if (!page) {
+    throw new TRPCError({
+      code: "NOT_FOUND",
+      message: "Page not found",
     })
+  }
+
+  if (!page.draftBlobId) {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: "No drafts to publish for this page",
+    })
+  }
+
+  let newVersionNum = 1
+  if (page.publishedVersionId) {
+    const currentVersion = await getVersionById({
+      versionId: page.publishedVersionId,
+    })
+    newVersionNum = Number(currentVersion.versionNum) + 1
+  }
+
+  // Create the new version
+  const newVersion = await createVersion(tx, {
+    versionNum: newVersionNum,
+    resourceId,
+    blobId: page.draftBlobId,
+    publisherId: userId,
+  })
+
+  // Update resource with new versionId and draft to be null
+  await updatePageById(
+    {
+      ...page,
+      id: parseInt(page.id),
+      publishedVersionId: newVersion.versionId,
+      draftBlobId: null,
+      state: ResourceState.Published,
+      siteId,
+      parentId: page.parentId ? parseInt(page.parentId) : undefined,
+    },
+    tx,
+  )
+  return newVersion
 }
