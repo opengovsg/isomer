aecb33f7e2077e5d16dd7bbce8a0cb1217afc386
adriangohjw <adriangohjw@gmail.com>
2025-03-26 03:56:51 +0800

    chore: increase sample runs in unlighthouse configuration for improved accuracy

--- tooling/benchmarking/unlighthouse/unlighthouse.config.ts
+++ tooling/benchmarking/unlighthouse/unlighthouse.config.ts
@@ -1,9 +1,5 @@
-// Note: maxConcurrency is set to 1 because we want to run the CI for each page one at a time for most accurate results
 export default {
-  puppeteerClusterOptions: {
-    maxConcurrency: 1,
-  },
   scanner: {
-    samples: 1,
+    samples: 3, // Run the CI 3 times for each page to improve accuracy
   },
 };
