332d41b0372219276da2b7a8f8836f28facfb32c
seaerchin <jiachin@open.gov.sg>
2025-03-29 04:22:08 +0800

    fix: update asset uploadig to await

--- apps/studio/src/features/editing-experience/components/form-builder/hooks/useAssetUpload.ts
+++ apps/studio/src/features/editing-experience/components/form-builder/hooks/useAssetUpload.ts
@@ -3,38 +3,6 @@ import { backOff } from "exponential-backoff"
 
 import { ASSETS_BASE_URL } from "~/utils/generateAssetUrl"
 
-const waitFor = (baseTimeoutMs = 500) => {
-  return new Promise((resolve) => setTimeout(resolve, baseTimeoutMs))
-}
-
-const retry = async (
-  promise: () => Promise<string>,
-  baseTimeoutMs: number,
-  maxRetries: number,
-) => {
-  const retryWithBackoff = async (retries: number) => {
-    try {
-      // Make sure we don't wait on the first attempt
-      if (retries > 0) {
-        const timeToWait = baseTimeoutMs * 2 ** retries
-        await waitFor(timeToWait)
-      }
-      const result = await promise()
-      return result
-    } catch (e) {
-      // NOTE: only retry if we didn't reach the limit
-      // otherwise, let the caller handle the error
-      if (retries < maxRetries) {
-        return retryWithBackoff(retries + 1)
-      } else {
-        throw e
-      }
-    }
-  }
-
-  return retryWithBackoff(0)
-}
-
 interface UseAssetUploadProps {
   retries?: number
   baseTimeoutMs?: number
@@ -48,7 +16,7 @@ export const useAssetUpload = ({
   const handleAssetUpload = async (src: string) => {
     setIsLoading(true)
     try {
-      const res = await retry(
+      const res = await backOff(
         async () => {
           const response = await fetch(`${ASSETS_BASE_URL}${src}`)
           if (!response.ok) {
@@ -56,8 +24,11 @@ export const useAssetUpload = ({
           }
           return src
         },
-        baseTimeoutMs,
-        retries,
+        {
+          startingDelay: baseTimeoutMs,
+          numOfAttempts: retries,
+          delayFirstAttempt: true,
+        },
       )
       setIsLoading(false)
       return res
