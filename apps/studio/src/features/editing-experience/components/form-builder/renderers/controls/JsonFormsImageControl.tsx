2d2558dac20e023804f1d3bef2e5c1dea1018e15
seaerchin <jiachin@open.gov.sg>
2025-04-03 13:23:18 +0800

    chore: remove unused imports

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsImageControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsImageControl.tsx
@@ -1,13 +1,8 @@
 import type { ControlProps, RankedTester } from "@jsonforms/core"
-import { Box, Flex, FormControl, Text } from "@chakra-ui/react"
+import { Box, FormControl } from "@chakra-ui/react"
 import { and, isStringControl, rankWith, schemaMatches } from "@jsonforms/core"
 import { withJsonFormsControlProps } from "@jsonforms/react"
-import {
-  FormErrorMessage,
-  FormLabel,
-  IconButton,
-} from "@opengovsg/design-system-react"
-import { BiTrash } from "react-icons/bi"
+import { FormErrorMessage, FormLabel } from "@opengovsg/design-system-react"
 import { z } from "zod"
 
 import { AttachmentData } from "~/components/AttachmentData"
