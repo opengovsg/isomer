4b6a47608f284802bc48df6c55febde85fed480e
seaerchin <jiachin@open.gov.sg>
2025-04-10 14:01:55 +0800

    chore: generated typings

--- apps/studio/prisma/generated/generatedTypes.ts
+++ apps/studio/prisma/generated/generatedTypes.ts
@@ -1,10 +1,15 @@
 import type { ColumnType, GeneratedAlways } from "kysely"
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>
+export type Timestamp = ColumnType<Date, Date | string, Date | string>
 
 import type {
-  AuditLogEvent,
   ResourceState,
   ResourceType,
   RoleType,
+  AuditLogEvent,
 } from "./generatedEnums"
 
 export type Generated<T> =
