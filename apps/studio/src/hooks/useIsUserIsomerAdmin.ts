a605dc3c0c815a5399ecad35f3c15d608c2e6f8e
adriangohjw <adriangohjw@gmail.com>
2025-04-14 18:00:05 +0800

    refactor: update Growthbook types for Isomer admin feature

--- apps/studio/src/hooks/useIsUserIsomerAdmin.ts
+++ apps/studio/src/hooks/useIsUserIsomerAdmin.ts
@@ -1,15 +1,8 @@
 import { useFeatureValue } from "@growthbook/growthbook-react"
 
+import type { GrowthbookIsomerAdminFeature } from "~/lib/growthbook"
 import { useMe } from "~/features/me/api"
 import { ISOMER_ADMIN_FEATURE_KEY } from "~/lib/growthbook"
-
-// Growthbook has a constraint in the typings that requires the index signature
-// of the object to be defined as a string instead of being specific to the keys
-// that we want. Hence, we have to define it as a type instead of an interface.
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type GrowthbookIsomerAdminFeature = {
-  users: string[]
-}
 
 export const useIsUserIsomerAdmin = () => {
   const {
