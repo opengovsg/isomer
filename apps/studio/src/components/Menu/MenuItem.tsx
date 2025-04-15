258a1d7d299905c81ed96cd41d03e9640251b392
adriangohjw <adriangohjw@gmail.com>
2025-04-15 17:59:01 +0800

    feat: add tooltip support to MenuItem component

--- apps/studio/src/components/Menu/MenuItem.tsx
+++ apps/studio/src/components/Menu/MenuItem.tsx
@@ -1,15 +1,17 @@
 import type { MenuItemProps as ChakraMenuItemProps } from "@chakra-ui/react"
 import { useMemo } from "react"
-import { MenuItem as ChakraMenuItem, cssVar } from "@chakra-ui/react"
+import { MenuItem as ChakraMenuItem, cssVar, Tooltip } from "@chakra-ui/react"
 
 const $bg = cssVar("menu-bg")
 
 export interface MenuItemProps extends ChakraMenuItemProps {
   colorScheme?: "critical"
+  tooltip?: string
 }
 
 export const MenuItem = ({
   colorScheme,
+  tooltip,
   ...menuItemProps
 }: MenuItemProps): JSX.Element => {
   // Unable to use useMultiStyleConfig here because Menu parent still controls
@@ -39,5 +41,13 @@ export const MenuItem = ({
     }
   }, [colorScheme])
 
-  return <ChakraMenuItem {...menuItemProps} sx={extraStyles} />
+  const menuItem = <ChakraMenuItem {...menuItemProps} sx={extraStyles} />
+
+  return tooltip ? (
+    <Tooltip label={tooltip} placement="right">
+      {menuItem}
+    </Tooltip>
+  ) : (
+    menuItem
+  )
 }
