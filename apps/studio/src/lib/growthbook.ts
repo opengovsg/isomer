9e138aab956395507d9725441f6ade6fb5a93000
adriangohjw <adriangohjw@gmail.com>
2025-04-15 16:45:03 +0800

    remove

--- apps/studio/src/lib/growthbook.ts
+++ apps/studio/src/lib/growthbook.ts
@@ -1,19 +1,5 @@
-import type { GrowthBook } from "@growthbook/growthbook"
-
 export const BANNER_FEATURE_KEY = "isomer-next-banner"
 export const ISOMER_ADMIN_FEATURE_KEY = "isomer_admins"
 export const CATEGORY_DROPDOWN_FEATURE_KEY = "category-dropdown"
 export const IS_SINGPASS_ENABLED_FEATURE_KEY = "is-singpass-enabled"
 export const IS_SINGPASS_ENABLED_FEATURE_KEY_FALLBACK_VALUE = false
-
-interface GetIsSingpassEnabledProps {
-  gb: GrowthBook
-}
-export const getIsSingpassEnabled = ({
-  gb,
-}: GetIsSingpassEnabledProps): boolean => {
-  return gb.getFeatureValue(
-    IS_SINGPASS_ENABLED_FEATURE_KEY,
-    IS_SINGPASS_ENABLED_FEATURE_KEY_FALLBACK_VALUE,
-  )
-}
