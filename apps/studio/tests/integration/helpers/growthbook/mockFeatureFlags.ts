57f3591ccf9d28ac870b529bfed270e6be4e05ba
adriangohjw <adriangohjw@gmail.com>
2025-04-15 17:11:56 +0800

    feat: add mock feature flag for Singpass to enable testing

--- apps/studio/tests/integration/helpers/growthbook/mockFeatureFlags.ts
+++ apps/studio/tests/integration/helpers/growthbook/mockFeatureFlags.ts
@@ -1,3 +1,10 @@
-const mockFeatureFlags = new Map<string, unknown>()
+import { IS_SINGPASS_ENABLED_FEATURE_KEY } from "~/lib/growthbook"
+
+const mockFeatureFlags = new Map<string, unknown>([
+  [
+    IS_SINGPASS_ENABLED_FEATURE_KEY,
+    true, // For testing purposes, we always enable Singpass
+  ],
+])
 
 export { mockFeatureFlags }
