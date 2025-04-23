fa7ca5400d6f5dcd66941d82bea0cbc5f944caf4
seaerchin <jiachin@open.gov.sg>
2025-04-24 03:51:59 +0800

    chore: set default values

--- packages/components/src/schemas/childpage.ts
+++ packages/components/src/schemas/childpage.ts
@@ -4,14 +4,17 @@ export const ChildpagesSchema = Type.Object({
   layout: Type.Union([Type.Literal("boxes"), Type.Literal("rows")], {
     title: "Layout",
     format: "childpages",
+    default: "boxes",
   }),
   summary: Type.Boolean({
     title: "Show summary of all child pages",
+    default: false,
   }),
   thumbnail: Type.Boolean({
     title: "Show thumbnail of all child pages",
     description:
       "We will use the child pages' feature images as their thumbnail.",
+    default: false,
   }),
 })
 
