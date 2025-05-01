f7699cf36a0d3d4d8f64b36b9b98027ffe22b2ad
adriangohjw <adriangohjw@gmail.com>
2025-05-02 01:43:49 +0800

    enhance AltTextSchema to enforce descriptive alt text for images, preventing generic terms and ensuring accessibility

--- packages/components/src/interfaces/complex/Image.ts
+++ packages/components/src/interfaces/complex/Image.ts
@@ -25,6 +25,14 @@ export const AltTextSchema = Type.String({
   maxLength: 120,
   description:
     "Add a descriptive text so that visually impaired users can understand your image",
+  pattern:
+    // Note: ajv pattern does not support the use of patternFlag like "i" for case-insensitive
+    // Thus, we manually add the case-insensitive flag to the regex pattern
+    "^(?=.*\\S)(?!(?:[Ii][Mm][Aa][Gg][Ee]|[Pp][Ii][Cc][Tt][Uu][Rr][Ee]|[Pp][Hh][Oo][Tt][Oo]|[Ll][Oo][Gg][Oo]|[Ss][Cc][Rr][Ee][Ee][Nn][Ss][Hh][Oo][Tt]|[Gg][Rr][Aa][Pp][Hh]|[Cc][Hh][Aa][Rr][Tt]|[Dd][Ii][Aa][Gg][Rr][Aa][Mm]|[Ii][Cc][Oo][Nn])$).*$",
+  errorMessage: {
+    pattern:
+      "must be descriptive. It cannot be empty, contain only spaces, or use generic terms like 'image', 'logo', 'graph', etc.",
+  },
 })
 
 export const ImageSchema = Type.Object(
