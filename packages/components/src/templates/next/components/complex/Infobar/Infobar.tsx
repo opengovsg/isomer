d368bb1444c05f6c66aa05eb45687aeb7ca50122
adriangohjw <adriangohjw@gmail.com>
2025-05-07 01:46:14 +0800

    refactor Infobar component to support dynamic color schemes and improve styling structure

--- packages/components/src/templates/next/components/complex/Infobar/Infobar.tsx
+++ packages/components/src/templates/next/components/complex/Infobar/Infobar.tsx
@@ -1,16 +1,18 @@
 import type { InfobarProps } from "~/interfaces"
+import { DEFAULT_INFOBAR_VARIANT } from "~/interfaces"
 import { tv } from "~/lib/tv"
 import { getReferenceLinkHref, getTailwindVariantLayout } from "~/utils"
 import { ComponentContent } from "../../internal/customCssClass"
 import { LinkButton } from "../../internal/LinkButton"
 
-const createInfobarStyles = tv({
+export const createInfobarStyles = tv({
   slots: {
+    screenWideOuterContainer: "",
     outerContainer: `${ComponentContent}`,
     innerContainer: "mx-auto flex flex-col items-start",
     headingContainer: "flex flex-col gap-6",
     title: "break-words",
-    description: "text-base-content",
+    description: "",
     buttonContainer: "flex flex-col gap-x-5 gap-y-4 sm:flex-row",
   },
   variants: {
@@ -19,28 +21,67 @@ const createInfobarStyles = tv({
         outerContainer: "mx-6 py-16 sm:mx-10 lg:py-24",
         innerContainer: "items-center gap-9 text-center lg:max-w-3xl",
         headingContainer: "gap-6",
-        title: "prose-display-lg text-base-content-strong",
+        title: "prose-display-lg",
         description: "prose-headline-lg-regular",
         buttonContainer: "items-center",
       },
       default: {
-        outerContainer: "mt-12 rounded-lg bg-base-canvas-backdrop first:mt-0",
+        outerContainer: "mt-12 rounded-lg first:mt-0",
         innerContainer: "items-start gap-7 p-8",
         headingContainer: "gap-4",
-        title: "prose-display-sm text-base-content",
+        title: "prose-display-sm",
         description: "prose-body-base",
         buttonContainer: "items-start",
       },
     },
+    colorScheme: {
+      dark: {},
+      light: {},
+    },
   },
+  compoundVariants: [
+    {
+      colorScheme: "dark",
+      layout: "homepage",
+      className: {
+        screenWideOuterContainer: "bg-base-canvas-inverse",
+        outerContainer: "bg-base-canvas-inverse text-base-canvas",
+      },
+    },
+    {
+      // NOTE: Should not have dark mode on non-homepage for now
+      // Copy the light + default variant
+      colorScheme: "dark",
+      layout: "default",
+      className: {
+        screenWideOuterContainer: "bg-base-canvas-backdrop",
+        outerContainer: "bg-base-canvas-backdrop",
+      },
+    },
+    {
+      colorScheme: "light",
+      layout: "homepage",
+      className: {
+        outerContainer: "text-base-content",
+      },
+    },
+    {
+      colorScheme: "light",
+      layout: "default",
+      className: {
+        screenWideOuterContainer: "bg-base-canvas-backdrop",
+        outerContainer: "bg-base-canvas-backdrop",
+      },
+    },
+  ],
   defaultVariants: {
     layout: "homepage",
+    colorScheme: DEFAULT_INFOBAR_VARIANT,
   },
 })
 
-export const compoundStyles = createInfobarStyles()
-
 const Infobar = ({
+  variant,
   title,
   description,
   buttonLabel,
@@ -55,73 +96,67 @@ const Infobar = ({
   const hasPrimaryCTA = !!buttonLabel && !!buttonUrl
   const hasSecondaryCTA = !!secondaryButtonLabel && !!secondaryButtonUrl
 
+  const styles = createInfobarStyles({
+    layout: simplifiedLayout,
+    colorScheme: variant,
+  })
+
+  // NOTE: Should not have dark mode on non-homepage for now
+  const buttonColorScheme =
+    simplifiedLayout === "default"
+      ? "default"
+      : variant === "dark"
+        ? "inverse"
+        : "default"
+
   return (
-    <section
-      className={compoundStyles.outerContainer({ layout: simplifiedLayout })}
-    >
-      <div
-        className={compoundStyles.innerContainer({
-          layout: simplifiedLayout,
-        })}
-      >
-        <div
-          className={compoundStyles.headingContainer({
-            layout: simplifiedLayout,
-          })}
-        >
-          <h2 className={compoundStyles.title({ layout: simplifiedLayout })}>
-            {title}
-          </h2>
-
-          {description && (
-            <p
-              className={compoundStyles.description({
-                layout: simplifiedLayout,
-              })}
-            >
-              {description}
-            </p>
-          )}
-        </div>
-
-        {(hasPrimaryCTA || hasSecondaryCTA) && (
-          <div
-            className={compoundStyles.buttonContainer({
-              layout: simplifiedLayout,
-            })}
-          >
-            {hasPrimaryCTA && (
-              <LinkButton
-                href={getReferenceLinkHref(
-                  buttonUrl,
-                  site.siteMap,
-                  site.assetsBaseUrl,
-                )}
-                size={simplifiedLayout === "homepage" ? "lg" : "base"}
-                LinkComponent={LinkComponent}
-                isWithFocusVisibleHighlight
-              >
-                {buttonLabel}
-              </LinkButton>
-            )}
-
-            {hasSecondaryCTA && (
-              <LinkButton
-                href={getReferenceLinkHref(
-                  secondaryButtonUrl,
-                  site.siteMap,
-                  site.assetsBaseUrl,
-                )}
-                size={simplifiedLayout === "homepage" ? "lg" : "base"}
-                variant="outline"
-                LinkComponent={LinkComponent}
-                isWithFocusVisibleHighlight
-              >
-                {secondaryButtonLabel}
-              </LinkButton>
+    <section className={styles.screenWideOuterContainer()}>
+      <div className={styles.outerContainer()}>
+        <div className={styles.innerContainer()}>
+          <div className={styles.headingContainer()}>
+            <h2 className={styles.title()}>{title}</h2>
+            {description && (
+              <p className={styles.description()}>{description}</p>
             )}
           </div>
-        )}
+
+          {(hasPrimaryCTA || hasSecondaryCTA) && (
+            <div className={styles.buttonContainer()}>
+              {hasPrimaryCTA && (
+                <LinkButton
+                  href={getReferenceLinkHref(
+                    buttonUrl,
+                    site.siteMap,
+                    site.assetsBaseUrl,
+                  )}
+                  size={simplifiedLayout === "homepage" ? "lg" : "base"}
+                  colorScheme={buttonColorScheme}
+                  LinkComponent={LinkComponent}
+                  isWithFocusVisibleHighlight
+                >
+                  {buttonLabel}
+                </LinkButton>
+              )}
+
+              {hasSecondaryCTA && (
+                <LinkButton
+                  href={getReferenceLinkHref(
+                    secondaryButtonUrl,
+                    site.siteMap,
+                    site.assetsBaseUrl,
+                  )}
+                  size={simplifiedLayout === "homepage" ? "lg" : "base"}
+                  variant="outline"
+                  colorScheme={buttonColorScheme}
+                  LinkComponent={LinkComponent}
+                  isWithFocusVisibleHighlight
+                >
+                  {secondaryButtonLabel}
+                </LinkButton>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </section>
   )
