ab84496f64d1820048f3c8dd7b6cec8f67eb665f
adriangohjw <adriangohjw@gmail.com>
2025-05-13 12:17:20 +0800

    fix conflicting aniamation

--- packages/components/src/templates/next/components/complex/Hero/HeroLargeImage/ScrollForMoreButton.tsx
+++ packages/components/src/templates/next/components/complex/Hero/HeroLargeImage/ScrollForMoreButton.tsx
@@ -5,25 +5,27 @@ interface ScrollForMoreButtonProps {
 export const ScrollForMoreButton = ({ isFixed }: ScrollForMoreButtonProps) => {
   return (
     <div
-      className={`${
-        isFixed ? "fixed animate-button-pulse" : "absolute"
-      } bottom-16 left-1/2 z-20 flex -translate-x-1/2 animate-slide-up-fade-in flex-row items-center gap-0.5 rounded-full bg-black/65 px-3 py-2 text-base`}
+      className={`${isFixed ? "fixed" : "absolute"} animate-slide-up-fade-in bottom-16 left-1/2 -translate-x-1/2 rounded-full`}
     >
-      <span className="prose-headline-base-medium whitespace-nowrap text-white">
-        Scroll for more
-      </span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="28"
-        height="28"
-        viewBox="0 0 28 28"
-        fill="none"
+      <div
+        className={`${isFixed ? "animate-button-pulse" : ""} z-20 flex flex-row items-center gap-0.5 rounded-full bg-black/65 px-3 py-2 text-base`}
       >
-        <path
-          d="M7.3418 12.4917L14 19.1499L20.6581 12.4917L19.0085 10.8421L14 15.8506L8.99146 10.8421L7.3418 12.4917Z"
-          fill="white"
-        />
-      </svg>
+        <span className="prose-headline-base-medium whitespace-nowrap text-white">
+          Scroll for more
+        </span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+        >
+          <path
+            d="M7.3418 12.4917L14 19.1499L20.6581 12.4917L19.0085 10.8421L14 15.8506L8.99146 10.8421L7.3418 12.4917Z"
+            fill="white"
+          />
+        </svg>
+      </div>
     </div>
   )
 }
