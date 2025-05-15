cbf2bd3c91b3359140c011ee3d3dfe3d4d3f8f1f
seaerchin <jiachin@open.gov.sg>
2025-05-15 18:13:34 +0800

    chore: update styling for info pic

--- packages/components/src/templates/next/components/complex/Infopic/common.ts
+++ packages/components/src/templates/next/components/complex/Infopic/common.ts
@@ -5,7 +5,7 @@ import { InfopicVariants } from "~/interfaces/complex/Infopic"
 export const infopicStyles = tv({
   slots: {
     container:
-      "grid min-h-[360px] bg-base-canvas-backdrop [grid-template-areas:'img''content'] [grid-template-rows:auto_1fr] lg:grid-cols-2 lg:[grid-template-rows:auto]",
+      "grid min-h-[360px] [grid-template-areas:'img''content'] [grid-template-rows:auto_1fr] lg:grid-cols-2 lg:[grid-template-rows:auto]",
     image: "inset-0 h-full w-full object-cover lg:absolute",
     imageContainer:
       "relative max-h-[400px] min-h-[200px] w-full [grid-area:img] lg:max-h-full",
@@ -13,7 +13,7 @@ export const infopicStyles = tv({
     // since content is half of screen width in desktop.
     content:
       "px-6 pb-16 pt-10 text-base-content [grid-area:content] md:max-w-[760px] md:px-10 md:pb-20 md:pt-16 lg:max-w-[620px] lg:content-center lg:py-24 lg:pl-10",
-    title: "prose-display-md break-words text-base-content-strong",
+    title: "prose-display-md break-words",
     description: "prose-body-base mt-4 md:mt-6",
     button: "mt-9",
   },
@@ -28,7 +28,19 @@ export const infopicStyles = tv({
         content: "lg:justify-self-end lg:pr-24",
       },
     },
-    colorScheme: { default: "", inverse: "" },
+    colorScheme: {
+      default: {
+        container: "bg-base-canvas-backdrop",
+        content: "",
+        title: "text-base-content-strong",
+        description: "",
+      },
+      inverse: {
+        container: "",
+        title: "text-base-content-inverse",
+        description: "text-base-content-inverse",
+      },
+    },
     variant: {
       [InfopicVariants.Block.value]: {
         container: "",
@@ -36,7 +48,7 @@ export const infopicStyles = tv({
       },
       [InfopicVariants.Full.value]: {
         container: "",
-        image: "",
+        image: "[grid-area:img-start / content-end]",
       },
     },
   },
