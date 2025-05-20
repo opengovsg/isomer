d20dcccddc2471066dc84d385b7bf6885b35edee
seaerchin <jiachin@open.gov.sg>
2025-05-20 13:48:40 +0800

    chore: update styling

--- packages/components/src/templates/next/components/complex/Infopic/common.ts
+++ packages/components/src/templates/next/components/complex/Infopic/common.ts
@@ -5,13 +5,13 @@ import { InfopicVariants } from "~/interfaces/complex/Infopic"
 export const infopicStyles = tv({
   slots: {
     container:
-      "grid min-h-[360px] [grid-template-areas:'img''content'] [grid-template-rows:auto_1fr] lg:grid-cols-2 lg:[grid-template-rows:auto]",
+      "grid min-h-[360px] [grid-template-areas:'img''content'] lg:grid-cols-2 lg:[grid-template-rows:auto]",
     image: "inset-0 h-full w-full object-cover lg:absolute",
-    imageContainer: "relative max-h-[400px] min-h-[200px] w-full lg:max-h-full",
+    imageContainer: "[grid-area:img]",
     // max-width of content in desktop is HALF of max-w-screen-xl for correct alignment of content
     // since content is half of screen width in desktop.
     content:
-      "px-6 pb-16 pt-10 text-base-content [grid-area:content] md:px-10 md:pb-20 md:pt-16 lg:content-center lg:py-24 lg:pl-10",
+      "px-6 pb-16 pt-10 text-base-content [grid-area:content] md:max-w-[760px] md:px-10 md:pb-20 md:pt-16 lg:max-w-[620px] lg:content-center lg:py-24 lg:pl-10",
     title: "prose-display-md break-words",
     description: "prose-body-base mt-4 md:mt-6",
     button: "mt-9",
@@ -31,24 +31,22 @@ export const infopicStyles = tv({
     colorScheme: {
       default: {
         container: "bg-base-canvas-backdrop",
-        content: "",
         title: "text-base-content-strong",
-        description: "",
       },
       inverse: {
-        container: "",
         title: "text-base-content-inverse",
         description: "text-base-content-inverse",
       },
     },
     variant: {
       [InfopicVariants.Block.value]: {
-        imageContainer: "[grid-area:img]",
-        content: "md:max-w-[760px] lg:max-w-[620px]",
+        imageContainer:
+          "relative max-h-[400px] min-h-[200px] w-full lg:max-h-full",
+        container: "[grid-template-rows:auto_1fr]",
       },
       [InfopicVariants.Full.value]: {
-        content: "md:max-w-[760px] lg:max-w-[620px]",
-        overlay: "bg-brand-canvas-inverse/35 bg-base-canvas-inverse-overlay/65",
+        container: "[grid-template-rows:1fr_1fr]",
+        overlay: "bg-base-canvas-inverse-overlay/65",
       },
     },
   },
--- packages/components/src/templates/next/components/complex/Infopic/components/FullInfopic.tsx
+++ packages/components/src/templates/next/components/complex/Infopic/components/FullInfopic.tsx
@@ -26,32 +26,45 @@ export const FullInfopic = ({
     <section
       style={{
         backgroundImage: `url('${imageSrc}')`,
-        objectFit: "cover",
+        backgroundSize: "cover",
       }}
       id={id}
     >
       <div className={compoundStyles.overlay()}>
-        <div className={compoundStyles.container()}>
-          <div className={compoundStyles.content()}>
-            <h2 className={compoundStyles.title()}>{title}</h2>
-            <p className={compoundStyles.description()}>{description}</p>
-            {hasLinkButton && (
-              <div className={compoundStyles.button()}>
-                <LinkButton
-                  LinkComponent={LinkComponent}
-                  href={getReferenceLinkHref(
-                    buttonUrl,
-                    site.siteMap,
-                    site.assetsBaseUrl,
-                  )}
-                  isWithFocusVisibleHighlight
-                  colorScheme="inverse"
-                >
-                  {buttonLabel}
-                </LinkButton>
-              </div>
-            )}
-            <div className=""></div>
+        <div
+          // NOTE: This cannot be a tailwind css className
+          // as we are dynamically setting it at runtime and tailwind
+          // won't pickup on the color at build time.
+          style={{
+            backgroundColor: `color-mix(
+                              in srgb,
+                              var(--color-brand-canvas-inverse) 65%,
+                              transparent
+                            )`,
+          }}
+        >
+          <div className={compoundStyles.container()}>
+            <div className={compoundStyles.content()}>
+              <h2 className={compoundStyles.title()}>{title}</h2>
+              <p className={compoundStyles.description()}>{description}</p>
+              {hasLinkButton && (
+                <div className={compoundStyles.button()}>
+                  <LinkButton
+                    LinkComponent={LinkComponent}
+                    href={getReferenceLinkHref(
+                      buttonUrl,
+                      site.siteMap,
+                      site.assetsBaseUrl,
+                    )}
+                    isWithFocusVisibleHighlight
+                    colorScheme="inverse"
+                  >
+                    {buttonLabel}
+                  </LinkButton>
+                </div>
+              )}
+            </div>
+            <div className={compoundStyles.imageContainer()}></div>
           </div>
         </div>
       </div>
