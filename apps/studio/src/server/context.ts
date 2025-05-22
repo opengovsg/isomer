a325c3f2cb3a1e1c5e4f8e0991f1db4c3ca1470f
adriangohjw <adriangohjw@gmail.com>
2025-05-22 16:22:46 +0800

    update session TTL configuration for VAPT environment

--- apps/studio/src/server/context.ts
+++ apps/studio/src/server/context.ts
@@ -36,7 +36,12 @@ export const createContext = async (opts: CreateNextContextOptions) => {
   const session = await getIronSession<SessionData>(
     opts.req,
     opts.res,
-    generateSessionOptions({}), // Note: this wouldn't overwrite the cookie (e.g. TTL) if it already exists
+    generateSessionOptions({
+      // Temporary as Singpass is live for VAPT
+      // Once VAPT is done and we are releasing to other environments,
+      // we do not need to specify and can let it fallback to default 1 hour
+      ttlInHours: env.NEXT_PUBLIC_APP_ENV === "vapt" ? 1 : 12,
+    }), // Note: this wouldn't overwrite the cookie (e.g. TTL) if it already exists
   )
 
   const innerContext = createContextInner({
