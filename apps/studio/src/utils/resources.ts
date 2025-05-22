ee976ddc91a457925eb2098001dfd3436d27cad1
adriangohjw <adriangohjw@gmail.com>
2025-05-23 04:37:38 +0800

    add getResourceStudioUrl function to generate resource-specific URLs

--- apps/studio/src/utils/resources.ts
+++ apps/studio/src/utils/resources.ts
@@ -10,6 +10,9 @@ import {
   BiSort,
 } from "react-icons/bi"
 
+import type { Resource } from "~prisma/generated/generatedTypes"
+import { env } from "~/env.mjs"
+
 export const isAllowedToHaveChildren = (
   resourceType: ResourceType,
 ): boolean => {
@@ -63,4 +66,29 @@ export const getUserViewableResourceTypes = (): ResourceType[] => {
     ResourceType.CollectionLink,
     ResourceType.CollectionPage,
   ]
+}
+
+export const getResourceStudioUrl = (resource: Resource): string => {
+  const siteUrlPrefix = `${env.NEXT_PUBLIC_APP_URL}/sites/${resource.siteId}`
+
+  switch (resource.type) {
+    case ResourceType.RootPage:
+      return siteUrlPrefix
+    case ResourceType.Page:
+    case ResourceType.IndexPage:
+    case ResourceType.CollectionPage:
+      return `${siteUrlPrefix}/pages/${String(resource.id)}`
+    case ResourceType.CollectionLink:
+      return `${siteUrlPrefix}/links/${String(resource.id)}`
+    case ResourceType.Folder:
+      return `${siteUrlPrefix}/folders/${String(resource.id)}`
+    case ResourceType.Collection:
+      return `${siteUrlPrefix}/collections/${String(resource.id)}`
+    case ResourceType.FolderMeta:
+    case ResourceType.CollectionMeta:
+      return "" // they aren't accessible by users
+    default:
+      const exhaustiveCheck: never = resource.type
+      return exhaustiveCheck
+  }
 }
