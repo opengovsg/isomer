52a29ee2a2a8a071b1b1326e69b77711b6a30243
adriangohjw <adriangohjw@gmail.com>
2025-05-24 22:44:15 +0800

    add temporary workaround for VAPT testers in isGovEmail function

--- apps/studio/src/utils/email.ts
+++ apps/studio/src/utils/email.ts
@@ -1,9 +1,20 @@
 import isEmail from "validator/lib/isEmail"
 
+import { env } from "~/env.mjs"
+
 /**
  * Returns whether the passed value is a valid government email.
  */
 export const isGovEmail = (value: unknown) => {
+  // Temp. workaround to allow VAPT testers to be added as admins
+  if (
+    env.NEXT_PUBLIC_APP_ENV === "vapt" &&
+    typeof value === "string" &&
+    ["marta@cure53.de", "rupp@cure53.de", "dennis@cure53.de"].includes(value)
+  ) {
+    return true
+  }
+
   return (
     typeof value === "string" && isEmail(value) && value.endsWith(".gov.sg")
   )
