d9b0b9dfa68d8ff811e8036049e02a2431ab2264
seaerchin <jiachin@open.gov.sg>
2025-05-28 15:33:10 +0800

    chore: remove resized last row

--- packages/components/src/templates/next/components/complex/InfoCards/InfoCards.tsx
+++ packages/components/src/templates/next/components/complex/InfoCards/InfoCards.tsx
@@ -35,7 +35,7 @@ const InfoCards = ({
       ? INFOCARD_VARIANT.bold
       : INFOCARD_VARIANT.default
 
-  const { cols, requiresResizingLastRow } = calculateGridDimensions(cards)
+  const { cols } = calculateGridDimensions(cards)
   const computedMaxCols = variant === CARDS_WITH_FULL_IMAGES ? cols : maxColumns
 
   const InfoCardsToRender = () => {
@@ -70,13 +70,9 @@ const InfoCards = ({
           </>
         )
       case CARDS_WITH_FULL_IMAGES: {
-        const cardsToRender = requiresResizingLastRow
-          ? cards.slice(0, 3)
-          : cards
-
         return (
           <>
-            {cardsToRender.map((card, idx) => (
+            {cards.map((card, idx) => (
               <InfoCardWithFullImage
                 key={idx}
                 {...card}
@@ -132,28 +128,6 @@ const InfoCards = ({
       >
         <InfoCardsToRender />
       </div>
-
-      {requiresResizingLastRow && variant === CARDS_WITH_FULL_IMAGES && (
-        <div
-          className={compoundStyles.grid({
-            maxColumns: "2",
-            variant: INFOCARD_VARIANT.bold,
-            isResizedLastRow: true,
-          })}
-        >
-          {cards.slice(3).map((card, idx) => (
-            <InfoCardWithFullImage
-              key={idx}
-              {...card}
-              maxColumns="2"
-              layout={layout}
-              site={site}
-              LinkComponent={LinkComponent}
-              shouldLazyLoad={shouldLazyLoad}
-            />
-          ))}
-        </div>
-      )}
 
       {!!url && !!label && (
         <div className={compoundStyles.urlButtonContainer()}>
--- packages/components/src/templates/next/components/complex/InfoCards/utils.ts
+++ packages/components/src/templates/next/components/complex/InfoCards/utils.ts
@@ -1,10 +1,8 @@
 export const calculateGridDimensions = (arr: unknown[]) => {
   const cols =
     arr.length === 4 || arr.length >= 7 ? ("4" as const) : ("3" as const)
-  const requiresResizingLastRow = arr.length === 5
 
   return {
     cols,
-    requiresResizingLastRow,
   }
 }
