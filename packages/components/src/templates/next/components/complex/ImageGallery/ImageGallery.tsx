0ded5b631a80531dd254c697e268571afa2d2df4
Adrian Goh Jun Wei <adriangohjw@gmail.com>
2025-06-03 17:43:26 +0800

    Enhance ImageGallery component to process image URLs based on asset base URL. Introduced isExternalUrl utility for URL validation and updated rendering logic to handle both external and internal image sources. (#1389)

--- packages/components/src/templates/next/components/complex/ImageGallery/ImageGallery.tsx
+++ packages/components/src/templates/next/components/complex/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,23 @@
 import type { ImageGalleryProps } from "~/interfaces/complex/ImageGallery"
+import { isExternalUrl } from "~/utils/isExternalUrl"
 import { ImageGalleryClient } from "./ImageGalleryClient"
 
 export const ImageGallery = (props: ImageGalleryProps) => {
-  const { site, ...rest } = props
-  return <ImageGalleryClient assetsBaseUrl={site.assetsBaseUrl} {...rest} />
+  const { site, images, ...rest } = props
+
+  const processedImages = images.map((image) => ({
+    ...image,
+    src:
+      isExternalUrl(image.src) || site.assetsBaseUrl === undefined
+        ? image.src
+        : `${site.assetsBaseUrl}${image.src}`,
+  }))
+
+  return (
+    <ImageGalleryClient
+      assetsBaseUrl={site.assetsBaseUrl}
+      images={processedImages}
+      {...rest}
+    />
+  )
 }
