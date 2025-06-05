628bbea7f6e1dbe156472e17b8fa2aabe60855d3
seaerchin <jiachin@open.gov.sg>
2025-06-05 18:16:23 +0800

    chore: add tests

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsChildrenPagesOrderingControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsChildrenPagesOrderingControl.tsx
@@ -16,42 +16,13 @@ import { editPageSchema } from "~/features/editing-experience/schema"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { trpc } from "~/utils/trpc"
 import { BaseBlock, BaseBlockDragHandle } from "../../../Block/BaseBlock"
+import { merge } from "./utils/merge"
 
 export const jsonFormsChildrenPagesOrderingControlTester: RankedTester =
   rankWith(
     JSON_FORMS_RANKING.ChildrenPagesOrderingControl,
     schemaMatches((schema) => schema.format === "childrenPagesOrdering"),
   )
-
-const merge = (
-  base: string[],
-  all: string[],
-  mappings: Map<string, string>,
-): string[] => {
-  const baseSet = new Set(base)
-  const allSet = new Set(all)
-
-  // NOTE: values that are in the user given ordering but not in
-  // the list of child pages returned from db implies
-  // that they have been removed from the current folder
-  const toRemoveFromBase = baseSet.difference(allSet)
-  // NOTE: values that are returned as concrete pages by the db
-  // and can be moved around by the user
-  const toAdd = allSet.difference(baseSet).values()
-
-  return base
-    .filter((resourceId) => !toRemoveFromBase.has(resourceId))
-    .concat(
-      // NOTE: We have to assume default sort order (alphabetical)
-      // when we shift in new items
-      Array.from(toAdd).toSorted((a, b) => {
-        const aTitle = mappings.get(a) ?? ""
-        const bTitle = mappings.get(b) ?? ""
-
-        return aTitle.localeCompare(bTitle, undefined)
-      }),
-    )
-}
 
 type ReorderingControlProps<T = string> = Omit<ControlProps, "data"> & {
   data: T[]
