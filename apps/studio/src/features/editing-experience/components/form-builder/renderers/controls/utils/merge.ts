cb3f2b3810e8261e538edee0bd888f74317eeff2
seaerchin <jiachin@open.gov.sg>
2025-06-09 13:01:26 +0800

    chore: update set difference as `set.difference` method isn't available in node 20

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/utils/merge.ts
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/utils/merge.ts
@@ -9,13 +9,14 @@ export const merge = (
   // NOTE: values that are in the user given ordering but not in
   // the list of child pages returned from db implies
   // that they have been removed from the current folder
-  const toRemoveFromBase = baseSet.difference(allSet)
+  const toRemoveFromBase = setDifference(baseSet, allSet)
   // NOTE: values that are returned as concrete pages by the db
   // and can be moved around by the user
-  const toAdd = allSet.difference(baseSet).values()
+  const toAdd = setDifference(allSet, baseSet)
+  const toRemoveFromBaseSet = new Set(toRemoveFromBase)
 
   return base
-    .filter((resourceId) => !toRemoveFromBase.has(resourceId))
+    .filter((resourceId) => !toRemoveFromBaseSet.has(resourceId))
     .concat(
       // NOTE: We have to assume default sort order (alphabetical)
       // when we shift in new items
@@ -26,4 +27,16 @@ export const merge = (
         return aTitle.localeCompare(bTitle, undefined)
       }),
     )
+}
+
+// NOTE: Computes items that are in base but not in other
+const setDifference = <T>(base: Set<T>, other: Set<T>) => {
+  const result = []
+  for (const v of base.values()) {
+    if (!other.has(v)) {
+      result.push(v)
+    }
+  }
+
+  return result
 }
