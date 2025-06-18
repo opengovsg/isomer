521ae767efd403b132dbc869a729205561da9c30
seaerchin <jiachin@open.gov.sg>
2025-06-18 13:04:27 +0800

    chore: add stories for thumbnail

--- apps/studio/tests/msw/handlers/collection.ts
+++ apps/studio/tests/msw/handlers/collection.ts
@@ -72,5 +72,28 @@ export const collectionHandlers = {
         title: "yet another link",
       }))
     },
+    thumbnail: () => {
+      return trpcMsw.collection.readCollectionLink.query(() => ({
+        content: {
+          page: {
+            ref: "",
+            date: "24-10-2024",
+            summary: "",
+            category: "Others",
+            image: {
+              src: "www.google.com",
+              alt: "fake news",
+            },
+          },
+          layout: "link",
+          content: [],
+          version: "0.1.0",
+          // TODO: not too sure why this error happens
+          // but there's a typing issue where we are not satisfying the opaque type
+          // oddly enough, this doesn't happen on other handlers
+        } as unknown as PrismaJson.BlobJsonContent,
+        title: "Link with image",
+      }))
+    },
   },
 }
