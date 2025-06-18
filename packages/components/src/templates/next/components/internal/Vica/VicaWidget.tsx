10453a34f4de89fb3ad565c0ed6d5dec9e3c98b7
adriangohjw <adriangohjw@gmail.com>
2025-06-18 11:57:33 +0800

    refactor(VicaWidget): update color attributes to use new dynamic color variables for improved consistency

--- packages/components/src/templates/next/components/internal/Vica/VicaWidget.tsx
+++ packages/components/src/templates/next/components/internal/Vica/VicaWidget.tsx
@@ -17,17 +17,17 @@ export const VicaWidget = (props: VicaWidgetProps) => {
       // We ignore config passed in from DB and manually overwrite
       // the following attributes to ensure consistency and best brand appearance
       app-font-family="Inter, system-ui, sans-serif"
-      app-foreground-color={colors.base.content.DEFAULT}
-      app-color="var(--color-brand-canvas-inverse)"
-      app-button-border-color="var(--color-brand-canvas-inverse)"
-      app-canvas-background-color="var(--color-brand-canvas-default)"
-      app-quick-reply-button-background-color={colors.base.content.DEFAULT}
-      app-auto-complete-background-color="var(--color-brand-canvas-default)"
-      app-auto-complete-hover-color="var(--color-brand-canvas-alt)"
+      app-foreground-color={colors.base.canvas.DEFAULT}
+      app-color={colors.brand.canvas.inverse}
+      app-button-border-color={colors.brand.canvas.inverse}
+      app-canvas-background-color={colors.brand.canvas.DEFAULT}
+      app-quick-reply-button-background-color={colors.base.canvas.DEFAULT}
+      app-auto-complete-background-color={colors.brand.canvas.DEFAULT}
+      app-auto-complete-hover-color={colors.brand.canvas.alt}
       app-auto-complete-foreground-color={colors.base.content.strong}
       app-auto-complete-divider-color={colors.base.divider.medium}
-      app-recommendations-background-color="var(--color-brand-canvas-default)"
-      app-recommendations-hover-color="var(--color-brand-canvas-alt)"
+      app-recommendations-background-color={colors.brand.canvas.DEFAULT}
+      app-recommendations-hover-color={colors.brand.canvas.alt}
       app-recommendations-foreground-color={colors.base.content.strong}
     />
   )
