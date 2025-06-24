61a445b3a396804574fcfb1e902ea24a2a570e45
adriangohjw <adriangohjw@gmail.com>
2025-06-25 01:47:17 +0800

    enhance: Update Google Tag Manager header to use script source instead of inline HTML

--- packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerHeader.tsx
+++ packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerHeader.tsx
@@ -10,13 +10,8 @@ const GoogleTagManagerHeaderScript = ({
   return (
     <ScriptComponent
       id={`_next-gtm-init-${gtmId}`}
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${gtmId}');`,
-      }}
+      strategy="afterInteractive" // next/script's default but just in case Vercel changes it in the future
+      src={`https://www.googletagmanager.com/gtm.js?id=${gtmId}`}
     />
   )
 }
