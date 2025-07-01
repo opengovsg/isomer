6001ef77e7d4ff96aeb1d83303143bb87fc39181
adriangohjw <adriangohjw@gmail.com>
2025-07-02 06:08:34 +0800

    fix(GoogleTagManager): change script loading from defer to async for improved performance

--- packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerHeader.tsx
+++ packages/components/src/templates/next/components/internal/GoogleTagManager/GoogleTagManagerHeader.tsx
@@ -12,11 +12,9 @@ const GoogleTagManagerHeaderScript = ({
       id={`_next-gtm-init-${gtmId}`}
       strategy="afterInteractive" // next/script's default but just in case Vercel changes it in the future
       dangerouslySetInnerHTML={{
-        // Note: we use j.defer instead of j.async to ensure the script is non-blocking
-        // However, this means potentially losing some data of users who bounced really quickly (1-5% based on online articles),
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${gtmId}');`,
       }}
