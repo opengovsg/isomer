c30a80e605cbcae0f1ac85af572ff0149fadf0c3
adriangohjw <adriangohjw@gmail.com>
2025-07-07 14:03:16 +0800

    fix: update inHowManyDays type in AccountDeactivationWarningEmailTemplateData to restrict values to 1, 7, or 14

--- apps/studio/src/features/mail/templates/types.ts
+++ apps/studio/src/features/mail/templates/types.ts
@@ -30,7 +30,7 @@ export interface PublishAlertSiteAdminEmailTemplateData
 export interface AccountDeactivationWarningEmailTemplateData
   extends BaseEmailTemplateData {
   siteNames: string[]
-  inHowManyDays: number
+  inHowManyDays: 1 | 7 | 14 // note: this is arbitrarily set to when we want to remind users to log in
 }
 
 export interface AccountDeactivationEmailTemplateData
