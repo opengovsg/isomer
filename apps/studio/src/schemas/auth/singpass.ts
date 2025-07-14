bae6b9b4d3c19815f5d6d9d3a8418104b3709520
dcshzj <27919917+dcshzj@users.noreply.github.com>
2025-07-14 10:41:20 +0800

    fix: use a random state with Singpass OIDC

--- apps/studio/src/schemas/auth/singpass.ts
+++ apps/studio/src/schemas/auth/singpass.ts
@@ -1,6 +1,5 @@
 import { z } from "zod"
 
-import { SINGPASS_SIGN_IN_STATE } from "~/server/modules/auth/singpass/singpass.constants"
 import { callbackUrlSchema } from "../url"
 
 export const singpassLoginSchema = z.object({
@@ -10,8 +9,4 @@ export const singpassLoginSchema = z.object({
 export const singpassCallbackSchema = z.object({
   state: z.string(),
   code: z.string(),
-})
-
-export const singpassStateSchema = z.object({
-  state: z.literal(SINGPASS_SIGN_IN_STATE),
 })
