34257a11e3cf6ce2671d1f385285b6f3577981d0
adriangohjw <adriangohjw@gmail.com>
2025-07-17 03:48:59 +0800

    fix: simplify handleChange function in JsonFormsCollectionDropdownControl and remove unused linkAtom

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsCollectionDropdownControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsCollectionDropdownControl.tsx
@@ -4,11 +4,9 @@ import { and, rankWith, schemaMatches } from "@jsonforms/core"
 import { withJsonFormsControlProps } from "@jsonforms/react"
 import { FormLabel, SingleSelect } from "@opengovsg/design-system-react"
 import { COLLECTION_DROPDOWN_FORMAT } from "@opengovsg/isomer-components"
-import { useSetAtom } from "jotai"
 
 import Suspense from "~/components/Suspense"
 import { JSON_FORMS_RANKING } from "~/constants/formBuilder"
-import { linkAtom } from "~/features/editing-experience/atoms"
 import { siteSchema } from "~/features/editing-experience/schema"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { trpc } from "~/utils/trpc"
@@ -45,9 +43,7 @@ function SuspendableJsonFormsCollectionDropdownControl({
         }
       })}
       isClearable={false}
-      onChange={(value) => {
-        handleChange(path, value)
-      }}
+      onChange={(value) => handleChange(path, value)}
     />
   )
 }
@@ -58,13 +54,6 @@ export function JsonFormsCollectionDropdownControl({
   label,
   ...props
 }: ControlProps) {
-  const setLink = useSetAtom(linkAtom)
-
-  const handleChange: ControlProps["handleChange"] = (path, value: string) => {
-    props.handleChange(path, value)
-    setLink((prev) => ({ ...prev, category: value }))
-  }
-
   return (
     <FormControl isRequired={required} gap="0.5rem">
       <FormLabel description={description}>{label}</FormLabel>
@@ -72,7 +61,6 @@ export function JsonFormsCollectionDropdownControl({
         <SuspendableJsonFormsCollectionDropdownControl
           {...props}
           label={label}
-          handleChange={handleChange}
         />
       </Suspense>
     </FormControl>
