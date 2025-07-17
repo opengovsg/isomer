6bf35bf1266263d9866d9ca140cd5e2ddbcb85ca
adriangohjw <adriangohjw@gmail.com>
2025-07-17 17:14:18 +0800

    Refactor SearchableTable schemas to use 'variant' for type definition, enhancing clarity and default handling. Update SearchableTableProps to include site and LinkComponent for improved type safety.

--- packages/components/src/interfaces/internal/SearchableTable.ts
+++ packages/components/src/interfaces/internal/SearchableTable.ts
@@ -2,13 +2,16 @@ import type { Static } from "@sinclair/typebox"
 import { Type } from "@sinclair/typebox"
 
 import type { IsomerSiteProps, LinkComponentType } from "~/types"
+import { ARRAY_RADIO_FORMAT } from "../format"
 
 // TODO: maybe also move this outside of this file as a shared interface
 export const NATIVE_SEARCHABLE_TABLE_TYPE = "native"
 export const DGS_SEARCHABLE_TABLE_TYPE = "dgs"
 
 export const NativeSearchableTableSchema = Type.Object({
-  type: Type.Optional(Type.Literal(NATIVE_SEARCHABLE_TABLE_TYPE)), // optional for backward compatibility
+  variant: Type.Literal(NATIVE_SEARCHABLE_TABLE_TYPE, {
+    default: NATIVE_SEARCHABLE_TABLE_TYPE,
+  }),
   title: Type.Optional(Type.String()),
   headers: Type.Array(Type.Union([Type.String(), Type.Number()])),
   items: Type.Array(Type.Array(Type.Union([Type.String(), Type.Number()]))),
@@ -22,7 +25,9 @@ export type NativeSearchableTableProps = Static<
 }
 
 export const DGSSearchableTableSchema = Type.Object({
-  type: Type.Literal(DGS_SEARCHABLE_TABLE_TYPE),
+  variant: Type.Literal(DGS_SEARCHABLE_TABLE_TYPE, {
+    default: DGS_SEARCHABLE_TABLE_TYPE,
+  }),
   dgsResourceId: Type.String({
     title: "DGS Resource ID",
     description: "The DGS resource ID to fetch the data from",
@@ -42,6 +47,7 @@ export const DGSSearchableTableSchema = Type.Object({
         description: "The key of the header in DGS table",
       }),
     }),
+    { minItems: 1 },
   ),
 })
 
@@ -52,14 +58,22 @@ export type DGSSearchableTableProps = Static<
   LinkComponent?: LinkComponentType
 }
 
-export const SearchableTableSchema = Type.Union([
-  NativeSearchableTableSchema,
-  DGSSearchableTableSchema,
-])
+export const SearchableTableSchema = Type.Intersect(
+  [
+    Type.Union([NativeSearchableTableSchema, DGSSearchableTableSchema], {
+      title: "Searchable Table",
+      format: ARRAY_RADIO_FORMAT,
+    }),
+  ],
+  {
+    title: "Searchable Table",
+  },
+)
 
-export type SearchableTableProps =
-  | NativeSearchableTableProps
-  | DGSSearchableTableProps
+export type SearchableTableProps = Static<typeof SearchableTableSchema> & {
+  site: IsomerSiteProps
+  LinkComponent?: LinkComponentType
+}
 
 // TODO: to move to somewhere else as shared interface
 const _DGSResponseSchema = Type.Object({
