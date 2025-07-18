8a0bdc6ee5cbd2d841b3498b8612ed4b577cf80f
adriangohjw <adriangohjw@gmail.com>
2025-07-18 12:10:25 +0800

    Implement useDGSData hook for fetching and managing DGS data in KeyStatistics components. This includes a new fetchDataFromDGS function for API calls and state management for loading and error handling.

---
+++ packages/components/src/templates/next/components/complex/KeyStatistics/DGS/useDGSData.tsx
@@ -0,0 +1,81 @@
+// NOTE: This hook should be shared across all DGS components
+
+import { useEffect, useState } from "react"
+
+import type { DGSResponse, DGSSuccessResponse } from "~/interfaces"
+
+const BASE_URL = "https://data.gov.sg/api/action/datastore_search"
+
+interface FetchDataFromDGSProps {
+  resourceId: string
+  filters: Record<string, string>
+}
+export const fetchDataFromDGS = async ({
+  resourceId,
+  filters,
+}: FetchDataFromDGSProps) => {
+  const params = new URLSearchParams({
+    resource_id: resourceId,
+  })
+
+  if (Object.keys(filters).length > 0) {
+    params.set("filters", JSON.stringify(filters))
+  }
+
+  const url = `${BASE_URL}?${params.toString()}`
+
+  const res = await fetch(url)
+
+  return res.json() as Promise<DGSResponse>
+}
+
+interface UseDGSDataProps {
+  dgsResourceId: string
+  dgsRow: {
+    dgsFieldKey: string
+    dgsFieldValue: string
+  }
+}
+interface UseDGSDataReturn {
+  row: DGSSuccessResponse["result"]["records"][0] | undefined
+  isLoading: boolean
+  isError: boolean
+}
+export const useDGSData = ({
+  dgsResourceId,
+  dgsRow,
+}: UseDGSDataProps): UseDGSDataReturn => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [data, setData] = useState<DGSResponse | null>(null)
+
+  useEffect(() => {
+    // Note: we can add better error handling here - skipping now as this is a POC
+    async function fetchData() {
+      try {
+        const response = await fetchDataFromDGS({
+          resourceId: dgsResourceId,
+          filters: {
+            [dgsRow.dgsFieldKey]: dgsRow.dgsFieldValue,
+          },
+        })
+        if (response.success) {
+          setData(response)
+        } else {
+          setIsError(true)
+        }
+      } catch {
+        setIsError(true)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    void fetchData()
+  }, [dgsResourceId, dgsRow.dgsFieldKey, dgsRow.dgsFieldValue])
+
+  // Assumption: we only have one row. If more than one, we will choose the first one
+  const row = data?.success ? data.result.records[0] : undefined
+
+  return { row, isLoading, isError }
+}
