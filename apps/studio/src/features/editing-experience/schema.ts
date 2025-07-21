fbcf862697d5c3f67c34e1be5c8d05da2234f92c
adriangohjw <adriangohjw@gmail.com>
2025-07-21 12:09:48 +0800

    fix type

--- apps/studio/src/features/editing-experience/schema.ts
+++ apps/studio/src/features/editing-experience/schema.ts
@@ -4,7 +4,8 @@ export const siteSchema = z.object({
   siteId: z.coerce.string(),
 })
 
-export const pageSchema = siteSchema.extend({
+export const pageSchema = z.object({
+  siteId: z.coerce.number(),
   pageId: z.coerce.number(),
 })
 
@@ -14,8 +15,9 @@ export const collectionItemSchema = pageSchema
   })
   .partial({ pageId: true, linkId: true })
 
-export const pageOrLinkSchema = siteSchema
-  .extend({
+export const pageOrLinkSchema = z
+  .object({
+    siteId: z.coerce.number(),
     pageId: z.coerce.number().optional(),
     linkId: z.coerce.number().optional(),
   })
