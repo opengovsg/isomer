36e0ad5c824708a06cd60a7ae77cbb9b8a6c4916
adriangohjw <adriangohjw@gmail.com>
2025-08-03 03:47:27 +0800

    feat: expand ContactInformation story with detailed contact data for Singapore embassy

--- packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.stories.tsx
+++ packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.stories.tsx
@@ -22,5 +22,66 @@ export default meta
 type Story = StoryObj<typeof ContactInformation>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    country: "Singapore",
+    city: "Sentosa",
+    entityDetails: [
+      {
+        displayText: "Ambassador (Non-Resident)",
+        values: ["Mr MOHAMMAD Alami Musa"],
+      },
+      {
+        displayText: "Chancery",
+        values: [
+          "c/o Ministry of Foreign Affairs",
+          "Tanglin",
+          "Singapore 248163",
+        ],
+      },
+    ],
+    description: "Embassy of the Republic of Singapore - Algeria",
+    telephone: {
+      displayText: "Telephone",
+      values: ["+65-63798000 (MFA)"],
+    },
+    fax: {
+      displayText: "Fax",
+      values: ["+65-64747885 (MFA)"],
+    },
+    email: {
+      displayText: "Email",
+      values: [
+        "do-not-reply@isomer.gov.sg",
+        "do-not-reply-pelase@isomer.gov.sg",
+      ],
+    },
+    website: {
+      displayText: "Website",
+      values: ["https://www.isomer.gov.sg", "https://sample.isomer.gov.sg"],
+    },
+    operatingHours: {
+      displayText: "Operating Hours",
+      values: ["Mon - Fri", "8.30 am to 5.00 pm", "Sat & Sun - Closed"],
+    },
+    otherMethods: [
+      {
+        displayText: "Emergency Contact",
+        values: [
+          "https://this-should-still-be-hyperlinked.isomer.gov.sg",
+          "this-should-still-be-hyperlinked@isomer.gov.sg",
+          "12345678",
+        ],
+      },
+      {
+        displayText: "Telegram",
+        values: ["https://t.me/isomer_gov_sg"],
+      },
+      {
+        displayText: "WhatsApp",
+        values: ["+65-63798000 (MFA)"],
+      },
+    ],
+    otherInformation:
+      "For cats and dogs enquiries, please write to this-should-not-by-hyperlinked@isomer.gov.sg. Please note that the Isomer is the <b>bold authority</b> responsible for <a href='https://this-should-not-be-showup.isomer.gov.sg'>cats and dogs matters</a>.",
+  },
 }
