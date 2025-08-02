6dbc1f613ccd5bde5befb28932022b2d241d4f3d
adriangohjw <adriangohjw@gmail.com>
2025-08-03 05:44:30 +0800

    feat: add DGS story and enhance DgsContactInformation component to fetch and display detailed contact data

--- packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.stories.tsx
+++ packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.stories.tsx
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react"
+import { http, HttpResponse } from "msw"
 
 import { withChromaticModes } from "@isomer/storybook-config"
 
 import type { ContactInformationProps } from "~/interfaces"
+import { generateDgsUrl } from "~/hooks/useDgsData/fetchDataFromDgsApi"
 import ContactInformation from "./ContactInformation"
 
 const meta: Meta<ContactInformationProps> = {
@@ -83,5 +85,118 @@ export const Default: Story = {
     ],
     otherInformation:
       "For cats and dogs enquiries, please write to this-should-not-by-hyperlinked@isomer.gov.sg. Please note that the Isomer is the <b>bold authority</b> responsible for <a href='https://this-should-not-be-showup.isomer.gov.sg'>cats and dogs matters</a>.",
+  },
+}
+
+export const Dgs: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get(
+          generateDgsUrl({
+            resourceId: "PLACEHOLDER_RESOURCE_ID",
+            filters: {
+              testFieldKey: "testFieldValue",
+            },
+          }),
+          () => {
+            return HttpResponse.json({
+              success: true,
+              result: {
+                records: [
+                  {
+                    country: "Singapore",
+                    entity_name: "Sentosa",
+                    description:
+                      "Embassy of the Republic of Singapore - Algeria",
+                    telephone: JSON.stringify({
+                      displayText: "Telephone",
+                      values: ["+65-63798000 (MFA)"],
+                    }),
+                    fax: JSON.stringify({
+                      displayText: "Fax",
+                      values: ["+65-64747885 (MFA)"],
+                    }),
+                    email: JSON.stringify({
+                      displayText: "Email",
+                      values: [
+                        "do-not-reply@isomer.gov.sg",
+                        "do-not-reply-pelase@isomer.gov.sg",
+                      ],
+                    }),
+                    website: JSON.stringify({
+                      displayText: "Website",
+                      values: [
+                        "https://www.isomer.gov.sg",
+                        "https://sample.isomer.gov.sg",
+                      ],
+                    }),
+                    operating_hours: JSON.stringify({
+                      displayText: "Operating Hours",
+                      values: [
+                        "Mon - Fri",
+                        "8.30 am to 5.00 pm",
+                        "Sat & Sun - Closed",
+                      ],
+                    }),
+                    entity_details: JSON.stringify([
+                      {
+                        displayText: "Ambassador (Non-Resident)",
+                        values: ["Mr MOHAMMAD Alami Musa"],
+                      },
+                      {
+                        displayText: "Chancery",
+                        values: [
+                          "c/o Ministry of Foreign Affairs",
+                          "Tanglin",
+                          "Singapore 248163",
+                        ],
+                      },
+                    ]),
+                    other_methods: JSON.stringify([
+                      {
+                        displayText: "Emergency Contact",
+                        values: [
+                          "https://this-should-still-be-hyperlinked.isomer.gov.sg",
+                          "this-should-still-be-hyperlinked@isomer.gov.sg",
+                          "12345678",
+                        ],
+                      },
+                      {
+                        displayText: "Telegram",
+                        values: ["https://t.me/isomer_gov_sg"],
+                      },
+                    ]),
+                    other_information:
+                      "For cats and dogs enquiries, please write to this-should-not-by-hyperlinked@isomer.gov.sg. Please note that the Isomer is the <b>bold authority</b> responsible for <a href='https://this-should-not-be-showup.isomer.gov.sg'>cats and dogs matters</a>.",
+                  },
+                ],
+              },
+            })
+          },
+        ),
+      ],
+    },
+  },
+  args: {
+    dataSource: {
+      type: "dgs",
+      resourceId: "PLACEHOLDER_RESOURCE_ID",
+      row: {
+        fieldKey: "testFieldKey",
+        fieldValue: "testFieldValue",
+      },
+    },
+    country: "[dgs:country]",
+    entityName: "[dgs:entity_name]",
+    description: "[dgs:description]",
+    telephone: "[dgs:telephone]",
+    fax: "[dgs:fax]",
+    email: "[dgs:email]",
+    website: "[dgs:website]",
+    operatingHours: "[dgs:operating_hours]",
+    entityDetails: "[dgs:entity_details]",
+    otherMethods: "[dgs:other_methods]",
+    otherInformation: "[dgs:other_information]",
   },
 }
--- packages/components/src/templates/next/components/complex/ContactInformation/DgsContactInformation.tsx
+++ packages/components/src/templates/next/components/complex/ContactInformation/DgsContactInformation.tsx
@@ -1,6 +1,81 @@
-import type { DgsContactInformationProps } from "~/interfaces/complex/ContactInformation"
+import pick from "lodash/pick"
+
+import type {
+  DgsContactInformationProps,
+  NativeContactInformationProps,
+} from "~/interfaces/complex/ContactInformation"
+import { transformDgsField, useDgsData } from "~/hooks/useDgsData"
 import { ContactInformationUI } from "./ContactInformationUI"
 
-export const DgsContactInformation = (props: DgsContactInformationProps) => {
-  return <ContactInformationUI />
+export const DgsContactInformation = ({
+  dataSource: { resourceId, row },
+  ...rest
+}: DgsContactInformationProps) => {
+  const { record, isLoading, isError } = useDgsData({ resourceId, row })
+
+  // TODO: better handling of these non-success states
+  // will check with SY for design
+  if (isLoading || isError || !record) {
+    return <div>Loading...</div>
+  }
+
+  const country = transformDgsField(rest.country, record)
+
+  const entityName = transformDgsField(rest.entityName, record)
+
+  const description = transformDgsField(rest.description, record)
+
+  const otherInformation = transformDgsField(rest.otherInformation, record)
+
+  const telephone = rest.telephone
+    ? (JSON.parse(
+        transformDgsField(rest.telephone, record),
+      ) as NativeContactInformationProps["telephone"])
+    : undefined
+
+  const fax = rest.fax
+    ? (JSON.parse(
+        transformDgsField(rest.fax, record),
+      ) as NativeContactInformationProps["fax"])
+    : undefined
+
+  const email = rest.email
+    ? (JSON.parse(
+        transformDgsField(rest.email, record),
+      ) as NativeContactInformationProps["email"])
+    : undefined
+
+  const website = rest.website
+    ? (JSON.parse(
+        transformDgsField(rest.website, record),
+      ) as NativeContactInformationProps["website"])
+    : undefined
+
+  const operatingHours = rest.operatingHours
+    ? (JSON.parse(
+        transformDgsField(rest.operatingHours, record),
+      ) as NativeContactInformationProps["operatingHours"])
+    : undefined
+
+  const entityDetails = rest.entityDetails
+    ? (JSON.parse(
+        transformDgsField(rest.entityDetails, record),
+      ) as NativeContactInformationProps["entityDetails"])
+    : undefined
+
+  return (
+    <ContactInformationUI
+      country={country}
+      entityName={entityName}
+      description={description}
+      telephone={telephone}
+      fax={fax}
+      email={email}
+      website={website}
+      operatingHours={operatingHours}
+      entityDetails={entityDetails}
+      otherInformation={otherInformation}
+      {...pick(rest, "type", "layout", "site", "LinkComponent")}
+    />
+  )
 }
