6649c49294ef34b09ac427f92335adae79676c27
seaerchin <jiachin@open.gov.sg>
2025-08-07 12:32:11 +0800

    chore: delete unused

--- apps/studio/src/features/editing-experience/components/form-builder/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { CollectionPagePageProps } from "@opengovsg/isomer-components"
-import { atom } from "jotai"
-
-type TagCategories = CollectionPagePageProps["tagCategories"]
-
-export const tagCategoriesAtom = atom<TagCategories | null>(null)
