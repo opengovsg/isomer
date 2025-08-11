139d5a44195c66053b3ec122c3fe91d183f0f027
adriangohjw <adriangohjw@gmail.com>
2025-08-12 04:43:53 +0800

    fix(DownloadButton, LinkButton): update small size variant styles for download and link buttons to improve visual consistency

--- packages/components/src/templates/next/components/internal/DownloadButton/DownloadButton.tsx
+++ packages/components/src/templates/next/components/internal/DownloadButton/DownloadButton.tsx
@@ -19,7 +19,7 @@ const downloadIconStyles = tv({
   base: "h-auto flex-shrink-0",
   variants: {
     size: {
-      sm: "w-2.5 lg:w-3",
+      sm: "w-3.5 lg:w-4",
       base: "w-3.5 lg:w-4",
       lg: "w-4.5 lg:w-5",
     },
--- packages/components/src/templates/next/components/internal/LinkButton/LinkButton.tsx
+++ packages/components/src/templates/next/components/internal/LinkButton/LinkButton.tsx
@@ -16,7 +16,7 @@ const externalIconStyles = tv({
   base: "h-auto flex-shrink-0",
   variants: {
     size: {
-      sm: "w-2.5 lg:w-3",
+      sm: "w-3.5 lg:w-4",
       base: "w-3.5 lg:w-4",
       lg: "w-4.5 lg:w-5",
     },
