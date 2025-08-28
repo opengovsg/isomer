2f9c61e27524a31e8456d59d11121badabff5d05
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-08-28 16:26:01 +0800

    feat: setup base navbar and footer editing pages

--- apps/studio/src/pages/sites/[siteId]/navbar.tsx
+++ apps/studio/src/pages/sites/[siteId]/navbar.tsx
@@ -1,7 +1,9 @@
+import { Grid, GridItem } from "@chakra-ui/react"
 import { ResourceType } from "~prisma/generated/generatedEnums"
 
 import { PermissionsBoundary } from "~/components/AuthWrappers"
 import { siteSchema } from "~/features/editing-experience/schema"
+import { EditNavbarPreview } from "~/features/settings/EditNavbarPreview"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { type NextPageWithLayout } from "~/lib/types"
 import { SiteSettingsLayout } from "~/templates/layouts/SiteSettingsLayout"
@@ -9,7 +11,16 @@ import { SiteSettingsLayout } from "~/templates/layouts/SiteSettingsLayout"
 const NavbarSettingsPage: NextPageWithLayout = () => {
   const { siteId } = useQueryParse(siteSchema)
 
-  return <p>Site {siteId}</p>
+  return (
+    <Grid h="full" w="100%" templateColumns="minmax(37.25rem, 1fr) 1fr" gap={0}>
+      <GridItem colSpan={1} overflow="auto" minW="30rem">
+        <p>Navigation menu</p>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <EditNavbarPreview siteId={Number(siteId)} />
+      </GridItem>
+    </Grid>
+  )
 }
 
 NavbarSettingsPage.getLayout = (page) => {
