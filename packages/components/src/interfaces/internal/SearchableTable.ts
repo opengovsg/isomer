0b0a6db16ccdf290fe8837b5fe333e4d6aec4317
adriangohjw <adriangohjw@gmail.com>
2025-09-01 18:01:28 +0800

    refactor: consolidate site and LinkComponent props into BaseSearchableTableClientProps for cleaner type definitions

--- packages/components/src/interfaces/internal/SearchableTable.ts
+++ packages/components/src/interfaces/internal/SearchableTable.ts
@@ -45,39 +45,35 @@ export const SearchableTableSchema = Type.Intersect([
   Type.Union([NativeSearchableTableSchema, DGSSearchableTableSchema]),
 ])
 
+type BaseSearchableTableClientProps = Static<
+  typeof BaseSearchableTableSchema
+> & {
+  site: IsomerSiteProps
+  LinkComponent?: LinkComponentType
+}
+
 // note: ideally we should not pass entire "site" object to the client component
 // as it can be quite large and increase page size
 // but since this is not a common component, we will allow it for now :(
-export type SearchableTableClientProps = Static<
-  typeof BaseSearchableTableSchema
-> &
+export type SearchableTableClientProps = BaseSearchableTableClientProps &
   Pick<NativeSearchableTableProps, "headers"> & {
     items: {
       row: NativeSearchableTableProps["items"][number]
       key: string
     }[]
   } & {
-    site: IsomerSiteProps
-    LinkComponent?: LinkComponentType
     isLoading?: boolean
     isError?: boolean
   }
 
-export type NativeSearchableTableProps = Static<
-  typeof BaseSearchableTableSchema
+export type NativeSearchableTableProps = BaseSearchableTableClientProps &
+  Static<typeof NativeSearchableTableSchema>
+
+export type DGSSearchableTableProps = BaseSearchableTableClientProps &
+  Static<typeof DGSSearchableTableSchema>
+
+export type SearchableTableProps = Pick<
+  BaseSearchableTableClientProps,
+  "site" | "LinkComponent"
 > &
-  Static<typeof NativeSearchableTableSchema> & {
-    site: IsomerSiteProps
-    LinkComponent?: LinkComponentType
-  }
-
-export type DGSSearchableTableProps = Static<typeof BaseSearchableTableSchema> &
-  Static<typeof DGSSearchableTableSchema> & {
-    site: IsomerSiteProps
-    LinkComponent?: LinkComponentType
-  }
-
-export type SearchableTableProps = Static<typeof SearchableTableSchema> & {
-  site: IsomerSiteProps
-  LinkComponent?: LinkComponentType
-}
+  Static<typeof SearchableTableSchema>
