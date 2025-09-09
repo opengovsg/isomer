e3da30acf191e026e7418c1ef87275115d47443a
adriangohjw <adriangohjw@gmail.com>
2025-09-10 01:54:42 +0800

    feat(layout): reintroduce Vica and Askgov widgets in RootLayout with conditional rendering based on config

--- tooling/template/app/layout.tsx
+++ tooling/template/app/layout.tsx
@@ -3,6 +3,11 @@ import config from "@/data/config.json"
 import "@/styles/globals.css"
 
 import type { Metadata } from "next"
+import {
+  AskgovWidget,
+  VicaStylesheet,
+  VicaWidget,
+} from "@opengovsg/isomer-components"
 
 export const dynamic = "force-static"
 
@@ -14,9 +19,32 @@ export const metadata: Metadata = {
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const { site } = config
+
   return (
     <html lang="en" data-theme={config.site.theme || "isomer-next"}>
       <body className="antialiased">{children}</body>
+
+      {/* Ensures that the webchat widget only loads after the page has loaded */}
+      {/* Note: did not account for both being added to the config as it's a very unlikely scenario and there's "correct" way to handle this */}
+      {/* @ts-expect-error sample config file not typed */}
+      {site.vica && (
+        <>
+          <VicaStylesheet
+            environment={process.env.NEXT_PUBLIC_ISOMER_NEXT_ENVIRONMENT}
+          />
+          {/* @ts-expect-error sample config file not typed */}
+          <VicaWidget site={site} {...site.vica} />
+        </>
+      )}
+      {/* @ts-expect-error sample config file not typed */}
+      {site.askgov && (
+        <AskgovWidget
+          environment={process.env.NEXT_PUBLIC_ISOMER_NEXT_ENVIRONMENT}
+          // @ts-expect-error sample config file not typed
+          {...site.askgov}
+        />
+      )}
     </html>
   )
 }
