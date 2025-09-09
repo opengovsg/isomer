3ac4e8dcdc50a1f235642fa30e58108de78b1ce3
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-09-09 14:49:38 +0800

    chore: remove unused context

--- apps/studio/src/templates/layouts/SiteSettingsLayout.tsx
+++ apps/studio/src/templates/layouts/SiteSettingsLayout.tsx
@@ -3,7 +3,6 @@ import { useDisclosure } from "@chakra-ui/react"
 import { EnforceLoginStatePageWrapper } from "~/components/AuthWrappers"
 import { CmsContainerWrapper } from "~/components/CmsSidebar"
 import { LayoutHead } from "~/components/LayoutHead"
-import { CmsSidenavContextProvider } from "~/contexts/CmsSidenavContext"
 import { siteSchema } from "~/features/editing-experience/schema"
 import { SettingsSidenav } from "~/features/settings/SettingsSidenav"
 import { useQueryParse } from "~/hooks/useQueryParse"
@@ -19,23 +18,18 @@ export const SiteSettingsLayout: GetLayout = (page) => {
 
   return (
     <EnforceLoginStatePageWrapper>
-      <CmsSidenavContextProvider
-        isSidenavOpen={isSidenavOpen}
-        onSidenavClose={onSidenavClose}
+      <LayoutHead />
+      <CmsContainerWrapper
+        siteId={siteId}
+        sidenav={
+          isSidenavOpen ? (
+            <SettingsSidenav onSidenavClose={onSidenavClose} />
+          ) : undefined
+        }
+        onSidenavOpen={onSidenavOpen}
       >
-        <LayoutHead />
-        <CmsContainerWrapper
-          siteId={siteId}
-          sidenav={
-            isSidenavOpen ? (
-              <SettingsSidenav onSidenavClose={onSidenavClose} />
-            ) : undefined
-          }
-          onSidenavOpen={onSidenavOpen}
-        >
-          {page}
-        </CmsContainerWrapper>
-      </CmsSidenavContextProvider>
+        {page}
+      </CmsContainerWrapper>
     </EnforceLoginStatePageWrapper>
   )
 }
