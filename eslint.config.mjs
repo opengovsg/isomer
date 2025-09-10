0efc1fb664b3c33a29dee3e7db0b7f8b19c0484a
gautam <gautam@open.gov.sg>
2025-09-10 10:57:15 +0800

    fix: add root level project globs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
 import baseConfig from "@isomer/eslint-config/base";
 
 /** @type {import('typescript-eslint').Config} */
-export default [...baseConfig, { files: ["**/*.ts", "**/*.tsx"] }];
+export default [
+  ...baseConfig,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: [
+          "./apps/*/tsconfig.json",
+          "./packages/*/tsconfig.json",
+          "./tooling/*/tsconfig.json",
+        ],
+        tsconfigRootDir: import.meta.dirname, // makes globs resolve correctly
+      },
+    },
+  },
+];
