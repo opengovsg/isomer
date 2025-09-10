0d7798ad9cec3dbd45d2ac3d5c9fcd5548187cd6
adriangohjw <adriangohjw@gmail.com>
2025-09-10 23:32:45 +0800

    refactor: update DGS resource ID to dataset ID and enhance schema descriptions

--- packages/components/src/interfaces/integration/dgs.ts
+++ packages/components/src/interfaces/integration/dgs.ts
@@ -8,8 +8,8 @@ const DgsDataSourceFieldsSchema = Type.Object({
     default: DATA_SOURCE_TYPE.dgs,
   }),
   resourceId: Type.String({
-    title: "DGS Resource ID",
-    description: "The resource ID to fetch data from DGS",
+    title: "DGS Dataset ID",
+    description: "Looks like this: d_1a2b3c4d5e6f7g8h9i0j",
   }),
   filters: Type.Optional(
     Type.Array(
@@ -17,9 +17,20 @@ const DgsDataSourceFieldsSchema = Type.Object({
         fieldKey: Type.String(),
         fieldValue: Type.String(),
       }),
+      {
+        // unlikely to be used for Studio users,
+        // so hiding it to reduce complexity of the UI for them
+        format: "hidden",
+      },
     ),
   ),
-  sort: Type.Optional(Type.String()),
+  sort: Type.Optional(
+    Type.String({
+      // unlikely to be used for Studio users,
+      // so hiding it to reduce complexity of the UI for them
+      format: "hidden",
+    }),
+  ),
 })
 
 export const DgsDataSourceSchema = Type.Object({
