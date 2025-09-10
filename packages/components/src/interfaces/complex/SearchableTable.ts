f14d94ebc0809acb6405497e3f593d21514f4bde
adriangohjw <adriangohjw@gmail.com>
2025-09-10 18:53:31 +0800

    add dropdown option naming

--- packages/components/src/interfaces/complex/SearchableTable.ts
+++ packages/components/src/interfaces/complex/SearchableTable.ts
@@ -2,7 +2,11 @@ import type { Static } from "@sinclair/typebox"
 import { Type } from "@sinclair/typebox"
 
 import type { IsomerSiteProps, LinkComponentType } from "~/types"
-import { DgsDataSourceSchema, NativeDataSourceSchema } from "../integration"
+import {
+  DATA_SOURCE_TYPE,
+  DgsDataSourceSchema,
+  NativeDataSourceSchema,
+} from "../integration"
 
 export const SEARCHABLE_TABLE_TYPE = "searchabletable"
 
@@ -16,36 +20,48 @@ const BaseSearchableTableSchema = Type.Object({
   ),
 })
 
-export const NativeSearchableTableSchema = Type.Intersect([
-  NativeDataSourceSchema,
-  Type.Object({
-    headers: Type.Array(Type.Union([Type.String(), Type.Number()])),
-    items: Type.Array(Type.Array(Type.Union([Type.String(), Type.Number()]))),
-  }),
-])
+export const NativeSearchableTableSchema = Type.Intersect(
+  [
+    NativeDataSourceSchema,
+    Type.Object({
+      headers: Type.Array(Type.Union([Type.String(), Type.Number()])),
+      items: Type.Array(Type.Array(Type.Union([Type.String(), Type.Number()]))),
+    }),
+  ],
+  {
+    title: "Native", // TODO: think of a better title that makes sense for user
+  },
+)
 
-export const DGSSearchableTableSchema = Type.Intersect([
-  DgsDataSourceSchema,
-  Type.Object({
-    headers: Type.Array(
-      Type.Object({
-        label: Type.String({
-          title: "Label",
-          description: "The label of the header",
+export const DGSSearchableTableSchema = Type.Intersect(
+  [
+    DgsDataSourceSchema,
+    Type.Object({
+      headers: Type.Array(
+        Type.Object({
+          label: Type.String({
+            title: "Label",
+            description: "The label of the header",
+          }),
+          key: Type.String({
+            title: "Key",
+            description: "The key of the header in DGS table",
+          }),
         }),
-        key: Type.String({
-          title: "Key",
-          description: "The key of the header in DGS table",
-        }),
-      }),
-      { minItems: 1 },
-    ),
-  }),
-])
+        { minItems: 1 },
+      ),
+    }),
+  ],
+  {
+    title: "DGS (data.gov.sg)",
+  },
+)
 
 export const SearchableTableSchema = Type.Intersect([
   BaseSearchableTableSchema,
-  Type.Union([NativeSearchableTableSchema, DGSSearchableTableSchema]),
+  Type.Union([NativeSearchableTableSchema, DGSSearchableTableSchema], {
+    title: "Data source",
+  }),
 ])
 
 type BaseSearchableTableClientProps = Static<
