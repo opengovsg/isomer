19787c02d95c7be573f38ee66c4c00b8e691ed05
seaerchin <jiachin@open.gov.sg>
2025-09-11 17:07:34 +0800

    chore: add s3 sync command

--- tooling/site-launch/index.ts
+++ tooling/site-launch/index.ts
@@ -6,69 +6,77 @@ import {
 import { archiveRepo } from "github"
 import { createIndirection } from "indirection"
 import { requestAcmViaClient } from "request-acm"
+import { s3sync } from "s3"
 
 import { cleanup, main as migrate } from "@isomer/seed-from-repo"
 
 const profile = process.env.AWS_PROFILE
 
-await confirm({
-  message: `Please ensure that this is your $AWS_PROFILE: \`${profile}\` and you have executed \`aws sso login\``,
-})
-
-if (!profile) throw new Error("No AWS_PROFILE found in environment variables")
-
-const domain = await input({
-  message: "Enter the domain (FQDN) of the site (eg: www.isomer.gov.sg):",
-})
-const needsAcm = await confirm({
-  message: `Do you need to generate the first window record for this domain?`,
-})
-
-if (needsAcm) await requestAcmViaClient(domain)
-
-const long = await input({ message: "Enter the long name of the site:" })
-const codebuildId = await input({
-  message: "Enter the code-build name of the site (eg: ogp-corp)",
-})
-
-const isGithub = await confirm({
-  message: `Is this a Github site?`,
-})
-
-if (isGithub) {
-  const repo = await input({
-    message: "Enter the github repo for the site (eg: `isomer-corp`):",
-  })
-  await archiveRepo(repo)
-  await createSearchSgClientForGithub({ domain, name: long, repo })
-  const siteId = await input({
-    message: "Enter the site id for the site:",
+const launch = async () => {
+  await confirm({
+    message: `Please ensure that this is your $AWS_PROFILE: \`${profile}\` and you have executed \`aws sso login\``,
   })
 
-  await confirm({ message: "Have you ran `npm run db:connect`?" })
-  // TODO: validate last amplify build
-  // cleanup old assets/repos folder
-  // connect to db automatically
-  // create empty site
-  await migrate(repo, siteId as unknown as number)
-  // TODO: s3 sync here
+  if (!profile) throw new Error("No AWS_PROFILE found in environment variables")
 
-  // NOTE: Perform cleanup after s3 sync is done
-  // cleanup(repo)
-} else {
-  // TODO: create site
-  await createSearchSgClientForStudio({ domain, name: long })
+  const domain = await input({
+    message: "Enter the domain (FQDN) of the site (eg: www.isomer.gov.sg):",
+  })
+  const needsAcm = await confirm({
+    message: `Do you need to generate the first window record for this domain?`,
+  })
+
+  if (needsAcm) await requestAcmViaClient(domain)
+
+  const long = await input({ message: "Enter the long name of the site:" })
+  const codebuildId = await input({
+    message: "Enter the code-build name of the site (eg: ogp-corp)",
+  })
+
+  const isGithub = await confirm({
+    message: `Is this a Github site?`,
+  })
+
+  if (isGithub) {
+    const repo = await input({
+      message: "Enter the github repo for the site (eg: `isomer-corp`):",
+    })
+    await archiveRepo(repo)
+    await createSearchSgClientForGithub({ domain, name: long, repo })
+    const siteId = await input({
+      message: "Enter the site id for the site:",
+    })
+
+    await confirm({ message: "Have you ran `npm run db:connect`?" })
+    // TODO: validate last amplify build
+    // cleanup old assets/repos folder
+    // connect to db automatically
+    // create empty site
+    await migrate(repo, siteId as unknown as number)
+    // TODO: s3 sync here
+    await s3sync(siteId as unknown as number)
+
+    // NOTE: Perform cleanup after s3 sync is done
+    // cleanup(repo)
+  } else {
+    // TODO: create site
+    await createSearchSgClientForStudio({ domain, name: long })
+  }
+
+  const hasInfra = await confirm({
+    message: `Have you spun up the cloudfront site?`,
+  })
+
+  if (hasInfra) {
+    await createIndirection(domain, codebuildId)
+  } else {
+    // tell the user to re-up and pause here?
+  }
+  // TODO: check amplify for github sites to ensure builds are successful
+  // TODO: await startCodeBuild(codebuildId)
+  // TODO: add admins
+  // await addUsersToSite({ siteId, users })
 }
 
-const hasInfra = await confirm({
-  message: `Have you spun up the cloudfront site?`,
-})
-
-if (hasInfra) {
-  await createIndirection(domain, codebuildId)
-} else {
-  // tell the user to re-up and pause here?
-}
-// TODO: check amplify for github sites to ensure builds are successful
-// TODO: await startCodeBuild(codebuildId)
-// TODO: add admins
+// await launch()
+await s3sync(9999)
