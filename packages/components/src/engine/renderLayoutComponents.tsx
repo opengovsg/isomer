8cf7ec01397a6494e5b2632fa9c45520b7fee8a3
adriangohjw <adriangohjw@gmail.com>
2025-09-14 03:21:24 +0800

    feat: add RenderLayoutComponents for conditional rendering of layout elements

---
+++ packages/components/src/engine/renderLayoutComponents.tsx
@@ -0,0 +1,60 @@
+import type { IsomerPageSchemaType } from "~/types"
+import {
+  AskgovWidget,
+  FontPreload,
+  GoogleTagManagerBody,
+  GoogleTagManagerHeader,
+  GoogleTagManagerPreload,
+  VicaStylesheet,
+  VicaWidget,
+  Wogaa,
+} from "../templates/next/components/internal"
+
+type RenderLayoutComponentsProps = Pick<
+  IsomerPageSchemaType,
+  "site" | "ScriptComponent"
+>
+
+export const RenderLayoutComponents = ({
+  site,
+  ScriptComponent,
+}: RenderLayoutComponentsProps) => {
+  const shouldIncludeGTM =
+    site.environment === "production" &&
+    (!!site.siteGtmId || !!site.isomerGtmId)
+
+  return (
+    <>
+      <FontPreload />
+
+      {site.isGovernment && <Wogaa ScriptComponent={ScriptComponent} />}
+
+      {shouldIncludeGTM && (
+        <>
+          <GoogleTagManagerPreload />
+          <GoogleTagManagerHeader
+            siteGtmId={site.siteGtmId}
+            isomerGtmId={site.isomerGtmId}
+            ScriptComponent={ScriptComponent}
+          />
+          <GoogleTagManagerBody
+            siteGtmId={site.siteGtmId}
+            isomerGtmId={site.isomerGtmId}
+          />
+        </>
+      )}
+
+      {/* Ensures that the webchat widget only loads after the page has loaded */}
+      {/* Note: did not account for both being added to the config as it's a very unlikely scenario and there's "correct" way to handle this */}
+      {site.vica && (
+        <>
+          <VicaStylesheet environment={site.environment} />
+          <VicaWidget site={site} {...site.vica} />
+        </>
+      )}
+      {site.askgov && (
+        <AskgovWidget environment={site.environment} {...site.askgov} />
+      )}
+    </>
+  )
+}
