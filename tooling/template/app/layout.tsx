17b73281c6eaa918b3e91c0213d7d4ac491930e6
adriangohjw <adriangohjw@gmail.com>
2025-09-14 04:31:46 +0800

    feat: integrate RenderLayoutComponents and update layout with sitemap support

--- tooling/template/app/layout.tsx
+++ tooling/template/app/layout.tsx
@@ -1,8 +1,11 @@
 import config from "@/data/config.json"
+import sitemap from "@/sitemap.json"
 
 import "@/styles/globals.css"
 
 import type { Metadata } from "next"
+import Script from "next/script"
+import { RenderLayoutComponents } from "@opengovsg/isomer-components"
 
 export const dynamic = "force-static"
 
@@ -16,6 +19,18 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" data-theme={config.site.theme || "isomer-next"}>
+      <RenderLayoutComponents
+        site={{
+          ...config.site,
+          environment: process.env.NEXT_PUBLIC_ISOMER_NEXT_ENVIRONMENT,
+          // TODO: fixup all the typing errors
+          // @ts-ignore to fix when types are proper
+          siteMap: sitemap,
+          assetsBaseUrl: process.env.NEXT_PUBLIC_ASSETS_BASE_URL,
+          isomerGtmId: process.env.NEXT_PUBLIC_ISOMER_GOOGLE_TAG_MANAGER_ID,
+        }}
+        ScriptComponent={Script}
+      />
       <body className="antialiased">{children}</body>
     </html>
   )
