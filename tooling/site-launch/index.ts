64eebb2626d9fd1e331464005993f20d56259a6d
seaerchin <jiachin@open.gov.sg>
2025-09-15 17:10:36 +0800

    chore: add migration for sites

--- tooling/site-launch/index.ts
+++ tooling/site-launch/index.ts
@@ -58,6 +58,7 @@ const launch = async () => {
 
     await confirm({ message: "Have you ran `npm run db:connect`?" })
     await migrate(repo, siteId)
+    await migrateTagsOfSite(siteId)
 
     await s3sync(siteId)
 
