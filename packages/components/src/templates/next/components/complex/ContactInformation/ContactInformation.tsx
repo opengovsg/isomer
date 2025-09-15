316b733be52e1abffe2dddbc4e95cd14fa4212ac
adriangohjw <adriangohjw@gmail.com>
2025-09-16 00:38:22 +0800

    refactor: streamline ContactInformation component by destructuring props and improving type handling

--- packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.tsx
+++ packages/components/src/templates/next/components/complex/ContactInformation/ContactInformation.tsx
@@ -1,46 +1,35 @@
 import omit from "lodash/omit"
 
-import type {
-  ContactInformationProps,
-  DgsContactInformationProps,
-  NativeContactInformationProps,
-} from "~/interfaces"
+import type { ContactInformationProps } from "~/interfaces"
 import { DATA_SOURCE_TYPE } from "~/interfaces/integration"
 import { getReferenceLinkHref } from "~/utils"
 import { DgsContactInformation } from "./DgsContactInformation"
 import { NativeContactInformation } from "./NativeContactInformation"
 
-const ContactInformation = (props: ContactInformationProps) => {
+const ContactInformation = ({
+  dataSource,
+  ...rest
+}: ContactInformationProps) => {
   const uiProps = {
-    ...omit(props, ["url", "site"]),
+    ...omit(rest, ["url", "site"]),
     referenceLinkHref: getReferenceLinkHref(
-      props.url,
-      props.site.siteMap,
-      props.site.assetsBaseUrl,
+      rest.url,
+      rest.site.siteMap,
+      rest.site.assetsBaseUrl,
     ),
   }
 
-  if (!props.dataSource) {
-    return (
-      <NativeContactInformation
-        {...(uiProps as NativeContactInformationProps)}
-      />
-    )
+  // For backward compatibility, where dataSource is not provided,
+  if (!dataSource) {
+    return <NativeContactInformation {...uiProps} />
   }
 
-  const { type } = props.dataSource
-
+  const { type } = dataSource
   switch (type) {
     case DATA_SOURCE_TYPE.native:
-      return (
-        <NativeContactInformation
-          {...(uiProps as NativeContactInformationProps)}
-        />
-      )
+      return <NativeContactInformation {...uiProps} />
     case DATA_SOURCE_TYPE.dgs:
-      return (
-        <DgsContactInformation {...(uiProps as DgsContactInformationProps)} />
-      )
+      return <DgsContactInformation dataSource={dataSource} {...uiProps} />
     default:
       const _exhaustiveCheck: never = type
       return _exhaustiveCheck
