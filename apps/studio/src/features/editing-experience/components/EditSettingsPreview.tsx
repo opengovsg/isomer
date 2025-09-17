fc9be9a1c7456fdfcebc2e4cc50a64655d95c9b4
seaerchin <jiachin@open.gov.sg>
2025-09-17 22:10:13 +0800

    feat: edit settings preview

---
+++ apps/studio/src/features/editing-experience/components/EditSettingsPreview.tsx
@@ -0,0 +1,32 @@
+import { useQueryParse } from "~/hooks/useQueryParse"
+import { trpc } from "~/utils/trpc"
+import { siteSchema } from "../schema"
+import Preview from "./Preview"
+import { ViewportContainer } from "./ViewportContainer"
+
+export const EditSettingsPreview = (): JSX.Element => {
+  const { siteId: rawSiteId } = useQueryParse(siteSchema)
+  const siteId = Number(rawSiteId)
+  const [{ id }] = trpc.page.getRootPage.useSuspenseQuery({
+    siteId,
+  })
+  const [{ content }] = trpc.page.readPageAndBlob.useSuspenseQuery({
+    pageId: Number(id),
+    siteId,
+  })
+
+  return (
+    <ViewportContainer siteId={siteId}>
+      <Preview
+        siteId={siteId}
+        resourceId={Number(id)}
+        permalink={"/"}
+        lastModified={new Date().toISOString()}
+        version="0.1.0"
+        content={content.content}
+        layout="homepage"
+        page={content.page}
+      />
+    </ViewportContainer>
+  )
+}
