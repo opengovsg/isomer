0ff9ac638eb734fe219768a92d680cfc3df2dd28
adriangohjw <adriangohjw@gmail.com>
2025-09-17 13:00:03 +0800

    feat: add HeroSearchbar component and integrate into Hero template

---
+++ packages/components/src/templates/next/components/complex/Hero/HeroSearchbar.tsx
@@ -0,0 +1,26 @@
+import type { HeroProps } from "~/interfaces/complex/Hero"
+import { SearchSGInputBox } from "../../internal"
+import { ComponentContent } from "../../internal/customCssClass"
+
+export const HeroSearchbar = ({ title, subtitle, site }: HeroProps) => {
+  const clientId =
+    (site.search?.type === "searchSG" && site.search?.clientId) || ""
+
+  return (
+    <section className="flex w-full flex-col bg-[linear-gradient(275deg,theme(colors.white)_6.27%,theme(colors.brand.canvas.DEFAULT)_100%)]">
+      <div
+        className={`mx-auto flex w-full flex-col gap-6 px-6 pb-12 pt-11 md:gap-9 lg:pb-24 lg:pt-16 ${ComponentContent}`}
+      >
+        <div className="flex flex-col items-center gap-5 text-center text-base-content-strong md:gap-6 lg:mx-auto lg:max-w-[66.67%]">
+          <h1 className="prose-display-xl w-full text-center">{title}</h1>
+          {subtitle && (
+            <p className="prose-title-lg-regular w-full text-center">
+              {subtitle}
+            </p>
+          )}
+          <SearchSGInputBox clientId={clientId} className="w-full pt-3" />
+        </div>
+      </div>
+    </section>
+  )
+}
