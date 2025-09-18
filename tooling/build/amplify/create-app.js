c4382fe18fc55158aeb08b23116a1f9ea9877eac
adriangohjw <adriangohjw@gmail.com>
2025-09-18 09:43:30 +0800

    feat: enhance create-app script to generate secure passwords and output app details to a file

--- tooling/build/amplify/create-app.js
+++ tooling/build/amplify/create-app.js
@@ -2,8 +2,12 @@ const {
   AmplifyClient,
   CreateAppCommand,
   CreateBranchCommand,
+  UpdateBranchCommand,
   StartJobCommand,
 } = require("@aws-sdk/client-amplify")
+const fs = require("fs")
+const crypto = require("crypto")
+require("dotenv").config()
 
 const AMPLIFY_BUILD_SPEC = `
 version: 1
@@ -24,9 +28,19 @@ frontend:
 
 const amplifyClient = new AmplifyClient({ region: "ap-southeast-1" })
 
+// Function to generate a secure random password using Node.js crypto module
+const generatePassword = () => {
+  return crypto
+    .randomBytes(12)
+    .toString("base64")
+    .replace(/[+/=]/g, "")
+    .substring(0, 12)
+}
+
 const createApp = async (appName) => {
   console.log("Creating app:", appName)
   let appId = ""
+  const password = generatePassword()
 
   const params = new CreateAppCommand({
     name: appName,
@@ -74,6 +88,18 @@ const createApp = async (appName) => {
     )
     .then(() =>
       amplifyClient.send(
+        new UpdateBranchCommand({
+          appId,
+          branchName: "staging",
+          enableBasicAuth: true,
+          basicAuthCredentials: Buffer.from(`user:${password}`).toString(
+            "base64",
+          ),
+        }),
+      ),
+    )
+    .then(() =>
+      amplifyClient.send(
         new StartJobCommand({
           appId,
           branchName: "main",
@@ -90,22 +116,39 @@ const createApp = async (appName) => {
         }),
       ),
     )
+
+  // Return app information for output file
+  return {
+    repoName: appName,
+    appId: appId,
+    password: password,
+  }
 }
 
 const main = async () => {
-  const apps = [
-    "moh-hpp-next",
-    "moh-healthwatch-next",
-    "moh-hcsa-next",
-    "moh-ifeelyoung-next",
-    "moh-biosafety-next",
-    "moh-dc-next",
-    "moh-prepare-next",
-  ]
+  // TODO: UPDATE THIS TO THE ACTUAL APPS
+  const apps = ["hello-adrian-test-script-next"]
+
+  const appResults = []
 
   for (const app of apps) {
-    await createApp(app)
+    try {
+      const result = await createApp(app)
+      appResults.push(result)
+      console.log(`Successfully created app: ${app}`)
+    } catch (error) {
+      console.error(`Failed to create app ${app}:`, error)
+    }
   }
+
+  // Generate output file
+  const outputContent = appResults
+    .map((result) => `${result.repoName},${result.appId},${result.password}`)
+    .join("\n")
+
+  fs.writeFileSync("amplify-apps-output.txt", outputContent)
+  console.log("Output file generated: amplify-apps-output.txt")
+  console.log("Format: REPO_NAME,AMPLIFY_APP_ID,STAGING_PASSWORD")
 }
 
 main()
