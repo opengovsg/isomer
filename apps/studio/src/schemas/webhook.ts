1031589f307c48060f609a842d7dea8938427cc9
Gautam Manek <gautam@open.gov.sg>
2025-09-24 09:50:24 +0800

    fix: PR comment fixes

--- apps/studio/src/schemas/webhook.ts
+++ apps/studio/src/schemas/webhook.ts
@@ -1,7 +1,15 @@
 import { BuildStatusType } from "@prisma/client"
 import { z } from "zod"
 
-// Extract everything after "build/" from the ARN string
+/**
+ * Extract the build ID from the ARN string. When eventbridge sends the ARN,
+ * it is in the format: arn:aws:codebuild:region:account-id:build/build-id
+ * The build ID is the part after "build/", which is the part returned by
+ * startProjectById in codebuild.service.ts, and is saved in the database
+ * We only require the build ID for our processing, and can ignore the rest of the ARN
+ * @param arn The ARN string to extract the build ID from
+ * @returns The extracted build ID, or null if not found
+ */
 export const buildIdFromArn = (arn: string) => {
   const regex = /build\/(.+)$/
   const match = regex.exec(arn)
