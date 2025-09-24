d2d129791eaf9ac0b178e98b149c1290c403a5cd
adriangohjw <adriangohjw@gmail.com>
2025-09-24 09:19:32 +0800

    fix: improve password generation logic in create-app script to ensure a minimum length of 12 characters

--- tooling/build/amplify/create-app.js
+++ tooling/build/amplify/create-app.js
@@ -36,11 +36,18 @@ const amplifyClient = new AmplifyClient({
 
 // Function to generate a secure random password using Node.js crypto module
 const generatePassword = () => {
-  return crypto
-    .randomBytes(12)
-    .toString("base64")
-    .replace(/[+/=]/g, "")
-    .substring(0, 12)
+  let password = ""
+
+  // Keep generating until we have at least 12 characters
+  while (password.length < 12) {
+    const randomString = crypto
+      .randomBytes(16)
+      .toString("base64")
+      .replace(/[+/=]/g, "")
+    password += randomString
+  }
+
+  return password.substring(0, 12)
 }
 
 const createApp = (appName) => {
