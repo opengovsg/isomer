658ebc913228b85b889c9d3d8ca456d456f5c883
seaerchin <jiachin@open.gov.sg>
2025-10-08 14:47:27 +0800

    chore: add character count

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/TipTapEditor/TiptapSimpleProseEditor.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/TipTapEditor/TiptapSimpleProseEditor.tsx
@@ -1,6 +1,10 @@
+import { Text } from "@chakra-ui/react"
+
 import { SimpleProseMenuBar } from "~/components/PageEditor/MenuBar"
 import { useSimpleProseEditor } from "~/features/editing-experience/hooks/useTextEditor/useTextEditor"
 import { Editor } from "./components"
+
+const BANNER_MAX_CHARACTERS = 250
 
 export function TiptapSimpleProseEditor({
   editor,
@@ -10,5 +14,13 @@ export function TiptapSimpleProseEditor({
   // TODO: Add a loading state or use suspense
   if (!editor) return null
 
-  return <Editor isNested menubar={SimpleProseMenuBar} editor={editor} />
+  return (
+    <>
+      <Editor isNested menubar={SimpleProseMenuBar} editor={editor} />
+      <Text textStyle="body-2" mt="0.5rem" color="base.content.medium">
+        {BANNER_MAX_CHARACTERS - editor.storage.characterCount.characters()}{" "}
+        characters left
+      </Text>
+    </>
+  )
 }
