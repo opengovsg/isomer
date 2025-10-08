1ab04ebc67d5a31031f104888045067f08f51d3a
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-10-08 10:04:33 +0800

    chore: finalise navbar footer editing experience

--- apps/studio/src/pages/sites/[siteId]/settings/navbar.tsx
+++ apps/studio/src/pages/sites/[siteId]/settings/navbar.tsx
@@ -1,8 +1,10 @@
 import type { NavbarSchemaType } from "@opengovsg/isomer-components"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 import { Grid, GridItem } from "@chakra-ui/react"
 import { useToast } from "@opengovsg/design-system-react"
 import { ResourceType } from "~prisma/generated/generatedEnums"
+import isEqual from "lodash/isEqual"
 
 import { PermissionsBoundary } from "~/components/AuthWrappers"
 import { ISOMER_SUPPORT_EMAIL } from "~/constants/misc"
@@ -10,15 +12,20 @@ import { BRIEF_TOAST_SETTINGS } from "~/constants/toast"
 import { siteSchema } from "~/features/editing-experience/schema"
 import { EditNavbarPreview } from "~/features/settings/EditNavbarPreview"
 import { NavbarEditor } from "~/features/settings/NavbarEditor"
+import { useNavigationEffect } from "~/hooks/useNavigationEffect"
+import { useNewSettingsPage } from "~/hooks/useNewSettingsPage"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { type NextPageWithLayout } from "~/lib/types"
 import { SiteSettingsLayout } from "~/templates/layouts/SiteSettingsLayout"
 import { trpc } from "~/utils/trpc"
 
 const NavbarSettingsPage: NextPageWithLayout = () => {
+  const isNewSettingsPageEnabled = useNewSettingsPage()
+  const router = useRouter()
   const { siteId } = useQueryParse(siteSchema)
   const utils = trpc.useUtils()
   const toast = useToast(BRIEF_TOAST_SETTINGS)
+  const [nextUrl, setNextUrl] = useState("")
 
   const [{ content }] = trpc.site.getNavbar.useSuspenseQuery({
     id: Number(siteId),
@@ -45,11 +52,21 @@ const NavbarSettingsPage: NextPageWithLayout = () => {
   const [previewNavbarState, setPreviewNavbarState] = useState<
     NavbarSchemaType | undefined
   >(content)
+  const isOpen = !!nextUrl
+  const isDirty = !isEqual(content, previewNavbarState)
 
   const handleSaveNavbar = (data: NavbarSchemaType | undefined) => {
     if (!data) return
     saveNavbar({ siteId: Number(siteId), navbar: JSON.stringify(data) })
   }
+
+  useNavigationEffect({ isOpen, isDirty, callback: setNextUrl })
+
+  useEffect(() => {
+    if (!isNewSettingsPageEnabled) {
+      void router.push(`/sites/${siteId}/settings`)
+    }
+  }, [isNewSettingsPageEnabled, router, siteId])
 
   return (
     <Grid h="full" w="100%" templateColumns="minmax(37.25rem, 1fr) 1fr" gap={0}>
