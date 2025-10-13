0302eca3a414e0c12d66d1f68215bddc9b456821
adriangohjw <adriangohjw@gmail.com>
2025-10-13 14:12:09 +0800

    docs: update comment in useDgsData types file to clarify the reason for not using "fields" and prevent runtime errors

--- packages/components/src/hooks/useDgsData/types.ts
+++ packages/components/src/hooks/useDgsData/types.ts
@@ -1,4 +1,4 @@
-// not using "fields" to prevent runtime errors,
+// NOTE: not using "fields" to prevent runtime errors,
 // as DGS returns errors for invalid fields,
 // which can change unpredictably if admins update the dataset schema.
 export interface DgsApiDatasetSearchParams {
