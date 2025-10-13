fc4a4fb08003c39f13df6ddb46da9c47d81e9166
gautam <gautam@open.gov.sg>
2025-10-13 18:01:40 +0800

    fix: types

--- apps/studio/prisma/migrations/20251013095353_unique_version_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[resourceId,versionNum]` on the table `Version` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Version_resourceId_versionNum_key" ON "Version"("resourceId", "versionNum");
