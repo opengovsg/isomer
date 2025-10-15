af4bee7fa2cbe75ba5cc9abb3547c41ba36c1b9e
seaerchin <jiachin@open.gov.sg>
2025-10-15 15:18:53 +0800

    chore: use `import type`

--- apps/studio/src/components/Settings/SettingsPreviewGridItem.tsx
+++ apps/studio/src/components/Settings/SettingsPreviewGridItem.tsx
@@ -1,4 +1,5 @@
-import { GridItem, GridItemProps } from "@chakra-ui/react"
+import type { GridItemProps } from "@chakra-ui/react"
+import { GridItem } from "@chakra-ui/react"
 
 export const SettingsPreviewGridItem = (props: GridItemProps) => {
   return <GridItem colSpan={1} overflowX="scroll" {...props} />
