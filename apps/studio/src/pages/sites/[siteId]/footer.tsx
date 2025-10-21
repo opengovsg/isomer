1a20061a3b8554ea66202772821efc431703cbce
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2025-10-21 15:32:29 +0800

    feat: setup base navbar and footer editing pages (#1565)
    * feat: setup base navbar and footer editing pages
    
    * chore: add storybook stories for navbar and footer
    
    * chore: set width of editor and preview
    
    * chore: restore stub
    
    * chore: fix type

--- apps/studio/src/pages/sites/[siteId]/footer.tsx
+++ apps/studio/src/pages/sites/[siteId]/footer.tsx
@@ -1,29 +1,33 @@
 import { Grid, GridItem } from "@chakra-ui/react"
 import { ResourceType } from "~prisma/generated/generatedEnums"
+import { z } from "zod"
 
 import { PermissionsBoundary } from "~/components/AuthWrappers"
-import { siteSchema } from "~/features/editing-experience/schema"
-import { EditNavbarPreview } from "~/features/settings/EditNavbarPreview"
+import { EditFooterPreview } from "~/features/settings/EditFooterPreview"
 import { useQueryParse } from "~/hooks/useQueryParse"
 import { type NextPageWithLayout } from "~/lib/types"
 import { SiteSettingsLayout } from "~/templates/layouts/SiteSettingsLayout"
 
-const NavbarSettingsPage: NextPageWithLayout = () => {
-  const { siteId } = useQueryParse(siteSchema)
+const footerSettingsSchema = z.object({
+  siteId: z.coerce.number(),
+})
+
+const FooterSettingsPage: NextPageWithLayout = () => {
+  const { siteId } = useQueryParse(footerSettingsSchema)
 
   return (
     <Grid h="full" w="100%" templateColumns="minmax(37.25rem, 1fr) 1fr" gap={0}>
       <GridItem colSpan={1} overflow="auto" minW="30rem">
-        <p>Navigation menu</p>
+        <p>Footer menu</p>
       </GridItem>
       <GridItem colSpan={1}>
-        <EditNavbarPreview siteId={Number(siteId)} />
+        <EditFooterPreview siteId={siteId} />
       </GridItem>
     </Grid>
   )
 }
 
-NavbarSettingsPage.getLayout = (page) => {
+FooterSettingsPage.getLayout = (page) => {
   return (
     <PermissionsBoundary
       resourceType={ResourceType.RootPage}
@@ -32,4 +36,4 @@ NavbarSettingsPage.getLayout = (page) => {
   )
 }
 
-export default NavbarSettingsPage
+export default FooterSettingsPage
