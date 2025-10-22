f43c3218138cddc2f82edaa6fd33b30b708513f4
seaerchin <jiachin@open.gov.sg>
2025-10-22 14:37:17 +0800

    chore: remove failing export

--- apps/studio/src/server/modules/database/utils.ts
+++ apps/studio/src/server/modules/database/utils.ts
@@ -1,7 +1,6 @@
 import type { Expression, RawBuilder, Simplify } from "kysely"
 import { sql } from "kysely"
-import { type DrainOuterGeneric } from "kysely/dist/cjs/util/type-utils"
-import { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/postgres"
+import { jsonArrayFrom } from "kysely/helpers/postgres"
 import { type Tagged } from "type-fest"
 
 export type DateTimeString = Tagged<string, "DateTimeString">
@@ -20,35 +19,6 @@ export function typesafeJsonArrayFrom<O>(
 ): RawBuilder<CastDateTimesToStrings<Simplify<O>>[]> {
   // @ts-expect-error #TS2322
   return jsonArrayFrom(expr)
-}
-
-export function typesafeJsonObjectFrom<O>(
-  expr: Expression<O>,
-): RawBuilder<CastDateTimesToStrings<
-  DrainOuterGeneric<{
-    [K in keyof O]: O[K]
-  }>
-> | null> {
-  // @ts-expect-error #TS2322
-  return jsonObjectFrom(expr)
-}
-
-/**
- * To be used when we are sure the object will exist.
- *
- * Difference from `typesafeJsonObjectFrom` is returned object type is casted to non-nullable.
- */
-export function typesafeJsonObjectFromStrict<O>(
-  expr: Expression<O>,
-): RawBuilder<
-  CastDateTimesToStrings<
-    DrainOuterGeneric<{
-      [K in keyof O]: O[K]
-    }>
-  >
-> {
-  // @ts-expect-error #TS2322
-  return jsonObjectFrom(expr)
 }
 
 // Create a jsonb object from a plain object.
