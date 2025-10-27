cff0ef8206585e309aab47107f706be8b77ca7da
seaerchin <jiachin@open.gov.sg>
2025-10-27 17:53:48 +0800

    chore: wip colors

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsColourPickerControl.tsx
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/JsonFormsColourPickerControl.tsx
@@ -1,15 +1,133 @@
-import { RankedTester, rankWith, schemaMatches } from "@jsonforms/core"
+import {
+  Box,
+  Flex,
+  FormControl,
+  HStack,
+  InputGroup,
+  InputLeftAddon,
+  VStack,
+} from "@chakra-ui/react"
+import {
+  ControlProps,
+  RankedTester,
+  rankWith,
+  schemaMatches,
+} from "@jsonforms/core"
 import { withJsonFormsControlProps } from "@jsonforms/react"
+import { FormLabel, Input } from "@opengovsg/design-system-react"
 
 import { JSON_FORMS_RANKING } from "~/constants/formBuilder"
+import {
+  calculateRelativeLuminance,
+  convertHexToRgb,
+  generateTheme,
+} from "~/features/settings/utils"
+import { useColorPalette } from "~/hooks/useColorPalete"
 
 export const jsonFormsColourPickerControlTester: RankedTester = rankWith(
   JSON_FORMS_RANKING.ColourPickerControl,
   schemaMatches((schema) => schema.format === "color-picker"),
 )
 
-const JsonFormsColourPickerControl = () => {
-  return "hello"
+const JsonFormsColourPickerControl = ({
+  data,
+  label,
+  handleChange,
+  path,
+  description,
+  required,
+  errors,
+}: ControlProps) => {
+  // NOTE: Tint is the colour brightened by 10% successively,
+  // shades are the colour darkened by 10% successively
+  const { tints, colour, shades } = useColorPalette(...convertHexToRgb(data))
+  // NOTE: only need 7 colours in total
+  // 3 tints, the original colour, and 3 shades is best
+  const _luminance = calculateRelativeLuminance(colour || "#FFFFFF")
+
+  return (
+    <Box>
+      <VStack gap="1.5rem">
+        <FormControl isRequired={required} isInvalid={!!errors}>
+          <FormLabel description={description} mb={0}>
+            {label}
+          </FormLabel>
+          <HStack spacing="0.75rem" alignItems="center" mt="0.75rem">
+            <InputGroup size="xs" w="fit-content">
+              <InputLeftAddon
+                borderLeftRadius="4px"
+                border="1px solid"
+                borderColor="base.divider.strong"
+                bgColor="interaction.support.disabled"
+                textColor="interaction.support.disabled-content"
+              >
+                #
+              </InputLeftAddon>
+              <Input
+                placeholder="FFFFFF"
+                w="86px"
+                onChange={(e) =>
+                  handleChange(
+                    path,
+                    `#${e.target.value.startsWith("#") ? e.target.value.slice(1) : e.target.value}`,
+                  )
+                }
+              />
+            </InputGroup>
+            <Box
+              borderRadius="50%"
+              border="1px solid"
+              borderColor="base.divider.strong"
+              bgColor={colour}
+              w="2rem"
+              h="2rem"
+            ></Box>
+          </HStack>
+        </FormControl>
+        <Box alignSelf="flex-start">
+          <FormLabel
+            description={
+              "This palette makes your website accessibility compliant."
+            }
+            mb={0}
+          >
+            Color palette
+          </FormLabel>
+          <Flex mt="0.75rem" h="3rem">
+            {generateTheme({ tints, colour, shades }).map((val, idx) => {
+              const isFirst = idx === 0
+              const isLast = idx === 6
+              return (
+                <Box
+                  bgColor={val}
+                  h="full"
+                  flex={1}
+                  borderTop="2px solid"
+                  borderBottom="2px solid"
+                  borderRight={isLast ? "2px solid" : "none"}
+                  borderLeft={isFirst ? "2px solid" : "none"}
+                  borderColor="base.divider.medium"
+                  borderLeftRadius={isFirst ? "6px" : "auto"}
+                  borderRightRadius={isLast ? "6px" : "auto"}
+                />
+              )
+            })}
+          </Flex>
+        </Box>
+        {/* TODO: Check if we need to split this up into another schema */}
+        <Box alignSelf="flex-start">
+          <FormLabel
+            description={
+              "If your site uses colours that are naturally light (e.g., orange, yellow), you may choose the light theme."
+            }
+            mb={0}
+          >
+            Theme
+          </FormLabel>
+        </Box>
+      </VStack>
+    </Box>
+  )
 }
 
 export default withJsonFormsControlProps(JsonFormsColourPickerControl)
---
+++ apps/studio/src/pages/sites/[siteId]/settings/colours.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { Box } from "@chakra-ui/react"
+import { useToast } from "@opengovsg/design-system-react"
+import { SiteThemeSchema } from "@opengovsg/isomer-components"
+import { ResourceType } from "~prisma/generated/generatedEnums"
+import { isEqual } from "lodash"
+import { BiPaint } from "react-icons/bi"
+
+import type { NextPageWithLayout } from "~/lib/types"
+import { PermissionsBoundary } from "~/components/AuthWrappers"
+import {
+  SettingsEditorGridItem,
+  SettingsGrid,
+  SettingsPreviewGridItem,
+} from "~/components/Settings"
+import {
+  BRIEF_TOAST_SETTINGS,
+  SETTINGS_TOAST_MESSAGES,
+} from "~/constants/toast"
+import { EditSettingsPreview } from "~/features/editing-experience/components/EditSettingsPreview"
+import { ErrorProvider } from "~/features/editing-experience/components/form-builder/ErrorProvider"
+import FormBuilder from "~/features/editing-experience/components/form-builder/FormBuilder"
+import { UnsavedSettingModal } from "~/features/editing-experience/components/UnsavedSettingModal"
+import { siteSchema } from "~/features/editing-experience/schema"
+import { SettingsEditingLayout } from "~/features/settings/SettingsEditingLayout"
+import { SettingsHeader } from "~/features/settings/SettingsHeader"
+import { useNavigationEffect } from "~/hooks/useNavigationEffect"
+import { useNewSettingsPage } from "~/hooks/useNewSettingsPage"
+import { useQueryParse } from "~/hooks/useQueryParse"
+import { SiteTheme, siteThemeValidator } from "~/schemas/site"
+import { SiteSettingsLayout } from "~/templates/layouts/SiteSettingsLayout"
+import { trpc } from "~/utils/trpc"
+
+const ColoursSettingsPage: NextPageWithLayout = () => {
+  const { siteId: rawSiteId } = useQueryParse(siteSchema)
+  const siteId = Number(rawSiteId)
+  const router = useRouter()
+  const isEnabled = useNewSettingsPage()
+  const trpcUtils = trpc.useUtils()
+  const toast = useToast(BRIEF_TOAST_SETTINGS)
+  const [theme] = trpc.site.getTheme.useSuspenseQuery({
+    id: siteId,
+  })
+  const [{ name: siteName }] = trpc.site.getSiteName.useSuspenseQuery({
+    siteId,
+  })
+
+  useEffect(() => {
+    if (!isEnabled) void router.replace(`/sites/${siteId}/settings`)
+  }, [isEnabled, router, siteId])
+
+  const [nextUrl, setNextUrl] = useState("")
+  const isOpen = !!nextUrl
+  const [siteTheme, setSiteTheme] = useState<SiteTheme>(
+    theme ?? {
+      colors: {
+        brand: {
+          canvas: { default: "", alt: "", backdrop: "", inverse: "" },
+          interaction: { default: "", hover: "", pressed: "" },
+        },
+      },
+    },
+  )
+
+  const isDirty = !isEqual(theme, siteTheme)
+
+  const updateSiteIntegrationsMutation =
+    trpc.site.updateSiteIntegrations.useMutation({
+      onSuccess: async () => {
+        toast({
+          ...SETTINGS_TOAST_MESSAGES.success,
+          status: "success",
+        })
+        await trpcUtils.site.getConfig.invalidate({ id: siteId })
+      },
+      onError: (error) => {
+        toast({
+          title: "Failed to update site",
+          description: error.message,
+          status: "error",
+        })
+      },
+    })
+
+  useNavigationEffect({ isOpen, isDirty, callback: setNextUrl })
+
+  const onSubmit = () => console.log(siteTheme)
+
+  return (
+    <ErrorProvider>
+      <UnsavedSettingModal
+        isOpen={isOpen}
+        onClose={() => setNextUrl("")}
+        nextUrl={nextUrl}
+      />
+      <SettingsGrid>
+        <SettingsEditorGridItem as={SettingsEditingLayout}>
+          <SettingsHeader
+            onClick={onSubmit}
+            title="Colours"
+            icon={BiPaint}
+            isLoading={updateSiteIntegrationsMutation.isPending}
+            isDisabled={!isDirty}
+          />
+          <Box w="100%">
+            <FormBuilder<SiteTheme>
+              schema={SiteThemeSchema}
+              validateFn={siteThemeValidator}
+              data={siteTheme}
+              handleChange={(data) => {
+                setSiteTheme(data)
+              }}
+            />
+          </Box>
+        </SettingsEditorGridItem>
+        <SettingsPreviewGridItem>
+          <EditSettingsPreview siteName={siteName} />
+        </SettingsPreviewGridItem>
+      </SettingsGrid>
+    </ErrorProvider>
+  )
+}
+
+ColoursSettingsPage.getLayout = (page) => {
+  return (
+    <PermissionsBoundary
+      resourceType={ResourceType.RootPage}
+      page={SiteSettingsLayout(page)}
+    />
+  )
+}
+
+export default ColoursSettingsPage
