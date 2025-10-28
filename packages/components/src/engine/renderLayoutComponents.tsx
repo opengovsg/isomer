c408304586bebe4e87e71adbabaa12c10c6f3158
seaerchin <44049504+seaerchin@users.noreply.github.com>
2025-10-28 12:50:11 +0800

    chore: remove flag check for wogaa (#1678)

--- packages/components/src/engine/renderLayoutComponents.tsx
+++ packages/components/src/engine/renderLayoutComponents.tsx
@@ -26,13 +26,12 @@ export const RenderApplicationScripts = ({
   return (
     <>
       <FontPreload />
-
-      {site.isGovernment && (
-        <Wogaa
-          environment={site.environment}
-          ScriptComponent={ScriptComponent}
-        />
-      )}
+      {/* NOTE: we load in wogaa regardless of whether the site is  */}
+      {/* a government site as wogaa still requires the agency to register their site */}
+      {/* and wogaa is still gated behind techpass login. */}
+      {/* Additionally, wogaa will still load but not track metrics if the site  */}
+      {/* is not registered, so no end impact to user */}
+      <Wogaa environment={site.environment} ScriptComponent={ScriptComponent} />
 
       {shouldIncludeGTM && (
         <>
