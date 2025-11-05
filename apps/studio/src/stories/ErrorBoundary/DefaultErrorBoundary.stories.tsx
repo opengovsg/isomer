5e49a0945bcebc5ef7df917f54f217da2c6b79c7
seaerchin <jiachin@open.gov.sg>
2025-11-05 14:05:37 +0800

    chore: fix tests

--- apps/studio/src/stories/ErrorBoundary/DefaultErrorBoundary.stories.tsx
+++ apps/studio/src/stories/ErrorBoundary/DefaultErrorBoundary.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react/*"
+import type { Meta, StoryObj } from "@storybook/react"
 
 import { DefaultTrpcError } from "~/components/ErrorBoundary/DefaultTrpcError"
 
