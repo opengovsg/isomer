d87283e04b36a4262b6c595cd560714ab9a5eb22
adriangohjw <adriangohjw@gmail.com>
2025-11-11 16:33:30 +0800

    feat(HeroSearchbar): add new variant with image support in stories

--- packages/components/src/templates/next/components/complex/Hero/Hero.stories.tsx
+++ packages/components/src/templates/next/components/complex/Hero/Hero.stories.tsx
@@ -169,3 +169,22 @@ export const Searchbar: Story = {
     variant: "searchbar",
   },
 }
+
+export const SearchbarWithImage: Story = {
+  decorators: [withSearchSgSetup()],
+  args: {
+    site: generateSiteConfig({
+      search: {
+        type: "searchSG",
+        clientId: SEARCHSG_TEST_CLIENT_ID,
+      },
+    }),
+
+    title: "Temasek Polytechnic",
+    subtitle:
+      "APEX connects agencies and the public through a single, secure hub for Singapore’s government APIs.",
+    backgroundUrl:
+      "https://images.unsplash.com/photo-1594318142972-1e2ea7487a3e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
+    variant: "searchbarWithImage",
+  },
+}
