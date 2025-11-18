a08089a3b19f6432628224120851010cd0e17ded
adriangohjw <adriangohjw@gmail.com>
2025-11-18 17:40:22 +0800

    refactor: enhance useInteractionScriptLoader to improve script loading behavior based on user interactions and timeout

--- packages/components/src/hooks/useInteractionScriptLoader.ts
+++ packages/components/src/hooks/useInteractionScriptLoader.ts
@@ -6,9 +6,13 @@ interface UseInteractionScriptLoaderOptions {
   timeout?: number
 }
 
+// TBT flags any script blocking the main thread for >50ms.
+// Large third-party widgets (Vica ~800kb, AskGov ~250kb) exceed this,
+// and Lighthouse still counts them even with async
+// Thus we use a custom loader hook to delay loading until after Wogaa has stopped measuring TBT.
 export const useInteractionScriptLoader = ({
   src,
-  timeout = 3000,
+  timeout = 3000, // 3 seconds from manual testing
 }: UseInteractionScriptLoaderOptions) => {
   const documentRef = useRef<Document | null>(
     typeof document !== "undefined" ? document : null,
@@ -17,11 +21,13 @@ export const useInteractionScriptLoader = ({
 
   const triggerLoad = () => setShouldLoad(true)
 
+  // Load script on user interactions (scroll, click, touchstart)
   useEventListener("scroll", triggerLoad, documentRef, { passive: true })
   useEventListener("click", triggerLoad, documentRef) // as we might need to call preventDefault
   useEventListener("touchstart", triggerLoad, documentRef, { passive: true })
 
-  useTimeout(() => setShouldLoad(true), timeout)
+  // Load script after timeout if user doesn't interact
+  useTimeout(() => triggerLoad, timeout)
 
   useScript(shouldLoad ? src : null)
 }
