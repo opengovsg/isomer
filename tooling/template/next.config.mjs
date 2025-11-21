027af91b86e92d87299fffb60aca609e4dea874a
seaerchin <jiachin@open.gov.sg>
2025-11-22 02:07:46 +0900

    chore: update next cnofig

--- tooling/template/next.config.mjs
+++ tooling/template/next.config.mjs
@@ -17,16 +17,24 @@ const nextConfig = {
           ...config.optimization.splitChunks.cacheGroups,
           components: {
             test: /[\\/]templates[\\/]next[\\/]components[\\/]/,
-            name: "components",
+            name(module) {
+              const match = module.context.match(
+                /[\\/]components[\\/](?:complex|internal|native)[\\/]([^\\/]+)/,
+              )
+              return match ? `component-${match[1]}` : "components"
+            },
             chunks: "all",
-            priority: 0,
+            priority: 10,
             minSize: 0,
           },
           layouts: {
             test: /[\\/]templates[\\/]next[\\/]layouts[\\/]/,
-            name: "layouts",
+            name(module) {
+              const match = module.context.match(/[\\/]layouts[\\/]([^\\/]+)/)
+              return match ? `layout-${match[1]}` : "layouts"
+            },
             chunks: "all",
-            priority: 0,
+            priority: 10,
             minSize: 0,
           },
         },
