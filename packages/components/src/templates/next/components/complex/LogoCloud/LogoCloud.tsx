d181c5d649ce5d2a1e30b0932200039e2fd6e08c
sehyunidaaa <sehyun@open.gov.sg>
2025-12-04 16:09:44 +0800

    adjust styling for logocloud

--- packages/components/src/templates/next/components/complex/LogoCloud/LogoCloud.tsx
+++ packages/components/src/templates/next/components/complex/LogoCloud/LogoCloud.tsx
@@ -1,10 +1,30 @@
 import type { LogoCloudProps } from "~/interfaces/complex/LogoCloud"
+import { tv } from "~/lib/tv"
 import { isExternalUrl } from "~/utils"
 import { ImageClient } from "../Image"
+
+const createLogoCloudStyles = tv({
+  slots: {
+    container: "flex flex-col gap-6 py-16",
+    title:
+      "prose-headline-base-medium self-center text-center text-base-content-subtle",
+    logoContainer: "flex flex-wrap justify-center gap-6",
+    logo: "inset-0 max-h-20 object-contain p-2 lg:max-h-24 lg:w-auto",
+  },
+  variants: {
+    style: {
+      greyscale: {
+        logo: "grayscale",
+      },
+    },
+  },
+})
+const compoundStyles = createLogoCloudStyles()
 
 export const LogoCloud = ({
   images: baseImages,
   title,
+  variant,
   site: { assetsBaseUrl },
   shouldLazyLoad = true,
 }: LogoCloudProps) => {
@@ -17,23 +37,19 @@ export const LogoCloud = ({
     return { src: transformedSrc, alt, lazyLoading: shouldLazyLoad }
   })
   return (
-    <div className="px-10 py-12">
-      <div className="flex flex-col gap-4">
-        <p className="prose-headline-base-medium self-center text-base-content-light">
-          {title ?? "With support from these agencies"}
-        </p>
-        <div className="flex flex-wrap justify-center">
-          {images.map((props) => (
-            <ImageClient
-              {...props}
-              // have to pass in here instead of w-fit because
-              // flex-wrap in parent div doesn't work well with w-fit for safari
-              width="auto"
-              className="inset-0 max-h-24 object-contain p-2"
-              assetsBaseUrl={assetsBaseUrl}
-            />
-          ))}
-        </div>
+    <div className={compoundStyles.container()}>
+      {title && <p className={compoundStyles.title()}>{title}</p>}
+      <div className={compoundStyles.logoContainer()}>
+        {images.map((props) => (
+          <ImageClient
+            {...props}
+            // have to pass in here instead of w-fit because
+            // flex-wrap in parent div doesn't work well with w-fit for safari
+            width="auto"
+            className={compoundStyles.logo({ style: variant })}
+            assetsBaseUrl={assetsBaseUrl}
+          />
+        ))}
       </div>
     </div>
   )
