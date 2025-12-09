b7a243a07fea0b2f52556d6ccec6fc76df28f6ff
adriangohjw <adriangohjw@gmail.com>
2025-12-09 14:41:25 +0800

    fix lint

--- apps/studio/src/features/editing-experience/components/Block/DraggableBlock.tsx
+++ apps/studio/src/features/editing-experience/components/Block/DraggableBlock.tsx
@@ -9,7 +9,8 @@ import {
 
 import { PROSE_COMPONENT_NAME } from "~/constants/formBuilder"
 import { TYPE_TO_ICON } from "../../constants"
-import { BaseBlock, BaseBlockDragHandle, BaseBlockProps } from "./BaseBlock"
+import type { BaseBlockProps } from "./BaseBlock";
+import { BaseBlock, BaseBlockDragHandle } from "./BaseBlock"
 
 interface DraggableBlockProps extends Pick<BaseBlockProps, "invalidProps"> {
   block: IsomerSchema["content"][number]
