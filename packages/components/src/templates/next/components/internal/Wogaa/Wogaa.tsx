b456ec16b57d29c7e69e02d59ee5b3a976f65b72
adriangohjw <adriangohjw@gmail.com>
2026-02-10 18:29:32 +0800

    refactor: update Wogaa component to preload CDN origin and improve script loading

--- packages/components/src/templates/next/components/internal/Wogaa/Wogaa.tsx
+++ packages/components/src/templates/next/components/internal/Wogaa/Wogaa.tsx
@@ -1,14 +1,26 @@
 import type { WogaaProps } from "~/interfaces"
+import { PreloadHelper } from "../utils"
 
 export const Wogaa = ({
   environment,
   ScriptComponent = "script",
 }: WogaaProps) => {
-  const scriptUrl =
+  const origin =
     environment === "production"
-      ? "https://assets.wogaa.sg/scripts/wogaa.js"
-      : "https://assets.dcube.cloud/scripts/wogaa.js"
+      ? "https://assets.wogaa.sg"
+      : "https://assets.dcube.cloud"
 
-  // setting beforeInteractive to ensure we do not lose analytical data
-  return <ScriptComponent src={scriptUrl} strategy="beforeInteractive" />
+  return (
+    <>
+      {/* preload the CDN origin so the script fetch starts sooner */}
+      <PreloadHelper href={origin} />
+
+      {/* setting beforeInteractive to ensure we do not lose analytical data */}
+      <ScriptComponent
+        src={`${origin}/scripts/wogaa.js`}
+        strategy="beforeInteractive"
+        fetchPriority="high"
+      />
+    </>
+  )
 }
