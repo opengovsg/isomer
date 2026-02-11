8e74f7054994f5f896f821f713424103d7670d8a
seaerchin <jiachin@open.gov.sg>
2026-02-11 15:49:02 +0800

    chore: fix page router

--- apps/studio/src/features/editing-experience/components/PublishButton.tsx
+++ apps/studio/src/features/editing-experience/components/PublishButton.tsx
@@ -43,6 +43,10 @@ const SuspendablePublishButton = ({
   const scheduledPublishingDisclosure = useDisclosure()
 
   const [currPage] = trpc.page.readPage.useSuspenseQuery({ pageId, siteId })
+  const [job] = trpc.page.getScheduledTime.useSuspenseQuery({
+    pageId,
+    siteId,
+  })
   const isChangesPendingPublish = !!currPage.draftBlobId
 
   const { mutate, isPending } = trpc.page.publishPage.useMutation({
@@ -98,11 +102,11 @@ const SuspendablePublishButton = ({
                   {...publishNowDisclosure}
                 />
               )}
-              {currPage.scheduledAt ? (
+              {job.scheduledAt ? (
                 <CancelSchedulePublishIndicator
                   siteId={siteId}
                   pageId={pageId}
-                  scheduledAt={currPage.scheduledAt}
+                  scheduledAt={job.scheduledAt}
                 />
               ) : (
                 <HStack spacing={0} position="relative">
