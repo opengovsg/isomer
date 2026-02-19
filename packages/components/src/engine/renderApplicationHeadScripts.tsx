250cf64938e09fe0ebeceeace1f2718fba6bca8f
adriangohjw <adriangohjw@gmail.com>
2026-02-19 16:11:17 +0800

    update wogaa to WOGAA

--- packages/components/src/engine/renderApplicationHeadScripts.tsx
+++ packages/components/src/engine/renderApplicationHeadScripts.tsx
@@ -10,10 +10,10 @@ export const RenderApplicationHeadScripts = ({
 }: RenderApplicationHeadScriptsProps) => {
   return (
     <>
-      {/* NOTE: we load in wogaa regardless of whether the site is  */}
-      {/* a government site as wogaa still requires the agency to register their site */}
-      {/* and wogaa is still gated behind techpass login. */}
-      {/* Additionally, wogaa will still load but not track metrics if the site  */}
+      {/* NOTE: we load in WOGAA regardless of whether the site is  */}
+      {/* a government site as WOGAA still requires the agency to register their site */}
+      {/* and WOGAA is still gated behind techpass login. */}
+      {/* Additionally, WOGAA will still load but not track metrics if the site  */}
       {/* is not registered, so no end impact to user */}
       <Wogaa environment={site.environment} />
     </>
