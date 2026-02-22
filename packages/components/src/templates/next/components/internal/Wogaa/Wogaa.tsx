17cd96bed0db8c4c5b7453da35fd6b1b59a9c811
adriangohjw <adriangohjw@gmail.com>
2026-02-22 16:28:23 +0800

    explicitly state wogaa to be text/javascript

--- packages/components/src/templates/next/components/internal/Wogaa/Wogaa.tsx
+++ packages/components/src/templates/next/components/internal/Wogaa/Wogaa.tsx
@@ -9,5 +9,12 @@ export const Wogaa = ({ environment }: WogaaProps) => {
   // NOTE: next/script works by injecting the script tag into the head during runtime
   // however, Wogaa require their script to be present in the head
   // as such, we cannot use next/script and need to use a regular script tag
-  return <script src={scriptUrl} crossOrigin="anonymous" async />
+  return (
+    <script
+      src={scriptUrl}
+      type="text/javascript"
+      crossOrigin="anonymous"
+      async
+    />
+  )
 }
