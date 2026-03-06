7cb3934e46254a40039c8df9cb95a2cf6a51cde1
sehyunidaaa <sehyun@open.gov.sg>
2026-03-06 10:50:57 +0800

    a11y: fix issues in ToC

--- packages/components/src/templates/next/components/internal/TableOfContents/TableOfContents.tsx
+++ packages/components/src/templates/next/components/internal/TableOfContents/TableOfContents.tsx
@@ -15,13 +15,13 @@ export const TableOfContents = ({
   LinkComponent,
 }: TableOfContentsProps) => {
   return (
-    <div className="flex flex-col gap-3 rounded-lg bg-base-canvas-alt p-6">
+    <nav className="flex flex-col gap-3 rounded-lg bg-base-canvas-alt p-6">
       <p className="prose-headline-lg-medium text-base-content-strong">
         On this page
       </p>
-      <div className="prose-body-base flex flex-col gap-3">
+      <ul className="prose-body-base flex flex-col gap-3">
         {items.map(({ anchorLink, content }) => (
-          <div className="flex flex-row items-start gap-2">
+          <li className="flex flex-row items-start gap-2" key={anchorLink}>
             <BiMinus className="size-5 shrink-0 self-center fill-base-content-strong" />
             <Link
               href={anchorLink}
@@ -30,9 +30,9 @@ export const TableOfContents = ({
             >
               {content}
             </Link>
-          </div>
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </nav>
   )
 }
