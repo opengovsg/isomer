23cdaf69fc85e415f23e796640f7db049548065d
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2026-03-13 17:35:18 +0800

    fix: passthrough all props and handle undefined showThumbnail

--- tooling/build/scripts/publishing/types.ts
+++ tooling/build/scripts/publishing/types.ts
@@ -26,6 +26,7 @@ type TagCategory = Tagged & {
 interface CollectionPagePageProps {
   defaultSortBy?: string
   defaultSortDirection?: string
+  sortOrder?: string
   tagCategories?: TagCategory[]
 }
 
