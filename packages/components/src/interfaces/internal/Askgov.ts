9c32f5af51708f8ea67629e48613de5dc23bc152
Adrian Goh Jun Wei <adriangohjw@gmail.com>
2026-03-18 12:36:46 +0800

    chore: update AskGov ID description for clarity and guidance (#1863)
    * chore: update AskGov ID description for clarity and guidance
    
    * chore: update AskGov ID description for clarity
    
    * chore: remove unused import

--- packages/components/src/interfaces/internal/Askgov.ts
+++ packages/components/src/interfaces/internal/Askgov.ts
@@ -1,14 +1,14 @@
 import type { Static } from "@sinclair/typebox"
 import { Type } from "@sinclair/typebox"
 
-import type { IsomerSiteProps, ScriptComponentType } from "~/types"
+import type { IsomerSiteProps } from "~/types"
 
 export const AskgovSchema = Type.Object(
   {
     "data-agency": Type.String({
       title: "AskGov ID",
       description:
-        "You can get this from [AskGov Support](https://form.gov.sg/689d8bf8bd05b72ca8a21d6b) after onboarding. If the widget doesn’t appear on your site, check that you have the correct ID.",
+        'This is what comes after ask.gov.sg. For example, for https://ask.gov.sg/help, "help" is the ID.',
     }),
     "data-topic": Type.Optional(
       Type.String({
