a516d3422932ad3d4514db7478cac20ff242dcce
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2026-03-24 10:53:18 +0800

    fix: prevent preview from jumping when loaded

--- apps/studio/src/features/editing-experience/components/preview/EditPagePreview.tsx
+++ apps/studio/src/features/editing-experience/components/preview/EditPagePreview.tsx
@@ -1,76 +1,54 @@
-import { useMemo } from "react"
 import { Box } from "@chakra-ui/react"
 import merge from "lodash/merge"
 
 import { useEditorDrawerContext } from "~/contexts/EditorDrawerContext"
+import { withSuspense } from "~/hocs/withSuspense"
 import { trpc } from "~/utils/trpc"
 import { LoadingPreview } from "./LoadingPreview"
 import PreviewWithCustomSitemap from "./PreviewWithCustomSitemap"
 import { ViewportContainer } from "./ViewportContainer"
 
-type PreviewState = "loading" | "interactive" | "error"
+const LoadingState = (): JSX.Element => {
+  return (
+    <Box bg="base.canvas.backdrop" height="100%" flexDirection="column">
+      <Box
+        px="2rem"
+        pb="2rem"
+        pt="1rem"
+        overflowX="auto"
+        height="100%"
+        width="100%"
+      >
+        <LoadingPreview />
+      </Box>
+    </Box>
+  )
+}
 
-export const EditPagePreview = (): JSX.Element => {
+const SuspendableEditPagePreview = (): JSX.Element => {
   const { previewPageState, pageId, updatedAt, siteId, permalink, title } =
     useEditorDrawerContext()
 
-  const {
-    data: siteMap,
-    isLoading,
-    isError,
-  } = trpc.site.getLocalisedSitemap.useQuery({
+  const [siteMap] = trpc.site.getLocalisedSitemap.useSuspenseQuery({
     siteId,
     resourceId: pageId,
   })
-  const previewState: PreviewState = useMemo(() => {
-    if (!isLoading && !isError && siteMap !== undefined) {
-      return "interactive"
-    }
 
-    if (isError) {
-      return "error"
-    }
-
-    return "loading"
-  }, [isLoading, isError, siteMap])
-
-  switch (previewState) {
-    case "loading":
-      return (
-        <Box bg="base.canvas.backdrop" height="100%" flexDirection="column">
-          <Box
-            px="2rem"
-            pb="2rem"
-            pt="1rem"
-            overflowX="auto"
-            height="100%"
-            width="100%"
-          >
-            <LoadingPreview />
-          </Box>
-        </Box>
-      )
-    case "error":
-      return <Box>Failed to load preview</Box>
-    case "interactive":
-      if (!siteMap) {
-        return <Box>Failed to load preview</Box>
-      }
-
-      return (
-        <ViewportContainer siteId={siteId}>
-          <PreviewWithCustomSitemap
-            {...merge(previewPageState, { page: { title } })}
-            siteId={siteId}
-            permalink={permalink}
-            lastModified={updatedAt.toISOString()}
-            version="0.1.0"
-            siteMap={siteMap}
-          />
-        </ViewportContainer>
-      )
-    default:
-      const _: never = previewState
-      return <></>
-  }
+  return (
+    <ViewportContainer siteId={siteId}>
+      <PreviewWithCustomSitemap
+        {...merge(previewPageState, { page: { title } })}
+        siteId={siteId}
+        permalink={permalink}
+        lastModified={updatedAt.toISOString()}
+        version="0.1.0"
+        siteMap={siteMap}
+      />
+    </ViewportContainer>
+  )
 }
+
+export const EditPagePreview = withSuspense(
+  SuspendableEditPagePreview,
+  <LoadingState />,
+)
