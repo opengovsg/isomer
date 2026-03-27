e80e69a4766de0f1f51b5186f92dd852bce1b07a
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2026-03-27 15:02:16 +0800

    feat: add sorting options to collection listing page

---
+++ apps/studio/src/features/dashboard/components/CollectionTable/constants.ts
@@ -0,0 +1,9 @@
+import type { CollectionTableSortOptions } from "./types"
+
+export const COLLECTION_TABLE_SORT_OPTIONS: Record<
+  CollectionTableSortOptions,
+  string
+> = {
+  "updated-desc": "Recently edited",
+  "title-asc": "Alphabetical",
+}
