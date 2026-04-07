7547228980a029a29a78ada4b5b3e50a839ed7fd
seaerchin <jiachin@open.gov.sg>
2026-04-07 14:16:06 +0800

    chore: fix missing logo

--- apps/studio/src/utils/generateAssetUrl.ts
+++ apps/studio/src/utils/generateAssetUrl.ts
@@ -4,6 +4,7 @@ export const ASSETS_BASE_URL = env.NEXT_PUBLIC_S3_ASSETS_DOMAIN_NAME
   ? `https://${env.NEXT_PUBLIC_S3_ASSETS_DOMAIN_NAME}`
   : ""
 
-export const generateAssetUrl = (url: string): string => {
-  return url.startsWith("/") ? `${ASSETS_BASE_URL}${url}` : url
+export const generateAssetUrl = (url?: string): string => {
+  console.log("URL", url)
+  return !!url && url.startsWith("/") ? `${ASSETS_BASE_URL}${url}` : url
 }
