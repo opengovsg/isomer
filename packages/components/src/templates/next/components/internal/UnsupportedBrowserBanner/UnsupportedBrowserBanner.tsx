f7d4c4208fa92c5988c1c8f85aeeed617947e21c
sehyunidaaa <sehyun@open.gov.sg>
2026-04-14 22:15:30 +0800

    style: copy style to unsuppported browsers

--- packages/components/src/templates/next/components/internal/UnsupportedBrowserBanner/UnsupportedBrowserBanner.tsx
+++ packages/components/src/templates/next/components/internal/UnsupportedBrowserBanner/UnsupportedBrowserBanner.tsx
@@ -35,11 +35,11 @@ export const UnsupportedBrowserBanner = ({
 
   return (
     <div className="bg-utility-feedback-warning">
-      <div className="relative mx-auto flex max-w-screen-xl flex-row gap-4 px-6 py-8 text-base-content md:px-10 md:py-6">
-        <BiInfoCircle className="mt-0.5 h-6 w-6 shrink-0" />
+      <div className="relative mx-auto flex max-w-screen-xl flex-row gap-2 px-6 py-4 text-base-content md:px-10 md:py-5">
+        <BiInfoCircle className="mt-0.5 h-5 w-5 shrink-0" />
         <div className="flex flex-1 flex-col gap-1">
-          <div className="base-content-default prose-headline-lg-medium [&:not(:first-child)]:mt-0 [&:not(:last-child)]:mb-0">
-            This browser is not supported
+          <div className="base-content-default prose-headline-base-medium [&:not(:first-child)]:mt-0 [&:not(:last-child)]:mb-0">
+            This browser is not supported.
           </div>
           <div className="prose-body-base [&:not(:first-child)]:mt-0 [&:not(:last-child)]:mb-0">
             Your experience on this site might not be ideal. Please update to
