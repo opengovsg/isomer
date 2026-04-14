6f66d53da58f1e66368a6c47d11583f74358a37f
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2026-04-14 15:33:36 +0800

    feat(polyglot): use staging/prod domain based on site environment

--- packages/components/src/templates/next/components/internal/Polyglot/Polyglot.tsx
+++ packages/components/src/templates/next/components/internal/Polyglot/Polyglot.tsx
@@ -1,11 +1,17 @@
 import type { PolyglotProps } from "~/interfaces"
 
-export const Polyglot = ({ ScriptComponent = "script" }: PolyglotProps) => {
+export const Polyglot = ({
+  ScriptComponent = "script",
+  isStaging = false,
+}: PolyglotProps) => {
+  const host = isStaging
+    ? "staging-assets.polyglot.gov.sg"
+    : "assets.polyglot.gov.sg"
+
   return (
     <ScriptComponent
       type="text/javascript"
-      // src="http://localhost:3001/polyglot.widget.js"
-      src="https://isomer-user-content-stg.by.gov.sg/polyglot/polyglot.widget.js"
+      src={`https://${host}/polyglot.widget.js`}
     />
   )
 }
