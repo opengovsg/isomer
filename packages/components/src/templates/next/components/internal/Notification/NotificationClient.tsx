eb26d9cc01c34b097a538e82ff5f29027c824fc1
sehyunidaaa <sehyun@open.gov.sg>
2026-04-14 22:14:56 +0800

    style: apply changes to notif banner

--- packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
+++ packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
@@ -19,17 +19,17 @@ export const NotificationClient = ({
   return (
     !isDismissed && (
       <div className="bg-utility-feedback-info-faint">
-        <div className="relative mx-auto flex max-w-screen-xl flex-row gap-4 px-6 py-8 text-base-content md:px-10 md:py-6">
-          <BiInfoCircle className="mt-0.5 h-6 w-6 shrink-0" />
+        <div className="relative mx-auto flex max-w-screen-xl flex-row gap-2 px-6 py-4 text-base-content md:px-10 md:py-5">
+          <BiInfoCircle className="mt-0.5 h-5 w-5 shrink-0" />
           <div className="flex flex-1 flex-col gap-1">
-            {!!title && <h2 className="prose-headline-lg-medium">{title}</h2>}
+            {!!title && <h2 className="prose-headline-base-medium">{title}</h2>}
             <div className="[&_p]:!mb-0 [&_p]:!mt-0">{children}</div>
           </div>
           <div aria-hidden className="flex h-6 w-6 shrink-0" />
           <IconButton
             onPress={onDismiss}
             icon={BiX}
-            className="absolute right-3 top-[22px] md:right-7 md:top-3.5"
+            className="absolute right-3 top-3 md:right-7 md:top-3"
             aria-label="Dismiss notification temporarily"
           />
         </div>
