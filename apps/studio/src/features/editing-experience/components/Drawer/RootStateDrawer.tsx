d00fd94f9b1f01bf3a119a5fae7368edf996689b
adriangohjw <adriangohjw@gmail.com>
2026-04-16 17:35:47 +0800

    feat(RootStateDrawer): add vertical variant to BaseBlock components for improved layout flexibility

--- apps/studio/src/features/editing-experience/components/Drawer/RootStateDrawer.tsx
+++ apps/studio/src/features/editing-experience/components/Drawer/RootStateDrawer.tsx
@@ -115,6 +115,7 @@ const FixedBlock = () => {
     return (
       <>
         <BaseBlock
+          variant="vertical"
           onClick={() => {
             setCurrActiveIdx(0)
             setDrawerState({ state: "collectionEditor" })
@@ -124,6 +125,7 @@ const FixedBlock = () => {
           icon={BiCog}
         />
         <BaseBlock
+          variant="vertical"
           onClick={() => console.log("to implement")}
           label="Filters"
           description="Define and manage filters for this Collection."
