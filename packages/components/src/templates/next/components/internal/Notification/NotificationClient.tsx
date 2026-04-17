c84c6428e33d1fe4ec4d024d8acffdcd7cf592ca
sehyunidaaa <sehyun@open.gov.sg>
2026-04-17 14:38:58 +0800

    style: reduce gap

--- packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
+++ packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
@@ -21,7 +21,7 @@ export const NotificationClient = ({
       <div className="bg-utility-feedback-info-faint">
         <div className="relative mx-auto flex max-w-screen-xl flex-row gap-2 px-6 py-4 text-base-content md:px-10 md:py-4">
           <BiInfoCircle className="mt-[3px] h-4 w-4 shrink-0" />
-          <div className="flex flex-1 flex-col gap-1">
+          <div className="flex flex-1 flex-col gap-0.5">
             {!!title && <h2 className="prose-headline-base-medium">{title}</h2>}
             <div className="[&_p]:!mb-0 [&_p]:!mt-0">{children}</div>
           </div>
