b9baf0d25913c6edadced77b68d8565a38a71c71
sehyunidaaa <sehyun@open.gov.sg>
2026-04-17 14:37:32 +0800

    style: tweak styling

--- packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
+++ packages/components/src/templates/next/components/internal/Notification/NotificationClient.tsx
@@ -19,8 +19,8 @@ export const NotificationClient = ({
   return (
     !isDismissed && (
       <div className="bg-utility-feedback-info-faint">
-        <div className="relative mx-auto flex max-w-screen-xl flex-row gap-2 px-6 py-4 text-base-content md:px-10 md:py-5">
-          <BiInfoCircle className="mt-0.5 h-5 w-5 shrink-0" />
+        <div className="relative mx-auto flex max-w-screen-xl flex-row gap-2 px-6 py-4 text-base-content md:px-10 md:py-4">
+          <BiInfoCircle className="mt-[3px] h-4 w-4 shrink-0" />
           <div className="flex flex-1 flex-col gap-1">
             {!!title && <h2 className="prose-headline-base-medium">{title}</h2>}
             <div className="[&_p]:!mb-0 [&_p]:!mt-0">{children}</div>
@@ -29,7 +29,7 @@ export const NotificationClient = ({
           <IconButton
             onPress={onDismiss}
             icon={BiX}
-            className="absolute right-3 top-3 md:right-7 md:top-3"
+            className="absolute right-3 top-1 md:right-7"
             aria-label="Dismiss notification temporarily"
           />
         </div>
