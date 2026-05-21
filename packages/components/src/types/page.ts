879c6c8825efa4f27a21cad0502dff94664ea20c
adriangohjw <adriangohjw@gmail.com>
2026-05-21 10:26:54 +0800

    fix: remove max 70 length

--- packages/components/src/types/page.ts
+++ packages/components/src/types/page.ts
@@ -59,7 +59,6 @@ const TagCategorySchema = Type.Composite([
     options: Type.Array(
       Type.Object({
         label: Type.String({
-          maxLength: 70,
           title: "Option name",
           pattern: TRIMMED_NON_EMPTY_STRING_REGEX,
           errorMessage: {
