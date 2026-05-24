f4d9e3403cba22b9335823979082f54ebfaa4c20
jovnc <95868357+jovnc@users.noreply.github.com>
2026-05-24 22:01:10 +0800

    fix: remove first-list top gap in contentpic

--- packages/components/src/templates/next/components/complex/Contentpic/Contentpic.tsx
+++ packages/components/src/templates/next/components/complex/Contentpic/Contentpic.tsx
@@ -11,7 +11,8 @@ const contentpicStyles = tv({
     container:
       "mb-7 flex flex-col gap-7 sm:flex-row [&:not(:first-child)]:mt-7",
     image: "aspect-[5/6] h-auto rounded object-cover sm:h-[240px] sm:w-[200px]",
-    content: "flex-1 break-words text-base-content lg:justify-self-start",
+    content:
+      "flex-1 break-words text-base-content lg:justify-self-start [&>ol:first-child>li:first-child]:mt-0 [&>ol:first-child]:mt-0 [&>ul:first-child>li:first-child]:mt-0 [&>ul:first-child]:mt-0",
   },
 })
 const compoundStyles = contentpicStyles()
