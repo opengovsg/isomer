7c619c816ef9f40ab49ec598b53015a22dc20880
jovnc <95868357+jovnc@users.noreply.github.com>
2026-06-01 11:22:00 +0800

    chore: update comment to make it more accurate

--- packages/components/src/utils/getTransformedPageContent.ts
+++ packages/components/src/utils/getTransformedPageContent.ts
@@ -20,7 +20,7 @@ export const getTransformedPageContent = (
             component.attrs.id === undefined
           ) {
             // generate a unique hash to auto-generate anchor links
-            // for backwards compatibility, use the same seed format for headings, but with an added collision count suffix only if there are multiple headings with the same content
+            // for backwards compatibility, use the same seed format for headings, but with an added index suffix only if there are multiple headings with the same content
             const seed = `${JSON.stringify(component)}_${componentIndex}`
             let anchorId = getDigestFromText(seed)
             if (usedHeadingIds.has(anchorId)) {
