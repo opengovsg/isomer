946128f1e780ed45c28e383f18853a0462ab778f
adriangohjw <adriangohjw@gmail.com>
2026-06-08 22:57:35 +0800

    fix: disable allErrors in AJV configuration
    Updated the AJV configuration to set allErrors to false, improving error handling behavior in validation processes.

--- apps/studio/src/utils/ajv.ts
+++ apps/studio/src/utils/ajv.ts
@@ -3,7 +3,7 @@ import addErrors from "ajv-errors"
 
 export const ajv = new Ajv({
   useDefaults: true,
-  allErrors: true,
+  allErrors: false,
   strict: false,
   logger: false,
   discriminator: true,
