5afc4681971e4ac370172684835a7485059e0143
Kar Rui <karrui@open.gov.sg>
2026-06-15 13:33:09 +0800

    fix: resend otp button styling

--- apps/studio/src/features/sign-in/components/EmailLogin/ResendOtpButton.tsx
+++ apps/studio/src/features/sign-in/components/EmailLogin/ResendOtpButton.tsx
@@ -1,4 +1,5 @@
 import type { ButtonProps } from "react-aria-components"
+import { cn } from "@opengovsg/oui-theme"
 import { LinkButton } from "~/components/oui-bridge/LinkButton"
 
 interface ResendOtpButtonProps extends ButtonProps {
@@ -7,10 +8,15 @@ interface ResendOtpButtonProps extends ButtonProps {
 
 export const ResendOtpButton = ({
   timer,
+  className,
   ...buttonProps
 }: ResendOtpButtonProps): JSX.Element => {
   return (
-    <LinkButton type="button" {...buttonProps}>
+    <LinkButton
+      type="button"
+      className={cn("prose-subhead-2 whitespace-pre-wrap gap-0", className)}
+      {...buttonProps}
+    >
       Resend OTP
       {timer > 0 && <span data-chromatic="ignore">{` in ${timer}s`}</span>}
     </LinkButton>
