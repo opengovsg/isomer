c8c5cdd8803f05d2a2d626984eb19da95b7b5643
adriangohjw <adriangohjw@gmail.com>
2026-07-08 00:46:40 +0800

    refactor(components): remove TagSeparator component and its export
    - Deleted the TagSeparator component and its associated export file as part of a cleanup process. This change simplifies the component structure and removes unused code.

--- packages/components/src/templates/next/components/internal/PlaintextTags/PlaintextTags.tsx
+++ packages/components/src/templates/next/components/internal/PlaintextTags/PlaintextTags.tsx
@@ -1,7 +1,5 @@
 import { Fragment } from "react"
 
-import { TagSeparator } from "../TagSeparator"
-
 interface PlaintextTagsProps {
   tags?: { category: string; selected: string[] }[]
   className?: string
@@ -21,5 +19,21 @@ export const PlaintextTags = ({ tags = [], className }: PlaintextTagsProps) => {
         </Fragment>
       ))}
     </div>
+  )
+}
+
+const TagSeparator = () => {
+  return (
+    <svg
+      width="2"
+      height="2"
+      viewBox="0 0 2 2"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden
+      className="shrink-0"
+    >
+      <circle cx="1" cy="1" r="1" fill="#374151" />
+    </svg>
   )
 }
