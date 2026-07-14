bfe8bc21f7d9c414ede2cf431116d9e7b883df70
sehyunidaaa <sehyun@open.gov.sg>
2026-07-15 00:20:43 +0800

    align naming in callout interface

--- packages/components/src/interfaces/complex/Callout.ts
+++ packages/components/src/interfaces/complex/Callout.ts
@@ -2,18 +2,15 @@ import type { Static } from "@sinclair/typebox"
 import type { IsomerSiteProps } from "~/types"
 import { Type } from "@sinclair/typebox"
 
-import { ARRAY_RADIO_FORMAT } from "../format"
+import { CALLOUT_VARIANT_FORMAT } from "../format"
 import { CalloutProseSchema } from "../native/Prose"
 
 export const CalloutVariant = {
-  Important: { value: "important", label: "Important information (Default)" },
-  GoodNews: { value: "goodNews", label: "Good news" },
-  Note: { value: "note", label: "Please note" },
-  ActionNeeded: { value: "actionNeeded", label: "Action needed" },
-  AdditionalInformation: {
-    value: "additionalInformation",
-    label: "Additional information",
-  },
+  Information: { value: "information", label: "Information (Default)" },
+  GoodToKnow: { value: "goodToKnow", label: "Good to know" },
+  Warning: { value: "warning", label: "Warning" },
+  Urgent: { value: "urgent", label: "Urgent" },
+  Note: { value: "note", label: "Note" },
 } as const
 
 export const CalloutSchema = Type.Object(
@@ -22,26 +19,26 @@ export const CalloutSchema = Type.Object(
     variant: Type.Optional(
       Type.Union(
         [
-          Type.Literal(CalloutVariant.Important.value, {
-            title: CalloutVariant.Important.label,
+          Type.Literal(CalloutVariant.Information.value, {
+            title: CalloutVariant.Information.label,
           }),
-          Type.Literal(CalloutVariant.GoodNews.value, {
-            title: CalloutVariant.GoodNews.label,
+          Type.Literal(CalloutVariant.GoodToKnow.value, {
+            title: CalloutVariant.GoodToKnow.label,
+          }),
+          Type.Literal(CalloutVariant.Warning.value, {
+            title: CalloutVariant.Warning.label,
+          }),
+          Type.Literal(CalloutVariant.Urgent.value, {
+            title: CalloutVariant.Urgent.label,
           }),
           Type.Literal(CalloutVariant.Note.value, {
             title: CalloutVariant.Note.label,
           }),
-          Type.Literal(CalloutVariant.ActionNeeded.value, {
-            title: CalloutVariant.ActionNeeded.label,
-          }),
-          Type.Literal(CalloutVariant.AdditionalInformation.value, {
-            title: CalloutVariant.AdditionalInformation.label,
-          }),
         ],
         {
-          title: "You're communicating",
-          default: CalloutVariant.Important.value,
-          format: ARRAY_RADIO_FORMAT,
+          title: "Message type",
+          default: CalloutVariant.Information.value,
+          format: CALLOUT_VARIANT_FORMAT,
         },
       ),
     ),
