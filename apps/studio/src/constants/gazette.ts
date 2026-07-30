a8928b4de029fae29fa540a53bc788053eed76c1
seaerchin <jiachin@open.gov.sg>
2026-07-30 14:45:49 +0800

    chore: add docs

--- apps/studio/src/constants/gazette.ts
+++ apps/studio/src/constants/gazette.ts
@@ -1,1 +1,7 @@
+// NOTE: We are updating to 30 minutes because the full grace period
+// specified is 30 minutes. If toppan published a gazette wrongly between
+// 15-30 minutes, we (Isomer) are technically allowed to remove it
+// but at present, Toppan requires an escalation via the emergency form
+// to Isomer. Rather than doing this, allow Toppan the full grace period
+// to fix the wrong publication.
 export const ALLOWED_GAZETTE_DELETION_TIMEFRAME_IN_MINUTES = 30
