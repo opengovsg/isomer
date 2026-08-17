0eb8272b41a1f959aebdc62cf4a7f458aa5ce11a
adriangohjw <adriangohjw@gmail.com>
2026-08-18 03:25:16 +0800

    feat(studio): add admin UI and backend support for date filters

--- apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/constants.ts
+++ apps/studio/src/features/editing-experience/components/form-builder/renderers/controls/constants.ts
@@ -1,23 +1,36 @@
-import type { CollectionPagePageProps } from "@opengovsg/isomer-components"
+import type {
+  DateFilterSchemaType,
+  TextFilterSchemaType,
+} from "@opengovsg/isomer-components"
 import {
+  DEFAULT_DATE_FILTER_STATUS_LABELS,
   DEFAULT_TAG_CATEGORY_DISPLAY,
   IMAGE_ACCEPTED_MIME_TYPE_MAPPING,
+  TAG_CATEGORY_TYPE,
 } from "@opengovsg/isomer-components"
 
 export const ACCEPTED_IMAGE_TYPES_MESSAGE = Object.keys(
   IMAGE_ACCEPTED_MIME_TYPE_MAPPING,
 ).join(", ")
 
-type TagCategory = NonNullable<CollectionPagePageProps["tagCategories"]>[number]
-export const createDefaultTagCategory = (): TagCategory => ({
+export const createDefaultTagCategory = (): TextFilterSchemaType => ({
   id: crypto.randomUUID(),
   label: "New filter",
+  type: TAG_CATEGORY_TYPE.Text,
   isRequired: true,
   display: DEFAULT_TAG_CATEGORY_DISPLAY,
   options: [],
 })
 
-type TagOption = TagCategory["options"][number]
+export const createDefaultDateFilter = (): DateFilterSchemaType => ({
+  id: crypto.randomUUID(),
+  label: "New filter",
+  type: TAG_CATEGORY_TYPE.Date,
+  isRequired: true,
+  statusLabels: DEFAULT_DATE_FILTER_STATUS_LABELS,
+})
+
+type TagOption = TextFilterSchemaType["options"][number]
 export const createDefaultTagOption = (): TagOption => ({
   id: crypto.randomUUID(),
   label: "New option",
