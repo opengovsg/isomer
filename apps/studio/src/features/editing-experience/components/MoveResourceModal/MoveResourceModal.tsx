4e9d5bcfcb1d6c5d4bd9d8e9680da94ace89a0ab
seaerchin <jiachin@open.gov.sg>
2026-08-18 12:24:01 +0800

    chore: show error message
    1. derive error message from the validation; if the source/dest is
    undefined, just show a placeholder because that should technically not
    be possible
    2. wire up an infobox to show this erorr

--- apps/studio/src/features/editing-experience/components/MoveResourceModal/MoveResourceModal.tsx
+++ apps/studio/src/features/editing-experience/components/MoveResourceModal/MoveResourceModal.tsx
@@ -117,11 +117,14 @@ const MoveResourceContent = withSuspense(
     })
 
     const movedItem = useAtomValue(moveResourceAtom)
-    const { isLoading: isValidMoveLoading, isValidMove } =
-      useValidateResourceMove({
-        sourceId: movedItem?.id,
-        destinationId: curResourceId ?? null,
-      })
+    const {
+      isLoading: isValidMoveLoading,
+      isValidMove,
+      errorMessage,
+    } = useValidateResourceMove({
+      sourceId: movedItem?.id,
+      destinationId: curResourceId ?? null,
+    })
     // movedResourceId: movedItem.id,
     // destinationResourceId: curResourceId ?? null,
 
@@ -183,6 +186,11 @@ const MoveResourceContent = withSuspense(
               existingResource={movedItem ?? undefined}
               onChange={(resourceId) => setCurResourceId(resourceId)}
             />
+            {curResourceId !== undefined && errorMessage && (
+              <Infobox variant="error" size="sm" w="full">
+                {errorMessage}
+              </Infobox>
+            )}
             {showUrlChangeNotice && (
               <VStack alignItems="flex-start" spacing="0.75rem" w="full">
                 <Box
@@ -251,7 +259,7 @@ const MoveResourceContent = withSuspense(
               ability.cannot("move", {
                 parentId: movedItem?.parentId ?? null,
               }) ||
-              !isValidMove
+              isValidMove !== true
             }
             isLoading={isPending || isValidMoveLoading}
             onClick={() =>
