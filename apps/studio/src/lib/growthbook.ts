929aa323d3fcd271e803f67e54ce9f9067680fa7
adriangohjw <adriangohjw@gmail.com>
2026-09-09 18:56:53 +0800

    fix(growthbook): set IS_DATE_FILTERS_ENABLED_FEATURE_KEY_FALLBACK_VALUE to false

--- apps/studio/src/lib/growthbook.ts
+++ apps/studio/src/lib/growthbook.ts
@@ -28,10 +28,8 @@ export const ENABLE_SEARCHSG_GAZETTE_INGESTION =
 export const IS_UNPUBLISH_ENABLED_FEATURE_KEY = "is-unpublish-enabled"
 
 // Gates the "Date filter" option when adding a new collection tag filter.
-// TODO: flip IS_DATE_FILTERS_ENABLED_FEATURE_KEY_FALLBACK_VALUE to false once
-// this key is set up in GrowthBook.
 export const IS_DATE_FILTERS_ENABLED_FEATURE_KEY = "is-date-filters-enabled"
-export const IS_DATE_FILTERS_ENABLED_FEATURE_KEY_FALLBACK_VALUE = true
+export const IS_DATE_FILTERS_ENABLED_FEATURE_KEY_FALLBACK_VALUE = false
 
 export const IS_SINGPASS_ENABLED_FEATURE_KEY_FALLBACK_VALUE = true
 
