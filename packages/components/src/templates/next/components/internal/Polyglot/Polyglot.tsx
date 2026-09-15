43fe7389f43a101bd4ad11ec5070e484cd027140
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2026-09-15 16:40:31 +0800

    fix: load Polyglot sooner when lang query is present

--- packages/components/src/templates/next/components/internal/Polyglot/Polyglot.tsx
+++ packages/components/src/templates/next/components/internal/Polyglot/Polyglot.tsx
@@ -2,15 +2,20 @@
 
 import type { PolyglotProps } from "~/interfaces"
 import { useInteractionScriptLoader } from "~/hooks/useInteractionScriptLoader"
+import { useQueryParams } from "~/hooks/useQueryParams"
 
 // Reference: https://polyglot.gov.sg
 export const Polyglot = ({ environment }: PolyglotProps) => {
+  const [queryParams] = useQueryParams()
   const scriptUrl =
     environment === "staging"
       ? "https://staging-assets.polyglot.gov.sg/widget.js"
       : "https://assets.polyglot.gov.sg/widget.js"
 
-  useInteractionScriptLoader({ src: scriptUrl })
+  useInteractionScriptLoader({
+    src: scriptUrl,
+    timeout: queryParams.lang !== undefined ? 500 : undefined,
+  })
 
   return <div id="polyglot-widget" />
 }
